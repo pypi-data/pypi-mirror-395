6d565bb24cdf5ec5f0057a4eb26a25cbfac48b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:38 +0000

    Add package fps_yjs/0.11.4/fps_yjs-0.11.4.tar.gz

---
+++ packages/fps_yjs/fps_yjs-0.11.4.tar.gz/fps_yjs-0.11.4/src/fps_yjs/main.py
@@ -0,0 +1,32 @@
+from anyio import create_task_group
+from fps import Module
+from jupyverse_api.app import App
+from jupyverse_api.auth import Auth
+from jupyverse_api.contents import Contents
+from jupyverse_api.file_id import FileId
+from jupyverse_api.main import Lifespan
+from jupyverse_api.yjs import Yjs, YjsConfig
+
+from .routes import _Yjs
+
+
+class YjsModule(Module):
+    def __init__(self, name: str, **kwargs):
+        super().__init__(name)
+        self.config = YjsConfig(**kwargs)
+
+    async def prepare(self) -> None:
+        self.put(self.config, YjsConfig)
+
+        app = await self.get(App)
+        auth = await self.get(Auth)  # type: ignore[type-abstract]
+        contents = await self.get(Contents)  # type: ignore[type-abstract]
+        file_id = await self.get(FileId)  # type: ignore[type-abstract]
+        lifespan = await self.get(Lifespan)
+
+        self.yjs = _Yjs(app, auth, contents, file_id, lifespan, self.config)
+
+        async with create_task_group() as tg:
+            await tg.start(self.yjs.start)
+            self.put(self.yjs, Yjs, teardown_callback=self.yjs.stop)
+            self.done()
---
+++ packages/fps_yjs/fps_yjs-0.11.4.tar.gz/fps_yjs-0.11.4/src/fps_yjs/ywidgets/widgets.py
@@ -0,0 +1,64 @@
+from importlib.metadata import entry_points
+from typing import Any
+
+try:
+    import ypywidgets  # noqa: F401
+    from pycrdt import (
+        TransactionEvent,
+        YMessageType,
+        YSyncMessageType,
+        create_sync_message,
+        create_update_message,
+        handle_sync_message,
+    )
+
+    ypywidgets_installed = True
+except ImportError:
+    ypywidgets_installed = False
+
+
+Widgets: Any
+
+if ypywidgets_installed:
+
+    class Widgets:  # type: ignore
+        def __init__(self):
+            self.ydocs = {ep.name: ep.load() for ep in entry_points(group="ypywidgets")}
+            self.widgets = {}
+
+        def comm_open(self, msg, comm) -> None:
+            target_name = msg["content"]["target_name"]
+            if target_name != "ywidget":
+                return
+
+            name = msg["metadata"]["ymodel_name"]
+            comm_id = msg["content"]["comm_id"]
+            self.comm = comm
+            model = self.ydocs[f"{name}Model"]()
+            self.widgets[comm_id] = {"model": model, "comm": comm}
+            msg = create_sync_message(model.ydoc)
+            comm.send(buffers=[msg])
+
+        def comm_msg(self, msg) -> None:
+            comm_id = msg["content"]["comm_id"]
+            message = bytes(msg["buffers"][0])
+            if message[0] == YMessageType.SYNC:
+                ydoc = self.widgets[comm_id]["model"].ydoc
+                reply = handle_sync_message(
+                    message[1:],
+                    ydoc,
+                )
+                if reply:
+                    self.widgets[comm_id]["comm"].send(buffers=[reply])
+                if message[1] == YSyncMessageType.SYNC_STEP2:
+                    ydoc.observe(self._send)
+
+        def _send(self, event: TransactionEvent):
+            update = event.update  # type: ignore
+            message = create_update_message(update)
+            try:
+                self.comm.send(buffers=[message])
+            except Exception:
+                pass
+else:
+    Widgets = None
