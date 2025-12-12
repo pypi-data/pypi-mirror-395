dc8bdfea72be203cf6c4ac023da0520da0f56edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:37 +0000

    Add package KratosMultiphysics/10.4.0/kratosmultiphysics-10.4.0-cp314-cp314-manylinux_2_28_x86_64.whl

---
+++ packages/KratosMultiphysics/kratosmultiphysics-10.4.0-cp314-cp314-manylinux_2_28_x86_64.whl/KratosMultiphysics/CreateTriangleMeshFromNodes.pyi
@@ -0,0 +1,5 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics as Kratos
+
+def CreateTriangleMeshFromNodes(arg0: Kratos.ModelPart) -> None:
+    """CreateTriangleMeshFromNodes(arg0: Kratos.ModelPart) -> None"""
