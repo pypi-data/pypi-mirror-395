41bc56dd750e026c16017bb9c858258d047b3f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 20:06:36 +0000

    Add package geo-activity-playground/1.18.0/geo_activity_playground-1.18.0.tar.gz

---
+++ packages/geo-activity-playground/geo_activity_playground-1.18.0.tar.gz/geo_activity_playground-1.18.0/geo_activity_playground/core/parametric_plot.py
@@ -0,0 +1,178 @@
+import altair as alt
+import pandas as pd
+
+from .datamodel import PlotSpec
+
+
+MARKS = {
+    "point": "Point",
+    "circle": "Circle",
+    "area": "Area",
+    "bar": "Bar",
+    "rect": "Rectangle",
+    "line": "Line",
+}
+CONTINUOUS_VARIABLES = {
+    "distance_km": "Distance / km",
+    "distance_km_cumsum": "Cumulative Distance / km",
+    "sum(distance_km)": "Total distance / km",
+    "mean(distance_km)": "Average distance / km",
+    "start_local": "Date",
+    "hours": "Elapsed time / h",
+    "hours_moving": "Moving time / h",
+    "calories": "Energy / kcal",
+    "steps": "Steps",
+    "elevation_gain": "Elevation gain / m",
+    "start_elevation": "Start elevation / m",
+    "end_elevation": "End elevation / m",
+    "sum(elevation_gain)": "Total elevation gain / m",
+    "mean(elevation_gain)": "Average elevation gain / m",
+    "num_new_tiles_14": "New tiles 14",
+    "num_new_tiles_14": "New tiles 17",
+    "average_speed_moving_kmh": "Average moving speed / km/h",
+    "average_speed_elapsed_kmh": "Average elapsed speed / km/h",
+    "start_latitude": "Start latitude / °",
+    "start_longitude": "Start longitude / °",
+    "end_latitude": "End latitude / °",
+    "end_longitude": "End longitude / °",
+}
+DISCRETE_VARIABLES = {
+    "equipment": "Equipment",
+    "kind": "Activity kind",
+    "consider_for_achievements": "Consider for achievements",
+    "year(start_local):O": "Year",
+    "iso_year:O": "ISO Year",
+    "yearquarter(start_local)": "Year, Quarter",
+    "yearquartermonth(start_local)": "Year, Quarter, Month",
+    "yearmonth(start_local)": "Year, Month",
+    "quarter(start_local)": "Quarter",
+    "quartermonth(start_local)": "Quarter, Month",
+    "month(start_local)": "Month",
+    "week:O": "ISO Week",
+    "date(start_local)": "Day of month",
+    "weekday(start_local)": "Day of week",
+    "iso_day:O": "ISO Day",
+}
+GROUP_BY_VARIABLES = {
+    "": "(no grouping)",
+    "equipment": "Equipment",
+    "kind": "Activity kind",
+    "consider_for_achievements": "Consider for achievements",
+    "year": "Year",
+    "iso_year": "ISO Year",
+    "week": "ISO Week",
+}
+
+VARIABLES_1 = {"": "", **DISCRETE_VARIABLES}
+VARIABLES_2 = {"": "", **DISCRETE_VARIABLES, **CONTINUOUS_VARIABLES}
+
+RENAMES = {
+    "year(start):O": "year(start_local):O",
+    "yearquarter(start)": "yearquarter(start_local)",
+    "yearquartermonth(start)": "yearquartermonth(start_local)",
+    "yearmonth(start)": "yearmonth(start_local)",
+    "quarter(start)": "quarter(start_local)",
+    "quartermonth(start)": "quartermonth(start_local)",
+    "month(start)": "month(start_local)",
+    "date(start)": "date(start_local)",
+    "weekday(start)": "weekday(start_local)",
+}
+
+
+def make_parametric_plot(df: pd.DataFrame, spec: PlotSpec) -> dict[str, str]:
+    # Update renamed fields.
+    for field in spec.FIELDS:
+        setattr(spec, field, RENAMES.get(getattr(spec, field), getattr(spec, field)))
+
+    if spec.group_by:
+        grouped = df.groupby(spec.group_by)
+    else:
+        grouped = [("", df)]
+
+    chart_groups = {}
+    for key, group in grouped:
+        chart = alt.Chart(group)
+
+        match spec.mark:
+            case "point":
+                chart = chart.mark_point()
+            case "circle":
+                chart = chart.mark_circle()
+            case "area":
+                chart = chart.mark_area()
+            case "bar":
+                chart = chart.mark_bar()
+            case "rect":
+                chart = chart.mark_rect()
+            case "line":
+                chart = chart.mark_line()
+            case _:
+                raise ValueError()
+
+        if spec.mark in ["area", "line"]:
+            group_by_variables = [
+                var.split(":")[0]
+                for var in [
+                    spec.color,
+                    spec.shape,
+                    spec.size,
+                    spec.opacity,
+                    spec.row,
+                    spec.column,
+                    spec.facet,
+                ]
+                if var
+            ]
+            for column in ["distance_km"]:
+                group[column + "_cumsum"] = (
+                    group[group_by_variables + [column]]
+                    .groupby(group_by_variables)
+                    .cumsum()
+                )
+
+        encodings = [
+            (
+                alt.X(spec.x, type="ordinal", title=VARIABLES_2[spec.x])
+                if spec.mark == "rect"
+                else alt.X(spec.x, title=VARIABLES_2[spec.x])
+            ),
+            (
+                alt.Y(spec.y, type="ordinal", title=VARIABLES_2[spec.y])
+                if spec.mark == "rect"
+                else alt.Y(spec.y, title=VARIABLES_2[spec.y])
+            ),
+        ]
+        tooltips = [
+            alt.Tooltip(spec.x, title=VARIABLES_2[spec.x]),
+            alt.Tooltip(spec.y, title=VARIABLES_2[spec.y]),
+        ]
+
+        if spec.color:
+            encodings.append(alt.Color(spec.color, title=VARIABLES_2[spec.color]))
+            tooltips.append(alt.Tooltip(spec.color, title=VARIABLES_2[spec.color]))
+        if spec.shape:
+            encodings.append(alt.Shape(spec.shape, title=VARIABLES_2[spec.shape]))
+            tooltips.append(alt.Tooltip(spec.shape, title=VARIABLES_2[spec.shape]))
+        if spec.size:
+            encodings.append(alt.Size(spec.size, title=VARIABLES_2[spec.size]))
+            tooltips.append(alt.Tooltip(spec.size, title=VARIABLES_2[spec.size]))
+        if spec.opacity:
+            encodings.append(alt.Opacity(spec.opacity, title=VARIABLES_2[spec.opacity]))
+            tooltips.append(alt.Tooltip(spec.opacity, title=VARIABLES_2[spec.opacity]))
+        if spec.row:
+            encodings.append(alt.Row(spec.row, title=VARIABLES_2[spec.row]))
+            tooltips.append(alt.Tooltip(spec.row, title=VARIABLES_2[spec.row]))
+        if spec.column:
+            encodings.append(alt.Column(spec.column, title=VARIABLES_2[spec.column]))
+            tooltips.append(alt.Tooltip(spec.column, title=VARIABLES_2[spec.column]))
+        if spec.facet:
+            encodings.append(
+                alt.Facet(spec.facet, columns=3, title=VARIABLES_2[spec.facet])
+            )
+            tooltips.append(alt.Tooltip(spec.facet, title=VARIABLES_2[spec.facet]))
+
+        key = str(int(key) if isinstance(key, float) else key)
+        chart_groups[key] = (
+            chart.encode(*encodings, tooltips).interactive().to_json(format="vega")
+        )
+    return chart_groups
---
+++ packages/geo-activity-playground/geo_activity_playground-1.18.0.tar.gz/geo_activity_playground-1.18.0/geo_activity_playground/webui/columns.py
@@ -0,0 +1,71 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class ColumnDescription:
+    name: str
+    display_name: str
+    unit: str
+    format: str
+
+
+column_distance = ColumnDescription(
+    name="distance_km",
+    display_name="Distance",
+    unit="km",
+    format=".1f",
+)
+column_elevation_gain = ColumnDescription(
+    name="elevation_gain",
+    display_name="Elevation Gain",
+    unit="m",
+    format=".0f",
+)
+column_hours = ColumnDescription(
+    name="hours",
+    display_name="Elapsed Time",
+    unit="h",
+    format=".1f",
+)
+column_hours_moving = ColumnDescription(
+    name="hours_moving",
+    display_name="Moving Time",
+    unit="h",
+    format=".1f",
+)
+column_calories = ColumnDescription(
+    name="calories",
+    display_name="Energy",
+    unit="kcal",
+    format=".1f",
+)
+column_steps = ColumnDescription(
+    name="steps",
+    display_name="Steps",
+    unit="1",
+    format=".1f",
+)
+META_COLUMNS = [
+    column_distance,
+    column_elevation_gain,
+    column_hours,
+    column_hours_moving,
+    column_calories,
+    column_steps,
+]
+
+column_speed = ColumnDescription(
+    name="speed",
+    display_name="Speed",
+    unit="km/h",
+    format=".1f",
+)
+
+column_elevation = ColumnDescription(
+    name="elevation",
+    display_name="Elevation",
+    unit="m",
+    format=".0f",
+)
+
+TIME_SERIES_COLUMNS = [column_speed, column_elevation]
