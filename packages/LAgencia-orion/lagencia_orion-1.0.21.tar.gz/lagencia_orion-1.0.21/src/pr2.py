c5f4f73bb05b16a001ea25afddf986293d845e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 20:05:43 +0000

    Add package LAgencia-orion/1.0.21/lagencia_orion-1.0.21.tar.gz

---
+++ packages/LAgencia-orion/lagencia_orion-1.0.21.tar.gz/lagencia_orion-1.0.21/src/pr2.py
@@ -0,0 +1,26 @@
+def main():
+    from orion.journey.journey_castillo.sends_castillo import case_1, case_2 # noqa: F401
+    produccion= False
+
+    REAL_ESTATE = "castillo"
+    #case_1(REAL_ESTATE=REAL_ESTATE)
+    case_2(REAL_ESTATE=REAL_ESTATE)
+
+    # # from orion.journey.journey_estrella.sends_estrella import case_1, case_2# noqa: F401
+    # # REAL_ESTATE = "estrella"
+    # # case_1(REAL_ESTATE=REAL_ESTATE)
+    # # case_2(REAL_ESTATE=REAL_ESTATE)
+
+    # from orion.journey.journey_livin.sends_livin import case_1, case_2  # noqa: F401
+    # REAL_ESTATE = "livin"
+    # #case_1(REAL_ESTATE=REAL_ESTATE)
+    # case_2(REAL_ESTATE=REAL_ESTATE)
+
+    # from orion.journey.journey_villacruz.sends_villacruz import case_1, case_2  # noqa: F401
+    # REAL_ESTATE = "villacruz"
+    # #case_1(REAL_ESTATE=REAL_ESTATE, produccion)
+    # case_2(REAL_ESTATE=REAL_ESTATE, produccion)
+
+
+if __name__ == "__main__":
+    main()
