3d42974b2f0f16fd76b972218ccbbb3f11dc00b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:14:20 +0000

    Add package binary-semantic-cache/1.0.0/binary_semantic_cache-1.0.0-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/binary-semantic-cache/binary_semantic_cache-1.0.0-cp310-cp310-macosx_10_9_x86_64.whl/binary_semantic_cache/embedding/__init__.py
@@ -0,0 +1,13 @@
+"""
+Embedding module for Binary Semantic Cache.
+
+Provides adapters for different embedding providers.
+
+Contains:
+- base: Abstract embedder interface
+- openai: OpenAI API embedder
+- local: Local sentence-transformers embedder
+"""
+
+# Imports will be added by Engineer after implementation
+
