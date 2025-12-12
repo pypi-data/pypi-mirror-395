732613c88d74f65cc7d77346669413d4d0e9f33e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:58 +0000

    Add package madsci.client/0.6.0rc2/madsci_client-0.6.0rc2.tar.gz

---
+++ packages/madsci.client/madsci_client-0.6.0rc2.tar.gz/madsci_client-0.6.0rc2/madsci/client/__init__.py
@@ -0,0 +1,31 @@
+"""The Modular Autonomous Discovery for Science (MADSci) Python Client and CLI."""
+
+import sys
+import types
+
+from madsci.client import workcell_client
+from madsci.client.data_client import DataClient
+from madsci.client.event_client import EventClient
+from madsci.client.experiment_client import ExperimentClient
+from madsci.client.location_client import LocationClient
+from madsci.client.node import NODE_CLIENT_MAP, AbstractNodeClient, RestNodeClient
+from madsci.client.resource_client import ResourceClient
+from madsci.client.workcell_client import WorkcellClient
+from madsci.client.workcell_client import WorkcellClient as WorkflowClient
+
+workflow_client = types.ModuleType("madsci.client.workflow_client")
+workflow_client.__dict__.update(workcell_client.__dict__)
+sys.modules["madsci.client.workflow_client"] = workflow_client
+
+__all__ = [
+    "NODE_CLIENT_MAP",
+    "AbstractNodeClient",
+    "DataClient",
+    "EventClient",
+    "ExperimentClient",
+    "LocationClient",
+    "ResourceClient",
+    "RestNodeClient",
+    "WorkcellClient",
+    "WorkflowClient",
+]
