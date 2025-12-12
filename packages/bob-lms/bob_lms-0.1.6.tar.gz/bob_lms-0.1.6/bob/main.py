0844b3abf6154fffeda136cda084bc8d6e380b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:09:14 +0000

    Add package bob-lms/0.1.6/bob_lms-0.1.6.tar.gz

---
+++ packages/bob-lms/bob_lms-0.1.6.tar.gz/bob_lms-0.1.6/bob/main.py
@@ -0,0 +1,54 @@
+import time
+from .data_store import validate_user, get_user_role, login, logout
+from .menu import user_menu
+from .admin import admin_menu
+
+def main():
+    print("=== Welcome To B.O.B ===\n")
+    print('''
+██████╗░░░░░█████╗░░░░██████╗░░░░
+██╔══██╗░░░██╔══██╗░░░██╔══██╗░░░
+██████╦╝░░░██║░░██║░░░██████╦╝░░░
+██╔══██╗░░░██║░░██║░░░██╔══██╗░░░
+██████╦╝██╗╚█████╔╝██╗██████╦╝██╗
+╚═════╝░╚═╝░╚════╝░╚═╝╚═════╝░╚═╝
+🅱🅾🅾🅺🆂 🅾🅽 🅱🅾🅰🆁🅳 - Ⓑⓡⓞⓦⓢⓔ ⓐⓝⓓ Ⓑⓞⓡⓡⓞⓦ
+===============================
+Your Library Management System
+===============================
+    ''')
+
+    while True:
+        print("\nSelect an option:")
+        print("1. Login")
+        print("2. Exit")
+
+        choice = input(">>> ").strip()
+
+        if choice == "1":
+            username = input("\nUsername: ").strip()
+            password = input("Password: ").strip()
+
+            if validate_user(username, password):
+                login(username)
+                role = get_user_role(username)
+                print(f"\n✅ Logged in as: {username} ({role})\n")
+
+                if role == "admin":
+                    admin_menu()
+                else:
+                    user_menu()
+
+                logout()
+                print("\nLogged out successfully.\n")
+
+            else:
+                print("❌ Invalid username or password. Try again.\n")
+
+        elif choice == "2":
+            print("\nThank you for using B.O.B. Goodbye!\n")
+            time.sleep(3)
+            break
+
+        else:
+            print("❌ Invalid option. Please choose 1 or 2.\n")
