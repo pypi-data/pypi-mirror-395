8981ad1f38821102af08651d80bad085c2620d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 20:06:42 +0000

    Add package nonebot-plugin-picmcstat/0.8.1/nonebot_plugin_picmcstat-0.8.1.tar.gz

---
+++ packages/nonebot-plugin-picmcstat/nonebot_plugin_picmcstat-0.8.1.tar.gz/nonebot_plugin_picmcstat-0.8.1/README.md
@@ -0,0 +1,366 @@
+<!-- markdownlint-disable MD033 MD036 MD041 -->
+
+<div align="center">
+
+<a href="https://v2.nonebot.dev/store">
+  <img src="https://raw.githubusercontent.com/lgc-NB2Dev/readme/main/picmcstat/picmcstat.png" width="180" height="180" alt="NoneBotPluginLogo">
+</a>
+
+<p>
+  <img src="https://raw.githubusercontent.com/lgc-NB2Dev/readme/main/template/plugin.svg" alt="NoneBotPluginText">
+</p>
+
+# NoneBot-Plugin-PicMCStat
+
+_✨ Minecraft 服务器 MOTD 查询 图片版 ✨_
+
+<img src="https://img.shields.io/badge/python-3.10+-blue.svg" alt="python">
+<a href="https://github.com/astral-sh/uv">
+  <img src="https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/uv/main/assets/badge/v0.json" alt="uv">
+</a>
+<a href="https://wakatime.com/badge/user/b61b0f9a-f40b-4c82-bc51-0a75c67bfccf/project/5bc0f141-d1ec-430a-8d21-0e312188fdae">
+  <img src="https://wakatime.com/badge/user/b61b0f9a-f40b-4c82-bc51-0a75c67bfccf/project/5bc0f141-d1ec-430a-8d21-0e312188fdae.svg" alt="wakatime">
+</a>
+
+<br />
+
+<a href="https://pydantic.dev">
+  <img src="https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/lgc-NB2Dev/readme/main/template/pyd-v1-or-v2.json" alt="Pydantic Version 1 Or 2" >
+</a>
+<a href="./LICENSE">
+  <img src="https://img.shields.io/github/license/lgc-NB2Dev/nonebot-plugin-picmcstat.svg" alt="license">
+</a>
+<a href="https://pypi.python.org/pypi/nonebot-plugin-picmcstat">
+  <img src="https://img.shields.io/pypi/v/nonebot-plugin-picmcstat.svg" alt="pypi">
+</a>
+<a href="https://pypi.python.org/pypi/nonebot-plugin-picmcstat">
+  <img src="https://img.shields.io/pypi/dm/nonebot-plugin-picmcstat" alt="pypi download">
+</a>
+
+<br />
+
+<a href="https://registry.nonebot.dev/plugin/nonebot-plugin-picmcstat:nonebot_plugin_picmcstat">
+  <img src="https://img.shields.io/endpoint?url=https%3A%2F%2Fnbbdg.lgc2333.top%2Fplugin%2Fnonebot-plugin-picmcstat" alt="NoneBot Registry">
+</a>
+<a href="https://registry.nonebot.dev/plugin/nonebot-plugin-picmcstat:nonebot_plugin_picmcstat">
+  <img src="https://img.shields.io/endpoint?url=https%3A%2F%2Fnbbdg.lgc2333.top%2Fplugin-adapters%2Fnonebot-plugin-picmcstat" alt="Supported Adapters">
+</a>
+
+</div>
+
+## 📖 介绍
+
+插件实际上是可以展示 **玩家列表**、**Mod 端信息 以及 Mod 列表（还未测试）** 的，这里没有找到合适的例子所以没在效果图里展示出来，如果遇到问题可以发 issue
+
+插件包体内并没有自带图片内 Unifont 字体，需要的话请参考 [这里](#字体) 安装字体
+
+<details open>
+<summary>效果图</summary>
+
+![example](https://raw.githubusercontent.com/lgc-NB2Dev/readme/main/picmcstat/example.png)  
+![example](https://raw.githubusercontent.com/lgc-NB2Dev/readme/main/picmcstat/example_je.png)
+
+</details>
+
+## 💿 安装
+
+### 插件
+
+以下提到的方法 任选**其一** 即可
+
+<details open>
+<summary>[推荐] 使用 nb-cli 安装</summary>
+在 nonebot2 项目的根目录下打开命令行, 输入以下指令即可安装
+
+```bash
+nb plugin install nonebot-plugin-picmcstat
+```
+
+</details>
+
+<details>
+<summary>使用包管理器安装</summary>
+在 nonebot2 项目的插件目录下, 打开命令行, 根据你使用的包管理器, 输入相应的安装命令
+
+<details>
+<summary>pip</summary>
+
+```bash
+pip install nonebot-plugin-picmcstat
+```
+
+</details>
+<details>
+<summary>pdm</summary>
+
+```bash
+pdm add nonebot-plugin-picmcstat
+```
+
+</details>
+<details>
+<summary>poetry</summary>
+
+```bash
+poetry add nonebot-plugin-picmcstat
+```
+
+</details>
+<details>
+<summary>conda</summary>
+
+```bash
+conda install nonebot-plugin-picmcstat
+```
+
+</details>
+
+打开 nonebot2 项目根目录下的 `pyproject.toml` 文件, 在 `[tool.nonebot]` 部分的 `plugins` 项里追加写入
+
+```toml
+[tool.nonebot]
+plugins = [
+    # ...
+    "nonebot_plugin_picmcstat"
+]
+```
+
+</details>
+
+### 字体
+
+字体文件请自行去自行去 [这里](http://ftp.gnu.org/gnu/unifont/unifont-15.0.01/unifont-15.0.01.ttf) 下载
+如需将英文部分变为游戏内字体，请另外下载安装 [这个](https://resources.download.minecraft.net/3d/3d009535ec7860c29603cd66cdb4db5c8b4aefd2) 字体（请自行修改文件扩展名为 `.ttf`）
+
+将字体文件直接安装在系统中即可  
+如果不行，请尝试右键字体文件点击 `为所有用户安装`  
+如果还是不行，请尝试修改插件字体配置
+
+## ⚙️ 配置
+
+### `MCSTAT_FONT` - 使用的字体名称 / 路径
+
+默认：`["Minecraft Seven", "unifont"]`
+
+请按需自行更改
+
+### `MCSTAT_SHOW_ADDR` - 是否在生成的图片中显示服务器地址
+
+默认：`False`
+
+### `MCSTAT_SHOW_DELAY` - 是否显示测试延迟
+
+默认：`True`
+
+### `MCSTAT_SHOW_MODS` - 是否在生成的图片中显示 Mod 列表
+
+默认：`False`
+
+由于某些整合包服务器的 Mod 数量过多，导致图片生成时间过长，且容易炸内存，所以默认不显示
+
+### `MCSTAT_REPLY_TARGET` - 是否回复指令发送者
+
+默认：`True`
+
+### `MCSTAT_SHORTCUTS` - 快捷指令列表
+
+这个配置项能够帮助你简化一些查询指令
+
+此配置项的类型是一个列表，里面的元素需要为一个特定结构的字典：
+
+- `regex` - 用于匹配指令的正则，例如 `^查服$`  
+  （注意，nb2 以 JSON 格式解析配置项，所以当你要在正则表达式里表示`\`时，你需要将其转义为`\\`）
+- `host` - 要查询的服务器地址，格式为 `<IP>[:端口]`，  
+  例如 `hypixel.net` 或 `example.com:1919`
+- `type` - 要查询服务器的类型，`je` 表示 Java 版服，`be` 表示基岩版服，`auto` 代表自动检测
+- `whitelist` - （仅支持 OneBot V11 适配器）群聊白名单，只有里面列出的群号可以查询，可以不填来对所有群开放查询
+
+最终的配置项看起来是这样子的，当你发送 `查服` 时，机器人会把 EaseCation 服务器的状态发送出来
+
+```env
+MCSTAT_SHORTCUTS='
+[
+  {"regex": "^查服$", "host": "asia.easecation.net", "type": "be"}
+]
+'
+```
+
+### `MCSTAT_RESOLVE_DNS` - 是否由插件解析 DNS 记录
+
+默认：`True`
+
+是否由插件解析一遍 DNS 记录后再进行查询，  
+如果你的服务器在运行 Clash 等拦截了 DNS 解析的软件，且查询部分地址时遇到了问题，请尝试关闭此配置项  
+此配置项不影响 Java 服务器的 SRV 记录解析
+
+### `MCSTAT_RESOLVE_DNS_IPV6` - 是否启用 IPv6 解析
+
+默认：`True`
+
+是否优先使用 IPv6 地址进行查询  
+当启用此配置项时，会优先尝试使用 IPv6 地址进行连接，如连接失败则自动回落到 IPv4  
+如果你的网络环境不支持 IPv6，可以关闭此配置项以避免不必要的等待  
+此配置项仅在 `MCSTAT_RESOLVE_DNS` 启用时生效
+
+### `MCSTAT_QUERY_TWICE` - 是否查询两遍服务器状态
+
+默认：`True`
+
+由于第一次测得的延迟一般不准，所以做了这个配置，  
+开启后每次查询时，会丢掉第一次的结果再查询一次，且使用第二次查询到的结果
+
+### `MCSTAT_JAVA_PROTOCOL_VERSION` - Motd Java 服务器时向服务器发送的客户端协议版本
+
+默认：`767`
+
+### `MCSTAT_ENABLE_AUTO_DETECT` - 是否在使用未指定服务器类型的 `motd` 指令时自动检测
+
+默认：`True`
+
+如设为 `False` 将默认指定为 Java 版
+
+## 🎉 使用
+
+发送 `motd` 指令 查看使用指南
+
+![usage](https://raw.githubusercontent.com/lgc-NB2Dev/readme/main/picmcstat/usage.png)
+
+## 📞 联系
+
+QQ：3076823485  
+Telegram：[@lgc2333](https://t.me/lgc2333)  
+吹水群：[168603371](https://qm.qq.com/q/EikuZ5sP4G)  
+邮箱：<lgc2333@126.com>
+
+## 💡 鸣谢
+
+### [pil-utils](https://github.com/MeetWq/pil-utils)
+
+超好用的 Pillow 辅助库，wq 佬是叠！快去用 awa
+
+## 💰 赞助
+
+**[赞助我](https://blog.lgc2333.top/donate)**
+
+感谢大家的赞助！你们的赞助将是我继续创作的动力！
+
+## 📝 更新日志
+
+### 0.8.1
+
+- 添加配置项 `MCSTAT_RESOLVE_DNS_IPV6`，用于禁用 IPv6 解析（[#29](https://github.com/lgc-NB2Dev/nonebot-plugin-picmcstat/issues/29)）
+- 当 IPv6 连接失败时自动回落到 IPv4
+
+### 0.8.0
+
+- 加入自动检测服务器类型的功能，默认启用（Thanks to [#28](https://github.com/lgc-NB2Dev/nonebot-plugin-picmcstat/pull/28)）
+
+### 0.7.1
+
+- 修复文字下对齐的 Bug
+
+### 0.7.0
+
+- 适配 pil-utils 0.2
+- 更改配置项 `MCSTAT_FONT` 类型为 `List[str]`（`str` 仍然受支持）
+
+### 0.6.3
+
+- fix [#22](https://github.com/lgc-NB2Dev/nonebot-plugin-picmcstat/issues/22)
+
+### 0.6.2
+
+- 允许自定义向 Java 服务器发送的客户端协议版本，且提高默认协议版本以解决部分服务器 Motd 渐变显示异常的问题
+
+### 0.6.1
+
+- fix [#21](https://github.com/lgc-NB2Dev/nonebot-plugin-picmcstat/issues/21)
+
+### 0.6.0
+
+- 适配 Pydantic V1 & V2
+
+### 0.5.1
+
+- 修复 玩家 / Mod 列表 中出现的一些 Bug ~~果然又出 Bug 了~~
+- 添加配置项 `MCSTAT_SHOW_DELAY`、`MCSTAT_QUERY_TWICE`
+
+### 0.5.0
+
+- 换用 Alconna 支持多平台
+- 快捷指令支持多平台（`whitelist` 依然仅支持 OneBot V11）
+- 添加配置项 `MCSTAT_RESOLVE_DNS`
+- 部分代码重构 ~~Bug 海与屎山代码又增加了~~
+
+### 0.4.0
+
+- 修复修复无法解析中文域名 IP 的错误（[#13](https://github.com/lgc-NB2Dev/nonebot-plugin-picmcstat/issues/13)）
+- 使用 SAA 支持多适配器（shortcut 依然仅支持 OB V11）
+- 添加配置项 `MCSTAT_REPLY_TARGET`
+
+### 0.3.5
+
+- 修复上个版本的小 Bug
+
+### 0.3.4
+
+- 修复无法正常绘制 Mod 列表的情况
+- 增加显示 Mod 列表的配置项 (`MCSTAT_SHOW_MODS`)
+
+### 0.3.3
+
+- 修复特殊情况下玩家列表排版错误的问题（虽然现在使用其他字体的情况下还是有点问题）
+- 添加显示服务器地址的配置项 (`MCSTAT_SHOW_ADDR`)
+
+### 0.3.2
+
+- 🎉 NoneBot 2.0 🚀
+
+### 0.3.1
+
+- 修复文本内含有 `§k` 时报错的问题
+
+### 0.3.0
+
+- 弃用 `nonebot-plugin-imageutils`，换用 `pil-utils`
+- 支持了更多字体样式
+- 支持自定义字体
+
+### 0.2.7
+
+- 修复 `shortcut` 的 `whitelist` 的奇怪表现
+
+### 0.2.6
+
+- 修复 `shortcut` 中没有 `whitelist` 项会报错的问题
+
+### 0.2.5
+
+- `shortcut` 加入 `whitelist` 项配置触发群白名单
+
+### 0.2.4
+
+- 修复玩家列表底下的多余空行
+
+### 0.2.3
+
+- 修复 JE 服务器 Motd 中粗体意外显示为蓝色的 bug
+
+### 0.2.2
+
+- 修复 motd 前后留的空去不干净的问题
+- 优化玩家列表显示效果
+
+### 0.2.1
+
+- 修复当最大人数为 0 时出错的问题
+
+### 0.2.0
+
+- 加入快捷指令，详见配置项
+- 修复某些 JE 服无法正确显示 Motd 的问题
+-
+
+### 0.1.1
+
+- 将查 JE 服时的 `游戏延迟` 字样 改为 `测试延迟`
---
+++ packages/nonebot-plugin-picmcstat/nonebot_plugin_picmcstat-0.8.1.tar.gz/nonebot_plugin_picmcstat-0.8.1/nonebot_plugin_picmcstat/draw.py
@@ -0,0 +1,539 @@
+import base64
+import errno
+import socket
+from collections.abc import Sequence
+from functools import partial
+from io import BytesIO
+from typing import TYPE_CHECKING, Any, Optional, TypeAlias, Union, cast
+
+from mcstatus import BedrockServer, JavaServer
+from mcstatus.motd import Motd
+from mcstatus.status_response import JavaStatusResponse
+from nonebot import get_driver
+from nonebot.log import logger
+from PIL.Image import Resampling
+from pil_utils import BuildImage, Text2Image
+
+from .config import config
+from .const import CODE_COLOR, GAME_MODE_MAP, STROKE_COLOR, ServerType, ServerTypeRaw
+from .res import DEFAULT_ICON_RES, DIRT_RES, GRASS_RES
+from .util import (
+    BBCodeTransformer,
+    chunks,
+    format_mod_list,
+    get_latency_color,
+    resolve_ip,
+    split_motd_lines,
+    trim_motd,
+)
+
+if TYPE_CHECKING:
+    from mcstatus.responses import BedrockStatusResponse
+    from pil_utils.typing import ColorType
+
+MARGIN = 32
+MIN_WIDTH = 512
+TITLE_FONT_SIZE = 8 * 5
+EXTRA_FONT_SIZE = 8 * 4
+EXTRA_STROKE_WIDTH = 2
+STROKE_RATIO = 0.0625
+SPACING = 12
+LIST_GAP = 12
+
+JE_HEADER = "[MCJE服务器信息]"
+BE_HEADER = "[MCBE服务器信息]"
+AUTO_HEADER = "[MC服务器信息]"
+SUCCESS_TITLE = "请求成功"
+DEFAULT_ERR_TITLE = "出错了！"
+
+ImageType: TypeAlias = Union[BuildImage, Text2Image, "ImageGrid"]
+
+
+def ex_default_style(text: str, color_code: str = "", **kwargs) -> Text2Image:
+    default_kwargs = {
+        "font_size": EXTRA_FONT_SIZE,
+        "fill": CODE_COLOR[color_code or "f"],
+        "font_families": config.font,
+        "stroke_ratio": STROKE_RATIO,
+        "stroke_fill": STROKE_COLOR[color_code or "f"],
+        # "spacing": EXTRA_SPACING,
+    }
+    default_kwargs.update(kwargs)
+    return Text2Image.from_bbcode_text(text, **default_kwargs)
+
+
+def calc_offset(*pos: tuple[float, float]) -> tuple[float, float]:
+    return (sum(x[0] for x in pos), sum(x[1] for x in pos))
+
+
+def draw_image_type_on(bg: BuildImage, it: ImageType, pos: tuple[float, float]):
+    if isinstance(it, ImageGrid):
+        it.draw_on(bg, pos)
+    elif isinstance(it, Text2Image):
+        it.draw_on_image(bg.image, pos)
+    else:
+        bg.paste(
+            it,
+            tuple(round(x) for x in pos),  # type: ignore
+            alpha=True,
+        )
+
+
+def width(obj: ImageType) -> float:
+    if isinstance(obj, Text2Image):
+        return obj.longest_line
+    return obj.width
+
+
+class ImageLine:
+    def __init__(
+        self,
+        left: ImageType | str,
+        right: ImageType | str | None = None,
+        gap: int = LIST_GAP,
+    ):
+        self.left = ex_default_style(left) if isinstance(left, str) else left
+        self.right = (
+            (ex_default_style(right) if isinstance(right, str) else right)
+            if right
+            else None
+        )
+        self.gap = gap
+
+    @property
+    def width(self) -> float:
+        rw = width(self.right) if self.right else 0
+        return width(self.left) + self.gap + rw
+
+    @property
+    def height(self) -> float:
+        return max(self.left.height, (self.right.height if self.right else 0))
+
+    @property
+    def size(self) -> tuple[float, float]:
+        return self.width, self.height
+
+
+class ImageGrid(list[ImageLine]):
+    def __init__(
+        self,
+        *lines: ImageLine,
+        spacing: int = SPACING,
+        gap: int | None = None,
+        align_items: bool = True,
+    ):
+        if gap is not None:
+            lines = tuple(ImageLine(x.left, x.right, gap=gap) for x in lines)
+        super().__init__(lines)
+        self.spacing = spacing
+        self.align_items = align_items
+
+    @classmethod
+    def from_list(cls, li: Sequence[ImageType | str], **kwargs) -> "ImageGrid":
+        return cls(
+            *(ImageLine(*cast("tuple[Any, Any]", x)) for x in chunks(li, 2)),
+            **kwargs,
+        )
+
+    @property
+    def width(self) -> float:
+        return max(x.width for x in self)
+
+    @property
+    def height(self) -> float:
+        return sum(x.height for x in self) + self.spacing * (len(self) - 1)
+
+    @property
+    def size(self) -> tuple[float, float]:
+        return self.width, self.height
+
+    def append_line(self, *args, **kwargs):
+        self.append(ImageLine(*args, **kwargs))
+
+    def draw_on(self, bg: BuildImage, offset_pos: tuple[float, float]) -> None:
+        max_lw = max(width(x.left) for x in self) if self.align_items else None
+        y_offset = 0
+        for line in self:
+            line_height = line.height
+            draw_image_type_on(
+                bg,
+                line.left,
+                calc_offset(
+                    offset_pos,
+                    (0, y_offset),
+                    # (0, (line_height - line.left.height)),
+                ),
+            )
+            if line.right:
+                draw_image_type_on(
+                    bg,
+                    line.right,
+                    calc_offset(
+                        offset_pos,
+                        ((max_lw or width(line.left)) + line.gap, y_offset),
+                        # (0, (line_height - line.right.height)),
+                    ),
+                )
+            y_offset += line_height + self.spacing
+
+    def to_image(
+        self,
+        background: Optional["ColorType"] = None,
+        padding: int = 2,
+    ) -> BuildImage:
+        size = calc_offset(self.size, (padding * 2, padding * 2))
+        bg = BuildImage.new(
+            "RGBA",
+            tuple(round(x) for x in size),  # type: ignore
+            background or (0, 0, 0, 0),
+        )
+        self.draw_on(bg, (padding, padding))
+        return bg
+
+
+def get_header_by_svr_type(svr_type: ServerType) -> str:
+    if svr_type == "je":
+        return JE_HEADER
+    if svr_type == "be":
+        return BE_HEADER
+    return AUTO_HEADER
+
+
+def draw_bg(width: int, height: int) -> BuildImage:
+    size = DIRT_RES.width
+    bg = BuildImage.new("RGBA", (width, height))
+
+    for hi in range(0, height, size):
+        for wi in range(0, width, size):
+            bg.paste(DIRT_RES if hi else GRASS_RES, (wi, hi))
+
+    return bg
+
+
+def build_img(
+    header1: str,
+    header2: str,
+    icon: BuildImage | None = None,
+    extra: ImageType | str | None = None,
+) -> BytesIO:
+    if not icon:
+        icon = DEFAULT_ICON_RES
+    if isinstance(extra, str):
+        extra = ex_default_style(extra)
+
+    header_text_color = CODE_COLOR["f"]
+    header_stroke_color = STROKE_COLOR["f"]
+
+    header_height = 128
+    half_header_height = int(header_height / 2)
+
+    bg_width = width(extra) + MARGIN * 2 if extra else MIN_WIDTH
+    bg_height = header_height + MARGIN * 2
+    bg_width = max(bg_width, MIN_WIDTH)
+    if extra:
+        bg_height += extra.height + int(MARGIN / 2)
+    bg = draw_bg(round(bg_width), round(bg_height))
+
+    if icon.size != (header_height, header_height):
+        icon = icon.resize_height(
+            header_height,
+            inside=False,
+            resample=Resampling.NEAREST,
+        )
+    bg.paste(icon, (MARGIN, MARGIN), alpha=True)
+
+    bg.draw_text(
+        (
+            header_height + MARGIN + MARGIN / 2,
+            MARGIN - 4,
+            bg_width - MARGIN,
+            half_header_height + MARGIN + 4,
+        ),
+        header1,
+        halign="left",
+        fill=header_text_color,
+        max_fontsize=TITLE_FONT_SIZE,
+        font_families=config.font,
+        stroke_ratio=STROKE_RATIO,
+        stroke_fill=header_stroke_color,
+    )
+    bg.draw_text(
+        (
+            header_height + MARGIN + MARGIN / 2,
+            half_header_height + MARGIN - 4,
+            bg_width - MARGIN,
+            header_height + MARGIN + 4,
+        ),
+        header2,
+        halign="left",
+        fill=header_text_color,
+        max_fontsize=TITLE_FONT_SIZE,
+        font_families=config.font,
+        stroke_ratio=STROKE_RATIO,
+        stroke_fill=header_stroke_color,
+    )
+
+    if extra:
+        draw_image_type_on(
+            bg,
+            extra,
+            (MARGIN, int(header_height + MARGIN + MARGIN / 2)),
+        )
+
+    return bg.convert("RGB").save("jpeg")
+
+
+def draw_help(svr_type: ServerType) -> BytesIO:
+    cmd_prefix_li = list(get_driver().config.command_start)
+    prefix = cmd_prefix_li[0] if cmd_prefix_li else ""
+
+    extra_txt = [
+        f"查询Java版服务器: {prefix}motdje <服务器IP>",
+        f"查询基岩版服务器: {prefix}motdpe <服务器IP>",
+    ]
+    if config.enable_auto_detect:
+        extra_txt.append(f"自动检测服务器类型: {prefix}motd <服务器IP>")
+    extra = ImageGrid()
+    for x in extra_txt:
+        extra.append_line(x)
+    return build_img(get_header_by_svr_type(svr_type), "使用帮助", extra=extra)
+
+
+def draw_java(res: JavaStatusResponse, addr: str) -> BytesIO:
+    transformer = BBCodeTransformer(bedrock=res.motd.bedrock)
+    # there're no line spacing in Text2Image since pil-utils 0.2.0
+    # so we split lines there then manually add the space
+    motd = [
+        transformer.transform(x) for x in split_motd_lines(trim_motd(res.motd.parsed))
+    ]
+    online_percent = (
+        f"{res.players.online / res.players.max * 100:.2f}"
+        if res.players.max
+        else "?.??"
+    )
+
+    mod_svr_type: str | None = None
+    mod_list: list[str] | None = None
+    if mod_info := res.raw.get("modinfo"):
+        if tmp := mod_info.get("type"):
+            mod_svr_type = tmp
+        if tmp := (mod_info.get("mods") or mod_info.get("modList")):
+            mod_list = format_mod_list(tmp)
+
+    l_style = partial(ex_default_style, color_code="7")
+    grid = ImageGrid(align_items=False)
+    for line in motd:
+        grid.append_line(line)
+    if config.show_addr:
+        grid.append_line(l_style("测试地址: "), addr)
+    grid.append_line(l_style("服务端名: "), res.version.name)
+    if mod_svr_type:
+        grid.append_line(l_style("Mod 端类型: "), mod_svr_type)
+    grid.append_line(l_style("协议版本: "), str(res.version.protocol))
+    grid.append_line(
+        l_style("当前人数: "),
+        f"{res.players.online}/{res.players.max} ({online_percent}%)",
+    )
+    if mod_list:
+        grid.append_line(l_style("Mod 总数: "), str(len(mod_list)))
+    grid.append_line(
+        l_style("聊天签名: "),
+        "必需" if res.enforces_secure_chat else "无需",
+    )
+    if config.show_delay:
+        grid.append_line(
+            l_style("测试延迟: "),
+            ex_default_style(f"{res.latency:.2f}ms", get_latency_color(res.latency)),
+        )
+    if mod_list and config.show_mods:
+        grid.append_line(
+            l_style("Mod 列表: "),
+            ImageGrid.from_list(mod_list),
+        )
+    if res.players.sample:
+        grid.append_line(
+            l_style("玩家列表: "),
+            ImageGrid.from_list(
+                [
+                    transformer.transform(Motd.parse(x.name).parsed)
+                    for x in res.players.sample
+                ],
+            ),
+        )
+
+    icon = (
+        BuildImage.open(BytesIO(base64.b64decode(res.icon.split(",")[-1])))
+        if res.icon
+        else None
+    )
+    return build_img(JE_HEADER, SUCCESS_TITLE, icon=icon, extra=grid)
+
+
+def draw_bedrock(res: "BedrockStatusResponse", addr: str) -> BytesIO:
+    transformer = BBCodeTransformer(bedrock=res.motd.bedrock)
+    motd = (
+        transformer.transform(x) for x in split_motd_lines(trim_motd(res.motd.parsed))
+    )
+    online_percent = (
+        f"{int(res.players.online) / int(res.players.max) * 100:.2f}"
+        if res.players.max
+        else "?.??"
+    )
+
+    l_style = partial(ex_default_style, color_code="7")
+    grid = ImageGrid(align_items=False)
+    for line in motd:
+        grid.append_line(line)
+    if config.show_addr:
+        grid.append_line(l_style("测试地址: "), addr)
+    grid.append_line(l_style("协议版本: "), str(res.version.protocol))
+    grid.append_line(l_style("游戏版本: "), res.version.version)
+    grid.append_line(
+        l_style("当前人数: "),
+        f"{res.players.online}/{res.players.max} ({online_percent}%)",
+    )
+    if res.map_name:
+        grid.append_line(l_style("存档名称: "), res.map_name)
+    if res.gamemode:
+        grid.append_line(
+            l_style("游戏模式: "),
+            GAME_MODE_MAP.get(res.gamemode, res.gamemode),
+        )
+    if config.show_delay:
+        grid.append_line(
+            l_style("测试延迟: "),
+            ex_default_style(f"{res.latency:.2f}ms", get_latency_color(res.latency)),
+        )
+
+    return build_img(BE_HEADER, SUCCESS_TITLE, extra=grid)
+
+
+def parse_error(e: Exception) -> tuple[str, str]:
+    if isinstance(e, TimeoutError):
+        return "请求超时", ""
+    if isinstance(e, socket.gaierror):
+        return "域名解析失败", str(e)
+    return DEFAULT_ERR_TITLE, f"{e.__class__.__name__}: {e}"
+
+
+def join_strings(*s: str, sp: str = "：") -> str:
+    return sp.join(x for x in s if x)
+
+
+def draw_error(
+    svr_type: ServerType,
+    *e: Exception | tuple[str, Exception],
+    title: str | None = None,
+) -> BytesIO:
+    if len(e) == 1 and (not title):
+        title, extra = (
+            (x[0], join_strings(*parse_error(x[1])))
+            if isinstance((x := e[0]), tuple)
+            else parse_error(x)
+        )
+        extras = [extra] if extra else None
+    else:
+        extras = [
+            (
+                join_strings(x[0], *parse_error(x[1]))
+                if isinstance(x, tuple)
+                else join_strings(*parse_error(x))
+            )
+            for x in e
+        ]
+    if not title:
+        title = DEFAULT_ERR_TITLE
+    if extras:
+        lines = [
+            ImageLine(ex_default_style(x).wrap((MIN_WIDTH * 1.5) - MARGIN * 2))
+            for x in extras
+        ]
+        extra_img = ImageGrid(*lines)
+    else:
+        extra_img = None
+    return build_img(get_header_by_svr_type(svr_type), title, extra=extra_img)
+
+
+def draw_resp(
+    resp: Union[JavaStatusResponse, "BedrockStatusResponse"],
+    addr: str,
+) -> BytesIO:
+    if isinstance(resp, JavaStatusResponse):
+        return draw_java(resp, addr)
+    return draw_bedrock(resp, addr)
+
+
+def is_ipv6_unreachable_error(e: BaseException) -> bool:
+    """Check if exception is due to IPv6 network being unreachable."""
+    if isinstance(e, OSError) and e.errno in (
+        errno.ENETUNREACH,
+        errno.EHOSTUNREACH,
+        errno.EADDRNOTAVAIL,
+    ):
+        return True
+    if e.__cause__:
+        return is_ipv6_unreachable_error(e.__cause__)
+    return False
+
+
+async def draw(ip: str, svr_type: ServerType) -> BytesIO:
+    async def _inner(t: ServerTypeRaw, *, resolve_dns_ipv6: bool | None = None) -> BytesIO:
+        is_java = t == "je"
+        host, port = await resolve_ip(ip, is_java, resolve_dns_ipv6=resolve_dns_ipv6)
+
+        svr = JavaServer(host, port) if is_java else BedrockServer(host, port)
+        kw = {"version": config.java_protocol_version} if is_java else {}
+        if config.query_twice:
+            await svr.async_status(**kw)  # 第一次延迟通常不准
+        resp = await svr.async_status(**kw)
+        return draw_resp(resp, ip)
+
+    async def _inner_with_fallback(t: ServerTypeRaw) -> BytesIO:
+        # If IPv6 is disabled, just use IPv4
+        if not config.resolve_dns_ipv6:
+            return await _inner(t, resolve_dns_ipv6=False)
+
+        # Try IPv6 first, fall back to IPv4 if unreachable
+        try:
+            return await _inner(t, resolve_dns_ipv6=True)
+        except Exception as e:
+            if is_ipv6_unreachable_error(e):
+                logger.debug(
+                    f"IPv6 connection failed with {e.__class__.__name__}, "
+                    "falling back to IPv4",
+                )
+                return await _inner(t, resolve_dns_ipv6=False)
+            raise
+
+    try:
+        if not ip:
+            return draw_help(svr_type)
+
+        if svr_type != "auto":
+            return await _inner_with_fallback(svr_type)
+
+        # auto
+        try:
+            return await _inner_with_fallback("je")
+        except Exception as e:
+            logger.exception("获取JE服务器状态/画服务器状态图出错")
+            je_exc = e
+        try:
+            return await _inner_with_fallback("be")
+        except Exception as e:
+            logger.exception("获取BE服务器状态/画服务器状态图出错")
+            be_exc = e
+        return draw_error(
+            svr_type,
+            ("JE", je_exc),
+            ("BE", be_exc),
+            title="所有尝试皆出错",
+        )
+
+    except Exception as e:
+        logger.exception("获取服务器状态/画服务器状态图出错")
+        try:
+            return draw_error(svr_type, e)
+        except Exception:
+            logger.exception("画异常状态图失败")
+            raise
