418b7d277bd31e9b54db6ecaec41b3e3e35e5f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 12:05:31 +0000

    Add package clmm/1.16.10/clmm-1.16.10.tar.gz

---
+++ packages/clmm/clmm-1.16.10.tar.gz/clmm-1.16.10/clmm/cosmology/ccl.py
@@ -0,0 +1,118 @@
+"""@file ccl.py
+Cosmology using CCL
+"""
+
+import numpy as np
+import pyccl as ccl
+
+from ..utils import _patch_rho_crit_to_cd2018
+from .parent_class import CLMMCosmology
+
+__all__ = []
+
+
+class CCLCosmology(CLMMCosmology):
+    """
+    Cosmology object
+
+    Attributes
+    ----------
+    backend: str
+        Name of back-end used
+    be_cosmo: cosmology library
+        Cosmology library used in the back-end
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # this tag will be used to check if the cosmology object is accepted by the modeling
+        self.backend = "ccl"
+
+        assert isinstance(self.be_cosmo, ccl.Cosmology)
+
+        # cor factor for sigma_critical
+        self.cor_factor = _patch_rho_crit_to_cd2018(ccl.physical_constants.RHO_CRITICAL)
+
+    def _init_from_cosmo(self, be_cosmo):
+        assert isinstance(be_cosmo, ccl.Cosmology)
+        self.be_cosmo = be_cosmo
+
+    def _init_from_params(self, H0, Omega_b0, Omega_dm0, Omega_k0):
+        # pylint: disable=arguments-differ
+        self.be_cosmo = ccl.Cosmology(
+            Omega_c=Omega_dm0,
+            Omega_b=Omega_b0,
+            Omega_k=Omega_k0,
+            h=H0 / 100.0,
+            sigma8=0.8,
+            n_s=0.96,
+            T_CMB=2.7255,
+            Neff=3.046,
+            m_nu=[0.06, 0.0, 0.0],
+            transfer_function="eisenstein_hu",
+            matter_power_spectrum="linear",
+        )
+
+    def _set_param(self, key, value):
+        raise NotImplementedError("CCL do not support changing parameters")
+
+    def _get_param(self, key):
+        if key == "Omega_m0":
+            value = ccl.omega_x(self.be_cosmo, 1.0, "matter")
+        elif key == "Omega_b0":
+            value = self.be_cosmo["Omega_b"]
+        elif key == "Omega_dm0":
+            value = self.be_cosmo["Omega_c"]
+        elif key == "Omega_k0":
+            value = self.be_cosmo["Omega_k"]
+        elif key == "h":
+            value = self.be_cosmo["h"]
+        elif key == "H0":
+            value = self.be_cosmo["h"] * 100.0
+        else:
+            raise ValueError(f"Unsupported parameter {key}")
+        return value
+
+    def _get_Omega_m(self, z):
+        a = self._get_a_from_z(z)
+        return ccl.omega_x(self.be_cosmo, a, "matter")
+
+    def _get_E2(self, z):
+        a = self._get_a_from_z(z)
+        return (ccl.h_over_h0(self.be_cosmo, a)) ** 2
+
+    def _get_E2Omega_m(self, z):
+        a = self._get_a_from_z(z)
+        return ccl.omega_x(self.be_cosmo, a, "matter") * (ccl.h_over_h0(self.be_cosmo, a)) ** 2
+
+    def _get_rho_m(self, z):
+        # total matter density in physical units [Msun/Mpc3]
+        a = self._get_a_from_z(z)
+        return ccl.rho_x(self.be_cosmo, a, "matter", is_comoving=False)
+
+    def _get_rho_c(self, z):
+        a = self._get_a_from_z(z)
+        return ccl.rho_x(self.be_cosmo, a, "critical", is_comoving=False)
+
+    def _eval_da_z1z2_core(self, z1, z2):
+        a1 = np.atleast_1d(self._get_a_from_z(z1))
+        a2 = np.atleast_1d(self._get_a_from_z(z2))
+        if len(a1) == 1 and len(a2) != 1:
+            a1 = np.full_like(a2, a1)
+        elif len(a2) == 1 and len(a1) != 1:
+            a2 = np.full_like(a1, a2)
+
+        da = ccl.angular_diameter_distance(self.be_cosmo, a1, a2)
+        res = da if np.iterable(z1) or np.iterable(z2) else da.item()
+
+        return res
+
+    def _eval_sigma_crit_core(self, z_len, z_src):
+        a_len = self._get_a_from_z(z_len)
+        a_src = self._get_a_from_z(z_src)
+
+        return self.be_cosmo.sigma_critical(a_lens=a_len, a_source=a_src) * self.cor_factor
+
+    def _eval_linear_matter_powerspectrum(self, k_vals, redshift):
+        return ccl.linear_matter_power(self.be_cosmo, k_vals, self._get_a_from_z(redshift))
---
+++ packages/clmm/clmm-1.16.10.tar.gz/clmm-1.16.10/clmm/theory/ccl.py
@@ -0,0 +1,212 @@
+"""@file ccl.py
+Modeling using CCL
+"""
+
+import pyccl as ccl
+
+# Functions to model halo profiles
+from ..cosmology.ccl import CCLCosmology
+from ..utils import _patch_rho_crit_to_cd2018
+from .parent_class import CLMModeling
+
+
+class CCLCLMModeling(CLMModeling):
+    r"""Object with functions for halo mass modeling
+
+    Attributes
+    ----------
+    backend: str
+        Name of the backend being used
+    massdef : str
+        Profile mass definition ("mean", "critical", "virial" - letter case independent)
+    delta_mdef : int
+        Mass overdensity definition.
+    halo_profile_model : str
+        Profile model parameterization ("nfw", "einasto", "hernquist" - letter case independent)
+    cosmo: Cosmology
+        Cosmology object
+    hdpm: Object
+        Backend object with halo profiles
+    mdef_dict: dict
+        Dictionary with the definitions for mass
+    hdpm_dict: dict
+        Dictionary with the definitions for profile
+    mdef: ccl.halos.MassDef, None
+        Internal MassDef object
+    conc: ccl.halos.ConcentrationConstant, None
+        Internal ConcentrationConstant object
+    """
+
+    # pylint: disable=too-many-instance-attributes
+
+    def __init__(
+        self,
+        massdef="mean",
+        delta_mdef=200,
+        halo_profile_model="nfw",
+        validate_input=True,
+    ):
+        CLMModeling.__init__(self, validate_input)
+        # Update class attributes
+        self.backend = "ccl"
+        self.mdef_dict = {
+            "mean": "matter",
+            "critical": "critical",
+            "virial": "critical",
+        }
+        self.hdpm_dict = {
+            "nfw": ccl.halos.HaloProfileNFW,
+            "einasto": ccl.halos.HaloProfileEinasto,
+            "hernquist": ccl.halos.HaloProfileHernquist,
+        }
+        self.cosmo_class = CCLCosmology
+        self.hdpm_opts = {
+            "nfw": {
+                "truncated": False,
+                "projected_analytic": True,
+                "cumul2d_analytic": True,
+            },
+            "einasto": {"truncated": False},
+            "hernquist": {
+                "truncated": False,
+                "projected_analytic": True,
+                "cumul2d_analytic": True,
+            },
+        }
+        self.cor_factor = _patch_rho_crit_to_cd2018(ccl.physical_constants.RHO_CRITICAL)
+        self.__mdelta_cor = 0.0  ## mass with corretion for input
+        # self.hdpm_opts['einasto'].update({'alpha': 0.25}) # same as NC default
+
+        # Set halo profile and cosmology
+        self.mdef = None
+        self.conc = None
+        self.set_halo_density_profile(halo_profile_model, massdef, delta_mdef)
+        self.set_cosmo(None)
+
+    def _set_projected_quad(self, use_projected_quad):
+        if hasattr(self.hdpm, "projected_quad"):
+            self.hdpm_opts["einasto"]["projected_quad"] = use_projected_quad
+            self._update_halo_density_profile()
+        else:
+            raise NotImplementedError("projected_quad is not available on this version of CCL.")
+
+    # Functions implemented by child class
+
+    def _update_halo_density_profile(self):
+        """updates halo density profile with set internal properties"""
+        # prepare mdef object
+        if self.massdef == "virial":
+            self.mdef = ccl.halos.MassDef("vir", self.mdef_dict[self.massdef])
+        else:
+            self.mdef = ccl.halos.MassDef(self.delta_mdef, self.mdef_dict[self.massdef])
+        # setting concentration (also updates hdpm)
+        self.cdelta = self.cdelta if self.hdpm else 4.0  # ccl always needs an input concentration
+
+    def _get_concentration(self):
+        """get concentration"""
+        return self.conc.c
+
+    def _get_mass(self):
+        """get mass"""
+        return self.__mdelta_cor * self.cor_factor
+
+    def _set_concentration(self, cdelta):
+        """set concentration. Also sets/updates hdpm"""
+        self.conc = ccl.halos.ConcentrationConstant(c=cdelta, mass_def=self.mdef)
+        self.hdpm = self.hdpm_dict[self.halo_profile_model](
+            concentration=self.conc, mass_def=self.mdef, **self.hdpm_opts[self.halo_profile_model]
+        )
+        self.hdpm.update_precision_fftlog(padding_lo_fftlog=1e-4, padding_hi_fftlog=1e3)
+
+    def _set_mass(self, mdelta):
+        """set mass"""
+        self.__mdelta_cor = mdelta / self.cor_factor
+
+    def _set_einasto_alpha(self, alpha):
+        if alpha is None:
+            self.hdpm.update_parameters(alpha="cosmo")
+        else:
+            self.hdpm.update_parameters(alpha=alpha)
+
+    def _get_einasto_alpha(self, z_cl=None):
+        """get the value of the Einasto slope"""
+        # pylint: disable=protected-access
+        if self.hdpm.alpha != "cosmo":
+            a_cl = 1  # a_cl does not matter in this case
+        else:
+            a_cl = self.cosmo.get_a_from_z(z_cl)
+        return self.hdpm._get_alpha(self.cosmo.be_cosmo, self.__mdelta_cor, a_cl)
+
+    def _get_delta_mdef_virial(self, z_cl):
+        "Gets the virial overdensity"
+        a_cl = self.cosmo.get_a_from_z(z_cl)
+        # This fuction can only return the virial overdensity
+        return self.mdef.get_Delta_vir(self.cosmo.be_cosmo, a_cl)
+
+    def _eval_3d_density(self, r3d, z_cl):
+        """eval 3d density"""
+        return self._call_ccl_profile_lens(self.hdpm.real, r3d, z_cl, ndim=3)
+
+    def _eval_surface_density(self, r_proj, z_cl):
+        """eval surface density"""
+        return self._call_ccl_profile_lens(self.hdpm.projected, r_proj, z_cl)
+
+    def _eval_mean_surface_density(self, r_proj, z_cl):
+        """eval mean surface density"""
+        return self._call_ccl_profile_lens(self.hdpm.cumul2d, r_proj, z_cl)
+
+    def _eval_excess_surface_density(self, r_proj, z_cl):
+        """eval excess surface density"""
+        return self._eval_mean_surface_density(r_proj, z_cl) - self._eval_surface_density(
+            r_proj, z_cl
+        )
+
+    def _eval_convergence_core(self, r_proj, z_cl, z_src):
+        """eval convergence"""
+        return self._call_ccl_profile_lens_src(self.hdpm.convergence, r_proj, z_cl, z_src)
+
+    def _eval_tangential_shear_core(self, r_proj, z_cl, z_src):
+        """eval tangential shear"""
+        return self._call_ccl_profile_lens_src(self.hdpm.shear, r_proj, z_cl, z_src)
+
+    def _eval_reduced_tangential_shear_core(self, r_proj, z_cl, z_src):
+        """eval reduced tangential shear with all background sources at the same plane"""
+        return self._call_ccl_profile_lens_src(self.hdpm.reduced_shear, r_proj, z_cl, z_src)
+
+    def _eval_magnification_core(self, r_proj, z_cl, z_src):
+        """eval magnification"""
+        return self._call_ccl_profile_lens_src(self.hdpm.magnification, r_proj, z_cl, z_src)
+
+    # Helper functions unique to this class
+
+    def _call_ccl_profile_lens(self, ccl_hdpm_func, radius, z_lens, ndim=2):
+        """call ccl profile functions that depend on the lens only"""
+        a_lens = self.cosmo.get_a_from_z(z_lens)
+
+        return (
+            ccl_hdpm_func(
+                self.cosmo.be_cosmo,
+                radius / a_lens,
+                self.__mdelta_cor,
+                a_lens,
+            )
+            * self.cor_factor
+            / a_lens**ndim
+        )
+
+    def _call_ccl_profile_lens_src(self, ccl_hdpm_func, radius, z_lens, z_src):
+        """call ccl profile functions that depend on the lens and the sources"""
+        a_lens = self.cosmo.get_a_from_z(z_lens)
+        a_src = self.cosmo.get_a_from_z(z_src)
+
+        return ccl_hdpm_func(
+            self.cosmo.be_cosmo,
+            radius / a_lens,
+            self.__mdelta_cor,
+            a_lens=a_lens,
+            a_source=a_src,
+        )
+
+
+Cosmology = CCLCosmology
+Modeling = CCLCLMModeling
