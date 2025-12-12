bb043b186c0a93211a1842b96a691bd905eda533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 20:05:59 +0000

    Add package pudb/2025.1.4/pudb-2025.1.4.tar.gz

---
+++ packages/pudb/pudb-2025.1.4.tar.gz/pudb-2025.1.4/debug_me.py
@@ -0,0 +1,67 @@
+from collections import namedtuple
+
+
+Color = namedtuple("Color", ["red", "green", "blue", "alpha"])
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self._b = [b]
+
+
+mc = MyClass(15, MyClass(12, None))
+
+
+from pudb import set_trace
+
+
+set_trace()
+
+
+def simple_func(x):
+    x += 1
+
+    s = range(20)
+    z = None  # noqa: F841
+    w = ()  # noqa: F841
+
+    y = {i: i**2 for i in s}  # noqa: F841
+
+    k = set(range(5, 99))  # noqa: F841
+    c = Color(137, 214, 56, 88)  # noqa: F841
+
+    try:
+        x.invalid  # noqa: B018
+    except AttributeError:
+        pass
+
+    # import sys
+    # sys.exit(1)
+
+    return 2*x
+
+
+def fermat(n):
+    """Returns triplets of the form x^n + y^n = z^n.
+    Warning! Untested with n > 2.
+    """
+
+    # source: "Fermat's last Python script"
+    # https://web.archive.org/web/20200815095147/https://earthboundkid.jottit.com/fermat.py
+    # :)
+
+    for x in range(100):
+        for y in range(1, x+1):
+            for z in range(1, x**n+y**n + 1):
+                if x**n + y**n == z**n:
+                    yield x, y, z
+
+
+print(f"SF {simple_func(10)}")
+
+for i in fermat(2):
+    print(i)
+
+print("FINISHED")
---
+++ packages/pudb/pudb-2025.1.4.tar.gz/pudb-2025.1.4/examples/remote-debug.py
@@ -0,0 +1,10 @@
+def debugged_function(x):
+    y = x + fail  # noqa: F821
+    return y
+
+
+from pudb.remote import debugger
+
+
+dbg = debugger()
+dbg.runcall(debugged_function, 5)
