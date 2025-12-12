245662f89a96c2b7e5fead2ffcc6a2b174856d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:05:42 +0000

    Add package dsw-engine-gateway/1.0.0/dsw_engine_gateway-1.0.0-py2.py3-none-any.whl

---
+++ packages/dsw-engine-gateway/dsw_engine_gateway-1.0.0-py2.py3-none-any.whl/engine_gateway/build_info.py
@@ -0,0 +1,15 @@
+# Generated file
+import collections
+
+BuildInfo = collections.namedtuple(
+    'BuildInfo',
+    ['version', 'built_at', 'sha', 'branch', 'tag'],
+)
+
+BUILD_INFO = BuildInfo(
+    version='v1.0.0~03a5fdb',
+    built_at='2025-12-05 13:35:01Z',
+    sha='03a5fdb719eaed7a21cc8466c48f4fd572bfe128',
+    branch='HEAD',
+    tag='v1.0.0',
+)
