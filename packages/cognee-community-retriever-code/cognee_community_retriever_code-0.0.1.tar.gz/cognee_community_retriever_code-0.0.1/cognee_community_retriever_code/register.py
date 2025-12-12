3e5096f62a297bc9d7cc277ec1bebe6310b5a811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 20:06:29 +0000

    Add package cognee-community-retriever-code/0.0.1/cognee_community_retriever_code-0.0.1.tar.gz

---
+++ packages/cognee-community-retriever-code/cognee_community_retriever_code-0.0.1.tar.gz/cognee_community_retriever_code-0.0.1/cognee_community_retriever_code/register.py
@@ -0,0 +1,5 @@
+from cognee.modules.retrieval.register_retriever import use_retriever
+from cognee.modules.search.types import SearchType
+from cognee_community_retriever_code.code_retriever import CodeRetriever, CodeSearchType
+
+use_retriever(SearchType[CodeSearchType.name], CodeRetriever)
