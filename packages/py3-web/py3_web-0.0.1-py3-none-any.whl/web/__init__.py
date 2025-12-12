7f2cd3d15978a65d5601d9f5d32719d859b43ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 17:05:15 +0000

    Add package py3-web/0.0.1/py3_web-0.0.1-py3-none-any.whl

---
+++ packages/py3-web/py3_web-0.0.1-py3-none-any.whl/web/__init__.py
@@ -0,0 +1,13 @@
+from . import _html as html
+from . import cookies
+from . import headers
+from . import response
+from . import url
+
+__all__ = [
+    "html",
+    "cookies",
+    "headers",
+    "response",
+    "url"
+]
