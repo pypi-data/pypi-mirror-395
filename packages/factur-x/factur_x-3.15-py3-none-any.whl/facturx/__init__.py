7bc6eabe3bcc70cb143b35b198e939ce3809b1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 20:06:41 +0000

    Add package factur-x/3.15/factur_x-3.15-py3-none-any.whl

---
+++ packages/factur-x/factur_x-3.15-py3-none-any.whl/facturx/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "3.15"
+from .facturx import generate_from_file, \
+    generate_facturx_from_file, \
+    generate_from_binary, \
+    generate_facturx_from_binary, \
+    get_xml_namespaces, \
+    get_facturx_flavor, \
+    get_flavor, \
+    get_facturx_level, \
+    get_level, \
+    check_facturx_xsd, \
+    xml_check_xsd, \
+    get_facturx_xml_from_pdf, \
+    get_orderx_xml_from_pdf, \
+    get_xml_from_pdf, \
+    get_orderx_type
