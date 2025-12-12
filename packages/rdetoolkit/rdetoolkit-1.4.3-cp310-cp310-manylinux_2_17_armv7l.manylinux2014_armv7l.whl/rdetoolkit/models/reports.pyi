d139c2f7fcab54351371fb3e722c364264020d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:08:51 +0000

    Add package rdetoolkit/1.4.3/rdetoolkit-1.4.3-cp310-cp310-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/rdetoolkit/rdetoolkit-1.4.3-cp310-cp310-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/rdetoolkit/models/reports.pyi
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class CodeSnippet(BaseModel):
+    file_path: str
+    snippet: str
+    description: str | None
+
+class ReportItem(BaseModel):
+    exec_date: str
+    dockerfile_path: str
+    requirements_path: str
+    include_dirs: list[str]
+    code_security_scan_results: list[CodeSnippet]
+    code_ext_requests_scan_results: list[CodeSnippet]
