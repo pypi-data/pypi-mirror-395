181ac74106ecaed1db6c9666c7adba7eb6f02ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 11:05:35 +0000

    Add package qssh/0.2.1/qssh-0.2.1.tar.gz

---
+++ packages/qssh/qssh-0.2.1.tar.gz/qssh-0.2.1/src/qssh/__init__.py
@@ -0,0 +1,9 @@
+"""qssh - Quick SSH session manager."""
+
+__version__ = "0.2.1"
+__author__ = "benne"
+
+from .session import SessionManager
+from .connector import SSHConnector
+
+__all__ = ["SessionManager", "SSHConnector", "__version__"]
