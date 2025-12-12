fd6cb02cbb2d57f330d9d9f5fdde87035a08f861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:30:46 +0000

    Add package polygon-geohasher-2/0.2.0/polygon_geohasher_2-0.2.0-py3-none-any.whl

---
+++ packages/polygon-geohasher-2/polygon_geohasher_2-0.2.0-py3-none-any.whl/polygon_geohasher/__init__.py
@@ -0,0 +1,3 @@
+from .polygon_geohasher import geohash_to_polygon, geohashes_to_polygon, polygon_to_geohashes
+
+__all__ = ["geohash_to_polygon", "geohashes_to_polygon", "polygon_to_geohashes"]
---
+++ packages/polygon-geohasher-2/polygon_geohasher_2-0.2.0-py3-none-any.whl/polygon_geohasher/version.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+
+def _safe_int(string: str) -> Union[int, str]:
+    try:
+        return int(string)
+    except ValueError:
+        return string
+
+
+__version__ = "0.2.0"
+VERSION = tuple(_safe_int(x) for x in __version__.split("."))
