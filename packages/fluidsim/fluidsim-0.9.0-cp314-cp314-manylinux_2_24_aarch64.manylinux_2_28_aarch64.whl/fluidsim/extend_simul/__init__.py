2836d102d6f2bd89878d0b91df51d99065ad9016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 07:06:46 +0000

    Add package fluidsim/0.9.0/fluidsim-0.9.0-cp314-cp314-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/fluidsim/fluidsim-0.9.0-cp314-cp314-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/fluidsim/extend_simul/__init__.py
@@ -0,0 +1,21 @@
+"""Mechanism to extend a Simul class with just a simple class
+=============================================================
+
+.. autofunction:: extend_simul_class
+
+.. autoclass:: SimulExtender
+   :members:
+   :private-members:
+
+.. autosummary::
+   :toctree:
+
+   spatial_means_regions_milestone
+
+"""
+
+from fluidsim_core.extend_simul import (
+    extend_simul_class,
+    SimulExtender,
+    _extend_simul_class_from_path,
+)
