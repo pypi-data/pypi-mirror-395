d833dc70a8a29c24b2aa9c24d64627122fc22f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 23:05:06 +0000

    Add package celerity/0.40.0/celerity-0.40.0-py3-none-any.whl

---
+++ packages/celerity/celerity-0.40.0-py3-none-any.whl/celerity/planet.py
@@ -0,0 +1,25 @@
+# **************************************************************************************
+
+# @author         Michael Roberts <michael@observerly.com>
+# @package        @observerly/celerity
+# @license        Copyright © 2021-2026 observerly
+
+# **************************************************************************************
+
+from enum import Enum
+
+# **************************************************************************************
+
+
+class Planet(Enum):
+    MERCURY = "Mercury"
+    VENUS = "Venus"
+    EARTH = "Earth"
+    MARS = "Mars"
+    JUPITER = "Jupiter"
+    SATURN = "Saturn"
+    URANUS = "Uranus"
+    NEPTUNE = "Neptune"
+
+
+# **************************************************************************************
