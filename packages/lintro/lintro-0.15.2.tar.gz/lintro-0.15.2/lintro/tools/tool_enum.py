4e2987ab2c631a77db955ac643161a4773909fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:38 +0000

    Add package lintro/0.15.2/lintro-0.15.2.tar.gz

---
+++ packages/lintro/lintro-0.15.2.tar.gz/lintro-0.15.2/lintro/tools/tool_enum.py
@@ -0,0 +1,27 @@
+"""ToolEnum for all Lintro tools, mapping to their classes."""
+
+from enum import Enum
+
+from lintro.tools.implementations.tool_actionlint import ActionlintTool
+from lintro.tools.implementations.tool_bandit import BanditTool
+from lintro.tools.implementations.tool_black import BlackTool
+from lintro.tools.implementations.tool_darglint import DarglintTool
+from lintro.tools.implementations.tool_hadolint import HadolintTool
+from lintro.tools.implementations.tool_prettier import PrettierTool
+from lintro.tools.implementations.tool_pytest import PytestTool
+from lintro.tools.implementations.tool_ruff import RuffTool
+from lintro.tools.implementations.tool_yamllint import YamllintTool
+
+
+class ToolEnum(Enum):
+    """Enumeration mapping tool names to their implementation classes."""
+
+    BLACK = BlackTool
+    DARGLINT = DarglintTool
+    HADOLINT = HadolintTool
+    PRETTIER = PrettierTool
+    PYTEST = PytestTool
+    RUFF = RuffTool
+    YAMLLINT = YamllintTool
+    ACTIONLINT = ActionlintTool
+    BANDIT = BanditTool
