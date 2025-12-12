6fe8dcb75e14d7ddb8c0b8fb7821a5ac974c2642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 15:07:42 +0000

    Add package pixel-font-builder/0.0.44/pixel_font_builder-0.0.44-py3-none-any.whl

---
+++ packages/pixel-font-builder/pixel_font_builder-0.0.44-py3-none-any.whl/pixel_font_builder/opentype/config.py
@@ -0,0 +1,29 @@
+from typing import Final
+
+from pixel_font_builder.opentype.feature import FeatureFile
+from pixel_font_builder.opentype.metric import BoundingBox
+from pixel_font_builder.opentype.outline import OutlinesPainter, SolidOutlinesPainter
+
+
+class Config:
+    DEFAULT_OUTLINES_PAINTER: Final = SolidOutlinesPainter()
+
+    px_to_units: int
+    has_vertical_metrics: bool
+    head_bounding_box_override: BoundingBox | None
+    outlines_painter: OutlinesPainter
+    feature_files: list[FeatureFile]
+
+    def __init__(
+            self,
+            px_to_units: int = 100,
+            has_vertical_metrics: bool = True,
+            head_bounding_box_override: BoundingBox | None = None,
+            outlines_painter: OutlinesPainter | None = None,
+            feature_files: list[FeatureFile] | None = None,
+    ):
+        self.px_to_units = px_to_units
+        self.has_vertical_metrics = has_vertical_metrics
+        self.head_bounding_box_override = head_bounding_box_override
+        self.outlines_painter = Config.DEFAULT_OUTLINES_PAINTER if outlines_painter is None else outlines_painter
+        self.feature_files = [] if feature_files is None else feature_files
