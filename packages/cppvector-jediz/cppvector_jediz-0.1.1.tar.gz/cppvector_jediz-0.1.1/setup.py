ffab107b42c2dce2774d1bb1d0193098ebffee7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:08:40 +0000

    Add package cppvector-jediz/0.1.1/cppvector_jediz-0.1.1.tar.gz

---
+++ packages/cppvector-jediz/cppvector_jediz-0.1.1.tar.gz/cppvector_jediz-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cppvector",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A C++ style vector implementation for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JedizLaPulga/lapython",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    install_requires=[],
+)
---
+++ packages/cppvector-jediz/cppvector_jediz-0.1.1.tar.gz/cppvector_jediz-0.1.1/tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for cppvector package"""
