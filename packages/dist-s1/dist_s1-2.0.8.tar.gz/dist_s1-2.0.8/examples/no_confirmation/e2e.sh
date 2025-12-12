60b56dbce57d8187088a6b4a7465f559101a8693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 07:06:49 +0000

    Add package dist-s1/2.0.8/dist_s1-2.0.8.tar.gz

---
+++ packages/dist-s1/dist_s1-2.0.8.tar.gz/dist_s1-2.0.8/examples/no_confirmation/e2e.sh
@@ -0,0 +1,9 @@
+dist-s1 run \
+    --mgrs_tile_id '11SLT' \
+    --post_date '2025-01-21' \
+    --track_number 71 \
+    --dst_dir '../../notebooks/los-angeles' \
+    --memory_strategy 'low' \
+    --device 'cpu' \
+    --n_workers_for_norm_param_estimation 5 \
+    --apply_water_mask true \
