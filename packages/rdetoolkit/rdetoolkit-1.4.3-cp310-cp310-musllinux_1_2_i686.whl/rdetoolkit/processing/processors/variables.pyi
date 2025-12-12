846e0e848ebd014e2b54e975a6c2cb03caa8c458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:08:55 +0000

    Add package rdetoolkit/1.4.3/rdetoolkit-1.4.3-cp310-cp310-musllinux_1_2_i686.whl

---
+++ packages/rdetoolkit/rdetoolkit-1.4.3-cp310-cp310-musllinux_1_2_i686.whl/rdetoolkit/processing/processors/variables.pyi
@@ -0,0 +1,8 @@
+from _typeshed import Incomplete as Incomplete
+from rdetoolkit.processing.context import ProcessingContext as ProcessingContext
+from rdetoolkit.processing.pipeline import Processor as Processor
+
+logger: Incomplete
+
+class VariableApplier(Processor):
+    def process(self, context: ProcessingContext) -> None: ...
