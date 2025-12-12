e71d750fcf17a61d21474a42ce2b0538680e6910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 21:08:13 +0000

    Add package omspy/0.20.0/omspy-0.20.0.tar.gz

---
+++ packages/omspy/omspy-0.20.0.tar.gz/omspy-0.20.0/omspy/algos/trailing.py
@@ -0,0 +1,286 @@
+import pendulum
+from omspy.order import Order, CompoundOrder
+from pydantic import BaseModel, PrivateAttr, Field, ConfigDict
+from sqlite_utils import Database
+from typing import Optional, Any, NamedTuple
+from collections import namedtuple
+
+trailing_values = namedtuple("trailing", ["stop", "target"], defaults=[None, None])
+
+
+class TrailingResult(NamedTuple):
+    done: bool
+    stop: Optional[float] = None
+    target: Optional[float] = None
+    next_trail_at: Optional[float] = None
+
+
+def _get_trailing_stop_by_percent(
+    max_mtm: float, trailing_percent: float, trailing_step: Optional[float] = None
+) -> float:
+    """
+    max_mtm
+        max_mtm hit
+    trailing_percent
+        trailing_percentage, pass 10 percent as 10
+    trailing_step
+        optional trailing step as absolute value
+    """
+    # TODO: Handle percentages greater than 100
+    if trailing_step:
+        m = (max_mtm // trailing_step) * trailing_step
+        calc = m * (1 - (trailing_percent / 100))
+        if calc >= 0:
+            return max(calc, trailing_step)
+        else:
+            return calc + trailing_step * 0.5
+    else:
+        return max_mtm * (1 - (trailing_percent / 100))
+
+
+def _get_trailing_stop_by_mtm(
+    max_mtm: float, trailing_mtm: float, trailing_step: Optional[float] = None
+) -> float:
+    """
+    get the trailing stop by mtm value
+    max_mtm
+        max mtm hit
+    trailing_mtm
+        trailing value of mtm, this is the mtm at which trailing is reset
+    trailing_step
+        optional trailing step, this is the distance to be maintained between max_mtm and trailing stop
+    Note
+    -----
+    1) If trailing_step is not given, trailing_stop will be max_mtm - trailing_mtm
+    2) If trailing_step is greater than trailing mtm, then trailing_stop will be trailing_step
+    """
+    if trailing_step:
+        m = (max_mtm // trailing_step) * trailing_step
+        if max_mtm >= 0:
+            return max(m - trailing_mtm, trailing_step)
+        else:
+            return m + trailing_mtm + trailing_step
+    return max_mtm - trailing_mtm if max_mtm >= 0 else max_mtm + trailing_mtm
+
+
+def get_trailing_stop_and_target(
+    max_mtm: float,
+    target: Optional[float] = None,
+    trailing_stop: Optional[float] = None,
+    trailing_percent: Optional[float] = None,
+    trailing_mtm: Optional[float] = None,
+    trailing_step: Optional[float] = None,
+    start_trailing_at: Optional[float] = None,
+) -> trailing_values:
+    """
+    Get trailing stop and target value based on the given parameters
+    last_price
+        last traded price of the instrument
+    max_mtm
+        maximum mtm hit
+    min_mtm
+        minimum mtm hit
+    target
+        target value to exit
+    trailing_stop
+        the initial trailing stop value
+    trailing_percent
+        trailing percent from mtm; specify 10 percent as 10
+        this is the value at which the next trailing is done, in percentage
+    trailing_mtm
+        trailing mtm value, this is the value at which the next trailing is done, in absolute mtm
+    trailing_step
+        trailing step value, this is the value at which the next trailing is done, in absolute mtm
+    start_trailing_at
+        start trailing at this value, this is an absolute number
+    returns
+        trailing_stop, target.
+        None if only last price is given
+    Note
+    ----
+    1) provide either trailing_percent or trailing_mtm, if both are given,
+    trailing_percent takes precedence
+    """
+
+    def all_none() -> bool:
+        return (
+            trailing_stop is None
+            and target is None
+            and trailing_percent is None
+            and trailing_mtm is None
+            and start_trailing_at is None
+        )
+
+    def trailing_none() -> bool:
+        return (
+            trailing_percent is None
+            and trailing_mtm is None
+            and start_trailing_at is None
+        )
+
+    if all_none():
+        return trailing_values()
+    if target and not trailing_stop and trailing_none():
+        return trailing_values(target=target)
+    if not target and trailing_stop and trailing_none():
+        return trailing_values(stop=trailing_stop)
+    if target and trailing_stop and trailing_none():
+        return trailing_values(target=target, stop=trailing_stop)
+    if trailing_percent:
+        stop = _get_trailing_stop_by_percent(max_mtm, trailing_percent, trailing_step)
+        if start_trailing_at:
+            if max_mtm >= start_trailing_at:
+                return trailing_values(stop=stop, target=target)
+            else:
+                return trailing_values(stop=trailing_stop, target=target)
+
+        return trailing_values(stop=stop, target=target)
+    if trailing_mtm:
+        # TODO: Check for max trailing stop if mtm greater than trailing stop
+        stop = _get_trailing_stop_by_mtm(max_mtm, trailing_mtm, trailing_step)
+        if start_trailing_at:
+            if max_mtm >= start_trailing_at:
+                return trailing_values(stop=stop, target=target)
+            else:
+                return trailing_values(stop=trailing_stop, target=target)
+        return trailing_values(stop=stop, target=target)
+
+
+class Trailing(BaseModel):
+    """
+    start_time
+        starting time to trail
+        this takes precendence over others and trailing is
+        only started after this
+    end_time
+        end time for trailing
+        trailing is not done after this
+    target
+        target value to exit
+    trailing_stop
+        the initial trailing stop value
+    start_trailing_at
+        start trailing at this value, this is an absolute number
+    trailing_step
+        trailing step value, this is the value at which the next trailing is done, in absolute mtm
+    trailing_percent
+        trailing percent from mtm, this is the value at which the next trailing is done, in percentage
+    trailing_mtm
+        trailing mtm value, this is the value at which the next trailing is done, in absolute mtm
+    order
+        the `CompoundOrder` containing all the orders
+    Note
+    ----
+    1) start and end time takes precendence over all other arguments.
+    2) provide either trailing_percent or trailing_mtm, if both are given, trailing_percent takes precedence
+    """
+
+    start_time: pendulum.DateTime
+    end_time: pendulum.DateTime
+    target: Optional[float] = None
+    trailing_stop: Optional[float] = None
+    start_trailing_at: Optional[float] = None
+    trailing_step: Optional[float] = None
+    trailing_percent: Optional[float] = None
+    trailing_mtm: Optional[float] = None
+    cycle: int = 0
+    broker: Optional[Any] = None
+    connection: Optional[Database] = None
+    order: Optional[CompoundOrder] = None
+    ltps: dict[str, float] = Field(default_factory=dict)
+    _can_start_mtm_trailing: bool = False
+    _next_trail: Optional[float] = None
+
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True
+    )
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        if self.order is None:
+            self.order = CompoundOrder(broker=self.broker, connection=self.connection)
+
+    @property
+    def done(self) -> bool:
+        if self.mtm:
+            # Force extreme values in case of None
+            target = self.target or 1e100
+            stop = self.trailing_stop or -1e100
+            return self.mtm >= target or self.mtm <= stop
+        else:
+            return False
+
+    @property
+    def can_start_mtm_trailing(self) -> bool:
+        return self._can_start_mtm_trailing
+
+    @property
+    def next_trail(self) -> Optional[float]:
+        return self._next_trail
+
+    @property
+    def mtm(self) -> float:
+        return self.order.total_mtm
+
+    @property
+    def can_trail(self) -> bool:
+        time_trail = self.start_time <= pendulum.now(tz="local") <= self.end_time
+        if self.start_trailing_at:
+            if not self._can_start_mtm_trailing:
+                self._can_start_mtm_trailing = self.mtm >= self.start_trailing_at
+            return time_trail and self.can_start_mtm_trailing
+        else:
+            return time_trail
+
+    def add(self, order: Order) -> None:
+        """
+        Add an order to the existing compound order
+        """
+        self.order.add(order)
+
+    def update(self, data: dict[str, float]) -> TrailingResult:
+        """
+        return the trailing result
+        """
+        self.ltps.update(data)
+        self.order.ltp.update(data)
+        if self.can_trail:
+            mtm = self.mtm
+            result = get_trailing_stop_and_target(
+                max_mtm=mtm,
+                target=self.target,
+                trailing_stop=self.trailing_stop,
+                trailing_percent=self.trailing_percent,
+                trailing_mtm=self.trailing_mtm,
+                trailing_step=self.trailing_step,
+                start_trailing_at=self.start_trailing_at,
+            )
+            tstop = result.stop
+            if tstop:
+                if tstop != self.trailing_stop:
+                    # Trailing stop must always be an increasing value
+                    # Do not revert back trailing stop to a lower value
+                    self.trailing_stop = max(self.trailing_stop, tstop)
+                    if self.trailing_step:
+                        nt = (
+                            mtm // self.trailing_step
+                        ) * self.trailing_step + self.trailing_step
+                        if self.next_trail:
+                            self._next_trail = max(nt, self.next_trail)
+                        else:
+                            self._next_trail = nt
+
+        return TrailingResult(
+            done=self.done,
+            stop=self.trailing_stop,
+            target=self.target,
+            next_trail_at=self.next_trail,
+        )
+
+    def run(self, data: dict[str, float]) -> None:
+        """
+        run the trailing logic with ltp data
+        data
+            ltp data as dictionary
+        """
+        self.order.ltp.update(data)
---
+++ packages/omspy/omspy-0.20.0.tar.gz/omspy-0.20.0/omspy/utils.py
@@ -0,0 +1,243 @@
+"""
+General utility functions for conversion and else
+"""
+
+from typing import Dict, Any, List, Union, NamedTuple
+from omspy.models import BasicPosition
+from collections import defaultdict
+import pathlib
+import yaml
+import logging
+
+
+class UQty(NamedTuple):
+    q: float
+    f: float
+    p: float
+    c: float
+
+
+def create_basic_positions_from_orders_dict(
+    orders: List[Dict],
+) -> Dict[str, BasicPosition]:
+    """
+    Create a dictionary of positions from list of orders received from broker
+    orders
+        list of orders
+    returns a dictionary with key being the symbol and values as a BasicPosition model
+    """
+    dct: Dict[str, BasicPosition] = {}
+    for order in orders:
+        symbol = order.get("symbol")
+        if symbol:
+            price = max(
+                order.get("average_price", 0),
+                order.get("price", 0),
+                order.get("trigger_price", 0),
+            )
+            quantity = abs(order.get("quantity", 0))
+            side = order["side"].lower()
+            if symbol not in dct.keys():
+                # Create symbol if it doesn't exist
+                dct[symbol] = BasicPosition(symbol=symbol)
+            position = dct[symbol]
+            if side == "buy":
+                position.buy_quantity += quantity
+                position.buy_value += price * quantity
+            elif side == "sell":
+                position.sell_quantity += quantity
+                position.sell_value += price * quantity
+    return dct
+
+
+def dict_filter(lst: List[Dict], **kwargs) -> List[Dict]:
+    """
+    Filter a list of dictionary to conditions matching
+    in kwargs
+    lst
+        list of dictionaries
+    kwargs
+        key values to filter; key is the dictionary key
+        and value is the value to match.
+        **This is an AND filter**
+
+    Note
+    -----
+    For each dictionary in the list, each of the arguments
+    in kwargs are matched and only those dictionaries that
+    match all the conditions are returned
+    """
+    if len(lst) == 0:
+        print("Nothing in the list")
+        return []
+    new_lst = []
+    for d in lst:
+        case = True
+        for k, v in kwargs.items():
+            if d.get(k) != v:
+                case = False
+        if case:
+            new_lst.append(d)
+    return new_lst
+
+
+def tick(price, tick_size=0.05):
+    """
+    Rounds a given price to the requested tick
+    """
+    return round(price / tick_size) * tick_size
+
+
+def stop_loss_step_decimal(
+    price: float, side: str = "B", dec: float = 0.45, step: int = 2
+) -> float:
+    """
+    Truncates down the stop loss value to the desired step
+    and adds the given decimal
+    price
+        stop loss price
+    side
+        side to place order, the actual stop loss side
+        B for BUY, S for SELL
+    dec
+        fixed decimal to be added
+    step
+        step size to determine stop
+    Note
+    ----
+    1. Step object is always a positive number
+    2. Side is the actual stop loss side you are placing the order
+    """
+    step = abs(step)
+    m = int(price / step)
+    val = (m + 1.0) * step if side == "S" else (m * step) - 1.0
+    val = val + 1 - dec if side == "S" else val + dec
+    return val
+
+
+def update_quantity(q: float, f: float, p: float, c: float) -> UQty:
+    """
+    Update filled,pending and canceled quantity based on the given data
+    q
+        actual quantity orders
+    f
+        filled quantity
+    p
+        pending quantity
+    c
+        canceled/rejected quantity
+    returns a named tuple containing the calculated values
+    Note
+    -----
+    1) q is taken to be the actual quantity
+    2) formula used is **quantity = filled_quantity + pending_quantiy - canceled quantity**
+    3) canceled quantity is given preference over other quantities, filled quantity
+    the next and pending the last priority
+    4) if filled or canceled quantity is greater than given quantity, it is set equal
+    to the quantity and all other quantities are discarded
+    """
+    if c > 0:
+        c = min(c, q)
+        f = q - c
+        p = q - c - f
+    elif f > 0:
+        f = min(f, q)
+        p = q - f
+    elif p > 0:
+        p = min(p, q)
+        f = q - p
+    else:
+        p = q - p
+    return UQty(q, f, p, c)
+
+
+def load_broker(
+    credentials: Union[
+        str, dict, list[dict], pathlib.PurePath, pathlib.Path
+    ] = "credentials.yaml",
+    index: int = 0,
+) -> Any:
+    """
+    load broker based on given data in the expected format
+    expected format for a broker is a dictionary with 2 keys, name and config. name is the broker module name and config is a dictionary containing arguments to the broker
+    ```
+    {
+        "name": "broker_module_name",
+        "config": {
+            "api_key": "api key",
+            "api_secret": "api secret",
+            "access_token": "access token",
+            "refresh_token": "refresh token",
+            "user_id": "user id",
+    }
+    credentials
+        string , dictionary or list of dictionaries containing broker credentials in requested format
+        default filename `credentials.yaml` in the current directory
+        * if credentials is a string or a pathlib path, it will be treated as filename
+        * if name is a dict, then it will be treated as dictionary and the index argument is discarded
+        * if name is a list, then it will be treated as list of dictionaries
+    index
+        index of the broker in the list
+        this argument is discared if a dictionary is given
+    Note
+    -----
+    1. This only initializes the broker, it does not authenticate the broker.
+    2. the `config` key is a dictionary containing arguments specific  to a broker and it must contain arguments matching the specific broker
+    3. returns `None` if the specified broker is not found
+    """
+    from omspy.brokers.zerodha import Zerodha
+    from omspy.brokers.finvasia import Finvasia
+    from omspy.brokers.icici import Icici
+    from omspy.brokers.neo import Neo
+    from omspy.brokers.noren import Noren
+
+    broker_mapping = {
+        "zerodha": Zerodha,
+        "finvasia": Finvasia,
+        "icici": Icici,
+        "neo": Neo,
+        "noren": Noren,
+    }
+    print(broker_mapping)
+    print(credentials)
+
+    def _load_instance(creds):
+        try:
+            name = creds["name"].lower()
+            print(name)
+            broker_module = broker_mapping[name]
+            print(name, broker_module)
+            if broker_module:
+                try:
+                    broker = broker_module(**creds["config"])
+                    return broker
+                except Exception as e:
+                    logging.error(e)
+                    return
+        except KeyError as e:
+            logging.error(e)
+            error_msg = f"""
+             Broker {creds["name"]} not available.
+             Available brokers are {list(broker_mapping.keys())}
+             If you have this broker installed locally,
+             kindly submit a pull request
+             """
+            raise ValueError(error_msg)
+
+    if isinstance(credentials, str):
+        with open(credentials, "r") as f:
+            broker_list = yaml.safe_load(f)
+        creds = broker_list[index]
+        return _load_instance(creds)
+    elif isinstance(credentials, (pathlib.PurePath, pathlib.Path)):
+        with open(str(credentials), "r") as f:
+            broker_list = yaml.safe_load(f)
+        creds = broker_list[index]
+        return _load_instance(creds)
+    elif isinstance(credentials, dict):
+        return _load_instance(credentials)
+    elif isinstance(credentials, list):
+        creds = credentials[index]
+        return _load_instance(creds)
+    else:
+        raise ValueError("Invalid credentials format")
