1217d0ec120a8cd0568ebfc857422f66c5dcba26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 15:06:02 +0000

    Add package accelbyte-py-sdk-service-ams/0.25.0/accelbyte_py_sdk_service_ams-0.25.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-ams/accelbyte_py_sdk_service_ams-0.25.0.tar.gz/accelbyte_py_sdk_service_ams-0.25.0/accelbyte_py_sdk/api/ams/operations/ams_qo_s/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the Fleet Commander."""
+
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .qo_s_regions_get import QoSRegionsGet
+from .qo_s_regions_update import QoSRegionsUpdate
---
+++ packages/accelbyte-py-sdk-service-ams/accelbyte_py_sdk_service_ams-0.25.0.tar.gz/accelbyte_py_sdk_service_ams-0.25.0/accelbyte_py_sdk/api/ams/operations/artifacts/fleet_artifact_sampling_739743.py
@@ -0,0 +1,408 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+# Fleet Commander
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import ApiError, ApiResponse
+from accelbyte_py_sdk.core import Operation
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import HttpResponse
+from accelbyte_py_sdk.core import deprecated
+
+from ...models import ApiFleetArtifactsSampleRules
+from ...models import ResponseErrorResponse
+
+
+class FleetArtifactSamplingRulesGet(Operation):
+    """get the sampling rules for a fleet (FleetArtifactSamplingRulesGet)
+
+    Required Permission: ADMIN:NAMESPACE:{namespace}:AMS:ARTIFACT [READ]
+
+    Properties:
+        url: /ams/v1/admin/namespaces/{namespace}/fleets/{fleetID}/artifacts-sampling-rules
+
+        method: GET
+
+        tags: ["Artifacts"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        fleet_id: (fleetID) REQUIRED str in path
+
+        namespace: (namespace) REQUIRED str in path
+
+    Responses:
+        200: OK - ApiFleetArtifactsSampleRules (success)
+
+        400: Bad Request - ResponseErrorResponse (invalid fleet ID)
+
+        401: Unauthorized - ResponseErrorResponse (no authorization provided)
+
+        403: Forbidden - ResponseErrorResponse (insufficient permissions)
+
+        404: Not Found - ResponseErrorResponse (artifact sampling rules not found for fleet)
+
+        500: Internal Server Error - ResponseErrorResponse (internal server error)
+    """
+
+    # region fields
+
+    _url: str = (
+        "/ams/v1/admin/namespaces/{namespace}/fleets/{fleetID}/artifacts-sampling-rules"
+    )
+    _path: str = (
+        "/ams/v1/admin/namespaces/{namespace}/fleets/{fleetID}/artifacts-sampling-rules"
+    )
+    _base_path: str = ""
+    _method: str = "GET"
+    _consumes: List[str] = ["application/json"]
+    _produces: List[str] = ["application/json"]
+    _securities: List[List[str]] = [["BEARER_AUTH"]]
+    _location_query: str = None
+
+    service_name: Optional[str] = "ams"
+
+    fleet_id: str  # REQUIRED in [path]
+    namespace: str  # REQUIRED in [path]
+
+    # endregion fields
+
+    # region properties
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    @property
+    def base_path(self) -> str:
+        return self._base_path
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    @property
+    def consumes(self) -> List[str]:
+        return self._consumes
+
+    @property
+    def produces(self) -> List[str]:
+        return self._produces
+
+    @property
+    def securities(self) -> List[List[str]]:
+        return self._securities
+
+    @property
+    def location_query(self) -> str:
+        return self._location_query
+
+    # endregion properties
+
+    # region get methods
+
+    # endregion get methods
+
+    # region get_x_params methods
+
+    def get_all_params(self) -> dict:
+        return {
+            "path": self.get_path_params(),
+        }
+
+    def get_path_params(self) -> dict:
+        result = {}
+        if hasattr(self, "fleet_id"):
+            result["fleetID"] = self.fleet_id
+        if hasattr(self, "namespace"):
+            result["namespace"] = self.namespace
+        return result
+
+    # endregion get_x_params methods
+
+    # region is/has methods
+
+    # endregion is/has methods
+
+    # region with_x methods
+
+    def with_fleet_id(self, value: str) -> FleetArtifactSamplingRulesGet:
+        self.fleet_id = value
+        return self
+
+    def with_namespace(self, value: str) -> FleetArtifactSamplingRulesGet:
+        self.namespace = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "fleet_id") and self.fleet_id:
+            result["fleetID"] = str(self.fleet_id)
+        elif include_empty:
+            result["fleetID"] = ""
+        if hasattr(self, "namespace") and self.namespace:
+            result["namespace"] = str(self.namespace)
+        elif include_empty:
+            result["namespace"] = ""
+        return result
+
+    # endregion to methods
+
+    # region response methods
+
+    class Response(ApiResponse):
+        data_200: Optional[ApiFleetArtifactsSampleRules] = None
+        error_400: Optional[ResponseErrorResponse] = None
+        error_401: Optional[ResponseErrorResponse] = None
+        error_403: Optional[ResponseErrorResponse] = None
+        error_404: Optional[ResponseErrorResponse] = None
+        error_500: Optional[ResponseErrorResponse] = None
+
+        def ok(self) -> FleetArtifactSamplingRulesGet.Response:
+            if self.error_400 is not None:
+                err = self.error_400.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_401 is not None:
+                err = self.error_401.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_403 is not None:
+                err = self.error_403.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_404 is not None:
+                err = self.error_404.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            if self.error_500 is not None:
+                err = self.error_500.translate_to_api_error()
+                exc = err.to_exception()
+                if exc is not None:
+                    raise exc  # pylint: disable=raising-bad-type
+            return self
+
+        def __iter__(self):
+            if self.data_200 is not None:
+                yield self.data_200
+                yield None
+            elif self.error_400 is not None:
+                yield None
+                yield self.error_400
+            elif self.error_401 is not None:
+                yield None
+                yield self.error_401
+            elif self.error_403 is not None:
+                yield None
+                yield self.error_403
+            elif self.error_404 is not None:
+                yield None
+                yield self.error_404
+            elif self.error_500 is not None:
+                yield None
+                yield self.error_500
+            else:
+                yield None
+                yield self.error
+
+    # noinspection PyMethodMayBeStatic
+    def parse_response(self, code: int, content_type: str, content: Any) -> Response:
+        """Parse the given response.
+
+        200: OK - ApiFleetArtifactsSampleRules (success)
+
+        400: Bad Request - ResponseErrorResponse (invalid fleet ID)
+
+        401: Unauthorized - ResponseErrorResponse (no authorization provided)
+
+        403: Forbidden - ResponseErrorResponse (insufficient permissions)
+
+        404: Not Found - ResponseErrorResponse (artifact sampling rules not found for fleet)
+
+        500: Internal Server Error - ResponseErrorResponse (internal server error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        result = FleetArtifactSamplingRulesGet.Response()
+
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+
+        if error is not None:
+            if not error.is_no_content():
+                result.error = ApiError.create_from_http_response(error)
+        else:
+            code, content_type, content = pre_processed_response
+
+            if code == 200:
+                result.data_200 = ApiFleetArtifactsSampleRules.create_from_dict(content)
+            elif code == 400:
+                result.error_400 = ResponseErrorResponse.create_from_dict(content)
+                result.error = result.error_400.translate_to_api_error()
+            elif code == 401:
+                result.error_401 = ResponseErrorResponse.create_from_dict(content)
+                result.error = result.error_401.translate_to_api_error()
+            elif code == 403:
+                result.error_403 = ResponseErrorResponse.create_from_dict(content)
+                result.error = result.error_403.translate_to_api_error()
+            elif code == 404:
+                result.error_404 = ResponseErrorResponse.create_from_dict(content)
+                result.error = result.error_404.translate_to_api_error()
+            elif code == 500:
+                result.error_500 = ResponseErrorResponse.create_from_dict(content)
+                result.error = result.error_500.translate_to_api_error()
+            else:
+                result.error = ApiError.create_from_http_response(
+                    HttpResponse.create_undocumented_response(
+                        code=code, content_type=content_type, content=content
+                    )
+                )
+
+        result.status_code = str(code)
+        result.content_type = content_type
+
+        if 400 <= code <= 599 or result.error is not None:
+            result.is_success = False
+
+        return result
+
+    # noinspection PyMethodMayBeStatic
+    @deprecated
+    def parse_response_x(
+        self, code: int, content_type: str, content: Any
+    ) -> Tuple[
+        Union[None, ApiFleetArtifactsSampleRules],
+        Union[None, HttpResponse, ResponseErrorResponse],
+    ]:
+        """Parse the given response.
+
+        200: OK - ApiFleetArtifactsSampleRules (success)
+
+        400: Bad Request - ResponseErrorResponse (invalid fleet ID)
+
+        401: Unauthorized - ResponseErrorResponse (no authorization provided)
+
+        403: Forbidden - ResponseErrorResponse (insufficient permissions)
+
+        404: Not Found - ResponseErrorResponse (artifact sampling rules not found for fleet)
+
+        500: Internal Server Error - ResponseErrorResponse (internal server error)
+
+        ---: HttpResponse (Undocumented Response)
+
+        ---: HttpResponse (Unexpected Content-Type Error)
+
+        ---: HttpResponse (Unhandled Error)
+        """
+        pre_processed_response, error = self.pre_process_response(
+            code=code, content_type=content_type, content=content
+        )
+        if error is not None:
+            return None, None if error.is_no_content() else error
+        code, content_type, content = pre_processed_response
+
+        if code == 200:
+            return ApiFleetArtifactsSampleRules.create_from_dict(content), None
+        if code == 400:
+            return None, ResponseErrorResponse.create_from_dict(content)
+        if code == 401:
+            return None, ResponseErrorResponse.create_from_dict(content)
+        if code == 403:
+            return None, ResponseErrorResponse.create_from_dict(content)
+        if code == 404:
+            return None, ResponseErrorResponse.create_from_dict(content)
+        if code == 500:
+            return None, ResponseErrorResponse.create_from_dict(content)
+
+        return self.handle_undocumented_response(
+            code=code, content_type=content_type, content=content
+        )
+
+    # endregion response methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls, fleet_id: str, namespace: str, **kwargs
+    ) -> FleetArtifactSamplingRulesGet:
+        instance = cls()
+        instance.fleet_id = fleet_id
+        instance.namespace = namespace
+        if x_flight_id := kwargs.get("x_flight_id", None):
+            instance.x_flight_id = x_flight_id
+        return instance
+
+    @classmethod
+    def create_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> FleetArtifactSamplingRulesGet:
+        instance = cls()
+        if "fleetID" in dict_ and dict_["fleetID"] is not None:
+            instance.fleet_id = str(dict_["fleetID"])
+        elif include_empty:
+            instance.fleet_id = ""
+        if "namespace" in dict_ and dict_["namespace"] is not None:
+            instance.namespace = str(dict_["namespace"])
+        elif include_empty:
+            instance.namespace = ""
+        return instance
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "fleetID": "fleet_id",
+            "namespace": "namespace",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "fleetID": True,
+            "namespace": True,
+        }
+
+    # endregion static methods
