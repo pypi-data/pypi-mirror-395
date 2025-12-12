ba6dde884101228b710cb3966649450bc2c964e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:09:22 +0000

    Add package delta-kernel-rust-sharing-wrapper/0.3.1/delta_kernel_rust_sharing_wrapper-0.3.1-cp38-abi3-manylinux_2_39_x86_64.whl

---
+++ packages/delta-kernel-rust-sharing-wrapper/delta_kernel_rust_sharing_wrapper-0.3.1-cp38-abi3-manylinux_2_39_x86_64.whl/delta_kernel_rust_sharing_wrapper/__init__.py
@@ -0,0 +1,5 @@
+from .delta_kernel_rust_sharing_wrapper import *
+
+__doc__ = delta_kernel_rust_sharing_wrapper.__doc__
+if hasattr(delta_kernel_rust_sharing_wrapper, "__all__"):
+    __all__ = delta_kernel_rust_sharing_wrapper.__all__
