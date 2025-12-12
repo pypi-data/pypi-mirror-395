d4a7fcf2f1c79119897e6722b75ad2778415e3ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 11:04:51 +0000

    Add package ruff/0.14.8/ruff-0.14.8.tar.gz

---
+++ packages/ruff/ruff-0.14.8.tar.gz/ruff-0.14.8/crates/ruff_linter/src/rules/ruff/rules/unused_noqa.rs
@@ -0,0 +1,110 @@
+use itertools::Itertools;
+
+use ruff_macros::{ViolationMetadata, derive_message_formats};
+
+use crate::AlwaysFixableViolation;
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) struct UnusedCodes {
+    pub disabled: Vec<String>,
+    pub duplicated: Vec<String>,
+    pub unknown: Vec<String>,
+    pub unmatched: Vec<String>,
+}
+
+/// ## What it does
+/// Checks for `noqa` directives that are no longer applicable.
+///
+/// ## Why is this bad?
+/// A `noqa` directive that no longer matches any diagnostic violations is
+/// likely included by mistake, and should be removed to avoid confusion.
+///
+/// ## Example
+/// ```python
+/// import foo  # noqa: F401
+///
+///
+/// def bar():
+///     foo.bar()
+/// ```
+///
+/// Use instead:
+/// ```python
+/// import foo
+///
+///
+/// def bar():
+///     foo.bar()
+/// ```
+///
+/// ## Options
+/// - `lint.external`
+///
+/// ## References
+/// - [Ruff error suppression](https://docs.astral.sh/ruff/linter/#error-suppression)
+#[derive(ViolationMetadata)]
+#[violation_metadata(stable_since = "v0.0.155")]
+pub(crate) struct UnusedNOQA {
+    pub codes: Option<UnusedCodes>,
+}
+
+impl AlwaysFixableViolation for UnusedNOQA {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        match &self.codes {
+            Some(codes) => {
+                let mut codes_by_reason = vec![];
+                if !codes.unmatched.is_empty() {
+                    codes_by_reason.push(format!(
+                        "unused: {}",
+                        codes
+                            .unmatched
+                            .iter()
+                            .map(|code| format!("`{code}`"))
+                            .join(", ")
+                    ));
+                }
+                if !codes.disabled.is_empty() {
+                    codes_by_reason.push(format!(
+                        "non-enabled: {}",
+                        codes
+                            .disabled
+                            .iter()
+                            .map(|code| format!("`{code}`"))
+                            .join(", ")
+                    ));
+                }
+                if !codes.duplicated.is_empty() {
+                    codes_by_reason.push(format!(
+                        "duplicated: {}",
+                        codes
+                            .duplicated
+                            .iter()
+                            .map(|code| format!("`{code}`"))
+                            .join(", ")
+                    ));
+                }
+                if !codes.unknown.is_empty() {
+                    codes_by_reason.push(format!(
+                        "unknown: {}",
+                        codes
+                            .unknown
+                            .iter()
+                            .map(|code| format!("`{code}`"))
+                            .join(", ")
+                    ));
+                }
+                if codes_by_reason.is_empty() {
+                    "Unused `noqa` directive".to_string()
+                } else {
+                    format!("Unused `noqa` directive ({})", codes_by_reason.join("; "))
+                }
+            }
+            None => "Unused blanket `noqa` directive".to_string(),
+        }
+    }
+
+    fn fix_title(&self) -> String {
+        "Remove unused `noqa` directive".to_string()
+    }
+}
---
+++ packages/ruff/ruff-0.14.8.tar.gz/ruff-0.14.8/crates/ruff_python_formatter/src/expression/expr_lambda.rs
@@ -0,0 +1,81 @@
+use ruff_formatter::write;
+use ruff_python_ast::AnyNodeRef;
+use ruff_python_ast::ExprLambda;
+use ruff_text_size::Ranged;
+
+use crate::comments::dangling_comments;
+use crate::expression::parentheses::{NeedsParentheses, OptionalParentheses};
+use crate::other::parameters::ParametersParentheses;
+use crate::prelude::*;
+
+#[derive(Default)]
+pub struct FormatExprLambda;
+
+impl FormatNodeRule<ExprLambda> for FormatExprLambda {
+    fn fmt_fields(&self, item: &ExprLambda, f: &mut PyFormatter) -> FormatResult<()> {
+        let ExprLambda {
+            range: _,
+            node_index: _,
+            parameters,
+            body,
+        } = item;
+
+        let comments = f.context().comments().clone();
+        let dangling = comments.dangling(item);
+
+        write!(f, [token("lambda")])?;
+
+        if let Some(parameters) = parameters {
+            // In this context, a dangling comment can either be a comment between the `lambda` the
+            // parameters, or a comment between the parameters and the body.
+            let (dangling_before_parameters, dangling_after_parameters) = dangling
+                .split_at(dangling.partition_point(|comment| comment.end() < parameters.start()));
+
+            if dangling_before_parameters.is_empty() {
+                write!(f, [space()])?;
+            } else {
+                write!(f, [dangling_comments(dangling_before_parameters)])?;
+            }
+
+            write!(
+                f,
+                [parameters
+                    .format()
+                    .with_options(ParametersParentheses::Never)]
+            )?;
+
+            write!(f, [token(":")])?;
+
+            if dangling_after_parameters.is_empty() {
+                write!(f, [space()])?;
+            } else {
+                write!(f, [dangling_comments(dangling_after_parameters)])?;
+            }
+        } else {
+            write!(f, [token(":")])?;
+
+            // In this context, a dangling comment is a comment between the `lambda` and the body.
+            if dangling.is_empty() {
+                write!(f, [space()])?;
+            } else {
+                write!(f, [dangling_comments(dangling)])?;
+            }
+        }
+
+        write!(f, [body.format()])
+    }
+}
+
+impl NeedsParentheses for ExprLambda {
+    fn needs_parentheses(
+        &self,
+        parent: AnyNodeRef,
+        _context: &PyFormatContext,
+    ) -> OptionalParentheses {
+        if parent.is_expr_await() {
+            OptionalParentheses::Always
+        } else {
+            OptionalParentheses::Multiline
+        }
+    }
+}
