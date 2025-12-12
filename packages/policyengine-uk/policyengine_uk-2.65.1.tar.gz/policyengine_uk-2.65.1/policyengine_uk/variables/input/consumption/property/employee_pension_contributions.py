7562c9547c56bbb1f6c8f20fa5b95489d9c3c335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 11:05:01 +0000

    Add package policyengine-uk/2.65.1/policyengine_uk-2.65.1.tar.gz

---
+++ packages/policyengine-uk/policyengine_uk-2.65.1.tar.gz/policyengine_uk-2.65.1/policyengine_uk/variables/input/consumption/property/employee_pension_contributions.py
@@ -0,0 +1,11 @@
+from policyengine_uk.model_api import *
+
+
+class employee_pension_contributions(Variable):
+    label = "employee pension contributions"
+    entity = Person
+    definition_period = YEAR
+    value_type = float
+    unit = GBP
+    uprating = "gov.economic_assumptions.indices.obr.average_earnings"
+    adds = ["salary_sacrifice_returned_to_income"]
