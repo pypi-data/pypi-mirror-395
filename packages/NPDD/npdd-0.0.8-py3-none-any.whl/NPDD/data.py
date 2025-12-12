dc5e0fa5b3d3000294985b3e86ebebc801d7c90c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 07:07:39 +0000

    Add package NPDD/0.0.8/npdd-0.0.8-py3-none-any.whl

---
+++ packages/NPDD/npdd-0.0.8-py3-none-any.whl/NPDD/data.py
@@ -0,0 +1,68 @@
+
+from importlib import resources
+import torch
+from torch_geometric.loader import DataLoader
+import pickle
+from .models import LigandPocketDataset, normalize_binding, device, NUM_POCKETS, NUM_TOP_POCKETS
+
+
+def load_dataset(data, batch_size=32, verbose=True, example=True):
+
+    if example:
+        with resources.files("NPDD.data_files").joinpath(f'ki_training_w_pocket.pkl').open("rb") as f:
+            data = pickle.load(f)
+
+    train = data['train']
+    val = data['val']
+    test = data['test']
+
+    if verbose:
+        print('Training data: ')
+        print('\t#Positive samples: ', len([i.binary_label for _,i in train.items() if i.binary_label==1]))
+        print('\t#Negative samples: ', len([i.binary_label for _,i in train.items() if i.binary_label==0]))
+
+        print('Validation data: ')
+        print('\t#Positive samples: ', len([i.binary_label for _,i in val.items() if i.binary_label==1]))
+        print('\t#Negative samples: ', len([i.binary_label for _,i in val.items() if i.binary_label==0]))
+
+        print('Test data: ')
+        print('\t#Positive samples: ', len([i.binary_label for _,i in test.items() if i.binary_label==1]))
+        print('\t#Negative samples: ', len([i.binary_label for _,i in test.items() if i.binary_label==0]))
+
+    for s in [train, val, test]:
+        for k, v in s.items():
+            v.farm = v.farm.sum(0).unsqueeze(0) # (1,768)
+            v.normed_label = normalize_binding(v.raw_label)
+            pocket_esm_list = v.pocket_emb
+            num_pockets = min(len(pocket_esm_list), NUM_POCKETS)
+            pocket_esm_list = torch.stack([pocket_esm_list[i] for i in range(num_pockets)]).unsqueeze(0)
+            try:
+                batch_size, current_pockets, feat_dim = pocket_esm_list.size()
+            except:
+                pocket_esm_list = pocket_esm_list.squeeze().unsqueeze(0)
+                batch_size, current_pockets, feat_dim = pocket_esm_list.size()
+
+            padding = torch.zeros(batch_size, NUM_POCKETS - current_pockets, feat_dim, device=pocket_esm_list.device)
+            pocket_esm_list = torch.cat([pocket_esm_list, padding], dim=1)
+            v.pocket_emb = pocket_esm_list
+
+    train = {k: v for k, v in train.items()}
+    val = {k: v for k, v in val.items()}
+    test = {k: v for k, v in test.items()}
+
+    del data
+
+    train = list(train.values())
+    val = list(val.values())
+    test = list(test.values())
+    train_dataset = LigandPocketDataset(train)
+    val_dataset = LigandPocketDataset(val)
+    test_dataset = LigandPocketDataset(test)
+    del train; del val; del test
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    del train_dataset; del val_dataset; del test_dataset
+
+    return {'train_loader': train_loader, 'val_loader': val_loader, 'test_loader': test_loader}
---
+++ packages/NPDD/npdd-0.0.8-py3-none-any.whl/NPDD/training_n_inference.py
@@ -0,0 +1,103 @@
+import torch
+from importlib import resources
+from .models import LigandPocketBindingPredictor, Pooling_3, bag_level_loss_1, bag_level_loss_2, bag_level_loss_3, device, NUM_POCKETS, NUM_TOP_POCKETS, spearmanr, r2_score
+from tqdm import tqdm
+import pickle
+from tqdm import tqdm
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from scipy.stats import spearmanr
+
+
+def Predict(test_loader, verbose=True, train_mode=False):
+    model = LigandPocketBindingPredictor()
+    if not train_mode:
+        weight_path = resources.files('NPDD.weights').joinpath('NPASS_pocket_mse_gat_best_1.pt')
+        model.load_state_dict(torch.load(weight_path, map_location=device))
+    model = model.to(device)
+    model.eval()
+    results = []
+
+    with torch.no_grad():
+        for data in tqdm(test_loader):
+            data = data.to(device)
+            all_props = model(data)  # [batch_size, num_instances]
+            if all_props.size(1) > NUM_TOP_POCKETS:
+                testues, _ = torch.topk(all_props, NUM_TOP_POCKETS, dim=1)
+            else:
+                testues, _ = all_props, None
+            pooled = Pooling_3(testues).detach().cpu()  # [batch_size]
+            label = data.normed_label  # [batch_size]
+            results.append([label.cpu(), pooled])
+            loss = bag_level_loss_2(testues, label).mean()
+
+        # Concatenate all labels and predictions for metrics
+        gt = torch.cat([i[0] for i in results]).cpu().numpy()
+        pr = torch.cat([i[1] for i in results]).cpu().numpy()
+
+    spearman = spearmanr(gt, pr).correlation
+    r2 = r2_score(gt, pr)
+    if verbose:
+        print(f'Spearman: {spearman:.4f}')
+        print(f'R2      : {r2:.4f}')
+
+    return {'Groundtruth': gt, 'Prediction': pr}
+
+
+def Training(dataset, epochs=10, lr=1e-4, device=device):
+
+    train_loader = dataset['train_loader']
+    val_loader = dataset['val_loader']
+    test_loader = dataset['test_loader']
+
+    model = LigandPocketBindingPredictor()
+    model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    best_metric = 0
+    best_epoch = 0
+
+    for epoch in range(1, epochs):
+        model.train()
+        train_loss = 0
+        for data in tqdm(train_loader, desc=f'epoch {epoch}/{epochs}, best spearman = {best_metric:.2f}'):
+            data = data.to(device)
+            all_props = model(data).to(device)
+            if all_props.size(1) > NUM_TOP_POCKETS:
+                values, _ = torch.topk(all_props, NUM_TOP_POCKETS, dim=1)
+            else:
+                values = all_props
+            label = data.normed_label
+            if epoch <= 5:
+                loss = bag_level_loss_1(values, label).mean()
+            elif epoch <= 10:
+                loss = bag_level_loss_2(values, label).mean()
+            else:
+                loss = bag_level_loss_3(values, label).mean()
+            train_loss += loss.item()
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # ========== Validation ==========
+        infer = Predict(val_loader, verbose=False, train_mode=True)
+        gt, pr = infer['Groundtruth'], infer['Prediction']
+
+        current_loss = train_loss / len(train_loader)
+
+        spearman = spearmanr(gt, pr).correlation
+        r2 = r2_score(gt, pr)
+        if spearman > best_metric:
+            best_metric = spearman
+            best_epoch = epoch
+            best_model = model.state_dict()
+        last_model = model.state_dict()
+        print(f'Training loss: {current_loss:.4f}, best val spearman: {best_metric:.4f}, val spearman: {spearman:.4f}, val r2: {r2:.4f}')
+
+    print('---------TEST---------')
+    Predict(test_loader)
+    return best_model, last_model
