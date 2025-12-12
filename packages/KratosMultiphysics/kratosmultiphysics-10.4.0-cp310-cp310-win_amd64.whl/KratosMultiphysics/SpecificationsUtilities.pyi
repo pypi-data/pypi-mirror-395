ae39f62327747fc7e0b685a7e7ea4807d6726c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:19 +0000

    Add package KratosMultiphysics/10.4.0/kratosmultiphysics-10.4.0-cp310-cp310-win_amd64.whl

---
+++ packages/KratosMultiphysics/kratosmultiphysics-10.4.0-cp310-cp310-win_amd64.whl/KratosMultiphysics/SpecificationsUtilities.pyi
@@ -0,0 +1,37 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics as Kratos
+
+def AddMissingDofs(arg0: Kratos.ModelPart) -> None:
+    """AddMissingDofs(arg0: Kratos.ModelPart) -> None"""
+def AddMissingDofsFromEntitiesList(arg0: Kratos.ModelPart, arg1) -> None:
+    """AddMissingDofsFromEntitiesList(arg0: Kratos.ModelPart, arg1: Kratos::Parameters) -> None"""
+def AddMissingVariables(arg0: Kratos.ModelPart) -> None:
+    """AddMissingVariables(arg0: Kratos.ModelPart) -> None"""
+def AddMissingVariablesFromEntitiesList(arg0: Kratos.ModelPart, arg1) -> None:
+    """AddMissingVariablesFromEntitiesList(arg0: Kratos.ModelPart, arg1: Kratos::Parameters) -> None"""
+def CheckCompatibleConstitutiveLaws(arg0: Kratos.ModelPart) -> bool:
+    """CheckCompatibleConstitutiveLaws(arg0: Kratos.ModelPart) -> bool"""
+def CheckGeometricalPolynomialDegree(arg0: Kratos.ModelPart) -> int:
+    """CheckGeometricalPolynomialDegree(arg0: Kratos.ModelPart) -> int"""
+def DetermineFlagsUsed(arg0: Kratos.ModelPart) -> None:
+    """DetermineFlagsUsed(arg0: Kratos.ModelPart) -> None"""
+def DetermineFramework(arg0: Kratos.ModelPart) -> str:
+    """DetermineFramework(arg0: Kratos.ModelPart) -> str"""
+def DetermineIfCompatibleGeometries(arg0: Kratos.ModelPart) -> bool:
+    """DetermineIfCompatibleGeometries(arg0: Kratos.ModelPart) -> bool"""
+def DetermineIfRequiresTimeIntegration(arg0: Kratos.ModelPart) -> bool:
+    """DetermineIfRequiresTimeIntegration(arg0: Kratos.ModelPart) -> bool"""
+def DeterminePositiveDefiniteLHS(arg0: Kratos.ModelPart) -> bool:
+    """DeterminePositiveDefiniteLHS(arg0: Kratos.ModelPart) -> bool"""
+def DetermineSymmetricLHS(arg0: Kratos.ModelPart) -> bool:
+    """DetermineSymmetricLHS(arg0: Kratos.ModelPart) -> bool"""
+def DetermineTimeIntegration(arg0: Kratos.ModelPart) -> list[str]:
+    """DetermineTimeIntegration(arg0: Kratos.ModelPart) -> list[str]"""
+def GetDocumention(*args, **kwargs):
+    """GetDocumention(arg0: Kratos.ModelPart) -> Kratos::Parameters"""
+def GetDofsListFromConditionsSpecifications(arg0: Kratos.ModelPart) -> list[str]:
+    """GetDofsListFromConditionsSpecifications(arg0: Kratos.ModelPart) -> list[str]"""
+def GetDofsListFromElementsSpecifications(arg0: Kratos.ModelPart) -> list[str]:
+    """GetDofsListFromElementsSpecifications(arg0: Kratos.ModelPart) -> list[str]"""
+def GetDofsListFromSpecifications(arg0: Kratos.ModelPart) -> list[str]:
+    """GetDofsListFromSpecifications(arg0: Kratos.ModelPart) -> list[str]"""
