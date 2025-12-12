a6722dfc72c9f2d714c0e42eb85668b56db76836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:42 +0000

    Add package odd-or-even-s/0.1.0/odd_or_even_s-0.1.0.tar.gz

---
+++ packages/odd-or-even-s/odd_or_even_s-0.1.0.tar.gz/odd_or_even_s-0.1.0/README.md
@@ -0,0 +1,15 @@
+# odd-or-even
+
+A simple Python package to check if a number is odd or even.
+
+## Installation
+
+pip install odd-or-even
+
+## Usage
+
+```python
+from odd_or_even import is_even, is_odd
+
+print(is_even(10))  # True
+print(is_odd(7))    # True
---
+++ packages/odd-or-even-s/odd_or_even_s-0.1.0.tar.gz/odd_or_even_s-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="odd-or-even",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A simple package to check odd or even numbers",
+    author="Your Name",
+    license="MIT",
+    include_package_data=True,
+)
