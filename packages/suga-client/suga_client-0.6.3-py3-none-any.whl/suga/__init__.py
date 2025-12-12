3a660e9a707c2e22a1b112aa6e0905bc247f943e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 07:06:47 +0000

    Add package suga-client/0.6.3/suga_client-0.6.3-py3-none-any.whl

---
+++ packages/suga-client/suga_client-0.6.3-py3-none-any.whl/suga/__init__.py
@@ -0,0 +1,5 @@
+"""Suga Python Client - A client library for interacting with Suga cloud resources."""
+
+from .buckets import Bucket, Mode, PresignUrlOptions
+
+__all__ = ["Bucket", "Mode", "PresignUrlOptions"]
