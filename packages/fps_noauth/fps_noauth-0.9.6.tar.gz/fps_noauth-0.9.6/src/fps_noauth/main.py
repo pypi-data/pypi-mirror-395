2c714af727ebf4d318635f979ff9adbc9722251b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:37 +0000

    Add package fps_noauth/0.9.6/fps_noauth-0.9.6.tar.gz

---
+++ packages/fps_noauth/fps_noauth-0.9.6.tar.gz/fps_noauth-0.9.6/src/fps_noauth/main.py
@@ -0,0 +1,10 @@
+from fps import Module
+from jupyverse_api.auth import Auth
+
+from .backends import _NoAuth
+
+
+class NoAuthModule(Module):
+    async def prepare(self) -> None:
+        no_auth = _NoAuth()
+        self.put(no_auth, Auth)
