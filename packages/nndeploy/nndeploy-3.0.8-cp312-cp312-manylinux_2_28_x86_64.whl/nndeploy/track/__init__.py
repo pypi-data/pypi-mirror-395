b54482dc76bece491b4221bb33a939719e28aa7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 12:05:52 +0000

    Add package nndeploy/3.0.8/nndeploy-3.0.8-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/nndeploy/nndeploy-3.0.8-cp312-cp312-manylinux_2_28_x86_64.whl/nndeploy/track/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from .result import MOTResult
+    from .fairmot import FairMotPreParam, FairMotPostParam, FairMotPreProcess, FairMotPostProcess, FairMotGraph
+    from .vis_mot import VisMOT
+except:
+    pass
