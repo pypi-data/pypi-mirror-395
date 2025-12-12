af72fc9916f59c47676665f4dac242021c6e45c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:05:55 +0000

    Add package imf-reader/1.4.1/imf_reader-1.4.1.tar.gz

---
+++ packages/imf-reader/imf_reader-1.4.1.tar.gz/imf_reader-1.4.1/src/imf_reader/sdr/__init__.py
@@ -0,0 +1,71 @@
+"""Special Drawing Rights (SDR) reader module.
+
+This module offers access to the IMF's Special Drawing Rights (SDR) data.
+The SDR is an international reserve asset created by the IMF in 1969.
+It is not a currency, but the holder of SDRs can exchange them for usable currencies in times of need.
+
+Read more about SDRs at: https://www.imf.org/en/About/Factsheets/Sheets/2023/special-drawing-rights-sdr
+
+
+Usage:
+
+Import the module
+
+```python
+from imf_reader import sdr
+```
+
+Read allocations and holdings data
+
+```python
+sdr.fetch_allocations_holdings()
+```
+SDRs holdings and allocations are published at a monthly frequency. The function fetches the latest data available by
+default. Check the latest available date
+
+```python
+sdr.fetch_latest_allocations_holdings_date()
+```
+
+To retrieve SDR holdings and allocations for a specific month and year, eg April 2021, pass the year and month as a tuple
+
+```python
+sdr.fetch_allocations_holdings((2021, 4))
+```
+
+Read interest rates
+
+```python
+sdr.fetch_interest_rates()
+```
+
+Read exchange rates
+
+```python
+sdr.fetch_exchange_rates()
+```
+By default, the exchange rate is in USDs per 1 SDR. To get the exchange rate in SDRs per 1 USD, pass the unit basis as "USD"
+
+```python
+sdr.fetch_exchange_rates("USD")
+```
+
+Clear cached data
+
+```python
+sdr.clear_cache()
+```
+
+"""
+
+from imf_reader.sdr.read_interest_rate import (
+    fetch_interest_rates as fetch_interest_rates,
+)
+from imf_reader.sdr.read_exchange_rate import (
+    fetch_exchange_rates as fetch_exchange_rates,
+)
+from imf_reader.sdr.read_announcements import (
+    fetch_allocations_holdings as fetch_allocations_holdings,
+    fetch_latest_allocations_holdings_date as fetch_latest_allocations_holdings_date,
+)
+from imf_reader.sdr.clear_cache import clear_cache as clear_cache
---
+++ packages/imf-reader/imf_reader-1.4.1.tar.gz/imf_reader-1.4.1/src/imf_reader/weo/scraper.py
@@ -0,0 +1,100 @@
+"""Functions to scrape the IMF WEO website"""
+
+from bs4 import BeautifulSoup
+import io
+from zipfile import ZipFile, BadZipFile
+
+from imf_reader.config import NoDataError, logger
+from imf_reader.utils import make_request
+
+BASE_URL = "https://www.imf.org/"
+
+
+def get_soup(month: str, year: str | int) -> BeautifulSoup:
+    """Get the BeautifulSoup object of the IMF WEO website.
+
+    Args:
+        month: The month of the data to download. Can be April or October.
+        year: The year of the data to download.
+
+    Returns:
+        BeautifulSoup object of the IMF WEO website.
+    """
+
+    url = f"{BASE_URL}/en/Publications/WEO/weo-database/{year}/{month}/download-entire-database"
+    response = make_request(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    return soup
+
+
+class SDMXScraper:
+    """Class to scrape the IMF WEO website for SDMX files.
+    To use this class, call the scrape method with the month and year of the data to download.
+    """
+
+    @staticmethod
+    def get_sdmx_url(soup: BeautifulSoup) -> str:
+        """Get the url to download the WEO data in SDMX format.
+
+        Args:
+            soup: BeautifulSoup object of the IMF WEO website.
+
+        Returns:
+            The url to download the SDMX data.
+        """
+
+        try:
+            href = soup.find("a", string="SDMX Data").get("href")
+        except AttributeError:
+            raise NoDataError("SDMX data not found")
+
+        if href is None:
+            raise NoDataError("SDMX data not found")
+
+        logger.debug("SDMX URL found")
+        return f"{href}"
+
+    @staticmethod
+    def get_sdmx_folder(sdmx_url: str) -> ZipFile:
+        """download SDMX data files as a zip file object
+
+        Args:
+            sdmx_url: The url to download the SDMX data files.
+
+        Returns:
+            The zip file object containing the SDMX data files.
+        """
+
+        response = make_request(sdmx_url)
+        folder = ZipFile(io.BytesIO(response.content))
+
+        # Validate the zip file
+        if folder.testzip():
+            raise BadZipFile("Corrupt zip file")
+
+        logger.debug("Zip folder downloaded successfully")
+        return folder
+
+    @staticmethod
+    def scrape(month: str, year: str | int) -> ZipFile:
+        """Pipeline to scrape SDMX files
+
+        Args:
+            month: The month of the data to download. Can be April or October.
+            year: The year of the data to download.
+
+        Returns:
+            The zip file object containing the SDMX data files.
+        """
+
+        # Scrape the IMF WEO website
+        soup = get_soup(month, year)
+
+        # find the url to download the SDMX data
+        sdmx_url = SDMXScraper.get_sdmx_url(soup)
+
+        # download the SDMX data files
+        sdmx_folder = SDMXScraper.get_sdmx_folder(sdmx_url)
+
+        return sdmx_folder
