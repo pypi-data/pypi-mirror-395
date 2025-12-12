b69f2e8baff451229718ca1258f7d780096ce56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:23 +0000

    Add package KratosCSharpWrapperApplication/10.4.0/kratoscsharpwrapperapplication-10.4.0-cp38-cp38-manylinux_2_28_x86_64.whl

---
+++ packages/KratosCSharpWrapperApplication/kratoscsharpwrapperapplication-10.4.0-cp38-cp38-manylinux_2_28_x86_64.whl/KratosMultiphysics/CSharpWrapperApplication/__init__.pyi
@@ -0,0 +1,6 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics as Kratos
+
+class KratosCSharpWrapperApplication(Kratos.KratosApplication):
+    def __init__(self) -> None:
+        """__init__(self: KratosCSharpWrapperApplication.KratosCSharpWrapperApplication) -> None"""
