4ff6367ee8df8c61c60066c6b0cb9f894122d0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 09:06:25 +0000

    Add package quadis-chat-input-multimodal/1.0.0/quadis_chat_input_multimodal-1.0.0.tar.gz

---
+++ packages/quadis-chat-input-multimodal/quadis_chat_input_multimodal-1.0.0.tar.gz/quadis_chat_input_multimodal-1.0.0/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quadis-chat-input-multimodal",
+    version="1.0.0",
+    author="Jon Goncalves",
+    author_email="jon@quadis.com",
+    description="Streamlit multimodal chat input component with text, image, and voice support",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/quadis/st-chat-input-multimodal",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "streamlit >= 1.2",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=7.0",
+            "pytest-cov>=4.0",
+            "black>=22.0",
+            "flake8>=5.0",
+            "mypy>=1.0",
+        ]
+    },
+    keywords="streamlit, component, multimodal, chat, input, image, voice, audio, screenshot, quadis",
+)
