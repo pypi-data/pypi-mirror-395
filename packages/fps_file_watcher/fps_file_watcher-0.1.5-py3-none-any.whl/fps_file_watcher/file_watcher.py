be73a7d977fb3f3e2b0d3cffed7a9f66fc92594b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:35 +0000

    Add package fps_file_watcher/0.1.5/fps_file_watcher-0.1.5-py3-none-any.whl

---
+++ packages/fps_file_watcher/fps_file_watcher-0.1.5-py3-none-any.whl/fps_file_watcher/file_watcher.py
@@ -0,0 +1,22 @@
+import logging
+from collections.abc import AsyncGenerator
+from pathlib import Path
+
+import structlog
+from anyio import Event
+from jupyverse_api.file_watcher import FileChange, FileWatcher
+from watchfiles import awatch
+
+logger = structlog.get_logger()
+watchfiles_logger = logging.getLogger("watchfiles")
+watchfiles_logger.setLevel(logging.WARNING)
+
+
+class _FileWatcher(FileWatcher):
+    async def watch(  # type: ignore[override]
+        self,
+        path: Path | str,
+        stop_event: Event | None = None,
+    ) -> AsyncGenerator[set[FileChange], None]:
+        async for changes in awatch(path, stop_event=stop_event):
+            yield changes  # type: ignore[misc]
