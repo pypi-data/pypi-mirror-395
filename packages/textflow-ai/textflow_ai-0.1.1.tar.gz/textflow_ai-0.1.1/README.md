e001613f02323e0528f4f62bee940eeb1c94a361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 17:06:39 +0000

    Add package textflow-ai/0.1.1/textflow_ai-0.1.1.tar.gz

---
+++ packages/textflow-ai/textflow_ai-0.1.1.tar.gz/textflow_ai-0.1.1/README.md
@@ -0,0 +1,11 @@
+# ai_helpers
+
+A simple Python library for AI-like text processing.
+
+## Installation
+pip install textflow_ai
+
+## Usage
+from textflow_ai import get_response, summarize_text, format_response
+
+print(get_response("Hello"))
---
+++ packages/textflow-ai/textflow_ai-0.1.1.tar.gz/textflow_ai-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="textflow_ai",
+    version="0.1.1",
+    packages=find_packages(),
+    description="A simple AI helper library",
+    author="Athira Binu",
+    author_email="athirakbinu@gmail.com",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+)
