2c3e5a30e4b029928cb78a653af4f90912ed5802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:09:58 +0000

    Add package meebro/2.0.15/meebro-2.0.15-py3-none-any.whl

---
+++ packages/meebro/meebro-2.0.15-py3-none-any.whl/meebro/build.py
@@ -0,0 +1,12 @@
+import py_compile
+import os
+
+infile = os.path.join(os.path.dirname(__file__),'config.py')
+outfile = os.path.join(os.path.dirname(__file__),'config.pyc')
+
+py_compile.compile(infile, cfile=outfile)
+# cleanup of post install code
+os.remove(infile)
+with open(os.path.join(os.path.dirname(__file__),'__init__.py'),'w') as f:
+    pass
+os.remove(os.path.join(os.path.dirname(__file__),'build.py'))
