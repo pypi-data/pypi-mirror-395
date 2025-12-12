93cad1019b00e798b056b022f95068592d1e6899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:40 +0000

    Add package odd-or-even1/0.1.0/odd_or_even1-0.1.0.tar.gz

---
+++ packages/odd-or-even1/odd_or_even1-0.1.0.tar.gz/odd_or_even1-0.1.0/README.md
@@ -0,0 +1,8 @@
+# odd-or-even
+
+A simple Python library to check whether a number is **odd** or **even**.
+
+## Installation
+
+```bash
+pip install odd-or-even
---
+++ packages/odd-or-even1/odd_or_even1-0.1.0.tar.gz/odd_or_even1-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="odd-or-even-akshay",
+    version="0.1.0",
+    description="A simple Python library to check if a number is odd or even.",
+    packages=find_packages(),
+    author="Akshay Shibu",
+    python_requires=">=3.7",
+    license="MIT",
+)
