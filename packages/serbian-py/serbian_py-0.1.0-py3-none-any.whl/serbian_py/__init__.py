db70f5f7fa5666826899e0940b83ae5b16ea7f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 23:05:01 +0000

    Add package serbian-py/0.1.0/serbian_py-0.1.0-py3-none-any.whl

---
+++ packages/serbian-py/serbian_py-0.1.0-py3-none-any.whl/serbian_py/__init__.py
@@ -0,0 +1,24 @@
+"""Serbian language adaptations and utility functions for Python programming."""
+
+from .main_commands import (
+    ispiši,
+    input_tekst,
+    input_broj,
+    unos_lista_sa_razmacima,
+    unos_tuple_sa_razmacima,
+    unos_set_sa_razmacima,
+    unos_dict_sa_razmacima,
+)
+
+__version__ = "0.1.0"
+__author__ = "Pavle012"
+
+__all__ = [
+    "ispiši",
+    "input_tekst",
+    "input_broj",
+    "unos_lista_sa_razmacima",
+    "unos_tuple_sa_razmacima",
+    "unos_set_sa_razmacima",
+    "unos_dict_sa_razmacima",
+]
