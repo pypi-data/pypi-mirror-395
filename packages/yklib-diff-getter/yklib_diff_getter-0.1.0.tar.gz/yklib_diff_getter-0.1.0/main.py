f75532a3a28c743b5260d15013b0dfc591647ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 07:07:19 +0000

    Add package yklib-diff-getter/0.1.0/yklib_diff_getter-0.1.0.tar.gz

---
+++ packages/yklib-diff-getter/yklib_diff_getter-0.1.0.tar.gz/yklib_diff_getter-0.1.0/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from uv-project-name!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/yklib-diff-getter/yklib_diff_getter-0.1.0.tar.gz/yklib_diff_getter-0.1.0/src/yklib_diff_getter/__main__.py
@@ -0,0 +1,4 @@
+from yklib_diff_getter import main
+
+if __name__ == "__main__":
+    main()
