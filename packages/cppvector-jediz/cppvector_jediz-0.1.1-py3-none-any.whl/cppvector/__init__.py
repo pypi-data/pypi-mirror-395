2ce243375937f395c70916d2e04e6e9d4ac87889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:08:40 +0000

    Add package cppvector-jediz/0.1.1/cppvector_jediz-0.1.1-py3-none-any.whl

---
+++ packages/cppvector-jediz/cppvector_jediz-0.1.1-py3-none-any.whl/cppvector/__init__.py
@@ -0,0 +1,8 @@
+"""cppvector - A C++ style vector implementation for Python
+"""
+
+__version__ = "0.1.0"
+
+from .vector import Vector, VectorBool, NumericVector
+
+__all__ = ["Vector", "VectorBool", "NumericVector"]
