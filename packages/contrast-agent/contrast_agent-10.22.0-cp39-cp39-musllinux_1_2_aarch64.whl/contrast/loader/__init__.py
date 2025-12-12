f4ccafccc7edc491257e045726d15debb97e5f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 20:06:37 +0000

    Add package contrast-agent/10.22.0/contrast_agent-10.22.0-cp39-cp39-musllinux_1_2_aarch64.whl

---
+++ packages/contrast-agent/contrast_agent-10.22.0-cp39-cp39-musllinux_1_2_aarch64.whl/contrast/loader/__init__.py
@@ -0,0 +1,4 @@
+# Copyright © 2025 Contrast Security, Inc.
+# See https://www.contrastsecurity.com/enduser-terms-0317a for more details.
+
+SITECUSTOMIZE_LOADED = False  # True when sitecustomize is imported
