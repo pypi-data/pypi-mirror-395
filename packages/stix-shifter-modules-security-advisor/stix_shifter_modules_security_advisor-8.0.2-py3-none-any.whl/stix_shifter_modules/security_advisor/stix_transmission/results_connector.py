ddf24b263752b83f0b2dab9a5704147aafb8d73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:22 +0000

    Add package stix-shifter-modules-security-advisor/8.0.2/stix_shifter_modules_security_advisor-8.0.2-py3-none-any.whl

---
+++ packages/stix-shifter-modules-security-advisor/stix_shifter_modules_security_advisor-8.0.2-py3-none-any.whl/stix_shifter_modules/security_advisor/stix_transmission/results_connector.py
@@ -0,0 +1,38 @@
+from stix_shifter_utils.modules.base.stix_transmission.base_json_results_connector import BaseJsonResultsConnector
+from .utils.stix_pattern_processor import StixPatternProcessor
+from stix_shifter_utils.utils.error_response import ErrorResponder
+from .auth import Auth
+
+
+class ResultsConnector(BaseJsonResultsConnector):
+    def __init__(self, host, auth ):
+        self.connector = __name__.split('.')[1]
+        self.auth = auth
+        api_key = auth.get("apiKey")
+        self.auth_token = Auth(api_key)
+        self.host = self.auth_token.find_location(self.auth["accountID"], host)
+        self.StixPatternProcessor = StixPatternProcessor()
+
+    async def create_results_connection(self, searchID , offset , length):
+
+        params = {}
+        return_obj = {}
+        params["accountID"] =  self.auth.get("accountID")
+        params["host"] = self.host
+
+        try:
+            params["accessToken"] = self.auth_token.obtainAccessToken()
+            
+        except Exception as e:
+            ErrorResponder.fill_error(return_obj, {'code':"Authorizaion Failed"}, message= str(e), connector=self.connector)
+            return return_obj
+
+        try:
+            data = self.StixPatternProcessor.process(searchID, params)
+            return_obj["success"] = True
+            return_obj["data"] =  data
+            return return_obj
+
+        except Exception as e:
+            ErrorResponder.fill_error(return_obj, {'code':"query_failed"}, message= str(e), connector=self.connector)
+            return return_obj
