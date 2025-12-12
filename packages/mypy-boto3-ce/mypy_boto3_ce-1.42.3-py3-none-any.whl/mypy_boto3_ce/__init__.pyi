1139aee66257eeb82b0c3ff77c3279f8f9012abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 13:16:13 +0000

    Add package mypy-boto3-ce/1.42.3/mypy_boto3_ce-1.42.3-py3-none-any.whl

---
+++ packages/mypy-boto3-ce/mypy_boto3_ce-1.42.3-py3-none-any.whl/mypy_boto3_ce/__init__.pyi
@@ -0,0 +1,52 @@
+"""
+Main interface for ce service.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ce/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_ce import (
+        Client,
+        CostExplorerClient,
+        GetAnomaliesPaginator,
+        GetAnomalyMonitorsPaginator,
+        GetAnomalySubscriptionsPaginator,
+        GetCostAndUsageComparisonsPaginator,
+        GetCostComparisonDriversPaginator,
+    )
+
+    session = Session()
+    client: CostExplorerClient = session.client("ce")
+
+    get_anomalies_paginator: GetAnomaliesPaginator = client.get_paginator("get_anomalies")
+    get_anomaly_monitors_paginator: GetAnomalyMonitorsPaginator = client.get_paginator("get_anomaly_monitors")
+    get_anomaly_subscriptions_paginator: GetAnomalySubscriptionsPaginator = client.get_paginator("get_anomaly_subscriptions")
+    get_cost_and_usage_comparisons_paginator: GetCostAndUsageComparisonsPaginator = client.get_paginator("get_cost_and_usage_comparisons")
+    get_cost_comparison_drivers_paginator: GetCostComparisonDriversPaginator = client.get_paginator("get_cost_comparison_drivers")
+    ```
+"""
+
+from .client import CostExplorerClient
+from .paginator import (
+    GetAnomaliesPaginator,
+    GetAnomalyMonitorsPaginator,
+    GetAnomalySubscriptionsPaginator,
+    GetCostAndUsageComparisonsPaginator,
+    GetCostComparisonDriversPaginator,
+)
+
+Client = CostExplorerClient
+
+__all__ = (
+    "Client",
+    "CostExplorerClient",
+    "GetAnomaliesPaginator",
+    "GetAnomalyMonitorsPaginator",
+    "GetAnomalySubscriptionsPaginator",
+    "GetCostAndUsageComparisonsPaginator",
+    "GetCostComparisonDriversPaginator",
+)
