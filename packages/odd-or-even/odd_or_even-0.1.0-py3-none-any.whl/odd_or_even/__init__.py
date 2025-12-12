60264478af2af17ed7735bedc4c016cc6db43218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:28 +0000

    Add package odd-or-even/0.1.0/odd_or_even-0.1.0-py3-none-any.whl

---
+++ packages/odd-or-even/odd_or_even-0.1.0-py3-none-any.whl/odd_or_even/__init__.py
@@ -0,0 +1,9 @@
+# odd_or_even/__init__.py
+
+def is_odd(number):
+    """Return True if the number is odd."""
+    return number % 2 != 0
+
+def is_even(number):
+    """Return True if the number is even."""
+    return number % 2 == 0
