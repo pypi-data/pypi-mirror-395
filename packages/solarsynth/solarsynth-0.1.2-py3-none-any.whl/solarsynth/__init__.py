947a271dd73f03c3c074db258e2c4876af7b3f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 22:06:44 +0000

    Add package solarsynth/0.1.2/solarsynth-0.1.2-py3-none-any.whl

---
+++ packages/solarsynth/solarsynth-0.1.2-py3-none-any.whl/solarsynth/__init__.py
@@ -0,0 +1,11 @@
+from .synth2 import (
+    generate_solar_actual,
+    generate_solar_forecast,
+    generate_load_demand
+)
+
+__all__ = [
+    "generate_solar_actual",
+    "generate_solar_forecast",
+    "generate_load_demand",
+]
