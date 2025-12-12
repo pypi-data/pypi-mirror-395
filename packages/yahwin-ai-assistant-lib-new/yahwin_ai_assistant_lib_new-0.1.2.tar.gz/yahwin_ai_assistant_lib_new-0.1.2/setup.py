c8066282e62eb52a27703be8ebc51cae9ffa0333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 17:05:19 +0000

    Add package yahwin-ai-assistant-lib-new/0.1.2/yahwin_ai_assistant_lib_new-0.1.2.tar.gz

---
+++ packages/yahwin-ai-assistant-lib-new/yahwin_ai_assistant_lib_new-0.1.2.tar.gz/yahwin_ai_assistant_lib_new-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yahwin_ai_assistant_lib_new",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "google-generativeai",
+        "markdown",  # For formatting response
+    ],
+    author="Yahwin Lukose",
+    description="A helper library for AI-powered web assistant",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
