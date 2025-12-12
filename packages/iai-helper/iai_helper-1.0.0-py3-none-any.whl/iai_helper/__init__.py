1dcda444056a560a9c536371976416a8fdda3a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 17:06:06 +0000

    Add package iai-helper/1.0.0/iai_helper-1.0.0-py3-none-any.whl

---
+++ packages/iai-helper/iai_helper-1.0.0-py3-none-any.whl/iai_helper/__init__.py
@@ -0,0 +1,7 @@
+from .core import IAIHelper
+from .utils import clean_response, summarize_text
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+
+__all__ = ["IAIHelper", "clean_response", "summarize_text"]
