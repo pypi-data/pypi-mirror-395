d741741cd3c56d4cbfa48d46ca56b3e9e553ccca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 23:05:07 +0000

    Add package windowsxpl/0.3.0/windowsxpl-0.3.0.tar.gz

---
+++ packages/windowsxpl/windowsxpl-0.3.0.tar.gz/windowsxpl-0.3.0/src/root/__init__.py
@@ -0,0 +1,19 @@
+"""
+项目示例和工具
+"""
+__all__ = ['model_automl_with_auto_dt']
+
+"""
+requirements.txt
+
+pandas==2.3.1
+numpy==2.2.6
+scikit-learn>=1.5.0
+xgboost>=3.1.2
+matplotlib==3.10.3
+seaborn>=0.13.0
+optuna>=3.6.1
+pyyaml>=6.0
+optbinning==0.21.0
+
+"""
