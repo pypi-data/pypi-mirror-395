7c382c50ef563ec6084a4742053e1c020e47d90b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:20 +0000

    Add package ecosystem-notebooks/0.1.48/ecosystem_notebooks-0.1.48.tar.gz

---
+++ packages/ecosystem-notebooks/ecosystem_notebooks-0.1.48.tar.gz/ecosystem-notebooks-0.1.48/prediction/endpoints/ecosystem_main.py
@@ -0,0 +1,22 @@
+# Ecosystem Main
+# Ecosystem Main Admin
+CREATE = {
+	"type": "post",
+	"endpoint": "/create",
+	"call_message": "{type} {endpoint}",
+	"error_message": "{type} {endpoint} {response_code}"
+}
+
+PROFILES = {
+	"type": "get",
+	"endpoint": "/profiles",
+	"call_message": "{type} {endpoint}",
+	"error_message": "{type} {endpoint} {response_code}"
+}
+
+CONFIG = {
+	"type": "get",
+	"endpoint": "/config",
+	"call_message": "{type} {endpoint}",
+	"error_message": "{type} {endpoint} {response_code}"
+}
