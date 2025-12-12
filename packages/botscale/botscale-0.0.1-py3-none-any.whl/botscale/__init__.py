1df01a49809e473aebfd9c3f9a26cbe020edd5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:44 +0000

    Add package botscale/0.0.1/botscale-0.0.1-py3-none-any.whl

---
+++ packages/botscale/botscale-0.0.1-py3-none-any.whl/botscale/__init__.py
@@ -0,0 +1,7 @@
+"""
+BotScale - Universal bot framework (name reservation package).
+
+This is a minimal package published to reserve the 'botscale' name on PyPI.
+The actual package will be published later with full functionality.
+"""
+__version__ = "0.0.1"
