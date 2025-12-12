54f14ba7d0eb859cabac53047160c6547ec491df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:26 +0000

    Add package tf-shell/0.5.0/tf_shell-0.5.0-cp311-cp311-manylinux_2_39_x86_64.whl

---
+++ packages/tf-shell/tf_shell-0.5.0-cp311-cp311-manylinux_2_39_x86_64.whl/tf_shell_ml/__init__.py
@@ -0,0 +1,32 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import absolute_import
+
+from tf_shell_ml.activation import (
+    relu,
+    relu_deriv,
+    sigmoid,
+    sigmoid_deriv,
+    serialize_activation,
+    deserialize_activation,
+)
+from tf_shell_ml.dense import ShellDense
+from tf_shell_ml.dropout import ShellDropout
+from tf_shell_ml.embedding import ShellEmbedding
+from tf_shell_ml.conv2d import Conv2D
+from tf_shell_ml.max_pool2d import MaxPool2D
+from tf_shell_ml.flatten import Flatten
+from tf_shell_ml.globalaveragepool1d import GlobalAveragePooling1D
+from tf_shell_ml.dpsgd_model import DpSgdModel
+from tf_shell_ml.postscale_model import PostScaleModel
