9c04af2e691d110ac85a2b5db7addf2d8f8bd437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 17:05:24 +0000

    Add package mpvytdlp/0.0.1/mpvytdlp-0.0.1.tar.gz

---
+++ packages/mpvytdlp/mpvytdlp-0.0.1.tar.gz/mpvytdlp-0.0.1/README.md
@@ -0,0 +1,12 @@
+# mpvytdlp
+lib s.t. python-mpv + yt-dlp. PR wellcome
+
+## What dose it do?
+
+it work as `pip install python-mpv yt-dlp`
+
+because it have depenancy of that.
+
+## cannot import this
+
+normal. it have no files. it just package setup file
---
+++ packages/mpvytdlp/mpvytdlp-0.0.1.tar.gz/mpvytdlp-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='mpvytdlp',
+    version='0.0.1',
+    description='exaclly which python-mpv + yp-dlp',
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/mpvytdlp',
+    install_requires=[
+        'python-mpv>=1.0',
+        'yt-dlp>=2025.11.29',
+    ],
+    keywords=['mpv', 'python-mpv', 'yp-dlp', 'youtube', 'linux', 'mpvytdlp', 'pythonmpv', 'pythonmpvytdlp', 'python-mpvytdlp', 'pythonmpvyt-dlp', 'pythonmpv-ytdlp', 'python-mpvyt-dlp', 'python-mpv-ytdlp', 'pythonmpv-yt-dlp', 'python-mpv-yt-dlp', 'mpvyt-dlp', 'mpv-ytdlp', 'mpv-yt-dlp', 'mpv-yp', 'python-mpv-yt', 'mpvyt', 'ytdlp', 'pythonmpvyt', 'python-mpvyt', 'pythonmpv-yt', 'simple', 'it', 'just', 'setup', 'file', 'huh', '#wellcome to PR', 'yt', 'dlp']
+)
