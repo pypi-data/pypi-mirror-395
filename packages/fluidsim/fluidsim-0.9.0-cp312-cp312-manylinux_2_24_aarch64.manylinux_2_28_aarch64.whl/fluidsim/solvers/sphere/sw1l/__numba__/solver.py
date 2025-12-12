9162e1dc9ac66da8cf76965cb018d34a2b9d205f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 07:06:42 +0000

    Add package fluidsim/0.9.0/fluidsim-0.9.0-cp312-cp312-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/fluidsim/fluidsim-0.9.0-cp312-cp312-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/fluidsim/solvers/sphere/sw1l/__numba__/solver.py
@@ -0,0 +1,15 @@
+
+from numba import njit
+
+
+@njit(cache=True, fastmath=True)
+def compute_Frot(rot, ux, uy, f_radial):
+    """Compute cross-product of absolute potential vorticity with velocity."""
+    rot_abs = rot + f_radial
+    F1x = rot_abs * uy
+    F1y = -rot_abs * ux
+    return (F1x, F1y)
+
+
+def __transonic__():
+    return '0.8.0'
