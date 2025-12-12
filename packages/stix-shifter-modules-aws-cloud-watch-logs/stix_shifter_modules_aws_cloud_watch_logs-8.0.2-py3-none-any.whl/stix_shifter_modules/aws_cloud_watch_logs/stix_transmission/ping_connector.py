c6445302efa989e7cd637cebd1d658afb91facef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:50 +0000

    Add package stix-shifter-modules-aws-cloud-watch-logs/8.0.2/stix_shifter_modules_aws_cloud_watch_logs-8.0.2-py3-none-any.whl

---
+++ packages/stix-shifter-modules-aws-cloud-watch-logs/stix_shifter_modules_aws_cloud_watch_logs-8.0.2-py3-none-any.whl/stix_shifter_modules/aws_cloud_watch_logs/stix_transmission/ping_connector.py
@@ -0,0 +1,26 @@
+from stix_shifter_utils.modules.base.stix_transmission.base_ping_connector import BasePingConnector
+from stix_shifter_utils.utils.error_response import ErrorResponder
+
+
+class PingConnector(BasePingConnector):
+
+    def __init__(self, client):
+        self.client = client
+        self.connector = __name__.split('.')[1]
+
+    async def ping_connection(self):
+        """
+        Ping the endpoint
+        :return: dict
+        """
+        return_obj = dict()
+        response_dict = dict()
+        try:
+            await self.client.makeRequest('logs', 'describe_log_groups', **{})
+            return_obj['success'] = True
+        except Exception as ex:
+            response_dict['__type'] = ex.__class__.__name__
+            response_dict['message'] = ex
+            ErrorResponder.fill_error(return_obj, response_dict, ['message'], connector=self.connector)
+
+        return return_obj
