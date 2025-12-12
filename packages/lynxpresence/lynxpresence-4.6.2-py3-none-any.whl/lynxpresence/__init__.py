59f78fdc9cf7ad7d2221a2d018a07f41d84dca6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:05:52 +0000

    Add package lynxpresence/4.6.2/lynxpresence-4.6.2-py3-none-any.whl

---
+++ packages/lynxpresence/lynxpresence-4.6.2-py3-none-any.whl/lynxpresence/__init__.py
@@ -0,0 +1,15 @@
+"""
+Python RPC Client for Discord
+"""
+
+from .baseclient import BaseClient
+from .client import AioClient, Client
+from .exceptions import *
+from .presence import AioPresence, Presence
+from .types import ActivityType, StatusDisplayType
+
+__title__ = "lynxpresence"
+__author__ = "C0rn3j"
+__copyright__ = "Copyright 2018 - 2025"
+__license__ = "MIT"
+__version__ = "4.6.2"
