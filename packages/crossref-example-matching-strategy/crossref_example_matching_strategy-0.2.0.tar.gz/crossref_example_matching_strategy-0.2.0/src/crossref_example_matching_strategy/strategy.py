b32663d084b6dd46d1e41fa26bf7101c137b1d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:04 +0000

    Add package crossref-example-matching-strategy/0.2.0/crossref_example_matching_strategy-0.2.0.tar.gz

---
+++ packages/crossref-example-matching-strategy/crossref_example_matching_strategy-0.2.0.tar.gz/crossref_example_matching_strategy-0.2.0/src/crossref_example_matching_strategy/strategy.py
@@ -0,0 +1,31 @@
+import re
+from crossref_matcher import Strategy, MatchTask
+
+
+def is_doi(input_str):
+    doi_pattern = re.compile(r"^10.\d{4,9}/[-._;()/:A-Z0-9]+$", re.IGNORECASE)
+    return bool(doi_pattern.match(input_str))
+
+
+class ExampleMatchingStrategy(Strategy):
+    strategy = "example-matching-strategy"
+    task = MatchTask.REFERENCE
+    description = "Example strategy. Takes a DOI and returns the same DOI. If the input is not a DOI, returns no match."
+    default = False
+
+    def __init__(self):
+        pass
+
+    def match(self, input_data):
+        input_data = (
+            input_data.replace("https://doi.org/", "").replace("doi:", "").strip()
+        )
+        if is_doi(input_data):
+            return [
+                {
+                    "id": f"https://doi.org/{input_data}",
+                    "confidence": 1.0,
+                    "strategies": [self.strategy],
+                }
+            ]
+        return []
