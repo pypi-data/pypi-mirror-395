eb4c8b1027d5235af6f66ad1bf223beaf2c67a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 21:07:50 +0000

    Add package pyftpkit/0.1.2/pyftpkit-0.1.2-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pyftpkit/pyftpkit-0.1.2-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/pyftpkit/connection_parameters.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2025 (c) Vladislav Punko <iam.vlad.punko@gmail.com>
+
+import argparse
+import collections
+import typing
+
+import pydantic
+import pydantic_settings
+
+__all__ = ["Credentials", "ConnectionParameters"]
+
+
+class Credentials(pydantic.BaseModel):
+    """Represents authentication credentials for an FTP connection."""
+
+    username: str
+    password: pydantic.SecretStr
+
+
+class ConnectionParameters(pydantic_settings.BaseSettings):
+    """Connection parameters for establishing and managing FTP connections."""
+
+    model_config = pydantic_settings.SettingsConfigDict(
+        env_file_encoding="utf-8",
+        env_file=".env",
+        env_nested_delimiter="__",
+        env_prefix="PYFTPKIT_",
+        extra="ignore",
+    )
+
+    host: str
+    port: pydantic.NonNegativeInt = pydantic.Field(0, gt=0)  # no ports
+    credentials: Credentials
+    timeout: pydantic.NonNegativeInt = pydantic.Field(
+        30, description="connection timeout in seconds"
+    )
+    max_connections: pydantic.NonNegativeInt = pydantic.Field(
+        10, gt=0, description="maximum number of simultaneous connections"
+    )
+    max_workers: pydantic.NonNegativeInt = pydantic.Field(
+        30, gt=0, description="maximum number of worker threads for parallel tasks"
+    )
+    extra_options: dict[int, str | int] = pydantic.Field(
+        default_factory=dict,
+        description="optional dictionary of additional cURL configuration options",
+    )
+
+    @classmethod
+    def from_arguments(
+        cls: type["ConnectionParameters"], arguments: argparse.Namespace
+    ) -> "ConnectionParameters":
+        overrides: typing.DefaultDict[str, typing.Any] = collections.defaultdict(dict)
+        for key, value in vars(arguments).items():
+            if value is None:
+                continue
+
+            match key:
+                case "host" | "port" | "timeout" | "max_connections" | "max_workers":
+                    overrides[key] = value
+
+                case "username" | "password":
+                    overrides["credentials"][key] = value
+
+                case _:
+                    continue
+
+        return cls(**overrides)
