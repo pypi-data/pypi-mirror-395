36d957a310e4bebf13ba69758618f0647947ac6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:41 +0000

    Add package weegit/0.0.7/weegit-0.0.7.tar.gz

---
+++ packages/weegit/weegit-0.0.7.tar.gz/weegit-0.0.7/weegit/gui/__init__.py
@@ -0,0 +1,10 @@
+from weegit.gui.windows import MainWindow
+from weegit.gui.panels.eeg_signal_panel import EegSignalPanel
+from weegit.gui.panels.analogue_signal_panel import AnalogueSignalPanel
+
+
+__all__ = [
+    "MainWindow",
+    "EegSignalPanel",
+    "AnalogueSignalPanel",
+]
