6c9f610a734a192f0cb947fe366dc5111d1b5b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:06:25 +0000

    Add package profound/0.13.0/profound-0.13.0.tar.gz

---
+++ packages/profound/profound-0.13.0.tar.gz/profound-0.13.0/src/profound/types/logs/raw_logs_response.py
@@ -0,0 +1,39 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Dict, List, Union, Optional
+from datetime import datetime
+from typing_extensions import TypeAlias
+
+from ..._models import BaseModel
+from ..report_response import ReportResponse
+
+__all__ = ["RawLogsResponse", "LogVisitList"]
+
+
+class LogVisitList(BaseModel):
+    host: str
+
+    ip: str
+
+    method: str
+
+    org_id: str
+
+    path: str
+
+    status_code: int
+
+    timestamp: datetime
+
+    user_agent: str
+
+    bytes_sent: Optional[int] = None
+
+    duration_ms: Optional[int] = None
+
+    query_params: Optional[Dict[str, str]] = None
+
+    referer: Optional[str] = None
+
+
+RawLogsResponse: TypeAlias = Union[List[LogVisitList], ReportResponse]
