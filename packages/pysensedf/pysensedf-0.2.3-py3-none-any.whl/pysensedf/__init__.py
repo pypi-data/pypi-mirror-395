f77fd7bafb035dbf7983d040551aece90cc941ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 09:06:51 +0000

    Add package pysensedf/0.2.3/pysensedf-0.2.3-py3-none-any.whl

---
+++ packages/pysensedf/pysensedf-0.2.3-py3-none-any.whl/pysensedf/__init__.py
@@ -0,0 +1,15 @@
+"""
+PySenseDF - Top-level package
+==============================
+
+The revolutionary DataFrame that kills Pandas.
+"""
+
+from .core.dataframe import DataFrame
+from . import datasets
+
+__version__ = "0.2.3"
+__author__ = "Idriss Bado"
+__email__ = "idrissbadoolivier@gmail.com"
+
+__all__ = ["DataFrame", "datasets"]
