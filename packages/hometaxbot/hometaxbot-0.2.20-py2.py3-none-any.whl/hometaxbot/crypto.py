e83ab8dd1539aeea1266e50d02f65ff672f9afde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:33 +0000

    Add package hometaxbot/0.2.20/hometaxbot-0.2.20-py2.py3-none-any.whl

---
+++ packages/hometaxbot/hometaxbot-0.2.20-py2.py3-none-any.whl/hometaxbot/crypto.py
@@ -0,0 +1,106 @@
+import base64
+import hashlib
+import hmac
+import re
+import typing
+from contextlib import contextmanager, ExitStack
+from datetime import date, datetime
+from typing import List
+
+from OpenSSL import crypto
+from pypinksign import pypinksign
+
+from hometaxbot import InvalidCertificate, HometaxException
+
+
+@contextmanager
+def open_files(files: List[str], mode: str = 'rb') -> List[typing.BinaryIO]:
+    with ExitStack() as stack:
+        yield [stack.enter_context(open(f, mode)) for f in files]
+
+
+def load_cert(files: List[typing.BinaryIO], password: str) -> pypinksign.PinkSign:
+    for f in files:
+        f.seek(0)
+
+    ders = [f for f in reversed(files) if f.name.endswith('.der')]
+    keys = [f for f in reversed(files) if f.name.endswith('.key')]
+    if len(files) == 1 and (files[0].name.endswith('.pfx') or files[0].name.endswith('.p12')):
+        try:
+            sign = pypinksign.PinkSign(p12_data=files[0].read(),
+                                       prikey_password=password.encode('utf8'))
+            return sign
+        except (ValueError, crypto.Error) as e:
+            raise InvalidCertificate('공인인증서 비밀번호를 확인해주세요.') from e
+    elif ders and keys:
+        found = False
+        for der in ders:
+            sign = pypinksign.PinkSign(pubkey_data=der.read())
+            for key in keys:
+                key.seek(0)
+                try:
+                    sign.load_prikey(prikey_data=key.read(), prikey_password=password.encode('utf-8'))
+                except ValueError:
+                    continue
+                if not getattr(sign, '_rand_num', None):
+                    continue
+                signed = sign.sign(password.encode('utf-8'))
+                if sign.verify(signed, password.encode('utf-8')):
+                    found = True
+                    return sign
+        if not found:
+            raise InvalidCertificate('유효한 공인인증서를 찾을 수 없습니다. 비밀번호를 확인해주세요.')
+    else:
+        raise InvalidCertificate('올바른 공인인증서 형식이 아닙니다. *.p12이나 *.pfx, 또는 *.der과 *.key의 묶음 중에 하나여야 합니다.')
+
+
+def validate_cert_expiry(sign):
+    if sign.valid_date()[1].date() < date.today():
+        raise HometaxException('공인인증서 유효기간이 지났습니다.')
+
+
+def nts_hash_param(payload):
+    # payload를 정확히 encode하지 않아도 API가 동작함. 그래서 형식만 맞추도록 일부만 구현함
+    # 필요 시 Hometax script를 보고 정확히 해싱하여 보내도록 수정
+    second = datetime.now().second
+    encodedParam = base64.b64encode(hashlib.sha256((payload + str(second)).encode('utf-8')).hexdigest().upper().encode('utf-8')).decode('utf-8')
+    return str(second + 12) + encodedParam + str(second)
+
+
+# 홈택스 js소스에 구현된 k1~k8암호화소스
+
+
+def k1(second):
+    # TODO testVal 매번 바뀌는지 확인필요
+    testVal = ["bakfuRUXvh9c3POvkdfUDHF91jijBhV2BvsuWE966SY", "rns6HuMkhT3FN8cIELHqYW51xHpk4oGOTetFjZ3Wog",
+               "ZobgiyO5GpHBj4XfBxpIsdtuxOVGOuxfvJ3cl7hg", "tQpnppnLO4DhApYt4Wpi2fP3ikontfDj5e4gL8fatL0",
+               "qyDYuOUwZO2GCykWTJJZrgRIGTg6z3FPBrIAyHxxI", "RF899ggdKY31TR3beawC7r7QbLAW1of4OrRaSWypA",
+               "ASNbDSqkpdq6ckOpIoGUyO5E6xeVulnMBIQJwOAvEI"]
+    return testVal[int(second) % 7]
+
+
+def k2(payload, testVal):
+    signature = hmac.new(testVal.encode('utf-8'), payload.encode('utf-8'), hashlib.sha256).digest()
+    return re.sub(r'[^0-9a-zA-Z]', r'', base64.b64encode(signature).decode('utf-8'))
+
+
+def k4(payload, second, userId=''):
+    return k2(payload+userId, k1(second))
+
+
+def k7():
+    return f'{datetime.now().second:02}'
+
+
+def k8(action_id, second):
+    return k4(action_id, second)
+
+
+def snake_oil_encrypt(pswd: str) -> str:
+    temp_string = ""
+    for ch in pswd:
+        encoded_char = chr(128 - ord(ch))
+        temp_string += encoded_char
+    encoded_bytes = temp_string.encode("latin1")  # 0~255 범위 안전하게 인코딩
+    base64_encoded = base64.b64encode(encoded_bytes).decode("ascii")
+    return base64_encoded
---
+++ packages/hometaxbot/hometaxbot-0.2.20-py2.py3-none-any.whl/hometaxbot/scraper/requestutil.py
@@ -0,0 +1,119 @@
+import json
+import random
+import re
+import time
+from xml.etree import ElementTree
+
+import requests.adapters
+import urllib3
+
+from requests import Response
+
+from hometaxbot import Throttled, HometaxException
+
+
+def action_params(action, screen):
+    return f"actionId={action}&screenId={screen}&popupYn=false&realScreenId="
+
+
+def nts_generate_random_string(length):
+    """
+    홈택스 스크립트에 있는 랜덤 스트링 제너레이션 포팅
+    :param length: 생성할 스트링 길이
+    :return:
+    """
+    seed = "qwertyuiopasdfghjklzxxcvbnm0123456789QWERTYUIOPASDDFGHJKLZXCVBNBM"
+    result = ''
+    for i in range(length):
+        result += seed[random.randint(0, len(seed) - 1)]
+    return result
+
+
+def check_error_on_response(response):
+    if '요청하신 서비스는 현재 서비스 중지 시간 입니다' in response.text:
+        raise Throttled(response.text.strip())
+
+    if '반복적인 호출은 시스템에 부하를 줄 수 있습니다' in response.text:
+        raise Throttled(response.text.strip())
+
+    if '조회에 실패하였습니다' in response.text:
+        raise Throttled(response.text.strip())
+
+    if '서비스 실행 중 오류가 발생하였습니다.' in response.text:
+        raise HometaxException(response.text)
+
+    if response.status_code == 400:
+        if 'Request Blocked' in response.text:
+            raise Throttled(response.text.strip())
+        raise HometaxException(response.text)
+
+
+def parse_response(response):
+    """
+    :param response: requests.Response
+    :return: validated된 reponse를 xml 파싱하여 ElementTree객체로 리턴
+    """
+    check_error_on_response(response)
+
+    element = ElementTree.fromstring(re.sub(' xmlns="[^"]+"', '', response.text, count=1))
+    if find_text_or_none(element, 'errorCd') in ['-9402', '-9404', '-9403', '-9405'] \
+            or find_text_or_none(element, 'msg') in ['-9402', '-9404', '-9403', '-9405']:
+        raise Throttled('홈택스 이중 로그인 방지로 인해 인증에 실패했습니다. 다시 시도해주세요.')
+
+    return element
+
+
+def ensure_xml_response(fn, *args, **kwargs):
+    try:
+        return parse_response(fn(*args, **kwargs))
+    except Throttled as e:
+        if e.wait:
+            time.sleep(e.wait)
+            return parse_response(fn(*args, **kwargs))
+        else:
+            raise
+
+
+def find_text_or_none(element, tag_name):
+    found = element.find('.//' + tag_name)
+    if found is not None:
+        return found.text
+    return None
+
+
+class CustomHttpAdapter(requests.adapters.HTTPAdapter):
+    # "Transport adapter" that allows us to use custom ssl_context.
+
+    def __init__(self, ssl_context=None, **kwargs):
+        self.ssl_context = ssl_context
+        super().__init__(**kwargs)
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = urllib3.poolmanager.PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_context=self.ssl_context)
+
+
+class XMLValueFinder:
+    element: ElementTree.Element
+
+    def __init__(self, element):
+        self.element = element
+
+    def get(self, path, default=None):
+        found = self.element.find(path)
+        if found is not None:
+            return found.text
+
+        return default
+
+    def sub_finders(self, path):
+        return (XMLValueFinder(e) for e in self.element.findall(path))
+
+
+def get_quarter_by_date(date):
+    return int((date.month - 1) / 3 + 1)
+
+
+def json_minified_dumps(obj):
+    return json.dumps(obj, separators=(',', ':'), ensure_ascii=False)
