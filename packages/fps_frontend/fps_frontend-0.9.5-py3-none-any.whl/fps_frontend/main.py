a6fa9a11ac0c5904af24967d7218dbb5ac52b655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:36 +0000

    Add package fps_frontend/0.9.5/fps_frontend-0.9.5-py3-none-any.whl

---
+++ packages/fps_frontend/fps_frontend-0.9.5-py3-none-any.whl/fps_frontend/main.py
@@ -0,0 +1,11 @@
+from fps import Module
+from jupyverse_api.frontend import FrontendConfig
+
+
+class FrontendModule(Module):
+    def __init__(self, name: str, **kwargs):
+        super().__init__(name)
+        self.config = FrontendConfig(**kwargs)
+
+    async def prepare(self) -> None:
+        self.put(self.config, FrontendConfig)
