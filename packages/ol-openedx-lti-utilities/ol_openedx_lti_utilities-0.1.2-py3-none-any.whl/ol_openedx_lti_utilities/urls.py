6f574a0bd53e1a8e8f4ab1a8f2a017a4fb49bd52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:06:38 +0000

    Add package ol-openedx-lti-utilities/0.1.2/ol_openedx_lti_utilities-0.1.2-py3-none-any.whl

---
+++ packages/ol-openedx-lti-utilities/ol_openedx_lti_utilities-0.1.2-py3-none-any.whl/ol_openedx_lti_utilities/urls.py
@@ -0,0 +1,19 @@
+"""
+OL Open edX LTI Utilities URLs
+"""
+
+from django.conf import settings
+from django.urls import re_path
+
+urlpatterns = []
+
+if settings.FEATURES.get("ENABLE_LTI_PROVIDER"):
+    from ol_openedx_lti_utilities.views import LtiUserFixView
+
+    urlpatterns = [
+        re_path(
+            r"^",
+            LtiUserFixView.as_view(),
+            name="lti_user_fix",
+        ),
+    ]
