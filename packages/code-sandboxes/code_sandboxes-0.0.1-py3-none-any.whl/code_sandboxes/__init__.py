b8789db2f42d08645c4fb8985cd0e6adfc1bb2a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 11:05:13 +0000

    Add package code-sandboxes/0.0.1/code_sandboxes-0.0.1-py3-none-any.whl

---
+++ packages/code-sandboxes/code_sandboxes-0.0.1-py3-none-any.whl/code_sandboxes/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2023-2024 Datalayer, Inc.
+#
+# BSD 3-Clause License
+
+"""Code Sandboxes."""
+
+__all__ = []
---
+++ packages/code-sandboxes/code_sandboxes-0.0.1-py3-none-any.whl/code_sandboxes/__version__.py
@@ -0,0 +1,6 @@
+#
+# BSD 3-Clause License
+
+"""Code Sandboxes."""
+
+__version__ = "0.0.1"
