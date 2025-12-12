6e9a2566683407dbdb9edcd39eb2189d8f738acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:38 +0000

    Add package fps_webdav/0.9.5/fps_webdav-0.9.5-py3-none-any.whl

---
+++ packages/fps_webdav/fps_webdav-0.9.5-py3-none-any.whl/fps_webdav/main.py
@@ -0,0 +1,17 @@
+from fps import Module
+from jupyverse_api.app import App
+
+from .config import WebDAVConfig
+from .routes import WebDAV
+
+
+class WebDAVModule(Module):
+    def __init__(self, name: str, **kwargs):
+        super().__init__(name)
+        self.config = WebDAVConfig(**kwargs)
+
+    async def prepare(self) -> None:
+        app = await self.get(App)
+
+        webdav = WebDAV(app, self.config)
+        self.put(webdav)
