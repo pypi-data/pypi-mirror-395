28e7165ea0063723e034ee3fa30afbc57a0d0ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 07:06:41 +0000

    Add package fluidsim/0.9.0/fluidsim-0.9.0-cp312-cp312-macosx_10_13_x86_64.whl

---
+++ packages/fluidsim/fluidsim-0.9.0-cp312-cp312-macosx_10_13_x86_64.whl/fluidsim/operators/__init__.py
@@ -0,0 +1,17 @@
+"""Numerical operators
+======================
+
+Provides
+
+.. autosummary::
+   :toctree:
+
+   base
+   operators0d
+   operators2d
+   operators3d
+   sphericalharmo
+   op_finitediff1d
+   op_finitediff2d
+
+"""
