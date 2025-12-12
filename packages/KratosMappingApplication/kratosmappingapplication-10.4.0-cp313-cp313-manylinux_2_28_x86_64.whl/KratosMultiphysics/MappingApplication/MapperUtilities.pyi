441842907457d9eecedf7702a097d48f6e4da74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:25 +0000

    Add package KratosMappingApplication/10.4.0/kratosmappingapplication-10.4.0-cp313-cp313-manylinux_2_28_x86_64.whl

---
+++ packages/KratosMappingApplication/kratosmappingapplication-10.4.0-cp313-cp313-manylinux_2_28_x86_64.whl/KratosMultiphysics/MappingApplication/MapperUtilities.pyi
@@ -0,0 +1,7 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics as Kratos
+
+def RestoreCurrentConfiguration(arg0: Kratos.ModelPart) -> None:
+    """RestoreCurrentConfiguration(arg0: Kratos.ModelPart) -> None"""
+def SaveCurrentConfiguration(arg0: Kratos.ModelPart) -> None:
+    """SaveCurrentConfiguration(arg0: Kratos.ModelPart) -> None"""
---
+++ packages/KratosMappingApplication/kratosmappingapplication-10.4.0-cp313-cp313-manylinux_2_28_x86_64.whl/KratosMultiphysics/MappingApplication/MappingIntersectionUtilities.pyi
@@ -0,0 +1,7 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics as Kratos
+
+def CreateQuadraturePointsCoupling1DGeometries2D(arg0: Kratos.ModelPart, arg1: float) -> None:
+    """CreateQuadraturePointsCoupling1DGeometries2D(arg0: Kratos.ModelPart, arg1: float) -> None"""
+def FindIntersection1DGeometries2D(arg0: Kratos.ModelPart, arg1: Kratos.ModelPart, arg2: Kratos.ModelPart, arg3: float) -> None:
+    """FindIntersection1DGeometries2D(arg0: Kratos.ModelPart, arg1: Kratos.ModelPart, arg2: Kratos.ModelPart, arg3: float) -> None"""
