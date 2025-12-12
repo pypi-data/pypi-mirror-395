2bd1056bcc11a75d64ab415299bcc34060cdf71c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 22:05:35 +0000

    Add package byteshare/0.0.1/byteshare-0.0.1-py3-none-any.whl

---
+++ packages/byteshare/byteshare-0.0.1-py3-none-any.whl/byteshare/__init__.py
@@ -0,0 +1,13 @@
+"""
+ByteShare - A lightweight, distributed tensor store for ML inference.
+
+The spiritual successor to Apache Plasma, without the Ray dependency.
+"""
+
+__version__ = "0.0.1"
+__all__ = ["__version__"]
+
+# TODO: Export main classes when implemented
+# from byteshare.core.store import ByteStore
+# from byteshare.core.object_ref import ObjectRef
+
