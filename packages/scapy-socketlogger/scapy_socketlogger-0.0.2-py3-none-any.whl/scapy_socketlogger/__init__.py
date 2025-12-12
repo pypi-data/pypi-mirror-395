ef10064f5d5a1b1775631fef5e4983d664cf2044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 06:04:56 +0000

    Add package scapy-socketlogger/0.0.2/scapy_socketlogger-0.0.2-py3-none-any.whl

---
+++ packages/scapy-socketlogger/scapy_socketlogger-0.0.2-py3-none-any.whl/scapy_socketlogger/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#
+# SPDX-License-Identifier: GPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
+#
+
+from .socket_logger import SocketLogger
+
+__all__ = ["SocketLogger"]
