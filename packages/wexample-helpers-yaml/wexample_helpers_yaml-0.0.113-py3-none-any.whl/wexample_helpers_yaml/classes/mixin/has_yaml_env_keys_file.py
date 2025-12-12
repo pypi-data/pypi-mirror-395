ce3138171918538d5c5f4baa47e8a77c8befb813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:30:42 +0000

    Add package wexample-helpers-yaml/0.0.113/wexample_helpers_yaml-0.0.113-py3-none-any.whl

---
+++ packages/wexample-helpers-yaml/wexample_helpers_yaml-0.0.113-py3-none-any.whl/wexample_helpers_yaml/classes/mixin/has_yaml_env_keys_file.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from wexample_helpers.classes.mixin.has_env_keys_file import HasEnvKeysFile
+from wexample_helpers.decorator.base_class import base_class
+
+
+@base_class
+class HasYamlEnvKeysFile(HasEnvKeysFile):
+    """Mixin for classes that need to load env from files."""
+
+    def _init_env_file_yaml(self, file_path: str) -> None:
+        from wexample_helpers_yaml.helpers.yaml_helpers import yaml_read_dict
+
+        self.env_config.update(yaml_read_dict(file_path))
+        self._validate_env_keys()
