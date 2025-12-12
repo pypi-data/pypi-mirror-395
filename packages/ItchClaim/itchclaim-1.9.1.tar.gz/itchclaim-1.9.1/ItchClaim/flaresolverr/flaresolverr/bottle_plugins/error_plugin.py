aa71e3a69515952caff4ed3f26c7fcb9c4f2b9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:56 +0000

    Add package ItchClaim/1.9.1/itchclaim-1.9.1.tar.gz

---
+++ packages/ItchClaim/itchclaim-1.9.1.tar.gz/itchclaim-1.9.1/ItchClaim/flaresolverr/flaresolverr/bottle_plugins/error_plugin.py
@@ -0,0 +1,24 @@
+from bottle import response
+import logging
+
+logger = logging.getLogger('flaresolverr')
+
+
+def error_plugin(callback):
+    """
+    Bottle plugin to handle exceptions
+    https://stackoverflow.com/a/32764250
+    """
+
+    def wrapper(*args, **kwargs):
+        try:
+            actual_response = callback(*args, **kwargs)
+        except Exception as e:
+            logger.error(str(e))
+            actual_response = {
+                "error": str(e)
+            }
+            response.status = 500
+        return actual_response
+
+    return wrapper
