2ee1707143b99a5ef22f4195f4f464b99748f705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 17:06:24 +0000

    Add package nose-py3/1.9.0/nose_py3-1.9.0-py3-none-any.whl

---
+++ packages/nose-py3/nose_py3-1.9.0-py3-none-any.whl/nose/__init__.py
@@ -0,0 +1,13 @@
+from nose.core import collector, main, run, run_exit, runmodule
+from nose.plugins.deprecated import DeprecatedTest
+from nose.plugins.skip import SkipTest
+from nose.tools import with_setup
+
+__author__ = 'Adam Bilbrough'
+__versioninfo__ = (1, 9, 0)
+__version__ = '.'.join(map(str, __versioninfo__))
+
+__all__ = [
+    'main', 'run', 'run_exit', 'runmodule', 'with_setup',
+    'SkipTest', 'DeprecatedTest', 'collector'
+    ]
