5252317e34352aca93d9a86eb71ac816d6d8f697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 14:05:27 +0000

    Add package meerschaum/3.1.3/meerschaum-3.1.3-py3-none-any.whl

---
+++ packages/meerschaum/meerschaum-3.1.3-py3-none-any.whl/meerschaum/core/User/__init__.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+"""
+Manager users' metadata via the User class
+"""
+
+from typing import Optional
+
+import meerschaum as mrsm
+from meerschaum.core.User._User import User, hash_password, verify_password
+
+
+__all__ = (
+    'User',
+    'hash_password',
+    'verify_password',
+    'is_user_allowed_to_execute',
+)
+
+
+def is_user_allowed_to_execute(
+    user: Optional[User],
+    debug: bool = False,
+) -> mrsm.SuccessTuple:
+    """
+    Return a `SuccessTuple` indicating whether a given user is allowed to execute actions.
+    """
+    if user is None:
+        return True, "Success"
+
+    user_type = user.instance_connector.get_user_type(user, debug=debug)
+
+    if user_type == 'admin':
+        return True, "Success"
+
+    allow_non_admin = mrsm.get_config('system', 'api', 'permissions', 'actions', 'non_admin')
+    if not allow_non_admin:
+        return False, "The administrator for this server has not allowed users to perform actions."
+
+    return True, "Success"
