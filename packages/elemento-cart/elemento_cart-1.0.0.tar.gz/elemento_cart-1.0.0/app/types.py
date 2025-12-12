4ff28124177bdeeafef93ceed9a0a7ccceade22d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:08 +0000

    Add package elemento-cart/1.0.0/elemento_cart-1.0.0.tar.gz

---
+++ packages/elemento-cart/elemento_cart-1.0.0.tar.gz/elemento_cart-1.0.0/app/types.py
@@ -0,0 +1,43 @@
+"""Basic data types."""
+
+from typing import Annotated, NewType
+
+from msgspec import Struct, Meta
+
+__all__ = [
+    "CustomerId", "CartId", "CartItem", "Cart", "CartTotal", "CalculatedCart", "CalculatedCartItem"
+]
+
+CustomerId = NewType("CustomerId", int)
+CartId = NewType("CartId", str)
+
+class CartItem(Struct):
+    id: str
+    quantity: Annotated[int, Meta(ge=1)]
+    picked: bool = True
+
+class Cart(Struct):
+    items: list[CartItem]
+
+class CalculatedCartItem(Struct):
+    id: str
+    quantity: int
+    picked: bool
+    price: int
+    price_old: int
+    discount: int
+    available_quantity: int
+
+class CartTotal(Struct):
+    price: int
+    quantity: int
+    discount: int
+    price_old: int
+
+class CalculatedCart(Struct):
+    id: CartId
+    store_id: str
+    total: CartTotal
+    items: list[CalculatedCartItem]
+    items_not_available: list[CartItem]
+    items_not_picked: list[CartItem]
