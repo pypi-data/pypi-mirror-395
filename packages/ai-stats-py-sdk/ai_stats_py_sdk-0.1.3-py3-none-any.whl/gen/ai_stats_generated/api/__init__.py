d8363bae84940785272534ac9146a1114df96fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 06:05:08 +0000

    Add package ai-stats-py-sdk/0.1.3/ai_stats_py_sdk-0.1.3-py3-none-any.whl

---
+++ packages/ai-stats-py-sdk/ai_stats_py_sdk-0.1.3-py3-none-any.whl/gen/ai_stats_generated/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from ai_stats_generated.api.analytics_api import AnalyticsApi
+from ai_stats_generated.api.completions_api import CompletionsApi
+from ai_stats_generated.api.generations_api import GenerationsApi
+from ai_stats_generated.api.images_api import ImagesApi
+from ai_stats_generated.api.models_api import ModelsApi
+from ai_stats_generated.api.moderations_api import ModerationsApi
+from ai_stats_generated.api.video_api import VideoApi
+
