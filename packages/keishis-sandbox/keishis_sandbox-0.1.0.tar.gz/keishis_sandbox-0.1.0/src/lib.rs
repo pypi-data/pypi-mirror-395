b9f15f407c00755128f9a4d757dca43b7185c536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 06:04:48 +0000

    Add package keishis-sandbox/0.1.0/keishis_sandbox-0.1.0.tar.gz

---
+++ packages/keishis-sandbox/keishis_sandbox-0.1.0.tar.gz/keishis_sandbox-0.1.0/src/lib.rs
@@ -0,0 +1,18 @@
+use pyo3::pymodule;
+
+fn sum_as_str(a: i64, b: i64) -> String {
+    (a + b).to_string()
+}
+
+#[pymodule]
+#[pyo3(name = "_keishis_sandbox")]
+mod keishis_sandbox {
+    use super::sum_as_str as _sum_as_str;
+    use pyo3::prelude::PyResult;
+    use pyo3::pyfunction;
+
+    #[pyfunction]
+    fn sum_as_str(a: i64, b: i64) -> PyResult<String> {
+        Ok(_sum_as_str(a, b))
+    }
+}
