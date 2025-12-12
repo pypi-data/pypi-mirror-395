992fce269a78404dac7a482df53d1d5ff7294d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:21 +0000

    Add package KratosMultiphysics/10.4.0/kratosmultiphysics-10.4.0-cp312-cp312-win_amd64.whl

---
+++ packages/KratosMultiphysics/kratosmultiphysics-10.4.0-cp312-cp312-win_amd64.whl/KratosMultiphysics/PropertiesUtilities.pyi
@@ -0,0 +1,5 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics as Kratos
+
+def CopyPropertiesValues(arg0: Kratos.Properties, arg1: Kratos.Properties) -> None:
+    """CopyPropertiesValues(arg0: Kratos.Properties, arg1: Kratos.Properties) -> None"""
