98d46ff3905170dbe7431e3b9b53a8e32bfe1f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:25 +0000

    Add package KratosMappingApplication/10.4.0/kratosmappingapplication-10.4.0-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/KratosMappingApplication/kratosmappingapplication-10.4.0-cp312-cp312-manylinux_2_28_x86_64.whl/KratosMultiphysics/MappingApplication/__init__.pyi
@@ -0,0 +1,15 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics as Kratos
+from . import IgaMappingIntersectionUtilities as IgaMappingIntersectionUtilities, MapperUtilities as MapperUtilities, MappingIntersectionUtilities as MappingIntersectionUtilities
+
+class BeamMapper(Kratos.Mapper):
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialize self.  See help(type(self)) for accurate signature."""
+    def InverseMap(self, origin_force_variable: Kratos.Array1DVariable3, origin_moment_variable: Kratos.Array1DVariable3, destination_force_variable: Kratos.Array1DVariable3, mapping_options: Kratos.Flags = ...) -> None:
+        """InverseMap(self: KratosMappingApplication.BeamMapper, origin_force_variable: Kratos.Array1DVariable3, origin_moment_variable: Kratos.Array1DVariable3, destination_force_variable: Kratos.Array1DVariable3, mapping_options: Kratos.Flags = <Kratos.Flags object at 0x7fd02dbbb6f0>) -> None"""
+    def Map(self, origin_displacement_variable: Kratos.Array1DVariable3, origin_rotation_variable: Kratos.Array1DVariable3, destination_displacement_variable: Kratos.Array1DVariable3, mapping_options: Kratos.Flags = ...) -> None:
+        """Map(self: KratosMappingApplication.BeamMapper, origin_displacement_variable: Kratos.Array1DVariable3, origin_rotation_variable: Kratos.Array1DVariable3, destination_displacement_variable: Kratos.Array1DVariable3, mapping_options: Kratos.Flags = <Kratos.Flags object at 0x7fd02db7b2f0>) -> None"""
+
+class KratosMappingApplication(Kratos.KratosApplication):
+    def __init__(self) -> None:
+        """__init__(self: KratosMappingApplication.KratosMappingApplication) -> None"""
