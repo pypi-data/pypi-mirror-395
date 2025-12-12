778bf3f37bdd6b999b7d0bfe8df8d6333fd08e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 09:07:11 +0000

    Add package promptbind/0.2.1/promptbind-0.2.1.tar.gz

---
+++ packages/promptbind/promptbind-0.2.1.tar.gz/promptbind-0.2.1/promptbind/__init__.py
@@ -0,0 +1,14 @@
+from.decorator import with_prompt
+from .container import PromptEntry
+from .util import set_prompt_key_patch, unset_prompt_key_patch, get_prompt_key_patch, get_effective_prompt_key
+
+__version__ = "0.2.1"
+
+__all__ = [
+    "with_prompt",
+    "PromptEntry",
+    "set_prompt_key_patch",
+    "unset_prompt_key_patch",
+    "get_prompt_key_patch",
+    "get_effective_prompt_key",
+]
