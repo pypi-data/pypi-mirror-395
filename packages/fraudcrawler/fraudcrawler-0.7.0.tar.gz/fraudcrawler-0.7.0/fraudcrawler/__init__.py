d584c6a587032b5dfc501a08d4fabf4ee3456566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:51 +0000

    Add package fraudcrawler/0.7.0/fraudcrawler-0.7.0.tar.gz

---
+++ packages/fraudcrawler/fraudcrawler-0.7.0.tar.gz/fraudcrawler-0.7.0/fraudcrawler/__init__.py
@@ -0,0 +1,44 @@
+from fraudcrawler.scraping.search import Searcher, SearchEngineName
+from fraudcrawler.scraping.enrich import Enricher
+from fraudcrawler.scraping.url import URLCollector
+from fraudcrawler.scraping.zyte import ZyteAPI
+from fraudcrawler.processing.processor import (
+    UserInputs,
+    Workflow,
+    OpenAIClassification,
+    OpenAIClassificationUserInputs,
+    Processor,
+)
+from fraudcrawler.base.orchestrator import Orchestrator
+from fraudcrawler.base.client import FraudCrawlerClient
+from fraudcrawler.base.base import (
+    Deepness,
+    Enrichment,
+    Host,
+    Language,
+    Location,
+    ProductItem,
+    HttpxAsyncClient,
+)
+
+__all__ = [
+    "Searcher",
+    "SearchEngineName",
+    "Enricher",
+    "URLCollector",
+    "ZyteAPI",
+    "UserInputs",
+    "Workflow",
+    "OpenAIClassification",
+    "OpenAIClassificationUserInputs",
+    "Processor",
+    "Orchestrator",
+    "ProductItem",
+    "FraudCrawlerClient",
+    "Language",
+    "Location",
+    "Host",
+    "Deepness",
+    "Enrichment",
+    "HttpxAsyncClient",
+]
