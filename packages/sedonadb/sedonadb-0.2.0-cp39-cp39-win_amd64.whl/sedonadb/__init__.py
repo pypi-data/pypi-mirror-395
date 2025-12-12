33c9a1db4e8dd8996ac9faa1f5e66572d052d84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 15:07:12 +0000

    Add package sedonadb/0.2.0/sedonadb-0.2.0-cp39-cp39-win_amd64.whl

---
+++ packages/sedonadb/sedonadb-0.2.0-cp39-cp39-win_amd64.whl/sedonadb/__init__.py
@@ -0,0 +1,62 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'sedonadb.libs'))
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-sedonadb-0.2.0')
+        if os.path.isfile(load_order_filepath):
+            import ctypes.wintypes
+            with open(os.path.join(libs_dir, '.load-order-sedonadb-0.2.0')) as file:
+                load_order = file.read().split()
+            kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+            kernel32.LoadLibraryExW.restype = ctypes.wintypes.HMODULE
+            kernel32.LoadLibraryExW.argtypes = ctypes.wintypes.LPCWSTR, ctypes.wintypes.HANDLE, ctypes.wintypes.DWORD
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if os.path.isfile(lib_path) and not kernel32.LoadLibraryExW(lib_path, None, 8):
+                    raise OSError('Error loading {}; {}'.format(lib, ctypes.FormatError(ctypes.get_last_error())))
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+from sedonadb import _lib
+from sedonadb.context import connect, configure_proj
+
+__version__ = _lib.sedona_python_version()
+
+__features__ = _lib.sedona_python_features()
+
+__all__ = ["connect", "options"]
+
+# Attempt to configure PROJ on import. This will warn if PROJ
+# can't be configured but should never error. The auto-configured
+# value can be overridden as long as the call to configure_proj()
+# occurs before actually creating a transform.
+configure_proj("auto")
---
+++ packages/sedonadb/sedonadb-0.2.0-cp39-cp39-win_amd64.whl/sedonadb/dbapi.py
@@ -0,0 +1,61 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from typing import Mapping, Any
+
+import adbc_driver_manager.dbapi
+
+import sedonadb.adbc
+from sedonadb.utility import sedona  # noqa: F401
+
+
+def connect(**kwargs: Mapping[str, Any]) -> "Connection":
+    """Connect to Sedona via Python DBAPI
+
+    Creates a DBAPI-compatible connection as a thin wrapper around the
+    ADBC Python driver manager's DBAPI compatibility layer. Support for
+    DBAPI is experimental.
+
+    Args:
+        kwargs: Extra keyword arguments passed to
+            `adbc_driver_manager.dbapi.Connection()`.
+
+    Examples:
+
+        >>> con = sedona.dbapi.connect()
+        >>> with con.cursor() as cur:
+        ...     _ = cur.execute("SELECT 1 as one")
+        ...     cur.fetchall()
+        [(1,)]
+    """
+    db = None
+    conn = None
+
+    try:
+        db = sedonadb.adbc.connect()
+        conn = adbc_driver_manager.AdbcConnection(db)
+        return adbc_driver_manager.dbapi.Connection(db, conn, **kwargs)
+    except Exception:
+        if conn:
+            conn.close()
+        if db:
+            db.close()
+        raise
+
+
+Connection = adbc_driver_manager.dbapi.Connection
+Cursor = adbc_driver_manager.dbapi.Cursor
