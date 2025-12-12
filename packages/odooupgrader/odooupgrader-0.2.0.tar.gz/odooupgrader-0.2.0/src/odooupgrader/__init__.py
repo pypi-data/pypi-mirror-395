91d47f487ea9f9ce2d631f897617aec9c503565e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 09:07:00 +0000

    Add package odooupgrader/0.2.0/odooupgrader-0.2.0.tar.gz

---
+++ packages/odooupgrader/odooupgrader-0.2.0.tar.gz/odooupgrader-0.2.0/src/odooupgrader/__init__.py
@@ -0,0 +1,11 @@
+"""
+OdooUpgrader - Professional Odoo database upgrade tool
+"""
+
+__version__ = "0.2.0"
+__author__ = "Fasil"
+__email__ = "fasilwdr@hotmail.com"
+
+from .core import OdooUpgrader
+
+__all__ = ["OdooUpgrader"]
