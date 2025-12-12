187883321e0307473892b64e0ec6c1c71aa62180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 07:06:36 +0000

    Add package policyengine-us-data/1.47.1/policyengine_us_data-1.47.1-py3-none-any.whl

---
+++ packages/policyengine-us-data/policyengine_us_data-1.47.1-py3-none-any.whl/policyengine_us_data/storage/download_private_prerequisites.py
@@ -0,0 +1,29 @@
+from policyengine_us_data.utils.huggingface import download
+from pathlib import Path
+
+FOLDER = Path(__file__).parent
+
+download(
+    repo="policyengine/irs-soi-puf",
+    repo_filename="puf_2015.csv",
+    local_folder=FOLDER,
+    version=None,
+)
+download(
+    repo="policyengine/irs-soi-puf",
+    repo_filename="demographics_2015.csv",
+    local_folder=FOLDER,
+    version=None,
+)
+download(
+    repo="policyengine/irs-soi-puf",
+    repo_filename="soi.csv",
+    local_folder=FOLDER,
+    version=None,
+)
+download(
+    repo="policyengine/irs-soi-puf",
+    repo_filename="np2023_d5_mid.csv",
+    local_folder=FOLDER,
+    version=None,
+)
