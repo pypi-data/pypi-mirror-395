3bd7239fd1fc86b48e960f07df411cc9ba6b9976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 09:06:55 +0000

    Add package acurl/1.2.1/acurl-1.2.1.tar.gz

---
+++ packages/acurl/acurl-1.2.1.tar.gz/acurl-1.2.1/setup.py
@@ -0,0 +1,35 @@
+"""Setup script for acurl - build Cython extensions."""
+import os
+
+from Cython.Build import cythonize
+from Cython.Compiler.Options import get_directive_defaults
+from setuptools import Extension, setup
+
+# https://stackoverflow.com/a/28301932/463500
+if "IS_TOX_BUILD" in os.environ:
+    directive_defaults = get_directive_defaults()
+    directive_defaults["linetrace"] = True
+    # directive_defaults['binding'] = True
+    kwargs = {"define_macros": [("CYTHON_TRACE", "1")]}
+else:
+    kwargs = {}
+
+extensions = [Extension("acurl", ["src/acurl.pyx"], libraries=["curl"], **kwargs)]
+
+# All metadata is now in pyproject.toml
+setup(
+    ext_modules=cythonize(
+        extensions,
+        gdb_debug=True,
+        compiler_directives={
+            "language_level": 3,
+            "warn.undeclared": True,
+            "warn.unreachable": True,
+            "warn.maybe_uninitialized": True,
+            "warn.unused": True,
+            "warn.unused_arg": True,
+            "warn.unused_result": True,
+        },
+    ),
+)
+
