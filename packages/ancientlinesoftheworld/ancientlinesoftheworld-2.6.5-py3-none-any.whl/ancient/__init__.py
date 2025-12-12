8cae4c41ddfd97454dbaa0ee562bc2f571f04a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 09:06:34 +0000

    Add package ancientlinesoftheworld/2.6.5/ancientlinesoftheworld-2.6.5-py3-none-any.whl

---
+++ packages/ancientlinesoftheworld/ancientlinesoftheworld-2.6.5-py3-none-any.whl/ancient/__init__.py
@@ -0,0 +1,23 @@
+from .core import AncientScripts
+from .mappings import (
+    persian_to_cuneiform_mapping, persian_to_manichaean_mapping, 
+    persian_to_hieroglyph_mapping, english_to_cuneiform_mapping,
+    english_to_pahlavi_mapping, persian_to_pahlavi_mapping,
+    english_to_manichaean_mapping, english_to_hieroglyph_mapping,
+    linear_b_dict, conversion_dict, convert_to_cuneiform,
+    convert_to_pahlavi, convert_to_manichaean, convert_to_hieroglyph,
+    text_to_linear_b_optimized, convert_to_akkadian, convert_to_oracle_bone,convert_to_avestan
+)
+from .timeline import AncientTimeline
+from .visualizer import AncientImageGenerator
+from .ai_chat import AncientScriptAI
+from .releases import get_releases
+from .web_app import AncientWeb
+
+
+
+__version__ = "2.6.5"
+__all__ = ['AncientScripts','AncientTimeline','AncientImageGenerator','AncientScriptAI','AncientWeb']
+__author__ = "Amir Hossein Khazaei"
+__description__ = "A library for converting Persian and English texts into ancient scripts such as Pahlavi, Avestan, Cuneiform, Manichaean, and more."
+__license__ = "MIT"
