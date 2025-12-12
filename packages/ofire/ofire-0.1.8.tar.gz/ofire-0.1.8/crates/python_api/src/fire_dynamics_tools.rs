c83852a0b264d0b75b500179a5570ea0ef3104e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 12:06:28 +0000

    Add package ofire/0.1.8/ofire-0.1.8.tar.gz

---
+++ packages/ofire/ofire-0.1.8.tar.gz/ofire-0.1.8/crates/python_api/src/fire_dynamics_tools.rs
@@ -0,0 +1,16 @@
+pub mod chapter_2;
+pub mod chapter_4;
+pub mod chapter_5;
+pub mod chapter_9;
+
+use pyo3::prelude::*;
+use pyo3::wrap_pymodule;
+
+#[pymodule]
+pub fn fire_dynamics_tools(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_wrapped(wrap_pymodule!(chapter_2::chapter_2))?;
+    m.add_wrapped(wrap_pymodule!(chapter_4::chapter_4))?;
+    m.add_wrapped(wrap_pymodule!(chapter_5::chapter_5))?;
+    m.add_wrapped(wrap_pymodule!(chapter_9::chapter_9))?;
+    Ok(())
+}
