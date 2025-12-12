86575266dd648f67f47c7101d5e46ad443fc11ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:30:52 +0000

    Add package BJSnum/0.1.3/bjsnum-0.1.3-py3-none-any.whl

---
+++ packages/BJSnum/bjsnum-0.1.3-py3-none-any.whl/BJSnum/BJSdiv_norm.py
@@ -0,0 +1,13 @@
+def div_norm(*number):
+    if not number:
+        return None
+    
+    try:
+        total = number[0]
+        for num in number[1:]:
+            if not isinstance(num, (int, float)):
+                raise TypeError(f"Type not valid for operation{number}")
+            total /= num
+        return total
+    except (TypeError, ZeroDivisionError, OverflowError) as err:
+        raise err
---
+++ packages/BJSnum/bjsnum-0.1.3-py3-none-any.whl/BJSnum/__init__.py
@@ -0,0 +1,7 @@
+from BJSadd import add
+from BJSsub import sub
+from BJSmult import mult
+from BJSdiv_norm import div_norm
+from BJSpow_ import pow_
+from BJSdiv_norm import div_norm
+from BJSdiv_flr import div_flr
