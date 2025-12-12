876a73eec1a1f6940c45545d884074563dafcc41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 12:06:10 +0000

    Add package radiopadre-client/1.2.4/radiopadre_client-1.2.4-py3-none-any.whl

---
+++ packages/radiopadre-client/radiopadre_client-1.2.4-py3-none-any.whl/iglesia/helpers.py
@@ -0,0 +1,274 @@
+import os, sys, subprocess, atexit, traceback, getpass, tempfile, psutil, stat, uuid
+from radiopadre_client.config import RADIOPADRE_VENV, NUM_PORTS
+
+import iglesia
+from iglesia import PadreError
+from .utils import find_which, chdir, find_unused_ports, DEVZERO, DEVNULL, \
+    message, warning, error, debug
+from . import logger
+
+_child_processes = []
+
+_child_resources = []
+
+
+def init_helpers(radiopadre_base, verbose=False, run_http=True, interactive=True, certificate=None):
+    """Starts up helper processes, if they are not already running"""
+    # set ports, else allocate ports
+    selected_ports = os.environ.get('RADIOPADRE_SELECTED_PORTS')
+    if selected_ports:
+        selected_ports = list(map(int, selected_ports.strip().split(":")))
+        debug(f"  ports configured as {selected_ports}")
+    else:
+        selected_ports = find_unused_ports(NUM_PORTS)
+        debug(f"  ports selected: {selected_ports}")
+
+    userside_ports = os.environ.get('RADIOPADRE_USERSIDE_PORTS')
+    if userside_ports:
+        userside_ports = list(map(int, userside_ports.strip().split(":")))
+        debug(f"  userside ports configured as {userside_ports}")
+    else:
+        userside_ports = selected_ports
+        debug(f"  userside ports are the same")
+
+    nbconvert = bool(os.environ.get('RADIOPADRE_NBCONVERT'))
+    iglesia.set_userside_ports(selected_ports if nbconvert else userside_ports)
+
+    jupyter_port, helper_port, http_port, carta_port, carta_ws_port, wetty_port = selected_ports
+
+    # JS9 init -- check if it's installed
+    js9_path = ""
+    js9_status_file = f"{sys.prefix}/js9status"
+    if os.path.exists(js9_status_file):
+        js9_path = open(js9_status_file).read()
+        if not os.path.exists(js9_path) or not js9_path.endswith("/js9-www"):
+            js9_path = ""
+
+    iglesia.JS9_DIR = os.environ.setdefault('RADIOPADRE_JS9_DIR', js9_path)
+
+    # accumulates rewrite rules for HTTP server
+    # add radiopadre/html/ to rewrite as /radiopadre-www/
+    http_rewrites = [f"/radiopadre-www/={radiopadre_base}/radiopadre/html/",
+                     f"/js9-www/={iglesia.JS9_DIR}/"]
+
+    # are we running inside a container?
+    in_container = bool(os.environ.get('RADIOPADRE_CONTAINER_NAME'))
+    in_docker = in_container and os.environ.get('RADIOPADRE_DOCKER') == 'True'
+
+    if verbose:
+        stdout, stderr = sys.stdout, sys.stderr
+    else:
+        stdout, stderr = DEVNULL, logger.logfile
+
+    ## is this even needed?
+    # import notebook
+    # http_rewrites.append("/js9-www/={}/".format(JS9_DIR))
+    # http_rewrites.append(
+    #     "/js9colormaps.js={}/static/js9colormaps.js".format(os.path.dirname(notebook.__file__)))
+    #
+    global _child_processes
+    session_id = os.environ.get('RADIOPADRE_SESSION_ID')
+
+    # # run wetty
+    # if interactive:
+    #     if 'RADIOPADRE_WETTY_PID' not in os.environ:
+    #         wetty = find_which("wetty")
+    #         if not wetty:
+    #             raise PadreError("unable to find wetty")
+    #         session_id = os.environ.get('RADIOPADRE_SESSION_ID')
+    #         wettycfg = tempfile.NamedTemporaryFile("wt")
+    #         os.chmod(wettycfg.name, stat.S_IRUSR)
+    #         wettycfg.write(f"""{{
+    #                 'server': {{ 'base': '/{session_id}/wetty' }},
+    #             }}
+    #         """)
+    #         wettycfg.flush()
+    #         _child_resources.append(wettycfg)
+    #         # message(f"Starting {wetty} on port {wetty_port} with config file {wettycfg.name} and base {session_id}")
+    #         wetty_opts = [wetty,
+    #                 "--conf", wettycfg.name, 
+    #                 "--ssh-host", "localhost",
+    #                 "--ssh-port", "22",
+    #                 "--ssh-user", getpass.getuser(),
+    #                 "--ssh-auth", "publickey,password",
+    #                 "--port", str(wetty_port),
+    #                 "--allow-iframe",
+    #             ]
+    #         if certificate:
+    #             wetty_opts += ["--ssl-key", certificate, "--ssl-cert", certificate]
+    #         message(f"Starting: {' '.join(wetty_opts)}")
+    #         _child_processes.append(subprocess.Popen(wetty_opts))
+    #         os.environ['RADIOPADRE_WETTY_PID'] = str(_child_processes[-1].pid)
+    #         message("  started as PID {}".format(_child_processes[-1].pid))
+    #     else:
+    #         debug("wetty should be running (pid {})".format(os.environ["RADIOPADRE_WETTY_PID"]))
+
+    # run JS9 helper
+    if interactive and iglesia.JS9_DIR:
+        if 'RADIOPADRE_JS9HELPER_PID' not in os.environ:
+            try:
+                js9helper = iglesia.JS9_DIR + "/js9Helper.js"
+
+                if not os.path.exists(iglesia.JS9_DIR):
+                    raise PadreError(f"{iglesia.JS9_DIR} does not exist")
+                if not os.path.exists(js9helper):
+                    raise PadreError(f"{js9helper} does not exist")
+
+                js9prefs = iglesia.SESSION_DIR + "/js9prefs.js"
+                if not in_container:
+                    # create JS9 settings file (in container mode, this is created and mounted inside container already)
+                    open(js9prefs, "w").write(f"JS9Prefs.globalOpts.helperPort = {iglesia.JS9HELPER_PORT};\n")
+                    debug(f"  writing {js9prefs} with helperPort={iglesia.JS9HELPER_PORT}")
+
+                # message(f"Starting {js9helper} on port {helper_port} in {iglesia.SHADOW_ROOTDIR}")
+                nodejs = find_which("nodejs") or find_which("node")
+                if not nodejs:
+                    raise PadreError("unable to find nodejs or node -- can't run js9helper.")
+                try:
+                    js9_opts = [nodejs.strip(), js9helper,
+                                f'{{"helperPort": {helper_port}, "debug": {iglesia.VERBOSE}, ' +
+                                f'"fileTranslate": ["^(http://localhost:[0-9]+/[0-9a-f]+{iglesia.ABSROOTDIR}|/static/)", ""] }}']
+                    message(f"Starting in {iglesia.SHADOW_ROOTDIR}: {' '.join(js9_opts)}")
+                    with chdir(iglesia.SHADOW_ROOTDIR):
+                        _child_processes.append(subprocess.Popen(js9_opts, 
+                                                stdin=DEVZERO, stdout=stdout, stderr=stderr))
+                        os.environ['RADIOPADRE_JS9HELPER_PID'] = str(_child_processes[-1].pid)
+                        message("  started as PID {}".format(_child_processes[-1].pid))
+                except Exception as exc:
+                    error(f"error running {nodejs} {js9helper}: {exc}")
+            except PadreError:
+                pass
+        else:
+            debug("JS9 helper should be running (pid {})".format(os.environ["RADIOPADRE_JS9HELPER_PID"]))
+
+    if run_http:
+        if 'RADIOPADRE_HTTPSERVER_PID' not in os.environ:
+            message(f"Starting HTTP server process in {iglesia.SHADOW_HOME} on port {http_port}")
+            server = find_which("radiopadre-http-server.py")
+            if server:
+                server_opts = [server, str(http_port)] + http_rewrites
+                if certificate:
+                    server_opts.append(certificate)
+                if in_docker:
+                    server_opts.append("0.0.0.0")
+                message(f"Starting in {iglesia.SHADOW_HOME}: {' '.join(server_opts)}")
+                with chdir(iglesia.SHADOW_HOME):
+                    _child_processes.append(subprocess.Popen(server_opts, stdin=DEVZERO)) #,  stdout=stdout, stderr=stderr))
+                    os.environ['RADIOPADRE_HTTPSERVER_PID'] = str(_child_processes[-1].pid)
+                    message("  started as PID {}".format(_child_processes[-1].pid))
+            else:
+                error("HTTP server script radiopadre-http-server.py not found, functionality will be restricted")
+        else:
+            debug("HTTP server should be running (pid {})".format(os.environ["RADIOPADRE_HTTPSERVER_PID"]))
+
+    if interactive:
+        if 'RADIOPADRE_CARTA_PID' not in os.environ:
+            # find CARTA backend or CARTA app
+            carta_exec = find_which('carta_backend')
+
+            if not carta_exec or not os.path.exists(carta_exec):
+                warning(f"CARTA backend not found, omitting")
+            else:
+                # assume 2.x or higher (i.e. carta_backend is in use)
+                iglesia.CARTA_VERSION = ">=2"
+
+                carta_dir = os.environ.get('RADIOPADRE_CARTA_DIR') or os.path.dirname(os.path.dirname(carta_exec))
+                message(f"Running CARTA {iglesia.CARTA_VERSION} backend {carta_exec} (in dir {carta_dir})")
+                carta_env = None
+
+                carta_dir = iglesia.ABSROOTDIR
+                cmdline = [carta_exec, f"--port={carta_port}", "--no_browser", # "--debug_no_auth",
+                            f"--top_level_folder={iglesia.ABSROOTDIR}" ]
+                # explicit frontend for packaged versions
+                if not carta_exec.endswith("appimage"): 
+                    cmdline.append(f"--frontend_folder=/usr/share/carta/frontend")
+                carta_stdout, carta_stderr = sys.stdout, sys.stderr
+                # use our session ID as the auth token for CARTA
+                carta_env = os.environ.copy()
+                carta_env['CARTA_AUTH_TOKEN'] = str(uuid.UUID(session_id))
+
+                message(f"Starting: {' '.join(cmdline)}")
+                with chdir(carta_dir):
+                    _child_processes.append(subprocess.Popen(cmdline, stdin=subprocess.PIPE,  stdout=carta_stdout, stderr=carta_stderr, shell=False, env=carta_env))
+                    os.environ['RADIOPADRE_CARTA_PID'] = str(_child_processes[-1].pid)
+                    ## doesn't exit cleanly, let it be eaten rather
+                    # atexit.register(_exit_carta, _child_processes[-1])
+                    message("  started as PID {}".format(_child_processes[-1].pid))
+        else:
+            debug("CARTA backend should be running (pid {})".format(os.environ["RADIOPADRE_CARTA_PID"]))
+
+
+def _exit_carta(proc):
+    try:
+        if proc.poll() is None:
+            message("Asking CARTA backend (pid {}) to exit".format(proc.pid))
+            try:
+                proc.communicate("q\n")
+            except TypeError: # because fuck you Python
+                proc.communicate(b"q\n")
+        else:
+            message("CARTA backend already exited with code {}".format(proc.returncode))
+    except Exception:
+        err = traceback.format_exc()
+        error(f"Exception in _exit_carta: {err}")
+
+
+def register_helpers(*procs):
+    """Registers another helper process started externally, mainly to make sure it is killed properly"""
+    global _child_processes
+    _child_processes += list(procs)
+
+def kill_helpers():
+    global _child_processes, _child_resources
+    _child_resources = []
+    try:
+        if _child_processes:
+            message("Terminating remaining child processes ({})".format(
+                    " ".join([str(proc.pid) for proc in _child_processes])))
+            for proc in _child_processes:
+                if proc.poll() is None:
+                    proc.terminate()
+                else:
+                    message("  child {} already exited with code {}".format(proc.pid, proc.returncode))
+            while _child_processes:
+                proc = _child_processes.pop()
+                proc.wait()
+        else:
+            debug("No child processes remaining")
+    except Exception:
+        err = traceback.format_exc()
+        error(f"Exception in kill_helpers: {err}")
+
+## This was not brutal enough
+# atexit.register(kill_helpers)
+
+def eat_children():
+    # ask children to terminate
+    procs = psutil.Process().children(recursive=True)
+    if not procs:
+        return
+
+    message("Terminating {} remaining child processes".format(len(procs)))
+    debug(" ".join(map(str, procs)))
+    for p in procs:
+        try:
+            p.terminate()
+        except psutil.NoSuchProcess:
+            pass
+
+    def on_terminate(proc):
+        debug("  Child process {} terminated with exit code {}".format(proc, proc.returncode))
+
+    gone, alive = psutil.wait_procs(procs, timeout=1, callback=on_terminate)
+
+    # if any are still alive, kill them
+    if alive:
+        message("Killing {} lingering child processes".format(len(alive)))
+        debug(" ".join(map(str, alive)))
+        for p in alive:
+            try:
+                p.kill()
+            except psutil.NoSuchProcess:
+                pass
+
+atexit.register(eat_children)
---
+++ packages/radiopadre-client/radiopadre_client-1.2.4-py3-none-any.whl/radiopadre_client/backends/singularity.py
@@ -0,0 +1,231 @@
+import os, subprocess, sys, time, re, calendar
+import datetime, getpass
+
+from iglesia.utils import message, warning, error, bye, make_dir, make_radiopadre_dir, shell, DEVNULL, INPUT, check_output
+from radiopadre_client import config
+
+singularity = None
+has_docker = None
+
+from . import docker
+from .docker import get_session_info_dir, save_session_info, _run_container, _init_session_dir, _collect_runscript_arguments
+import iglesia
+
+def init(binary, docker_binary=None):
+    global singularity, has_docker
+    singularity = binary
+    _init_session_dir()
+    if docker_binary:
+        # check that we actually have docker permissions
+        if check_output(docker_binary + " ps") is None:
+            message("can't connect to docker daemon, will proceed without docker")
+            has_docker = None
+        else:
+            has_docker = docker_binary
+            docker.init(docker_binary)
+
+def read_session_info(container_name):
+    raise NotImplementedError("not available in singularity mode")
+
+def list_sessions():
+    return {}
+
+def identify_session(session_dict, arg):
+    raise NotImplementedError("not available in singularity mode")
+
+def kill_sessions(session_dict, session_ids):
+    raise NotImplementedError("not available in singularity mode")
+
+def get_singularity_image(docker_image):
+    dir = config.SINGULARITY_IMAGE_DIR or os.environ.get('RADIOPADRE_SINGULARITY_IMAGE_DIR') or iglesia.RADIOPADRE_DIR
+    return "{}/{}.simg".format(dir, docker_image.replace("/", "_"))
+
+def update_installation(rebuild=False, docker_pull=True):
+    global docker_image
+    global singularity_image
+    docker_image = config.DOCKER_IMAGE
+    singularity_image = os.path.expanduser(get_singularity_image(docker_image))
+    # this will be True if we need to build the image
+    build_image = False
+
+    # clearly true if no image
+    if not os.path.exists(singularity_image):
+        if config.SINGULARITY_AUTO_BUILD or config.SINGULARITY_REBUILD:
+            message(f"Singularity image {singularity_image} does not exist, will build")
+            build_image = True
+        else:
+            error(f"Singularity image {singularity_image} does not exist, and auto-build is disabled")
+            bye(f"Re-run with --singularity-rebuild or --singularity-auto-build to proceed")
+    # also true if rebuild forced by flags or config or command line
+    elif rebuild or config.SINGULARITY_REBUILD:
+        config.SINGULARITY_AUTO_BUILD = build_image = True
+        message(f"--singularity-rebuild specified, removing singularity image {singularity_image}")
+
+    # pull down docker image first
+    if has_docker and docker_pull:
+        message("Checking docker image (from which our singularity image is built)")
+        docker.update_installation(enable_pull=True)
+    # if we're not forced to build yet, check for an update
+    if config.UPDATE and not build_image:
+        if has_docker:
+            # check timestamp of docker image
+            docker_image_time = None
+            output = check_output(f"{has_docker} image inspect {docker_image} -f '{{{{ .Created }}}}'").strip()
+            message(f"  docker image timestamp is {output}")
+            # in Python 3.7 we have datetime.fromisoformat(date_string), but for now we muddle:
+            match = output and re.match("(^.*)[.](\d+)Z", output)
+            if match:
+                try:
+                    docker_image_time = calendar.timegm(time.strptime(match.group(1), "%Y-%m-%dT%H:%M:%S"))
+                    docker_image_time = datetime.datetime.utcfromtimestamp(docker_image_time)
+                except ValueError:
+                    pass
+            sing_image_time = datetime.datetime.utcfromtimestamp(os.path.getmtime(singularity_image))
+            message("  singularity image timestamp is {}".format(sing_image_time.isoformat()))
+            if docker_image_time is None:
+                warning(f"can't parse docker image timestamp '{output}', rebuilding {singularity_image} just in case")
+                build_image = True
+            elif docker_image_time > sing_image_time:
+                warning(f"rebuilding outdated singularity image {singularity_image}")
+                build_image = True
+            else:
+                message(f"singularity image {singularity_image} is up-to-date")
+        else:
+            message(f"--update specified, will force-rebuild {singularity_image} from registry")
+            message(f"(If docker was available, we could check timestamps to see if this step is necessary. But we can't.)")
+            build_image = True
+
+    # now build if needed
+    if build_image:
+        warning(f"Rebuilding singularity image from docker://{docker_image}")
+        warning(f"  (This may take a few minutes....)")
+        singularity_image_new = os.path.splitext(singularity_image)[0] + ".new.simg"
+        if os.path.exists(singularity_image_new):
+            os.unlink(singularity_image_new)
+        cmd = [singularity, "build", singularity_image_new, f"docker://{docker_image}"]
+        message("running " + " ".join(cmd))
+        try:
+            subprocess.check_call(cmd)
+        except subprocess.CalledProcessError as exc:
+            if config.IGNORE_UPDATE_ERRORS:
+                if os.path.exists(singularity_image):
+                    warning("singularity build failed but --ignore-update-errors is set, proceeding with old image")
+                else:
+                    error("singularity build failed, --ignore-update-errors is set, but we have no older image")
+                    raise
+            else:
+                raise
+        if not os.path.exists(singularity_image_new):
+            if config.IGNORE_UPDATE_ERRORS and os.path.exists(singularity_image):
+                warning("singularity build did not return a error, but the new image did not appear")
+                warning("since --ignore-update-errors is set, we're proceeding with the old image")
+            else:
+                bye("singularity build did not return a error, but the new image did not appear")
+        # move old image
+        else:
+            message(f"Build successful, renaming to {singularity_image}")
+            os.rename(singularity_image_new, singularity_image)
+    else:
+        message(f"Using existing radiopadre singularity image {singularity_image}")
+
+    # not supported with Singularity
+    config.CONTAINER_PERSIST = False
+
+    # config.CONTAINER_DEBUG = False
+
+
+def start_session(container_name, selected_ports, userside_ports, notebook_path, browser_urls, run_browser=False):
+    from iglesia import ABSROOTDIR, SHADOW_SESSION_DIR
+    radiopadre_dir = make_radiopadre_dir()
+    docker_local = make_dir(radiopadre_dir + "/.docker-local")
+    js9_tmp = make_dir(radiopadre_dir + "/.js9-tmp")
+    session_info_dir = get_session_info_dir(container_name)
+    homedir = os.path.expanduser("~")
+
+    docker_opts = []
+
+    if homedir != ABSROOTDIR:
+        docker_opts += ["--workdir", ABSROOTDIR]
+
+    # add other options
+    if config.SINGULARITY_OPTIONS:
+        opts = config.SINGULARITY_OPTIONS.split(",")
+        if not opts[0].startswith("-"):
+            opts[0] = '--' + opts[0]
+        docker_opts += opts
+
+    # setup mounts for work dir and home dir, if needed
+    docker_opts += [
+        "-B", "{}:{}{}".format(ABSROOTDIR, ABSROOTDIR, ""), # ":ro" if orig_rootdir else ""),
+        "-B", "{}:{}".format(radiopadre_dir, radiopadre_dir),
+        # hides /home/user/.local, which if exposed, can confuse jupyter and ipython
+        "-B", "{}:{}".format(docker_local, os.path.realpath(os.path.join(homedir, ".local"))),
+        # mount session info directory (needed to serve e.g. js9prefs.js)
+        "-B", "{}:{}".format(session_info_dir, SHADOW_SESSION_DIR),
+        # mount a writeable tmp dir for the js9 install -- needed by js9helper
+        "-B", "{}:/.radiopadre/venv/js9-www/tmp".format(js9_tmp),
+        # mount home session dir
+        "-B", f"{homedir}/.radiopadre-session:{homedir}/.radiopadre-session"
+    ]
+    if config.CONTAINER_DEV:
+        if os.path.isdir(config.CLIENT_INSTALL_PATH):
+            docker_opts += ["-B", "{}:/radiopadre-client".format(config.CLIENT_INSTALL_PATH)]
+        if os.path.isdir(config.SERVER_INSTALL_PATH):
+            docker_opts += ["-B", "{}:/radiopadre".format(config.SERVER_INSTALL_PATH)]
+
+    # if not config.CONTAINER_DEBUG:
+    #     command = [singularity, "instance.start"] + docker_opts + \
+    #               [singularity_image, container_name]
+    #     message("running {}".format(" ".join(map(str, command))))
+    #     subprocess.call(command)
+    #     docker_opts = [singularity, "exec", "instance://{}".format(container_name)]
+    # else:
+    #     docker_opts = [singularity, "exec" ] + docker_opts + [singularity_image]
+    docker_opts = [singularity, "run" ] + docker_opts + [singularity_image]
+    container_ports = selected_ports
+
+    # build up command-line arguments
+    docker_opts += _collect_runscript_arguments(container_ports + userside_ports)
+
+    # add environment
+    docker_opts += ["--env", f"JUPYTER_DATA_DIR=/tmp/{getpass.getuser()}-jupyter",
+                    "--env", f"IPYTHONDIR=/tmp/{getpass.getuser()}-ipython",
+                    "--env", f"XDG_RUNTIME_DIR="]
+    os.environ["RADIOPADRE_CONTAINER_NAME"] = container_name
+    for name, value in os.environ.items():
+        if name.startswith("RADIOPADRE_"):
+            docker_opts += ["--env", f"{name}={value}"]
+
+    if notebook_path:
+        docker_opts.append(notebook_path)
+
+    _run_container(container_name, docker_opts, jupyter_port=selected_ports[0], 
+                    browser_urls=browser_urls, run_browser=run_browser, singularity=True)
+
+    if config.NBCONVERT:
+        return
+
+    try:
+        while True:
+            a = INPUT("Type 'exit' to kill the container session: ")
+            if a.lower() == 'exit':
+                sys.exit(0)
+    except BaseException as exc:
+        if type(exc) is KeyboardInterrupt:
+            message("Caught Ctrl+C")
+            status = 1
+        elif type(exc) is SystemExit:
+            status = getattr(exc, 'code', 0)
+            message("Exiting with status {}".format(status))
+        else:
+            message("Caught exception {} ({})".format(exc, type(exc)))
+            status = 1
+        if status:
+            message("Killing the container")
+            subprocess.call([singularity, "instance.stop", singularity_image, container_name], stdout=DEVNULL)
+        sys.exit(status)
+
+
+def kill_container(name):
+    singularity_image = get_singularity_image(config.DOCKER_IMAGE)
+    shell(f"{singularity} instance.stop {singularity_image} {name}")
