5bf7f5ba4e7dc34137373e4a577f5bb00b348f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 14:05:05 +0000

    Add package neon-skill-speed-test/1.0.5a3/neon_skill_speed_test-1.0.5a3.tar.gz

---
+++ packages/neon-skill-speed-test/neon_skill_speed_test-1.0.5a3.tar.gz/neon_skill_speed_test-1.0.5a3/README.md
@@ -0,0 +1,35 @@
+# <img src='https://0000.us/klatchat/app/files/neon_images/icons/neon_skill.png' card_color="#FF8600" width="50" style="vertical-align:bottom">Speed Test
+
+## Summary
+
+Skill used to test your internet speed
+
+## Description
+
+This skill uses speedtest.net to check your internet speed
+
+## Examples
+
+Ask Neon to start a test:
+
+- "Run a speed test."
+- "How fast is my internet?"
+
+## Contact Support
+
+Use the [link](https://neongecko.com/ContactUs) or [submit an issue on GitHub](https://help.github.com/en/articles/creating-an-issue)
+
+## Category
+**Daily**
+Productivity
+
+## Credits
+[NeonDaniel](https://github.com/NeonDaniel)
+[NeonGeckoCom](https://github.com/NeonGeckoCom)
+[reginaneon](https://github.com/reginaneon)
+
+## Tags
+#NeonGecko Original
+#NeonAI
+#Internet
+#Utilities
---
+++ packages/neon-skill-speed-test/neon_skill_speed_test-1.0.5a3.tar.gz/neon_skill_speed_test-1.0.5a3/test/test_skill.py
@@ -0,0 +1,68 @@
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
+# All trademark and other rights reserved by their respective owners
+# Copyright 2008-2025 Neongecko.com Inc.
+# Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
+# Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
+# BSD-3 License
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import unittest
+
+from mock import Mock
+from ovos_bus_client import Message
+from neon_minerva.tests.skill_unit_test_base import SkillTestCase
+
+from neon_skill_speed_test.models import SpeedTestResult
+
+
+class TestSkillMethods(SkillTestCase):
+    def test_handle_run_speed_test(self):
+        on_notification_set = Mock()
+        on_notification_clear = Mock()
+        self.skill.bus.on(
+            "ovos.notification.api.set.controlled", on_notification_set
+        )
+        self.skill.bus.on(
+            "ovos.notification.api.remove.controlled", on_notification_clear
+        )
+        self.skill.handle_run_speed_test(Message("test"))
+        self.skill.speak_dialog.assert_any_call("start_test")
+        args = self.skill.speak_dialog.call_args
+        self.assertEqual(args[0][0], "results")
+        self.assertEqual(set(args[0][1].keys()), {"down", "up", "ping"})
+        on_notification_set.assert_called_once()
+        on_notification_clear.assert_called_once()
+
+    def test_api_run_speed_test(self):
+        result = self.skill.run_speed_test()
+        self.assertIsInstance(result, SpeedTestResult)
+        self.assertIsInstance(result.download, float)
+        self.assertGreater(result.download, 1000)
+        self.assertIsInstance(result.upload, float)
+        self.assertGreater(result.upload, 1000)
+        self.assertIsInstance(result.ping, float)
+        self.assertLess(result.ping, 1000)
+
+
+if __name__ == "__main__":
+    unittest.main()
