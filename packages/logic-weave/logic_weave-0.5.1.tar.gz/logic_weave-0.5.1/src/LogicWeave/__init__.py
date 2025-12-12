7b3cf101bd21cfc67968232f2503b35886535290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 11:04:54 +0000

    Add package logic-weave/0.5.1/logic_weave-0.5.1.tar.gz

---
+++ packages/logic-weave/logic_weave-0.5.1.tar.gz/logic_weave-0.5.1/src/LogicWeave/__init__.py
@@ -0,0 +1,4 @@
+from .logicweave import LogicWeave, GPIO, UART, I2C, SPI, GpioFunction
+from .web import WebLogicWeave, WebLogicWeaveCore
+from .logicweave_core import LogicWeaveCore
+from .proto_gen import logicweave_pb2, logicweave_core_pb2
