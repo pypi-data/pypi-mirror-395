69ab1fac581eb34f07e45d09d6a2fb10473975ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:32:21 +0000

    Add package word-segment-grpc-client/0.1.7/word_segment_grpc_client-0.1.7.tar.gz

---
+++ packages/word-segment-grpc-client/word_segment_grpc_client-0.1.7.tar.gz/word_segment_grpc_client-0.1.7/word_segment_pb2.pyi
@@ -0,0 +1,18 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class SegmentRequest(_message.Message):
+    __slots__ = ("input",)
+    INPUT_FIELD_NUMBER: _ClassVar[int]
+    input: str
+    def __init__(self, input: _Optional[str] = ...) -> None: ...
+
+class SegmentResponse(_message.Message):
+    __slots__ = ("output",)
+    OUTPUT_FIELD_NUMBER: _ClassVar[int]
+    output: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, output: _Optional[_Iterable[str]] = ...) -> None: ...
