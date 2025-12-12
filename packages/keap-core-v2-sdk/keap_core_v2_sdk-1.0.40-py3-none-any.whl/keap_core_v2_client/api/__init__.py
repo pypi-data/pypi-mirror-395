4cf46a73f88e873b2723a25ae7edbeecd979bb6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 15:05:49 +0000

    Add package keap-core-v2-sdk/1.0.40/keap_core_v2_sdk-1.0.40-py3-none-any.whl

---
+++ packages/keap-core-v2-sdk/keap_core_v2_sdk-1.0.40-py3-none-any.whl/keap_core_v2_client/api/__init__.py
@@ -0,0 +1,46 @@
+# flake8: noqa
+
+# import apis into api package
+from keap_core_v2_client.api.affiliate_api import AffiliateApi
+from keap_core_v2_client.api.automation_api import AutomationApi
+from keap_core_v2_client.api.automation_category_api import AutomationCategoryApi
+from keap_core_v2_client.api.business_profile_api import BusinessProfileApi
+from keap_core_v2_client.api.campaign_api import CampaignApi
+from keap_core_v2_client.api.category_discounts_api import CategoryDiscountsApi
+from keap_core_v2_client.api.company_api import CompanyApi
+from keap_core_v2_client.api.contact_api import ContactApi
+from keap_core_v2_client.api.email_api import EmailApi
+from keap_core_v2_client.api.email_address_api import EmailAddressApi
+from keap_core_v2_client.api.files_api import FilesApi
+from keap_core_v2_client.api.free_trial_discounts_api import FreeTrialDiscountsApi
+from keap_core_v2_client.api.lead_score_api import LeadScoreApi
+from keap_core_v2_client.api.lead_source_categories_api import LeadSourceCategoriesApi
+from keap_core_v2_client.api.lead_source_expenses_api import LeadSourceExpensesApi
+from keap_core_v2_client.api.lead_source_recurring_expenses_api import LeadSourceRecurringExpensesApi
+from keap_core_v2_client.api.lead_sources_api import LeadSourcesApi
+from keap_core_v2_client.api.locale_api import LocaleApi
+from keap_core_v2_client.api.merchants_api import MerchantsApi
+from keap_core_v2_client.api.note_api import NoteApi
+from keap_core_v2_client.api.opportunity_api import OpportunityApi
+from keap_core_v2_client.api.order_total_discounts_api import OrderTotalDiscountsApi
+from keap_core_v2_client.api.orders_api import OrdersApi
+from keap_core_v2_client.api.payment_method_configs_api import PaymentMethodConfigsApi
+from keap_core_v2_client.api.payment_methods_api import PaymentMethodsApi
+from keap_core_v2_client.api.product_categories_api import ProductCategoriesApi
+from keap_core_v2_client.api.product_discounts_api import ProductDiscountsApi
+from keap_core_v2_client.api.product_interest_bundles_api import ProductInterestBundlesApi
+from keap_core_v2_client.api.products_api import ProductsApi
+from keap_core_v2_client.api.referral_api import ReferralApi
+from keap_core_v2_client.api.reporting_api import ReportingApi
+from keap_core_v2_client.api.sales_api import SalesApi
+from keap_core_v2_client.api.settings_api import SettingsApi
+from keap_core_v2_client.api.shipping_api import ShippingApi
+from keap_core_v2_client.api.shipping_discounts_api import ShippingDiscountsApi
+from keap_core_v2_client.api.subscription_plans_api import SubscriptionPlansApi
+from keap_core_v2_client.api.subscriptions_api import SubscriptionsApi
+from keap_core_v2_client.api.tags_api import TagsApi
+from keap_core_v2_client.api.task_api import TaskApi
+from keap_core_v2_client.api.user_groups_api import UserGroupsApi
+from keap_core_v2_client.api.users_api import UsersApi
+from keap_core_v2_client.api.webforms_api import WebformsApi
+
