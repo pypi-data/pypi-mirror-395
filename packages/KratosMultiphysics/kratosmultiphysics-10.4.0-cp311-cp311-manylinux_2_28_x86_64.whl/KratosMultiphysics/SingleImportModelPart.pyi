38f70df92bfe5a24f47c4820016738d47d0e74ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:34 +0000

    Add package KratosMultiphysics/10.4.0/kratosmultiphysics-10.4.0-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/KratosMultiphysics/kratosmultiphysics-10.4.0-cp311-cp311-manylinux_2_28_x86_64.whl/KratosMultiphysics/SingleImportModelPart.pyi
@@ -0,0 +1,5 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics as Kratos
+
+def Import(arg0: Kratos.ModelPart, arg1, arg2: str) -> None:
+    """Import(arg0: Kratos.ModelPart, arg1: Kratos::Parameters, arg2: str) -> None"""
