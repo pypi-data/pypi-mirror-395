f23844e8bce34e621164e6bb250c8af883e3f98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:34 +0000

    Add package oddorevenfebin/0.1.0/oddorevenfebin-0.1.0.tar.gz

---
+++ packages/oddorevenfebin/oddorevenfebin-0.1.0.tar.gz/oddorevenfebin-0.1.0/oddorevenfebin/checker.py
@@ -0,0 +1,7 @@
+def is_even(num: int) -> bool:
+    """Return True if num is even, else False."""
+    return num % 2 == 0
+
+def is_odd(num: int) -> bool:
+    """Return True if num is odd, else False."""
+    return num % 2 != 0
