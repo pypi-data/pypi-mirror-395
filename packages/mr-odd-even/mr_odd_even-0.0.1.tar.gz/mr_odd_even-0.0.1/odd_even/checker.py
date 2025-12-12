1390c1516c39f3ca63e0b8dfd643ce58db030198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:41 +0000

    Add package mr-odd-even/0.0.1/mr_odd_even-0.0.1.tar.gz

---
+++ packages/mr-odd-even/mr_odd_even-0.0.1.tar.gz/mr_odd_even-0.0.1/odd_even/checker.py
@@ -0,0 +1,8 @@
+def is_even(number: int) -> bool:
+    """Return True if number is even."""
+    return number % 2 == 0
+
+
+def is_odd(number: int) -> bool:
+    """Return True if number is odd."""
+    return number % 2 != 0
