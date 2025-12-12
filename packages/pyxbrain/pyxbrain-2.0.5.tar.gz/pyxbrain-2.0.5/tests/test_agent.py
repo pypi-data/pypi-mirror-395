61f0be26718b36284613cdbe44d44fd965333b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 12:05:46 +0000

    Add package pyxbrain/2.0.5/pyxbrain-2.0.5.tar.gz

---
+++ packages/pyxbrain/pyxbrain-2.0.5.tar.gz/pyxbrain-2.0.5/tests/test_agent.py
@@ -0,0 +1,66 @@
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 获取项目根目录 (即 tests 的上一级)
+project_root = os.path.dirname(current_dir)
+# 把根目录加入到 Python 搜索路径中
+sys.path.insert(0, project_root)
+from xbrain.core.xbrain_agent import Agent, WorkFlow
+from xbrain.utils.openai_utils import chat,chat_video
+import unittest
+class TestAgent(unittest.TestCase):
+    def test_agent(self):
+      
+        class A(Agent):
+            def run(self, input):
+                print("接收", input)
+     
+        class B(Agent):
+            def run(self, input):
+                print("接收", input)
+                return "agent2 输出"
+                
+        workflow = WorkFlow([A, B])
+        xbrain_res = workflow.run("test input")
+        assert xbrain_res == "agent2 输出"
+
+    def test_chat(self):
+        class A(Agent):
+            def run(self, input):
+                res = chat([{"role": "user", "content": input}], "你是一个智能助手")
+                return res
+        class B(Agent): 
+            def run(self, input):
+                res = chat([{"role": "user", "content": input}], "你是一个智能助手")
+                return res
+
+        workflow = WorkFlow([A, B])
+        xbrain_res = workflow.run("你好")
+        print(xbrain_res)
+
+    def test_global_context(self):
+        class A(Agent):
+            def run(self, input):
+                self.global_context["a"] = "a"
+                return "agent1 输出"
+        class B(Agent):
+            def run(self, input):
+                return self.global_context["a"]
+                
+        workflow = WorkFlow([A, B])
+        xbrain_res = workflow.run("test input")
+        assert xbrain_res == "a"
+
+
+
+    def test_workflow_with_args(self):
+        class A(Agent):
+            def run(self, input, arg1, arg2):
+                return f"{input} {arg1} {arg2}"
+        class B(Agent):
+            def run(self, input):
+                return f"agent2 输出 {input}"
+                
+        workflow = WorkFlow([A, B])
+        xbrain_res = workflow.run("test input", "arg1", "arg2")
+        assert xbrain_res == "agent2 输出 test input arg1 arg2"
---
+++ packages/pyxbrain/pyxbrain-2.0.5.tar.gz/pyxbrain-2.0.5/xbrain/core/xbrain_tool.py
@@ -0,0 +1,49 @@
+import openai
+import json
+import os
+from xbrain.core.context import context, Type, ActionRecord
+tools = []
+
+class Tool:
+    def __init__(self, model, hit_condition=None):
+        self.model = model
+        self.hit_condition = hit_condition
+
+    def __call__(self, func):
+        # 利用 openai 官方的转换函数，提取 name
+        function = openai.pydantic_function_tool(self.model)
+        tools.append({
+            "name": function["function"]["name"],
+            "description": function["function"].get("description", ""),
+            "hit_condition": self.hit_condition,
+            "model": self.model,
+            "func": func,
+            "path": func.__code__.co_filename
+        })
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            return result
+
+        return wrapper
+
+def _get_tool_by_name(name):
+    for tool in tools:
+        if tool["name"] == name:
+            return tool
+    return None
+
+def run_tool(openai_res):
+    res = []
+    for tool_call in openai_res.tool_calls:
+        info = _get_tool_by_name(tool_call.function.name)
+        tool_func = info["func"]
+        run_res = tool_func(**json.loads(tool_call.function.arguments))
+        run_res = run_res if run_res is not None else ""
+        print("\033[90mrun action: \n", 
+              "action name: ", tool_call.function.name, "\n",
+              "action arguments: ", json.loads(tool_call.function.arguments), "\033[0m", "\n", 
+              )
+        # 存储到上下文中
+        context[Type.PRE_ACTIONS].append(ActionRecord(tool_call.function.name, json.loads(tool_call.function.arguments)))
+        res.append(run_res)
+    return res
