6beecfefe89bb3fae11c4c568d74c7e7910b82de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:43 +0000

    Add package jararaca/0.3.39/jararaca-0.3.39.tar.gz

---
+++ packages/jararaca/jararaca-0.3.39.tar.gz/jararaca-0.3.39/src/jararaca/presentation/websocket/decorators.py
@@ -0,0 +1,70 @@
+# SPDX-FileCopyrightText: 2025 Lucas S
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from typing import TypedDict, TypeVar, cast
+
+from jararaca.presentation.websocket.base_types import WebSocketMessageBase
+
+DECORATED_CLASS = TypeVar("DECORATED_CLASS")
+
+
+class WebSocketEndpointOptions(TypedDict): ...
+
+
+class WebSocketEndpoint:
+
+    WEBSOCKET_ENDPOINT_ATTR = "__websocket_endpoint__"
+    ORDER_COUNTER = 0
+
+    def __init__(self, path: str, options: WebSocketEndpointOptions = {}) -> None:
+        self.path = path
+        self.options = options
+        WebSocketEndpoint.ORDER_COUNTER += 1
+        self.order = WebSocketEndpoint.ORDER_COUNTER
+
+    @staticmethod
+    def register(cls: DECORATED_CLASS, instance: "WebSocketEndpoint") -> None:
+        setattr(cls, WebSocketEndpoint.WEBSOCKET_ENDPOINT_ATTR, instance)
+
+    @staticmethod
+    def get(cls: DECORATED_CLASS) -> "WebSocketEndpoint | None":
+        if not hasattr(cls, WebSocketEndpoint.WEBSOCKET_ENDPOINT_ATTR):
+            return None
+
+        return cast(
+            WebSocketEndpoint, getattr(cls, WebSocketEndpoint.WEBSOCKET_ENDPOINT_ATTR)
+        )
+
+    def __call__(self, cls: DECORATED_CLASS) -> DECORATED_CLASS:
+        WebSocketEndpoint.register(cls, self)
+        return cls
+
+
+INHERITS_WS_MESSAGE = TypeVar("INHERITS_WS_MESSAGE", bound=WebSocketMessageBase)
+
+
+class RegisterWebSocketMessage:
+
+    REGISTER_WEBSOCKET_MESSAGE_ATTR = "__register_websocket_message__"
+
+    def __init__(self, *message_types: type[INHERITS_WS_MESSAGE]) -> None:
+        self.message_types = message_types
+
+    @staticmethod
+    def register(cls: DECORATED_CLASS, instance: "RegisterWebSocketMessage") -> None:
+        setattr(cls, RegisterWebSocketMessage.REGISTER_WEBSOCKET_MESSAGE_ATTR, instance)
+
+    @staticmethod
+    def get(cls: DECORATED_CLASS) -> "RegisterWebSocketMessage | None":
+        if not hasattr(cls, RegisterWebSocketMessage.REGISTER_WEBSOCKET_MESSAGE_ATTR):
+            return None
+
+        return cast(
+            RegisterWebSocketMessage,
+            getattr(cls, RegisterWebSocketMessage.REGISTER_WEBSOCKET_MESSAGE_ATTR),
+        )
+
+    def __call__(self, cls: DECORATED_CLASS) -> DECORATED_CLASS:
+        RegisterWebSocketMessage.register(cls, self)
+        return cls
---
+++ packages/jararaca/jararaca-0.3.39.tar.gz/jararaca-0.3.39/src/jararaca/scheduler/decorators.py
@@ -0,0 +1,144 @@
+# SPDX-FileCopyrightText: 2025 Lucas S
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import inspect
+from dataclasses import dataclass
+from types import MethodType
+from typing import Any, Awaitable, Callable, TypeVar, cast
+
+from jararaca.reflect.controller_inspect import (
+    ControllerMemberReflect,
+    inspect_controller,
+)
+
+DECORATED_FUNC = TypeVar("DECORATED_FUNC", bound=Callable[..., Any])
+
+
+class ScheduledAction:
+    SCHEDULED_ACTION_ATTR = "__scheduled_action__"
+
+    def __init__(
+        self,
+        cron: str,
+        allow_overlap: bool = False,
+        exclusive: bool = True,
+        timeout: int | None = None,
+        exception_handler: Callable[[BaseException], None] | None = None,
+        name: str | None = None,
+    ) -> None:
+        """
+        :param cron: A string representing the cron expression for the scheduled action.
+        :param allow_overlap: A boolean indicating if the scheduled action should new executions even if the previous one is still running.
+        :param exclusive: A boolean indicating if the scheduled action should be executed in one instance of the application. (Requires a distributed lock provided by a backend)
+        :param exception_handler: A callable that will be called when an exception is raised during the execution of the scheduled action.
+        :param timeout: An integer representing the timeout for the scheduled action in seconds. If the scheduled action takes longer than this time, it will be terminated.
+        :param name: An optional name for the scheduled action, used for filtering which actions to run.
+        """
+        self.cron = cron
+        """
+        A string representing the cron expression for the scheduled action.
+        """
+
+        self.allow_overlap = allow_overlap
+        """
+        A boolean indicating if the scheduled action should new executions even if the previous one is still running.
+        """
+
+        self.exclusive = exclusive
+        """
+        A boolean indicating if the scheduled action should be executed
+        in one instance of the application. (Requires a distributed lock provided by a backend)
+        """
+
+        self.exception_handler = exception_handler
+        """
+        A callable that will be called when an exception is raised during the execution of the scheduled action.
+        """
+
+        self.timeout = timeout
+        """
+        An integer representing the timeout for the scheduled action in seconds.
+        If the scheduled action takes longer than this time, it will be terminated.
+        """
+
+        self.name = name
+        """
+        An optional name for the scheduled action, used for filtering which actions to run.
+        """
+
+    def __call__(self, func: DECORATED_FUNC) -> DECORATED_FUNC:
+        ScheduledAction.register(func, self)
+        return func
+
+    @staticmethod
+    def register(func: DECORATED_FUNC, scheduled_action: "ScheduledAction") -> None:
+        setattr(func, ScheduledAction.SCHEDULED_ACTION_ATTR, scheduled_action)
+
+    @staticmethod
+    def get_scheduled_action(func: DECORATED_FUNC) -> "ScheduledAction | None":
+        if not hasattr(func, ScheduledAction.SCHEDULED_ACTION_ATTR):
+            return None
+
+        return cast(
+            ScheduledAction, getattr(func, ScheduledAction.SCHEDULED_ACTION_ATTR)
+        )
+
+    @staticmethod
+    def get_function_id(
+        func: Callable[..., Any],
+    ) -> str:
+        """
+        Get the function ID of the scheduled action.
+        This is used to identify the scheduled action in the message broker.
+        """
+        return f"{func.__module__}.{func.__qualname__}"
+
+
+@dataclass(frozen=True)
+class ScheduledActionData:
+    spec: ScheduledAction
+    controller_member: ControllerMemberReflect
+    callable: Callable[..., Awaitable[None]]
+
+
+def get_type_scheduled_actions(
+    instance: Any,
+) -> list[ScheduledActionData]:
+
+    _, member_metadata_map = inspect_controller(instance.__class__)
+
+    members: list[tuple[str, MethodType]] = []
+    for name in dir(instance):
+        try:
+            value = getattr(instance, name)
+        except Exception:
+            continue
+
+        if inspect.ismethod(value):
+            members.append((name, value))
+
+    scheduled_actions: list[ScheduledActionData] = []
+
+    for name, member in members:
+        scheduled_action = ScheduledAction.get_scheduled_action(member)
+
+        if scheduled_action is None:
+            continue
+
+        if name not in member_metadata_map:
+            raise Exception(
+                f"Member '{name}' is not a valid controller member in '{instance.__class__.__name__}'"
+            )
+
+        member_metadata = member_metadata_map[name]
+
+        scheduled_actions.append(
+            ScheduledActionData(
+                callable=member,
+                spec=scheduled_action,
+                controller_member=member_metadata,
+            )
+        )
+
+    return scheduled_actions
