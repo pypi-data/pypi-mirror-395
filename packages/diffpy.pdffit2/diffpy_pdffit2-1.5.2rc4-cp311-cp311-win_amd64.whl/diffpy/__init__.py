a0c7dee9922cd12a9dc7b8a1895d93e598b75de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:23 +0000

    Add package diffpy.pdffit2/1.5.2rc4/diffpy_pdffit2-1.5.2rc4-cp311-cp311-win_amd64.whl

---
+++ packages/diffpy.pdffit2/diffpy_pdffit2-1.5.2rc4-cp311-cp311-win_amd64.whl/diffpy/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2008 trustees of the Michigan State University.
+# All rights reserved.
+# (c) 2025 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/diffpy/diffpy.pdffit2/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
