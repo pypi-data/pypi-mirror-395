9cd9ba5f3ecb8065b67e960abfb2ce03a706dde4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:36 +0000

    Add package fps_kernel_subprocess/0.1.7/fps_kernel_subprocess-0.1.7-py3-none-any.whl

---
+++ packages/fps_kernel_subprocess/fps_kernel_subprocess-0.1.7-py3-none-any.whl/fps_kernel_subprocess/main.py
@@ -0,0 +1,10 @@
+from fps import Module
+from jupyverse_api.kernel import DefaultKernelFactory
+
+from .kernel_subprocess import KernelSubprocess
+
+
+class KernelSubprocessModule(Module):
+    async def prepare(self) -> None:
+        default_kernel_factory = DefaultKernelFactory(KernelSubprocess)
+        self.put(default_kernel_factory)
