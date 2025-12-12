b3e26d6398527b0b10c4b9d06e139677e03a06a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:08:59 +0000

    Add package picarlo/0.4.5/picarlo-0.4.5-cp312-cp312-manylinux_2_34_x86_64.whl

---
+++ packages/picarlo/picarlo-0.4.5-cp312-cp312-manylinux_2_34_x86_64.whl/picarlo/__init__.py
@@ -0,0 +1,5 @@
+from . import sim as sim
+
+print("==> in picarlo/__init__.py", ": ", "I run when picarlo is imported")
+# TODO: only print this if debug mode is enabled
+# print("dir(sim):", dir(sim))
