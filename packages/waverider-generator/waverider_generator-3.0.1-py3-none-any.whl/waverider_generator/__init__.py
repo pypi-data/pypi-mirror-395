c70eb7c48302a9ee64dcf86da5440097305855ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 15:05:13 +0000

    Add package waverider-generator/3.0.1/waverider_generator-3.0.1-py3-none-any.whl

---
+++ packages/waverider-generator/waverider_generator-3.0.1-py3-none-any.whl/waverider_generator/__init__.py
@@ -0,0 +1,3 @@
+from waverider_generator.generator import Waverider, Streams
+from waverider_generator.input_validation import GeometricParameters, FlowParameters
+__all__ = ["Waverider", "Streams", "GeometricParameters", "FlowParameters"]
