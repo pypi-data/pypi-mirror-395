593868c5fae36eb3f400d58cbc4980f3d222f55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:51 +0000

    Add package veriskgo/26.0.2/veriskgo-26.0.2-py3-none-any.whl

---
+++ packages/veriskgo/veriskgo-26.0.2-py3-none-any.whl/veriskgo/config.py
@@ -0,0 +1,11 @@
+# src/veriskgo/config.py
+import os 
+
+def get_cfg():
+    return {
+        "aws_profile": os.getenv("AWS_PROFILE" , "sandbox"),
+        "aws_region": os.getenv("AWS_REGION", "us-east-1"),
+        "aws_sqs_url": os.getenv("AWS_SQS_URL") or "https://sqs.us-east-1.amazonaws.com/982135724133/otel-telemetry-queue",
+        "env": os.getenv("ENV", "dev"),
+        "project": os.getenv("PROJECT", "default-genai-app"),
+    }
