103064999b06c1ef587a1f5fd66d088535d4580b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:35 +0000

    Add package powl/2.1.6/powl-2.1.6.tar.gz

---
+++ packages/powl/powl-2.1.6.tar.gz/powl-2.1.6/powl/discovery/total_order_based/inductive/fall_through/decision_graph/dfg_fall_through.py
@@ -0,0 +1,35 @@
+from typing import Optional, List, Dict, Any, Tuple
+
+from pm4py.algo.discovery.inductive.dtypes.im_ds import (
+    IMDataStructureUVCL,
+)
+from powl.discovery.total_order_based.inductive.variants.decision_graph.cyclic_dg_cut import CyclicDecisionGraphCutUVCL
+from pm4py.algo.discovery.inductive.fall_through.abc import FallThrough, T
+
+from powl.discovery.total_order_based.inductive.variants.decision_graph.dfg_cut import DFGCutUVCL
+from powl.objects.obj import DecisionGraph, POWL
+
+
+class DFGFallThroughUVCL(FallThrough[IMDataStructureUVCL]):
+
+    @classmethod
+    def holds(
+        cls,
+        obj: IMDataStructureUVCL,
+        parameters: Optional[Dict[str, Any]] = None,
+    ) -> bool:
+        return True
+
+
+    @classmethod
+    def apply(
+        cls,
+        obj: T,
+        pool=None,
+        manager=None,
+        parameters: Optional[Dict[str, Any]] = None,
+    ) -> Optional[Tuple[DecisionGraph, List[POWL]]]:
+        return DFGCutUVCL.apply(obj, parameters)
+
+
+
