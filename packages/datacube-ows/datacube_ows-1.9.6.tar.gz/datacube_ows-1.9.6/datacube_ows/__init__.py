403813eabd6ba080b640ec2a2c3265b104da9e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 07:07:41 +0000

    Add package datacube-ows/1.9.6/datacube_ows-1.9.6.tar.gz

---
+++ packages/datacube-ows/datacube_ows-1.9.6.tar.gz/datacube_ows-1.9.6/datacube_ows/__init__.py
@@ -0,0 +1,12 @@
+# This file is part of datacube-ows, part of the Open Data Cube project.
+# See https://opendatacube.org for more information.
+#
+# Copyright (c) 2017-2024 OWS Contributors
+# SPDX-License-Identifier: Apache-2.0
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    # Default version number.
+    # Will only be used when running datacube-ows direct from source code (not properly installed)
+    __version__ = "1.9.6"
