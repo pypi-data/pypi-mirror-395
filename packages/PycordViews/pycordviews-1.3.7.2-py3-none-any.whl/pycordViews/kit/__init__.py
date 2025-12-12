cb595a68b30fd1bb7d830ec52a4aac6bb3d32dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:06:56 +0000

    Add package PycordViews/1.3.7.2/pycordviews-1.3.7.2-py3-none-any.whl

---
+++ packages/PycordViews/pycordviews-1.3.7.2-py3-none-any.whl/pycordViews/kit/__init__.py
@@ -0,0 +1,3 @@
+from .poll import Poll
+from .confirm import Confirm
+from .back import Back
---
+++ packages/PycordViews/pycordviews-1.3.7.2-py3-none-any.whl/pycordViews/kit/back.py
@@ -0,0 +1,59 @@
+from ..views.easy_modified_view import EasyModifiedViews
+
+from typing import Optional, Union, Any, Callable
+from discord.ui import Button
+from discord import ButtonStyle, Role, Interaction
+from inspect import currentframe
+
+class Back:
+    """
+    Class
+    -------------
+    Allows you to easily setup a back button.
+    He save function parameters when he is called and recall the function with these parameters.
+    -------------
+    """
+
+    def __init__(self, timeout: Optional[float] = None,
+                 disabled_on_timeout: bool = False,
+                 row: int = 0,
+                 autorised_roles: Optional[list[Union[Role, int]]] = None,
+                 autorised_key: Optional[callable] = None):
+        """
+        Initialisation for back button
+        """
+        self.args_function: tuple = ()
+        self.kwargs_function: dict = {}
+
+        # get the calling function
+        self.back_function: Union[Callable] = None
+
+        self.__view = EasyModifiedViews(timeout=timeout, disabled_on_timeout=disabled_on_timeout)
+        self.__view.add_items(Button(label='⏪ Back', row=row, custom_id='back', style=ButtonStyle.gray))
+        self.__view.set_callable('back', _callable=self._back, autorised_roles=autorised_roles, autorised_key=autorised_key)
+
+    def set_parameters(self, *args, function: Callable, **kwargs):
+        """
+        Set the parameters to recall the function
+        :param args: The args to save
+        :param kwargs: The kwargs to save
+        :param function: The asynchronous function to call back
+        """
+        self.args_function = args
+        self.kwargs_function = kwargs
+        self.back_function = function
+
+    async def _back(self, button: Button, interaction: Interaction, data: dict[str, Any]):
+        """
+        Base asynchronous _back function called when button is pressed
+        """
+        await interaction.response.defer()
+        await self.back_function(*self.args_function, **self.kwargs_function)
+
+    @property
+    def get_view(self) -> EasyModifiedViews:
+        """
+        Get the view with the back button
+        :return: The view with the back button
+        """
+        return self.__view
