7d66eddf8c8fb50c4b5451a13c7395b882cfb516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 06:05:24 +0000

    Add package dcs-client-jer-project/0.1.1/dcs_client_jer_project-0.1.1.tar.gz

---
+++ packages/dcs-client-jer-project/dcs_client_jer_project-0.1.1.tar.gz/dcs_client_jer_project-0.1.1/dcs_client/__init__.py
@@ -0,0 +1,4 @@
+from .client import DCS
+from .exceptions import DCSException, ValidationError, ContractNotFoundError
+
+__all__ = ["DCS", "DCSException", "ValidationError", "ContractNotFoundError"]
