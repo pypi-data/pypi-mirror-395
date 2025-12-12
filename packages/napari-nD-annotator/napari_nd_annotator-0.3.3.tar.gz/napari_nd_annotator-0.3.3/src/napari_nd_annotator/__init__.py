167d4506f00583244794c2f69e6b2e6ed40ed057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:05:42 +0000

    Add package napari-nD-annotator/0.3.3/napari_nd_annotator-0.3.3.tar.gz

---
+++ packages/napari-nD-annotator/napari_nd_annotator-0.3.3.tar.gz/napari_nd_annotator-0.3.3/src/napari_nd_annotator/__init__.py
@@ -0,0 +1,13 @@
+
+__version__ = "0.3.3"
+
+from ._widgets import AnnotatorWidget, InterpolationWidget, MinimalSurfaceWidget
+from packaging import version
+from ._napari_version import NAPARI_VERSION
+
+if NAPARI_VERSION >= version.parse("0.4.15"):
+    from ._widgets import ListWidgetBB
+    __all__ = ["AnnotatorWidget", "InterpolationWidget", "MinimalSurfaceWidget", "ListWidgetBB", "NAPARI_VERSION"]
+else:
+    __all__ = ["AnnotatorWidget", "InterpolationWidget", "MinimalSurfaceWidget", "NAPARI_VERSION"]
+
