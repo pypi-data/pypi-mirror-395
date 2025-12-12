03cc395b6066272511e4413bc7a94119fbd8011f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:32:16 +0000

    Add package gbase8sdb/0.2.4/gbase8sdb-0.2.4-cp39-cp39-manylinux2014_aarch64.manylinux_2_17_aarch64.whl

---
+++ packages/gbase8sdb/gbase8sdb-0.2.4-cp39-cp39-manylinux2014_aarch64.manylinux_2_17_aarch64.whl/gbase8sdb/exceptions.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+
+class Warning(Exception):
+    pass
+
+
+class Error(Exception):
+    pass
+
+
+class DatabaseError(Error):
+    pass
+
+
+class DataError(DatabaseError):
+    pass
+
+
+class IntegrityError(DatabaseError):
+    pass
+
+
+class InterfaceError(Error):
+    pass
+
+
+class InternalError(DatabaseError):
+    pass
+
+
+class NotSupportedError(DatabaseError):
+    pass
+
+
+class OperationalError(DatabaseError):
+    pass
+
+
+class ProgrammingError(DatabaseError):
+    pass
