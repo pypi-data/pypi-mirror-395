d6a416101d061fa0ef5c44510c1efd89f3b46c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:39 +0000

    Add package histomicsui/1.7.3.dev97/histomicsui-1.7.3.dev97-py2.py3-none-any.whl

---
+++ packages/histomicsui/histomicsui-1.7.3.dev97-py2.py3-none-any.whl/histomicsui/web_client/views/layout/HeaderAnalysesView.js
@@ -0,0 +1,67 @@
+import _ from 'underscore';
+import $ from 'jquery';
+import {restRequest} from '@girder/core/rest';
+
+import events from '../../events';
+import router from '../../router';
+import View from '../View';
+import headerAnalysesTemplate from '../../templates/layout/headerAnalyses.pug';
+import '../../stylesheets/layout/headerAnalyses.styl';
+
+import 'bootstrap-submenu/dist/js/bootstrap-submenu';
+import 'bootstrap-submenu/dist/css/bootstrap-submenu.css';
+
+var HeaderUserView = View.extend({
+    events: {
+        'click .h-analysis-item': '_setAnalysis'
+    },
+    initialize() {
+        this.image = null;
+        this.listenTo(events, 'h:imageOpened', function (image) {
+            this.image = image;
+            this.render();
+        });
+    },
+    render() {
+        if (this.image) {
+            restRequest({
+                url: 'slicer_cli_web/docker_image'
+            }).then((analyses) => {
+                const keyCount = {};
+                for (const key of Object.keys(analyses)) {
+                    const key2 = key.split('/').slice(-2).join('/');
+                    keyCount[key2] = (keyCount[key2] || 0) + 1;
+                }
+                const keyMap = {};
+                for (const key of Object.keys(analyses)) {
+                    const key2 = key.split('/').slice(-2).join('/');
+                    keyMap[keyCount[key2] === 1 ? key2 : key] = key;
+                }
+                const maxRows = Math.max(5, Math.floor((($('.h-image-view-body').height() || 0) - 8) / 26));
+                if (_.keys(analyses || {}).length > 0) {
+                    this.$el.removeClass('hidden');
+                    this.$el.html(headerAnalysesTemplate({
+                        analyses: analyses || {},
+                        keyMap: keyMap,
+                        maxRows: maxRows
+                    }));
+                    this.$('.h-analyses-dropdown-link').submenupicker();
+                } else {
+                    this.$el.addClass('hidden');
+                }
+                return null;
+            });
+        } else {
+            this.$el.addClass('hidden');
+        }
+        return this;
+    },
+    _setAnalysis(evt) {
+        evt.preventDefault();
+        var target = $(evt.currentTarget).data();
+
+        router.setQuery('analysis', target.api, {trigger: true});
+    }
+});
+
+export default HeaderUserView;
