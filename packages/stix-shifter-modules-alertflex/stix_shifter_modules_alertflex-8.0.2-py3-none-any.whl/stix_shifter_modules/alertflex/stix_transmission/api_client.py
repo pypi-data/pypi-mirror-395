9dbe3724738f3301bddba6de374a2fe054343d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:49 +0000

    Add package stix-shifter-modules-alertflex/8.0.2/stix_shifter_modules_alertflex-8.0.2-py3-none-any.whl

---
+++ packages/stix-shifter-modules-alertflex/stix_shifter_modules_alertflex-8.0.2-py3-none-any.whl/stix_shifter_modules/alertflex/stix_transmission/api_client.py
@@ -0,0 +1,36 @@
+import base64
+from stix_shifter_utils.stix_transmission.utils.RestApiClientAsync import RestApiClientAsync
+
+
+class APIClient():
+
+    def __init__(self, connection, configuration):
+        self.endpoint_start = 'alertflex-ctrl/rest/stix-alerts'
+        headers = dict()
+        auth = configuration.get('auth')
+        token_decoded = auth['username'] + ':' + auth['password']
+        token = base64.b64encode(token_decoded.encode('ascii'))
+        headers['Authorization'] = "Basic %s" % token.decode('ascii')
+
+        url_modifier_function = None
+        self.timeout = connection['options'].get('timeout')
+        self.client = RestApiClientAsync(connection.get('host'),
+                                    connection.get('port'),
+                                    headers,
+                                    url_modifier_function,
+                                    cert_verify=connection.get('selfSignedCert'))
+
+    async def ping_data_source(self):
+        endpoint = self.endpoint_start + '/status'
+        return await self.client.call_api(endpoint, 'GET', timeout=self.timeout)
+
+    async def get_search_results(self, query_expression, offset=None, length=None):
+        endpoint = self.endpoint_start + '/search'
+        data = {'query': query_expression}
+        result = await self.client.call_api(endpoint, 'GET', urldata=data, timeout=self.timeout)
+        return result
+
+    async def delete_search(self, search_id):
+        # Optional since this may not be supported by the data source API
+        # Delete the search
+        return {"code": 200, "success": True}
