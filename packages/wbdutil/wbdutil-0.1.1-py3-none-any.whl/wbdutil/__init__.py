b6f8aaca2c8e855a832140fcb676fa40cd82638b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 17:06:11 +0000

    Add package wbdutil/0.1.1/wbdutil-0.1.1-py3-none-any.whl

---
+++ packages/wbdutil/wbdutil-0.1.1-py3-none-any.whl/wbdutil/__init__.py
@@ -0,0 +1,8 @@
+# The version number, in the form major.minor.patch
+__VERSION__ = "0.1.1"
+
+# The name of the module
+MODULE_NAME = "wbdutil"
+
+# The name of the command line interface.
+CLI_NAME = "wbdutil"
