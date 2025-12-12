1b2b0f66e9b343342a8d6a968e8ce2530204c624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:17 +0000

    Add package KratosContactStructuralMechanicsApplication/10.4.0/kratoscontactstructuralmechanicsapplication-10.4.0-cp313-cp313-manylinux_2_28_x86_64.whl

---
+++ packages/KratosContactStructuralMechanicsApplication/kratoscontactstructuralmechanicsapplication-10.4.0-cp313-cp313-manylinux_2_28_x86_64.whl/KratosMultiphysics/ContactStructuralMechanicsApplication/SelfContactUtilities.pyi
@@ -0,0 +1,9 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics as Kratos
+
+def ComputeSelfContactPairing(arg0: Kratos.ModelPart, arg1: int) -> None:
+    """ComputeSelfContactPairing(arg0: Kratos.ModelPart, arg1: int) -> None"""
+def FullAssignmentOfPairs(arg0: Kratos.ModelPart) -> None:
+    """FullAssignmentOfPairs(arg0: Kratos.ModelPart) -> None"""
+def NotPredefinedMasterSlave(arg0: Kratos.ModelPart) -> None:
+    """NotPredefinedMasterSlave(arg0: Kratos.ModelPart) -> None"""
