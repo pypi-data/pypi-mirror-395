48975330b2032f9061fa9cecc9d13ea1b56143aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:06:53 +0000

    Add package openquantum-sdk-qiskit/0.2.2/openquantum_sdk_qiskit-0.2.2-py3-none-any.whl

---
+++ packages/openquantum-sdk-qiskit/openquantum_sdk_qiskit-0.2.2-py3-none-any.whl/openquantum_sdk_qiskit/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from .oq_service import OpenQuantumService
+from .oq_backend import OpenQuantumBackend
+from .oq_sampler import OQSampler
+from .oq_estimator import OQEstimator
+
+SamplerV2 = OQSampler
+EstimatorV2 = OQEstimator
+
+__version__ = "0.2.2"
+__all__ = [
+    "OpenQuantumService",
+    "OpenQuantumBackend",
+    "OQSampler",
+    "SamplerV2",
+    "OQEstimator",
+    "EstimatorV2",
+]
