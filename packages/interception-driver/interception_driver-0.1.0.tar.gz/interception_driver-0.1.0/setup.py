a70749a04746b9c838d612dc0b0822f4bf2ad908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:05:01 +0000

    Add package interception-driver/0.1.0/interception_driver-0.1.0.tar.gz

---
+++ packages/interception-driver/interception_driver-0.1.0.tar.gz/interception_driver-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+long_description = ""
+if os.path.exists("README.md"):
+    with open("README.md", encoding="utf-8") as f:
+        long_description = f.read()
+
+setup(
+    name="interception_driver",
+    version="0.1.0",
+    author="Ibrahim Arslan",
+    author_email="ibrahimarslan226@gmail.com",
+    description="Python wrapper for Interception driver (Low-level input)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    
+    packages=find_packages(),
+    
+    include_package_data=True,
+    package_data={
+        "interception_driver": ["*.dll"],
+    },
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.6',
+)
