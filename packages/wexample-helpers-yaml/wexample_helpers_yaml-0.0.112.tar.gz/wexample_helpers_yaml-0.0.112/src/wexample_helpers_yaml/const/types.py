60dab4e18353570d15374c1efeb01a84374c686c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:31:25 +0000

    Add package wexample-helpers-yaml/0.0.112/wexample_helpers_yaml-0.0.112.tar.gz

---
+++ packages/wexample-helpers-yaml/wexample_helpers_yaml-0.0.112.tar.gz/wexample_helpers_yaml-0.0.112/src/wexample_helpers_yaml/const/types.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from typing import TypeAlias
+
+from wexample_helpers.const.types import StringKeysDict, StructuredData
+
+YamlContent: TypeAlias = StructuredData
+YamlContentDict = StringKeysDict
