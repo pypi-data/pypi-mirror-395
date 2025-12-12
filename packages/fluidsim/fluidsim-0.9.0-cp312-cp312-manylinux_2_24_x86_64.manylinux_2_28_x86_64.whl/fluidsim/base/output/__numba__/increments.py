dd5b063f9540ecd12b5ff3626fa7fb9fbad03dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 07:06:42 +0000

    Add package fluidsim/0.9.0/fluidsim-0.9.0-cp312-cp312-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/fluidsim/fluidsim-0.9.0-cp312-cp312-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/fluidsim/base/output/__numba__/increments.py
@@ -0,0 +1,19 @@
+
+from numba import njit
+import numpy as np
+
+
+@njit(cache=True, fastmath=True)
+def strfunc_from_pdf(rxs, pdf, values, order, absolute=False):
+    """Compute structure function of specified order from pdf"""
+    S_order = np.empty(rxs.shape)
+    if absolute:
+        values = abs(values)
+    for irx in range(rxs.size):
+        deltainc = abs(values[irx, 1] - values[irx, 0])
+        S_order[irx] = deltainc * np.sum(pdf[irx] * values[irx] ** order)
+    return S_order
+
+
+def __transonic__():
+    return '0.8.0'
---
+++ packages/fluidsim/fluidsim-0.9.0-cp312-cp312-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/fluidsim/solvers/ns2d/bouss/__python__/solver.py
@@ -0,0 +1,7 @@
+def tendencies_nonlin_ns2dbouss(ux, uy, px_rot, py_rot, px_b, py_b):
+    Frot = -ux * px_rot - uy * py_rot + px_b
+    Fb = -ux * px_b - uy * py_b
+    return (Frot, Fb)
+
+
+def __transonic__(): return "0.8.0"
