09501c2c87dcbcf4203e2af3e058867aedbb0168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 12:05:44 +0000

    Add package jupyterlab-chat-toy/0.1.0/jupyterlab_chat_toy-0.1.0.tar.gz

---
+++ packages/jupyterlab-chat-toy/jupyterlab_chat_toy-0.1.0.tar.gz/jupyterlab-chat-toy-0.1.0/eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+  }
+};
