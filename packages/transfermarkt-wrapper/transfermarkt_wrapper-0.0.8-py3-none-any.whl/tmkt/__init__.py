ea541d7eeea86574f70f7d3755de16020ad52ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:07:07 +0000

    Add package transfermarkt-wrapper/0.0.8/transfermarkt_wrapper-0.0.8-py3-none-any.whl

---
+++ packages/transfermarkt-wrapper/transfermarkt_wrapper-0.0.8-py3-none-any.whl/tmkt/__init__.py
@@ -0,0 +1,204 @@
+from .api import TMKTAPIClient
+
+from typing import Dict, Any, Optional
+
+class TMKT:
+    """Main Transfermarkt-Wrapper API wrapper interface"""
+    
+    def __init__(self):
+        self._api = TMKTAPIClient()
+    
+    async def __aenter__(self):
+        """Enter async context manager"""
+        await self._api._ensure_session()  
+        return self
+    
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        """Exit async context manager"""
+        await self._api.close()
+
+    async def close(self):
+        """Close context manager"""
+        await self._api.close()
+
+    async def get_player_transfers(self, playerId: int)  -> Dict[str, Any]:
+        """
+        Get a players full transfer history
+        """
+        return await self._api._get(f"/transfer/history/player/{playerId}")
+    
+    async def get_club_transfers(self, clubId: int)  -> Dict[str, Any]:
+        """
+        Get a clubs full transfer history
+        """
+        return await self._api._get(f"/transfer/history/club/{clubId}")
+    
+    async def get_competition_transfers(self, competitionId: str)  -> Dict[str, Any]:
+        """
+        Get a competitions full transfer history
+        """
+        return await self._api._get(f"/transfer/history/competition/{competitionId}")
+    
+    async def get_player(self, playerId: int)  -> Dict[str, Any]:
+        """
+        Get a players profile with general data.
+        """
+        return await self._api._get(f"/player/{playerId}")
+    
+    async def get_club(self, clubId: int)  -> Dict[str, Any]:
+        """
+        Get a clubs profile with general data.
+        """
+        return await self._api._get(f"/club/{clubId}")
+    
+    async def get_competition(self, competitionId: str)  -> Dict[str, Any]:
+        """
+        Get a competitions profile with general data.
+        """
+        return await self._api._get(f"/competition/{competitionId}")
+    
+    async def get_player_injuries(self, playerId: int)  -> Dict[str, Any]:
+        """
+        Get a players full injury list.
+        """
+        return await self._api._get(f"/player/{playerId}/injury")
+    
+    async def get_club_stadium(self, clubId: int)  -> Dict[str, Any]:
+        """
+        Get a clubs stadium basic info
+        """
+        return await self._api._get(f"/club/{clubId}/stadium")
+    
+    async def get_competition_clubs(self, competitionId: str)  -> Dict[str, Any]:
+        """
+        Get a competitions list of clubs
+        """
+        return await self._api._get(f"/competition/{competitionId}/club")
+    
+    async def get_competition_table(self, competitionId: str, seasonId: int = None)  -> Dict[str, Any]:
+        """
+        Get a competitions table standings.
+        """
+        url = f"/competition/{competitionId}/table"
+
+        if seasonId:
+            url += f"?season={seasonId}"
+
+        return await self._api._get(url)
+    
+    async def get_club_squad(self, clubId: int)  -> Dict[str, Any]:
+        """
+        Get a clubs full squad
+        """
+        return await self._api._get(f"/club/{clubId}/squad")
+    
+    async def get_all_transfers(self)  -> Dict[str, Any]:
+        """
+        Get a recent transfers
+        """
+        return await self._api._secondary_get(f"/ceapi/LatestTransfers/list/all")
+    
+    async def team_search(self, query: str) -> Dict[str, Any]:
+        """
+        Search for a team by name (e.g., "Arsenal").
+        Example URL: https://www.transfermarkt.co.uk/news/search?index=clubs_lang_new&q=arsenal
+        """
+        params = {
+            "index": "clubs_lang_new",
+            "q": query
+        }
+        return await self._api._secondary_get("/news/search", params=params)
+    
+    async def player_search(self, query: str) -> Dict[str, Any]:
+        """
+        Search for a player by name (e.g., "Saka").
+        Example URL: https://www.transfermarkt.co.uk/spieler/searchSpielerDaten?q=saka
+        Backup URL: https://www.transfermarkt.co.uk/search/spieler?id=text&data[q]=saka
+        """
+        params = {
+            "q": query
+        }
+        return await self._api._secondary_get("/spieler/searchSpielerDaten", params=params, clean_html_name=True)
+    
+    async def league_search(self, query: str) -> Dict[str, Any]:
+        """
+        Search for a league by name (e.g., "La Liga").
+        Example URL: https://www.transfermarkt.co.uk/bundesliga/searchwettbewerb/wettbewerb/L1?q=la+liga
+        """
+        params = {
+            "q": query
+        }
+        return await self._api._secondary_get("/bundesliga/searchwettbewerb/wettbewerb/L1", params=params, clean_html_name=True)
+    
+    async def get_player_stats(self, playerId: int, season: int = None) -> Dict[str, Any]:
+        """
+        Get a players stats for the current season or a specific season
+        """
+        url = f"/player/{playerId}/performance?season={season}" if season else f"/player/{playerId}/performance"
+        return await self._api.third_get(url) 
+    
+    async def get_player_stats_per_club(self, playerId: int) -> Dict[str, Any]:
+        """
+        Get a players stats per club in their career.
+        """
+        return await self._api.third_get(f"/player/{playerId}/performanceperclub") 
+    
+    async def get_player_stats_per_competition(self, playerId: int) -> Dict[str, Any]:
+        """
+        Get a players stats per competition in their career.
+        """
+        return await self._api.third_get(f"/player/{playerId}/performancepercompetition") 
+    
+    async def get_player_stats_national_career(self, playerId: int) -> Dict[str, Any]:
+        """
+        Get a players national career stats.
+        """
+        return await self._api._get(f"/player/{playerId}/national-career-history") 
+    
+    async def get_current_season(self, competitionId: str) -> int:
+        """
+        Get a competition's current season id to use elsewhere
+        """
+        leagueData = await self.get_competition(competitionId)
+
+        if leagueData:
+            return leagueData.get("data", {}).get("currentSeason", {}).get("id", None)
+        
+        return None
+    
+    async def get_match(self, matchId: int) -> Dict[str, Any]:
+        """
+        Get match details. Game actions are in German (DE) but can be easily translated 
+        using a library such as deep-translator 
+        """
+        return await self._api._get(f"/game/{matchId}")
+    
+    async def get_coach(self, coachId: int) -> Dict[str, Any]:
+        """
+        Get coach details.
+        """
+        return await self._api._get(f"/coach/{coachId}")
+    
+    async def get_player_news(self, playerId: int)  -> Dict[str, Any]:
+        """
+        Get a news relevant to your provided player
+        """
+        return await self._api._secondary_get(f"/ceapi/RelevantNews/player/{playerId}")
+    
+    async def get_coach_news(self, coachId: int)  -> Dict[str, Any]:
+        """
+        Get a news relevant to your provided coach
+        """
+        return await self._api._secondary_get(f"/ceapi/RelevantNews/trainer/{coachId}")
+    
+    async def get_club_news(self, clubId: int)  -> Dict[str, Any]:
+        """
+        Get a news relevant to your provided club
+        """
+        return await self._api._secondary_get(f"/ceapi/RelevantNews/team/{clubId}")
+    
+    async def get_competition_news(self, competitionId: str)  -> Dict[str, Any]:
+        """
+        Get a news relevant to your provided competition
+        """
+        return await self._api._secondary_get(f"/ceapi/RelevantNews/competition/{competitionId}")
