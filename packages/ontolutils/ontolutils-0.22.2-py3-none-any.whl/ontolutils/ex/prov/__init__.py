597ac35d8395ed948aa41dbf7982e7426bdef48f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 09:07:01 +0000

    Add package ontolutils/0.22.2/ontolutils-0.22.2-py3-none-any.whl

---
+++ packages/ontolutils/ontolutils-0.22.2-py3-none-any.whl/ontolutils/ex/prov/__init__.py
@@ -0,0 +1,9 @@
+from .attribution import Attribution, Person, Organization, Agent
+from .activity import Activity
+__all__ = [
+    "Attribution",
+    "Person",
+    "Organization",
+    "Agent",
+    "Activity",
+]
