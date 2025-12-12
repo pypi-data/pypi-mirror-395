e77ac70379fc7d6e20efd3c7a841fe8522a208de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:31:11 +0000

    Add package snapquery/0.2.2/snapquery-0.2.2.tar.gz

---
+++ packages/snapquery/snapquery-0.2.2.tar.gz/snapquery-0.2.2/snapquery/scholia.py
@@ -0,0 +1,107 @@
+"""
+Created on 2024-05-04
+
+@author: wf
+"""
+
+import requests
+
+from snapquery.snapquery_core import NamedQuery, NamedQueryManager, NamedQuerySet
+from snapquery.github_access import GitHub
+
+class ScholiaQueries:
+    """
+    A class to handle the extraction and management of Scholia queries.
+    """
+
+    def __init__(self, nqm: NamedQueryManager, debug: bool = False):
+        """
+        Constructor
+
+        Args:
+            nqm (NamedQueryManager): The NamedQueryManager to use for storing queries.
+            debug (bool): Enable debug output. Defaults to False.
+        """
+        self.nqm = nqm
+        self.named_query_set = NamedQuerySet(
+            domain="scholia.toolforge.org",
+            namespace="named_queries",
+            target_graph_name="wikidata",
+        )
+        self.github = GitHub(owner="WDscholia", repo="scholia")
+        self.debug = debug
+
+    def get_scholia_file_list(self):
+        """
+        Retrieve the list of SPARQL files from the Scholia repository.
+
+        Returns:
+            list: List of dictionaries representing file information.
+        """
+        file_list_lod=self.github.get_contents("scholia/app/templates")
+        return file_list_lod
+
+    def extract_query(self, file_info) -> NamedQuery:
+        """
+        Extract a single query from file information.
+
+        Args:
+            file_info (dict): Dictionary containing information about the file.
+
+        Returns:
+            NamedQuery: The extracted NamedQuery object.
+        """
+        file_name = file_info["name"]
+        if file_name.endswith(".sparql") and file_name[:-7]:
+            file_response = requests.get(file_info["download_url"])
+            file_response.raise_for_status()
+            query_str = file_response.text
+            name = file_name[:-7]
+            named_query=NamedQuery(
+                domain=self.named_query_set.domain,
+                namespace=self.named_query_set.namespace,
+                name=name,
+                url=file_info["download_url"],
+                title=name,
+                description=name,
+                comment="",
+                sparql=query_str,
+            )
+            return named_query
+
+    def extract_queries(self, limit: int = None):
+        """
+        Extract all queries from the Scholia repository.
+
+        Args:
+            limit (int, optional): Limit the number of queries fetched. Defaults to None.
+        """
+        file_list_json = self.get_scholia_file_list()
+        for i, file_info in enumerate(file_list_json, start=1):
+            named_query = self.extract_query(file_info)
+            if named_query:
+                self.named_query_set.queries.append(named_query)
+                if self.debug:
+                    if i % 80 == 0:
+                        print(f"{i}")
+                    print(".", end="", flush=True)
+                if limit and len(self.named_query_set.queries) >= limit:
+                    break
+
+        if self.debug:
+            print(f"found {len(self.named_query_set.queries)} scholia queries")
+
+    def save_to_json(self, file_path: str = "/tmp/scholia-queries.json"):
+        """
+        Save the NamedQueryList to a JSON file.
+
+        Args:
+            file_path (str): Path to the JSON file.
+        """
+        self.named_query_set.save_to_json_file(file_path, indent=2)
+
+    def store_queries(self):
+        """
+        Store the named queries into the database.
+        """
+        self.nqm.store_named_query_list(self.named_query_set)
---
+++ packages/snapquery/snapquery-0.2.2.tar.gz/snapquery-0.2.2/tests/test_scholia.py
@@ -0,0 +1,57 @@
+"""
+Created on 2024-05-04
+
+@author: wf
+"""
+
+import os
+import unittest
+
+from basemkit.basetest import Basetest
+
+from snapquery.scholia import ScholiaQueries
+from snapquery.snapquery_core import NamedQueryManager
+
+
+class TestScholia(Basetest):
+    """
+    test scholia queries
+    """
+
+    def setUp(self, debug=True, profile=True):
+        Basetest.setUp(self, debug=debug, profile=profile)
+
+    @unittest.skipIf(Basetest.inPublicCI(), "avoid github rate limit")
+    def test_scholia_queries(self):
+        """
+        Test retrieving Scholia queries.
+        """
+        db_path = "/tmp/scholia_queries.db"
+        if os.path.exists(db_path):
+            os.remove(db_path)
+
+        # Adjust limit based on environment (e.g., CI)
+        if self.inPublicCI():
+            limit = 10
+        else:
+            limit = None
+            # limit=10
+
+        # Create a NamedQueryManager and ScholiaQueries instance
+        nqm = NamedQueryManager.from_samples(db_path=db_path)
+        scholia_queries = ScholiaQueries(nqm, debug=self.debug)
+
+        # Extract, store, and save queries to JSON
+        scholia_queries.extract_queries(limit=limit)
+        scholia_queries.store_queries()
+        scholia_queries.save_to_json("/tmp/scholia.json")
+
+        # Verify the data was stored
+        records = nqm.sql_db.query(
+            f"""SELECT *
+FROM NamedQuery
+WHERE namespace='{scholia_queries.named_query_set.namespace}'
+AND domain='{scholia_queries.named_query_set.domain}'
+"""
+        )
+        self.assertEqual(len(records), len(scholia_queries.named_query_set.queries))
