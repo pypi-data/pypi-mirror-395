49276a6448f9901955d848cf81cdb3643bea17a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:07:56 +0000

    Add package httpx-socks/0.11.0/httpx_socks-0.11.0-py3-none-any.whl

---
+++ packages/httpx-socks/httpx_socks-0.11.0-py3-none-any.whl/httpx_socks/_async_transport.py
@@ -0,0 +1,103 @@
+import ssl
+import typing
+
+import httpcore
+from httpx import AsyncBaseTransport, Request, Response, AsyncByteStream, Limits
+
+# noinspection PyProtectedMember
+from httpx._config import DEFAULT_LIMITS, create_ssl_context
+# noinspection PyProtectedMember
+from httpx._transports.default import AsyncResponseStream, map_httpcore_exceptions
+from python_socks import ProxyType, parse_proxy_url
+
+from ._async_proxy import AsyncProxy
+
+
+class AsyncProxyTransport(AsyncBaseTransport):
+    def __init__(
+        self,
+        *,
+        proxy_type: ProxyType,
+        proxy_host: str,
+        proxy_port: int,
+        username=None,
+        password=None,
+        rdns=None,
+        proxy_ssl: ssl.SSLContext = None,
+        verify=True,
+        cert=None,
+        trust_env: bool = True,
+        limits: Limits = DEFAULT_LIMITS,
+        **kwargs,
+    ):
+        ssl_context = create_ssl_context(
+            verify=verify,
+            cert=cert,
+            trust_env=trust_env,
+        )
+
+        self._pool = AsyncProxy(
+            proxy_type=proxy_type,
+            proxy_host=proxy_host,
+            proxy_port=proxy_port,
+            username=username,
+            password=password,
+            rdns=rdns,
+            proxy_ssl=proxy_ssl,
+            ssl_context=ssl_context,
+            max_connections=limits.max_connections,
+            max_keepalive_connections=limits.max_keepalive_connections,
+            keepalive_expiry=limits.keepalive_expiry,
+            **kwargs,
+        )
+
+    async def handle_async_request(self, request: Request) -> Response:
+        assert isinstance(request.stream, AsyncByteStream)
+
+        req = httpcore.Request(
+            method=request.method,
+            url=httpcore.URL(
+                scheme=request.url.raw_scheme,
+                host=request.url.raw_host,
+                port=request.url.port,
+                target=request.url.raw_path,
+            ),
+            headers=request.headers.raw,
+            content=request.stream,
+            extensions=request.extensions,
+        )
+
+        with map_httpcore_exceptions():
+            resp = await self._pool.handle_async_request(req)
+
+        assert isinstance(resp.stream, typing.AsyncIterable)
+
+        return Response(
+            status_code=resp.status,
+            headers=resp.headers,
+            stream=AsyncResponseStream(resp.stream),
+            extensions=resp.extensions,
+        )
+
+    @classmethod
+    def from_url(cls, url, **kwargs):
+        proxy_type, host, port, username, password = parse_proxy_url(url)
+        return cls(
+            proxy_type=proxy_type,
+            proxy_host=host,
+            proxy_port=port,
+            username=username,
+            password=password,
+            **kwargs,
+        )
+
+    async def aclose(self) -> None:
+        await self._pool.aclose()  # pragma: no cover
+
+    async def __aenter__(self):
+        await self._pool.__aenter__()
+        return self
+
+    async def __aexit__(self, exc_type=None, exc_value=None, traceback=None):
+        with map_httpcore_exceptions():
+            await self._pool.__aexit__(exc_type, exc_value, traceback)
---
+++ packages/httpx-socks/httpx_socks-0.11.0-py3-none-any.whl/httpx_socks/_sync_transport.py
@@ -0,0 +1,104 @@
+import ssl
+import typing
+
+import httpcore
+
+from httpx import BaseTransport, Request, Response, SyncByteStream, Limits
+
+# noinspection PyProtectedMember
+from httpx._config import DEFAULT_LIMITS, create_ssl_context
+# noinspection PyProtectedMember
+from httpx._transports.default import ResponseStream, map_httpcore_exceptions
+
+from ._sync_proxy import SyncProxy
+from python_socks import ProxyType, parse_proxy_url
+
+
+class SyncProxyTransport(BaseTransport):
+    def __init__(
+        self,
+        *,
+        proxy_type: ProxyType,
+        proxy_host: str,
+        proxy_port: int,
+        username=None,
+        password=None,
+        rdns=None,
+        proxy_ssl: ssl.SSLContext = None,
+        verify=True,
+        cert=None,
+        trust_env: bool = True,
+        limits: Limits = DEFAULT_LIMITS,
+        **kwargs,
+    ):
+        ssl_context = create_ssl_context(
+            verify=verify,
+            cert=cert,
+            trust_env=trust_env,
+        )
+
+        self._pool = SyncProxy(
+            proxy_type=proxy_type,
+            proxy_host=proxy_host,
+            proxy_port=proxy_port,
+            username=username,
+            password=password,
+            rdns=rdns,
+            proxy_ssl=proxy_ssl,
+            ssl_context=ssl_context,
+            max_connections=limits.max_connections,
+            max_keepalive_connections=limits.max_keepalive_connections,
+            keepalive_expiry=limits.keepalive_expiry,
+            **kwargs,
+        )
+
+    def handle_request(self, request: Request) -> Response:
+        assert isinstance(request.stream, SyncByteStream)
+
+        req = httpcore.Request(
+            method=request.method,
+            url=httpcore.URL(
+                scheme=request.url.raw_scheme,
+                host=request.url.raw_host,
+                port=request.url.port,
+                target=request.url.raw_path,
+            ),
+            headers=request.headers.raw,
+            content=request.stream,
+            extensions=request.extensions,
+        )
+
+        with map_httpcore_exceptions():
+            resp = self._pool.handle_request(req)
+
+        assert isinstance(resp.stream, typing.Iterable)
+
+        return Response(
+            status_code=resp.status,
+            headers=resp.headers,
+            stream=ResponseStream(resp.stream),
+            extensions=resp.extensions,
+        )
+
+    @classmethod
+    def from_url(cls, url, **kwargs):
+        proxy_type, host, port, username, password = parse_proxy_url(url)
+        return cls(
+            proxy_type=proxy_type,
+            proxy_host=host,
+            proxy_port=port,
+            username=username,
+            password=password,
+            **kwargs,
+        )
+
+    def close(self) -> None:
+        self._pool.close()  # pragma: no cover
+
+    def __enter__(self):
+        self._pool.__enter__()
+        return self
+
+    def __exit__(self, exc_type=None, exc_value=None, traceback=None):
+        with map_httpcore_exceptions():
+            self._pool.__exit__(exc_type, exc_value, traceback)
