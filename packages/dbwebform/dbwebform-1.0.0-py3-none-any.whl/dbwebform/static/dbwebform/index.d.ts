3107242dca4e08142746a29853db60fc81d61090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:43 +0000

    Add package dbwebform/1.0.0/dbwebform-1.0.0-py3-none-any.whl

---
+++ packages/dbwebform/dbwebform-1.0.0-py3-none-any.whl/dbwebform/static/dbwebform/index.d.ts
@@ -0,0 +1,18 @@
+declare class HyperTextNotification {
+    constructor(options?: {
+        bottom?: string;
+        right?: string;
+        backgroundColor?: string;
+        color?: string;
+        padding?: string;
+        borderRadius?: string;
+        timeout?: number;
+    });
+    show(text: string, timeout?: number): void;
+}
+declare function input_form_control_unline(form: HTMLFormElement): void;
+declare function formInputReversed(form: HTMLFormElement): void;
+declare const form: HTMLFormElement;
+declare const script: HTMLOrSVGScriptElement;
+declare const notification_text: string | null, method: string;
+//# sourceMappingURL=index.d.ts.map
