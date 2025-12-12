2041704d480e66255cf15d92ee2fa630276e643b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 12:05:19 +0000

    Add package riyaaaaaaaaaaa/1.0.0/riyaaaaaaaaaaa-1.0.0.tar.gz

---
+++ packages/riyaaaaaaaaaaa/riyaaaaaaaaaaa-1.0.0.tar.gz/riyaaaaaaaaaaa-1.0.0/riyaaaaaaaaaaa/__init__.py
@@ -0,0 +1,11 @@
+"""
+Riyaaaaaaaaaaa - A cute package with Konkani love messages
+"""
+
+from .love import KonkaniLove, get_random_message, get_all_messages
+
+__version__ = "1.0.0"
+__author__ = "Your Love"
+
+__all__ = ["KonkaniLove", "get_random_message", "get_all_messages"]
+
