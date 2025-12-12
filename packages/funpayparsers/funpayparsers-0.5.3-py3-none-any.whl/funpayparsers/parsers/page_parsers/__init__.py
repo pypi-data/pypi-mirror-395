f8cd8d737f40c994dc572a8c6cfe9fbf8144b6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 12:05:25 +0000

    Add package funpayparsers/0.5.3/funpayparsers-0.5.3-py3-none-any.whl

---
+++ packages/funpayparsers/funpayparsers-0.5.3-py3-none-any.whl/funpayparsers/parsers/page_parsers/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from .chat_page_parser import *
+from .main_page_parser import *
+from .order_page_parser import *
+from .profile_page_parser import *
+from .subcategory_page_parser import *
+from .transactions_page_parser import *
