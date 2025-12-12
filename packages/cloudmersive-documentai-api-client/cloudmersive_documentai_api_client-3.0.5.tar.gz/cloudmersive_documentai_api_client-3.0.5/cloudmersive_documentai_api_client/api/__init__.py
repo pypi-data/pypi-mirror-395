c36636980d61e9bdac2b3532c099e3aebb605c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:41 +0000

    Add package cloudmersive-documentai-api-client/3.0.5/cloudmersive_documentai_api_client-3.0.5.tar.gz

---
+++ packages/cloudmersive-documentai-api-client/cloudmersive_documentai_api_client-3.0.5.tar.gz/cloudmersive_documentai_api_client-3.0.5/cloudmersive_documentai_api_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from cloudmersive_documentai_api_client.api.analyze_api import AnalyzeApi
+from cloudmersive_documentai_api_client.api.extract_api import ExtractApi
+from cloudmersive_documentai_api_client.api.run_batch_job_api import RunBatchJobApi
