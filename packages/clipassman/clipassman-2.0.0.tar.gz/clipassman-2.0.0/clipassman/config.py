e58a108e06278a3843281168737bc7581f06aa54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:07:59 +0000

    Add package clipassman/2.0.0/clipassman-2.0.0.tar.gz

---
+++ packages/clipassman/clipassman-2.0.0.tar.gz/clipassman-2.0.0/clipassman/config.py
@@ -0,0 +1,8 @@
+# Copyright © 2025, Alexander Suvorov
+
+
+class Config:
+    name = 'Smart Password Manager CLI v2.0.0'
+    url = 'https://github.com/smartlegionlab/clipassman'
+    help_url = 'https://github.com/smartlegionlab/smartpasslib'
+    info = 'Copyright © 2025, Alexander Suvorov'
