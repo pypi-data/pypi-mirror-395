6adb1d469dc519d1ab51954df6fe427232f9424f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 20:06:28 +0000

    Add package wingechr-python-package/0.0.0/wingechr_python_package-0.0.0-py3-none-any.whl

---
+++ packages/wingechr-python-package/wingechr_python_package-0.0.0-py3-none-any.whl/python_package/__init__.py
@@ -0,0 +1,29 @@
+"""MODULE DESCRIPTION"""
+
+import platform
+import sys
+
+__version__ = "0.0.0"
+__all__ = []
+
+
+def get_info() -> dict:
+    """Return information about the system.
+
+    Returns
+    -------
+    dict
+
+
+    Examples
+    --------
+    >>> sorted(get_info().keys())
+    ['platform', 'python', 'version']
+
+    """
+
+    return {
+        "platform": platform.system(),
+        "python": sys.version.split(" ")[0],
+        "version": __version__,
+    }
