aa735cbe8ddec9942f25ddb1d23df46a09c85ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:35 +0000

    Add package fps_contents/0.10.5/fps_contents-0.10.5.tar.gz

---
+++ packages/fps_contents/fps_contents-0.10.5.tar.gz/fps_contents-0.10.5/src/fps_contents/main.py
@@ -0,0 +1,15 @@
+from fps import Module
+from jupyverse_api.app import App
+from jupyverse_api.auth import Auth
+from jupyverse_api.contents import Contents
+
+from .routes import _Contents
+
+
+class ContentsModule(Module):
+    async def prepare(self) -> None:
+        app = await self.get(App)
+        auth = await self.get(Auth)  # type: ignore[type-abstract]
+
+        contents = _Contents(app, auth)
+        self.put(contents, Contents)
