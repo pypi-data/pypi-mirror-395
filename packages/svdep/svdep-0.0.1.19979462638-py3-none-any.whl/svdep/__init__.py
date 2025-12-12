43c7266f9aeeb4a54ecc7393584463265d9715b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 22:05:29 +0000

    Add package svdep/0.0.1.19979462638/svdep-0.0.1.19979462638-py3-none-any.whl

---
+++ packages/svdep/svdep-0.0.1.19979462638-py3-none-any.whl/svdep/__init__.py
@@ -0,0 +1,15 @@
+
+from .file_collection import FileCollection
+from .native import is_native_available, get_native_library_path
+
+# Import pure-Python implementations
+from .task_check_up_to_date import TaskCheckUpToDate as _PythonTaskCheckUpToDate
+from .task_build_file_collection import TaskBuildFileCollection as _PythonTaskBuildFileCollection
+
+# Use native implementations if available, otherwise fall back to pure-Python
+if is_native_available():
+    from .native import NativeTaskCheckUpToDate as TaskCheckUpToDate
+    from .native import NativeTaskBuildFileCollection as TaskBuildFileCollection
+else:
+    TaskCheckUpToDate = _PythonTaskCheckUpToDate
+    TaskBuildFileCollection = _PythonTaskBuildFileCollection
