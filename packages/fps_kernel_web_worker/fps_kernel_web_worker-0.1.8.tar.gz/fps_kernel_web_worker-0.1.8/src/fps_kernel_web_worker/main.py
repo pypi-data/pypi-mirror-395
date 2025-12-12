733cf29dea8a043157fd0551b2e68f9cff19d63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:36 +0000

    Add package fps_kernel_web_worker/0.1.8/fps_kernel_web_worker-0.1.8.tar.gz

---
+++ packages/fps_kernel_web_worker/fps_kernel_web_worker-0.1.8.tar.gz/fps_kernel_web_worker-0.1.8/src/fps_kernel_web_worker/main.py
@@ -0,0 +1,10 @@
+from fps import Module
+from jupyverse_api.kernel import DefaultKernelFactory
+
+from .kernel_web_worker import KernelWebWorker
+
+
+class KernelWebWorkerModule(Module):
+    async def prepare(self) -> None:
+        default_kernel_factory = DefaultKernelFactory(KernelWebWorker)
+        self.put(default_kernel_factory)
