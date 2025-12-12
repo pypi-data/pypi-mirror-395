38cb93be25d2c41ee8c1eb9db252adb6b89a71cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:17 +0000

    Add package gpustack/2.0.1rc2/gpustack-2.0.1rc2.tar.gz

---
+++ packages/gpustack/gpustack-2.0.1rc2.tar.gz/gpustack-2.0.1rc2/gpustack/__init__.py
@@ -0,0 +1,2 @@
+__version__ = 'v2.0.1rc2'
+__git_commit__ = '4312860'
---
+++ packages/gpustack/gpustack-2.0.1rc2.tar.gz/gpustack-2.0.1rc2/gpustack/envs/__init__.py
@@ -0,0 +1,53 @@
+"""Configurable environment variables for GPUStack."""
+
+import os
+
+# Database configuration
+DB_ECHO = os.getenv("GPUSTACK_DB_ECHO", "false").lower() == "true"
+DB_POOL_SIZE = int(os.getenv("GPUSTACK_DB_POOL_SIZE", 5))
+DB_MAX_OVERFLOW = int(os.getenv("GPUSTACK_DB_MAX_OVERFLOW", 10))
+DB_POOL_TIMEOUT = int(os.getenv("GPUSTACK_DB_POOL_TIMEOUT", 30))
+
+# Proxy configuration
+PROXY_TIMEOUT = int(os.getenv("GPUSTACK_PROXY_TIMEOUT_SECONDS", 1800))
+
+# HTTP client TCP connector configuration
+TCP_CONNECTOR_LIMIT = int(os.getenv("GPUSTACK_TCP_CONNECTOR_LIMIT", 1000))
+
+# JWT Expiration
+JWT_TOKEN_EXPIRE_MINUTES = int(os.getenv("GPUSTACK_JWT_TOKEN_EXPIRE_MINUTES", 120))
+
+# Higress plugin configuration
+HIGRESS_EXT_AUTH_TIMEOUT_MS = int(
+    os.getenv("GPUSTACK_HIGRESS_EXT_AUTH_TIMEOUT_MS", 3000)
+)
+
+# Worker configuration
+WORKER_HEARTBEAT_GRACE_PERIOD = int(
+    os.getenv("GPUSTACK_WORKER_HEARTBEAT_GRACE_PERIOD", 150)
+)  # 2.5 minutes in seconds
+WORKER_ORPHAN_WORKLOAD_CLEANUP_GRACE_PERIOD = int(
+    os.getenv("GPUSTACK_WORKER_ORPHAN_WORKLOAD_CLEANUP_GRACE_PERIOD", 300)
+)  # 5 minutes in seconds
+
+# Model instance configuration
+MODEL_INSTANCE_RESCHEDULE_GRACE_PERIOD = int(
+    os.getenv("GPUSTACK_MODEL_INSTANCE_RESCHEDULE_GRACE_PERIOD", 300)
+)  # 5 minutes in seconds
+MODEL_INSTANCE_HEALTH_CHECK_INTERVAL = int(
+    os.getenv("GPUSTACK_MODEL_INSTANCE_HEALTH_CHECK_INTERVAL", 3)
+)
+DISABLE_OS_FILELOCK = os.getenv("GPUSTACK_DISABLE_OS_FILELOCK", "false").lower() in [
+    "true",
+    "1",
+]
+
+# Model evaluation cache configuration
+MODEL_EVALUATION_CACHE_MAX_SIZE = int(
+    os.getenv("GPUSTACK_MODEL_EVALUATION_CACHE_MAX_SIZE", 1000)
+)
+MODEL_EVALUATION_CACHE_TTL = int(os.getenv("GPUSTACK_MODEL_EVALUATION_CACHE_TTL", 3600))
+
+MIGRATION_DATA_DIR = os.getenv("GPUSTACK_MIGRATION_DATA_DIR", None)
+
+DATA_MIGRATION = os.getenv("GPUSTACK_DATA_MIGRATION", "false").lower() == "true"
