1cf29de18e0f6e1d474bdcb649cbdc31537e542c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:06:14 +0000

    Add package ngsfragments/2.5.2/ngsfragments-2.5.2-cp312-cp312-macosx_26_0_arm64.whl

---
+++ packages/ngsfragments/ngsfragments-2.5.2-cp312-cp312-macosx_26_0_arm64.whl/ngsfragments/__init__.py
@@ -0,0 +1,13 @@
+"""Init for mylibrary."""
+from __future__ import absolute_import
+from .fragments import Fragments
+from . import utilities
+from .correct import correction
+from .segment import cnv
+from .sequence import *
+from .plot import plot_plt
+from .io import read_sam
+from .metrics import gene_activity
+
+# This is extracted automatically by the top-level setup.py.
+__version__ = '2.5.2'
---
+++ packages/ngsfragments/ngsfragments-2.5.2-cp312-cp312-macosx_26_0_arm64.whl/ngsfragments/plot/__init__.py
@@ -0,0 +1,3 @@
+"""Init for mylibrary."""
+from .plot_plt import *
+from .plot_bokeh import *
