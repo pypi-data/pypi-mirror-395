9dd7e8f4344499cc27a6f09d413dc9ad95e1159c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 21:07:04 +0000

    Add package uranography/1.4.1/uranography-1.4.1-py3-none-any.whl

---
+++ packages/uranography/uranography-1.4.1-py3-none-any.whl/tests/test_readjs.py
@@ -0,0 +1,16 @@
+import unittest
+
+import uranography.readjs
+
+JS_FNAME = "update.js"
+
+
+class TestUpdateJs(unittest.TestCase):
+    def test_update_js(self):
+        js_code = uranography.readjs.read_javascript(JS_FNAME)
+        self.assertGreater(len(js_code), 10)
+        self.assertIsInstance(js_code, str)
+
+
+if __name__ == "__main__":
+    unittest.main()
