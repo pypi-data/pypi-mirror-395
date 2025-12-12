4e3274b511f07b9854c1a4a18c5200e35da54074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:08:51 +0000

    Add package navfitx/0.0.3a0/navfitx-0.0.3a0-py3-none-any.whl

---
+++ packages/navfitx/navfitx-0.0.3a0-py3-none-any.whl/navfitx/models.py
@@ -0,0 +1,400 @@
+import re
+from datetime import date
+from enum import Enum, StrEnum
+
+from pydantic import field_validator
+from sqlmodel import Field, SQLModel
+
+
+class Report(SQLModel, table=True):
+    """
+    A model for the Reports table in NAVFIT98A Access database.
+
+    Args:
+        report_id (int): Primary key for Reports database table.
+        parent
+    """
+
+    report_id: int = Field(primary_key=True)
+    parent: str
+    report_type: str
+    full_name: str
+    first_name: str
+    mi: str
+    last_name: str
+    suffix: str
+    rate: str
+    desig: str
+    ssn: str
+    active: bool
+    tar: bool
+    inactive: bool
+    atadsw: bool
+    uic: str
+    ship_station: str
+    promotion_status: str
+    date_reported: date
+    periodic: bool
+    det_ind: bool
+    frocking: bool
+    special: bool
+    from_date: date
+    to_date: date
+    nob: bool
+    regular: bool
+    concurrent: bool
+    ops_cdr: bool
+    physical_readiness: str
+    physical_readiness2: str
+    physical_readiness_dt: date
+    billet_subcat: str
+    rs_last_name: str
+    rs_fi: str
+    rs_mi: str
+    reporting_senior: str
+    rs_grade: str
+    rs_desig: str
+    rs_title: str
+    rs_uic: str
+    rs_ssn: str
+    achievements: str
+    primary_duty: str
+    duties: str
+    date_counseled: date
+    counselor: str
+    counselor_ln: str
+    counselor_fi: str
+    counselor_mi: str
+    prof: int
+    prof_dn1: str
+    prof_dn2: str
+    prof_dn3: str
+    qual: int
+    qual_dn1: str
+    qual_dn2: str
+    qual_dn3: str
+    eo: int
+    eo_dn1: str
+    eo_dn2: str
+    eo_dn3: str
+    mil: int
+    mil_dn1: str
+    mil_dn2: str
+    mil_dn3: str
+    pa: int
+    pa_dn1: str
+    pa_dn2: str
+    pa_dn3: str
+    team: int
+    team_dn1: str
+    team_dn2: str
+    team_dn3: str
+    lead: int
+    lead_dn1: str
+    lead_dn2: str
+    lead_dn3: str
+    mis: int
+    mis_dn1: str
+    mis_dn2: str
+    mis_dn3: str
+    tac: int
+    tac_dn1: str
+    tac_dn2: str
+    tac_dn3: str
+    recommend_1: str
+    recommend_2: str
+    rater: str
+    rater_date: date
+    comments_pitch: str
+    comments: str
+    qualifications: str
+    promotion_recom: int
+    summary_rank: int
+    summary_sp: str
+    summary_prog: str
+    summary_prom: str
+    summary_mp: str
+    summary_ep: str
+    retention_yes: bool
+    retention_no: bool
+    rsca: int
+    rs_address: str
+    rs_address1: str
+    rs_address2: str
+    rs_city: str
+    rs_state: str
+    rs_zip_cd: str
+    rs_phone: str
+    rs_dsn: str
+    senior_rater: str
+    senior_rater_date: date
+    statement_yes: bool
+    statement_no: bool
+    rs_info: str
+    rrs_fi: str
+    rrs_mi: str
+    rrs_last_name: str
+    rrs_grade: str
+    rrs_command: str
+    rrs_uic: str
+    user_comments: str
+    psswrd: str
+    standards: str
+    is_validated: str
+
+
+class SummaryGroup(Enum):
+    BLANK = ""
+    ACT = "ACT"
+    TAR = "TAR"
+    INACT = "INACT"
+    ATADSW = "ATADSW"
+
+
+class PromotionStatus(StrEnum):
+    BLANK = ""
+    REGULAR = "REGULAR"
+    FROCKED = "FROCKED"
+    SELECTED = "SELECTED"
+    SPOT = "SPOT"
+
+
+# class TypeOfReport(Enum):
+#     REGULAR = "Regular"
+#     CONCURRENT = "Concurrent"
+#     OpsCdr = "OpsCdr"
+
+
+class PhysicalReadiness(StrEnum):
+    BLANK = ""
+    PASS = "P"
+    BCA_PASS = "B"
+    FAIL = "F"
+    MED_WAIVED = "M"
+    WAIVED = "W"
+    NO_PFA = "N"
+
+
+class BilletSubcategory(StrEnum):
+    BLANK = ""
+    NA = "NA"
+    BASIC = "BASIC"
+    APPROVED = "APPROVED"
+    INDIV_AUGMENT = "INDIV AUG"
+    CO_AFLOAT = "CO AFLOAT"
+    CO_ASHORE = "CO ASHORE"
+    OIC = "OIC"
+    SEA_COMP = "SEA COMP"
+    CRF = "CRF"
+    CANVASSER = "CANVASSER"
+    RESIDENT = "RESIDENT"
+    INTERN = "INTERN"
+    INSTRUCTOR = "INSTRUCTOR"
+    STUDENT = "STUDENT"
+    RESAC1 = "RESAC1"
+    RESAC6 = "RESAC6"
+    SCREENED = "SCREENED"
+    SPECIAL01 = "SPECIAL01"
+    SPECIAL02 = "SPECIAL02"
+    SPECIAL03 = "SPECIAL03"
+    SPECIAL04 = "SPECIAL04"
+    SPECIAL05 = "SPECIAL05"
+    SPECIAL06 = "SPECIAL06"
+    SPECIAL07 = "SPECIAL07"
+    SPECIAL08 = "SPECIAL08"
+    SPECIAL09 = "SPECIAL09"
+    SPECIAL10 = "SPECIAL10"
+    SPECIAL11 = "SPECIAL11"
+    SPECIAL12 = "SPECIAL12"
+    SPECIAL13 = "SPECIAL13"
+    SPECIAL14 = "SPECIAL14"
+    SPECIAL15 = "SPECIAL15"
+    SPECIAL16 = "SPECIAL16"
+    SPECIAL17 = "SPECIAL17"
+    SPECIAL18 = "SPECIAL18"
+    SPECIAL19 = "SPECIAL19"
+    SPECIAL20 = "SPECIAL20"
+
+
+class PromotionRecommendation(Enum):
+    NOB = 0
+    SIGNIFICANT_PROBLEMS = 1
+    PROGRESSING = 2
+    PROMOTABLE = 3
+    MUST_PROMOTE = 4
+    EARLY_PROMOTE = 5
+
+
+class Fitrep(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None)
+    name: str = ""
+    grade: str = ""
+    desig: str = ""
+    ssn: str = ""
+    group: SummaryGroup = SummaryGroup.BLANK
+    # active: bool = False
+    # tar: bool = False
+    # inactive: bool = False
+    # atadsw: bool = False
+    uic: str = ""
+    station: str = ""
+    promotion_status: PromotionStatus = PromotionStatus.BLANK
+    date_reported: date | None = None
+    periodic: bool = False
+    det_indiv: bool = False
+    det_rs: bool = False
+    special: bool = False
+    period_start: date | None = None
+    period_end: date | None = None
+    not_observed: bool = False
+    regular: bool = False
+    concurrent: bool = False
+    ops_cdr: bool = False
+    physical_readiness: PhysicalReadiness = PhysicalReadiness.BLANK
+    billet_subcategory: BilletSubcategory = BilletSubcategory.BLANK
+    senior_name: str = ""
+    senior_grade: str = ""
+    senior_desig: str = ""
+    senior_title: str = ""
+    senior_uic: str = ""
+    senior_ssn: str = ""
+    job: str = ""
+    duties_abbreviation: str = ""
+    duties_description: str = ""
+    date_counseled: date | None = None
+    counselor: str = ""
+    pro_expertise: int | None = None
+    cmd_climate: int | None = None
+    bearing_and_character: int | None = None
+    teamwork: int | None = None
+    accomp_and_initiative: int | None = None
+    leadership: int | None = None
+    tactical_performance: int | None = None
+    career_rec_1: str = ""
+    career_rec_2: str = ""
+    comments: str = ""
+    indiv_promo_rec: int | None = None
+    # summary_promo_rec: int = Field(default_factory=list)
+    senior_address: str = ""
+
+    @classmethod
+    @field_validator("name")
+    def validate_name(cls, name: str) -> str:
+        if len(name) > 27:
+            raise ValueError("Name must be 27 characters or less")
+        return name.upper()
+
+    @classmethod
+    @field_validator("grade")
+    def validate_grade(cls, grade: str) -> str:
+        if len(grade) > 5:
+            raise ValueError("Rate must be 5 characters or less")
+        return grade
+
+    @classmethod
+    @field_validator("desig")
+    def validate_desig(cls, desig: str) -> str:
+        if len(desig) > 12:
+            raise ValueError("Designator must be 12 characters or less")
+        return desig.upper()
+
+    @classmethod
+    @field_validator("ssn")
+    def validate_ssn(cls, ssn: str) -> str:
+        ssn = ssn.strip()
+        ssn_pattern = re.compile(r"^\d{3}-\d{2}-\d{4}$")
+        if not ssn_pattern.match(ssn):
+            raise ValueError("SSN must be in the format XXX-XX-XXXX")
+        return ssn
+
+    @classmethod
+    @field_validator("group")
+    def validate_group(cls, group: SummaryGroup | None) -> SummaryGroup:
+        if group is None:
+            raise ValueError("Summary Group must be specified.")
+        return group
+
+    @classmethod
+    @field_validator("uic")
+    def validate_uic(cls, uic: str) -> str:
+        if len(uic) > 5:
+            raise ValueError("UIC must be 5 characters or less.")
+        if not uic.isalnum():
+            raise ValueError("UIC must be alphanumeric.")
+        return uic.upper()
+
+    @classmethod
+    @field_validator("station")
+    def validate_station(cls, station: str) -> str:
+        if len(station) > 18:
+            raise ValueError("Station must be 18 characters or less.")
+        return station
+
+    @classmethod
+    @field_validator("promotion_status")
+    def validate_promotion_status(cls, promotion_status: PromotionStatus | None) -> PromotionStatus:
+        if promotion_status is None:
+            raise ValueError("Promotion Status must be specified.")
+        return promotion_status
+
+    @classmethod
+    @field_validator("billet_subcategory")
+    def validate_billet_subcategory(cls, billet_subcategory: BilletSubcategory | None) -> BilletSubcategory:
+        if billet_subcategory is None:
+            raise ValueError("Billet Subcategory must be specified.")
+        return billet_subcategory
+
+    @classmethod
+    @field_validator("career_rec_2", "career_rec_1")
+    def validate_career_rec(cls, career_rec: str) -> str:
+        if len(career_rec) > 20:
+            raise ValueError("Career Recommendation must be 20 characters or less (including whitespace).")
+        return career_rec
+
+    def member_trait_avg(self) -> str:
+        traits = [
+            self.pro_expertise,
+            self.cmd_climate,
+            self.bearing_and_character,
+            self.teamwork,
+            self.accomp_and_initiative,
+            self.leadership,
+            self.tactical_performance,
+        ]
+        observed = 0
+        total = 0
+        for trait in traits:
+            if trait:
+                observed += 1
+                total += trait
+        if observed != 0:
+            avg = total / observed
+            return f"{avg:.2f}"
+        return "0.00"
+
+    def summary_group_avg(self) -> str:
+        """
+        Get the text representation of the summary group average.
+        """
+        return "0.00"
+
+    def generate_report(self) -> Report:
+        return Report(
+            report_type="FitRep",
+            full_name=self.name,
+            desig=self.desig,
+            ssn=self.ssn,
+            uic=self.uic,
+            ship_station=self.station,
+            from_date=self.period_start,
+            to_date=self.period_end,
+            reporting_senior=self.senior_name,
+            rs_grade=self.senior_grade,
+            rs_desig=self.senior_desig,
+            rs_title=self.senior_title,
+            rs_uic=self.senior_uic,
+            rs_ssn=self.senior_ssn,
+            billet_subcat=self.billet_subcategory,
+            date_counseled=self.date_counseled,
+            counselor=self.counselor,
+        )
---
+++ packages/navfitx/navfitx-0.0.3a0-py3-none-any.whl/navfitx/overlay.py
@@ -0,0 +1,375 @@
+import textwrap
+import webbrowser
+from datetime import date
+from pathlib import Path
+
+import pymupdf
+import typer
+from pymupdf import Point
+from typing_extensions import Annotated
+
+from navfitx.utils import get_blank_report_path
+
+from .models import (
+    BilletSubcategory,
+    Fitrep,
+    PhysicalReadiness,
+    PromotionRecommendation,
+    PromotionStatus,
+    SummaryGroup,
+)
+
+
+def format_comments(txt: str) -> str:
+    """
+    Format the Comments on Performance text so that it conforms to the constraints:
+    - Max of 92 chars per line (not including newlines)
+    - Max of 18 lines
+
+    Note:
+        Soley using the wrap or fill function from the textwrap module to format the comments completely
+        isn't quite sufficient because it doesn't appropriately handle cases when users want
+        to add empty lines to the comments. The textwrap module by default eliminates newlines. This behavior
+        can be disabled, but then newlines are counted as characters that count towards the character limit
+        for each line. This function handles each situation appropriately.
+    """
+    parts = txt.split("\n")
+    all_lines = []
+    for part in parts:
+        lines = textwrap.wrap(part, 92)
+        if not lines:
+            lines = [""]
+        all_lines.extend(lines)
+    return "\n".join(all_lines)
+
+
+def wrap_duty_desc(text: str) -> str:
+    """
+    The duties description is weird because the user manual says the constraint on this
+    box is "up to 334 alhphanumeric characters without spaces".
+    In practice, there are 4 lines. The first can have up to 69 characters (not including newline),
+    and the next three can have up to 91 charaters (not including newline).
+    """
+    text = 22 * " " + text
+    text = textwrap.fill(text, width=91)
+    return text
+
+
+def format_job(text: str) -> str:
+    """Formats the 'Command employment and command achievements' to fit within the constraints of the FITREP form."""
+    text = textwrap.fill(text, width=91)
+    return text
+
+
+def format_date(dt: date | None) -> str:
+    """Formats a date object into the Fitrep date format (YYMMMDD)."""
+    if not dt:
+        return ""
+    return dt.strftime("%y%b%d").upper()
+
+
+def get_group_point(fitrep: Fitrep) -> Point | None:
+    if fitrep.group == SummaryGroup.ACT:
+        return Point(33, 64)
+    if fitrep.group == SummaryGroup.TAR:
+        return Point(63, 64)
+    if fitrep.group == SummaryGroup.INACT:
+        return Point(92, 64)
+    if fitrep.group == SummaryGroup.ATADSW:
+        return Point(120, 64)
+    return None
+
+
+def get_occasion_points(fitrep: Fitrep) -> set[Point]:
+    ret: set[Point] = set()
+    if fitrep.periodic:
+        ret.add(Point(76, 88))
+    if fitrep.det_indiv:
+        ret.add(Point(157, 88))
+    if fitrep.det_rs:
+        ret.add(Point(251, 88))
+    if fitrep.special:
+        ret.add(Point(329, 88))
+    return ret
+
+
+# def get_type_of_report_point(fitrep: Fitrep) -> Point:
+#     if TypeOfReport.REGULAR == fitrep.type_of_report:
+#         return Point(76, 130)
+
+
+def get_perfomance_points(fitrep: Fitrep) -> set[Point]:
+    ret = set()
+    match fitrep.pro_expertise:
+        case 0:
+            ret.add(Point(76, 403))
+        case 1:
+            ret.add(Point(205, 403))
+        case 2:
+            ret.add(Point(241, 403))
+        case 3:
+            ret.add(Point(377, 403))
+        case 4:
+            ret.add(Point(414, 403))
+        case 5:
+            ret.add(Point(551, 403))
+    match fitrep.cmd_climate:
+        case 0:
+            ret.add(Point(76, 486))
+        case 1:
+            ret.add(Point(205, 486))
+        case 2:
+            ret.add(Point(241, 486))
+        case 3:
+            ret.add(Point(377, 486))
+        case 4:
+            ret.add(Point(414, 486))
+        case 5:
+            ret.add(Point(551, 486))
+    match fitrep.bearing_and_character:
+        case 0:
+            ret.add(Point(76, 571))
+        case 1:
+            ret.add(Point(205, 571))
+        case 2:
+            ret.add(Point(241, 571))
+        case 3:
+            ret.add(Point(377, 571))
+        case 4:
+            ret.add(Point(414, 571))
+        case 5:
+            ret.add(Point(551, 571))
+    match fitrep.teamwork:
+        case 0:
+            ret.add(Point(76, 655))
+        case 1:
+            ret.add(Point(205, 655))
+        case 2:
+            ret.add(Point(241, 655))
+        case 3:
+            ret.add(Point(377, 655))
+        case 4:
+            ret.add(Point(414, 655))
+        case 5:
+            ret.add(Point(551, 655))
+    match fitrep.accomp_and_initiative:
+        case 0:
+            ret.add(Point(76, 739))
+        case 1:
+            ret.add(Point(205, 739))
+        case 2:
+            ret.add(Point(241, 739))
+        case 3:
+            ret.add(Point(377, 739))
+        case 4:
+            ret.add(Point(414, 739))
+        case 5:
+            ret.add(Point(551, 739))
+    return ret
+
+
+def get_perf_points_back(fitrep: Fitrep) -> set[Point]:
+    ret = set()
+    match fitrep.leadership:
+        case 0:
+            ret.add(Point(76, 186))
+        case 1:
+            ret.add(Point(205, 186))
+        case 2:
+            ret.add(Point(241, 186))
+        case 3:
+            ret.add(Point(377, 186))
+        case 4:
+            ret.add(Point(414, 186))
+        case 5:
+            ret.add(Point(551, 186))
+
+    match fitrep.tactical_performance:
+        case 0:
+            ret.add(Point(76, 282))
+        case 1:
+            ret.add(Point(205, 282))
+        case 2:
+            ret.add(Point(241, 282))
+        case 3:
+            ret.add(Point(377, 282))
+        case 4:
+            ret.add(Point(414, 282))
+        case 5:
+            ret.add(Point(551, 282))
+    return ret
+
+
+def get_promo_rec_point(fitrep: Fitrep) -> Point:
+    """Returns a Point where 'X' should be drawn given the fitrep's
+    Promotion Reccomendation.
+    """
+    match fitrep.indiv_promo_rec:
+        case PromotionRecommendation.NOB:
+            return Point(101, 606)
+        case PromotionRecommendation.SIGNIFICANT_PROBLEMS:
+            return Point(151, 606)
+        case PromotionRecommendation.PROGRESSING:
+            return Point(202, 606)
+        case PromotionRecommendation.PROMOTABLE:
+            return Point(253, 606)
+        case PromotionRecommendation.MUST_PROMOTE:
+            return Point(304, 606)
+        case PromotionRecommendation.EARLY_PROMOTE:
+            return Point(355, 606)
+        case _:
+            raise Exception()
+
+
+def create_eval_pdf():
+    """Fills out an Eval PDF with the provided Eval data."""
+    pass
+    # blank_eval = get_blank_report_path("eval")
+    # doc = pymupdf.open(str(blank_eval))
+    # front = doc[0]
+    # back = doc[1]
+
+
+def create_fitrep_pdf(fitrep: Fitrep, path: Path) -> None:
+    """Fills out a FITREP PDF report with the provided FITREP data."""
+    blank_fitrep = get_blank_report_path("fitrep")
+    doc = pymupdf.open(str(blank_fitrep))
+    meta = doc.metadata
+    meta["title"] = f"FITREP - {fitrep.name}"
+    doc.set_metadata(meta)
+    front = doc[0]
+    back = doc[1]
+    front.insert_text(Point(22, 43), fitrep.name, fontsize=12, fontname="Cour")
+    back.insert_text(Point(22, 43), fitrep.name, fontsize=12, fontname="Cour")
+    front.insert_text(Point(292, 43), fitrep.grade, fontsize=12, fontname="Cour")
+    back.insert_text(Point(292, 43), fitrep.grade, fontsize=12, fontname="Cour")
+    front.insert_text(Point(360, 43), fitrep.desig, fontsize=12, fontname="Cour")
+    back.insert_text(Point(360, 43), fitrep.desig, fontsize=12, fontname="Cour")
+    front.insert_text(Point(460, 43), fitrep.ssn, fontsize=12, fontname="Cour")
+    back.insert_text(Point(460, 43), fitrep.ssn, fontsize=12, fontname="Cour")
+    if group_point := get_group_point(fitrep):
+        front.insert_text(group_point, "X", fontsize=12, fontname="Cour")
+    front.insert_text(Point(170, 67), fitrep.uic, fontsize=12, fontname="Cour")
+    front.insert_text(Point(223, 67), fitrep.station, fontsize=12, fontname="Cour")
+    front.insert_text(Point(416, 67), fitrep.promotion_status, fontsize=12, fontname="Cour")
+    report_date_str = format_date(fitrep.date_reported)
+    front.insert_text(Point(496, 67), report_date_str, fontsize=12, fontname="Cour")
+
+    for point in get_occasion_points(fitrep):
+        front.insert_text(point, "X", fontsize=12, fontname="Cour")
+
+    from_date_str = format_date(fitrep.period_start)
+    front.insert_text(Point(395, 92), from_date_str, fontsize=12, fontname="Cour")
+    to_date_str = format_date(fitrep.period_end)
+    front.insert_text(Point(494, 92), to_date_str, fontsize=12, fontname="Cour")
+    if fitrep.not_observed:
+        front.insert_text(Point(77, 112), "X", fontsize=12, fontname="Cour")
+
+    front.insert_text(Point(156, 112), "X", fontsize=12, fontname="Cour")
+
+    front.insert_text(Point(361, 115), fitrep.physical_readiness, fontsize=12, fontname="Cour")
+    front.insert_text(Point(460, 115), fitrep.billet_subcategory, fontsize=12, fontname="Cour")
+
+    front.insert_text(Point(22, 140), fitrep.senior_name, fontsize=12, fontname="Cour")
+    front.insert_text(Point(172, 140), fitrep.senior_grade, fontsize=12, fontname="Cour")
+    front.insert_text(Point(222, 140), fitrep.senior_desig, fontsize=12, fontname="Cour")
+    front.insert_text(Point(273, 140), fitrep.senior_title, fontsize=12, fontname="Cour")
+    front.insert_text(Point(405, 140), fitrep.senior_uic, fontsize=12, fontname="Cour")
+    front.insert_text(Point(461, 140), fitrep.senior_ssn, fontsize=12, fontname="Cour")
+
+    front.insert_text(Point(24, 164), format_job(fitrep.job), fontsize=10, fontname="Cour", lineheight=1.0)
+    front.insert_text(Point(28, 212), fitrep.duties_abbreviation, fontsize=12, fontname="Cour")
+
+    duties_desc = wrap_duty_desc(fitrep.duties_description)
+    front.insert_text(Point(24, 212), duties_desc, fontsize=10, fontname="Cour", lineheight=1.0)
+
+    for point in get_perfomance_points(fitrep):
+        front.insert_text(point, "X", fontsize=12, fontname="Cour")
+
+    counsel_date_str = format_date(fitrep.date_counseled)
+    front.insert_text(Point(200, 272), counsel_date_str, fontsize=12, fontname="Cour")
+
+    front.insert_text(Point(279, 272), fitrep.counselor, fontsize=12, fontname="Cour")
+
+    for point in get_perf_points_back(fitrep):
+        back.insert_text(point, "X", fontsize=12, fontname="Cour")
+
+    back.insert_text(
+        Point(34, 354),
+        format_comments(fitrep.comments),
+        fontsize=9.2,
+        fontname="Cour",
+    )
+
+    back.insert_text(Point(388, 586), fitrep.senior_address, fontsize=9, fontname="Cour", lineheight=1.1)
+    back.insert_text(Point(105, 694), fitrep.member_trait_avg(), fontsize=12, fontname="Cour")
+    back.insert_text(Point(240, 694), fitrep.summary_group_avg(), fontsize=12, fontname="Cour")
+    back.insert_text(Point(370, 300), textwrap.fill(fitrep.career_rec_1, 13), fontsize=10, fontname="Cour")
+    back.insert_text(Point(467, 300), textwrap.fill(fitrep.career_rec_2, 13), fontsize=10, fontname="Cour")
+
+    doc.save(str(path))
+    doc.close()
+
+
+app = typer.Typer(no_args_is_help=True, add_completion=False)
+
+
+@app.command()
+def overlay_fitrep(
+    output: Annotated[Path, typer.Option(help="Path to the output file", dir_okay=False, writable=True)],
+):
+    """
+    Test creating a FITREP PDF with hardcoded data.
+    """
+    comments = (
+        "*MY #5 OF 19 LTJGs AMONG VERY COMPETITIVE PEERS! EXTREMELY TALENTED LEADER AND DEVELOPER!*\n\n"
+        "-MULTIDISCIPLINARY EXPERT. Actively identifies rare, high-demand skills to address key expertise gaps. Utilizes self-taught methods to reverse-engineer computer hardware, overcoming obstacles to critical vulnerability research and exploit development essential to mission success. Developed a solution to a major data analytics problem that had significantly impacted mission operations.\n"
+        "-DISTINGUISHED TECHNICAL LEADER. Hand-selected to join a team of senior developers at a partner site; promptly received recognition from partner's Commanding Officer for exceptional contributions, then solicited to apply for an O3 billet despite current grade of O2. Following these successes, chosen by leadership to be a Division Officer to lead Sailors at partner site, enhancing department's capacity to adapt to evolving requirements.\n"
+        "-PROACTIVE LEARNER. Dedicated substantial personal time to obtain the industry's leading certificate in offensive security; resulted in qualification of key role within partner organization and increased department's capacity to meet operational requirements.\n\n"
+        "PRESS 100 NOW"
+    )
+    lines = format_comments(comments)
+    fitrep = Fitrep(
+        name="WHITE, TRISTAN K",
+        rate="LTJG",
+        desig="1840",
+        ssn="123-45-6789",
+        uic="46439",
+        station="NAVCYBERWARDEVGRU",
+        group=SummaryGroup.ATADSW,
+        promotion_status=PromotionStatus.REGULAR,
+        date_reported=date(2022, 9, 9),
+        periodic=True,
+        det_indiv=True,
+        det_rs=True,
+        special=False,
+        period_start=date(2024, 5, 27),
+        period_end=date(2025, 2, 28),
+        not_observed=True,
+        physical_readiness=PhysicalReadiness.PASS,
+        billet_subcategory=BilletSubcategory.NA,
+        senior_name="LAST, FI MI",
+        senior_grade="CAPT",
+        senior_desig="1810",
+        senior_title="CO",
+        senior_uic="46439",
+        senior_ssn="123-45-6789",
+        job="Conducts information and Cyber Warfare R&D, intelligence, and operations. Develops deep\ntechnial understanding of adversary systems, discovers vulnerabilities, and builds\ncountermeasures using rapid prototyping and acquisition authorities.",
+        duties_abbreviation="12345678901234",
+        duties_description="Division Officer-3. Led four sailors in daily execution of Naval and Joint missions. Cyberspace Operations (CO) Developer-12. Researches technologies and develops custom cyber tools to support Navy and national level mission priorities. Cyber Operator Trainee-2. PFA: CY2024.",
+        date_counseled=date(2024, 5, 15),
+        counselor="LAST, FIRST MI",
+        pro_expertise=3,
+        bearing_and_character=4,
+        cmd_climate=4,
+        teamwork=5,
+        accomp_and_initiative=4,
+        leadership=5,
+        tactical_performance=3,
+        career_rec_1="1234567890123\n1234567",
+        career_rec_2="1234567890123\n1234567",
+        comments=lines,
+        senior_address="12345\n12345\n12345\n12345\n12345",
+    )
+    create_fitrep_pdf(fitrep, output)
+    webbrowser.open(str(output))
