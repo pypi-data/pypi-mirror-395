1d9d351a6eb55ace8cc4104f755c3ea860ceaa18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 09:06:25 +0000

    Add package Suryaansh/0.0.1/suryaansh-0.0.1.tar.gz

---
+++ packages/Suryaansh/suryaansh-0.0.1.tar.gz/suryaansh-0.0.1/README.md
@@ -0,0 +1,15 @@
+# FOR SALE
+
+A simple package that prints "happy holidays" when imported.
+
+## Installation
+
+```bash
+pip install pak-name
+```
+
+## Usage
+
+```python
+import pak-name
+```
---
+++ packages/Suryaansh/suryaansh-0.0.1.tar.gz/suryaansh-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Suryaansh",
+    version="0.0.1",
+    description="the is my read me",
+    author="ULTRON",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
