00aa40b0534baa65a255e90b58fbde032ef90019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:16 +0000

    Add package KratosContactStructuralMechanicsApplication/10.4.0/kratoscontactstructuralmechanicsapplication-10.4.0-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/KratosContactStructuralMechanicsApplication/kratoscontactstructuralmechanicsapplication-10.4.0-cp312-cp312-manylinux_2_28_x86_64.whl/KratosMultiphysics/ContactStructuralMechanicsApplication/ActiveSetUtilities.pyi
@@ -0,0 +1,7 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics as Kratos
+
+def ComputePenaltyFrictionalActiveSet(*args, **kwargs):
+    """ComputePenaltyFrictionalActiveSet(arg0: Kratos.ModelPart, arg1: bool, arg2: int) -> Kratos::array_1d<unsigned long, 2ul>"""
+def ComputePenaltyFrictionlessActiveSet(arg0: Kratos.ModelPart) -> int:
+    """ComputePenaltyFrictionlessActiveSet(arg0: Kratos.ModelPart) -> int"""
