717db237c1b8864dd57f9099a64147f09387384d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:45 +0000

    Add package zowe-zos-files-for-zowe-sdk/1.0.0.dev24/zowe_zos_files_for_zowe_sdk-1.0.0.dev24-py3-none-any.whl

---
+++ packages/zowe-zos-files-for-zowe-sdk/zowe_zos_files_for_zowe_sdk-1.0.0.dev24-py3-none-any.whl/zowe/zos_files_for_zowe_sdk/constants.py
@@ -0,0 +1,25 @@
+"""Zowe Client Python SDK.
+
+This program and the accompanying materials are made available under the terms of the
+Eclipse Public License v2.0 which accompanies this distribution, and is available at
+
+https://www.eclipse.org/legal/epl-v20.html
+
+SPDX-License-Identifier: EPL-2.0
+
+Copyright Contributors to the Zowe Project.
+"""
+
+zos_file_constants = {
+    "MaxAllocationQuantity": 16777215,
+    "ZoweFilesDefaultEncoding": "utf-8",
+}
+from enum import Enum
+
+
+class FileType(Enum):
+    """Class used to represent type of files."""
+
+    BINARY = "binary"
+    EXECUTABLE = "executable"
+    TEXT = "text"
