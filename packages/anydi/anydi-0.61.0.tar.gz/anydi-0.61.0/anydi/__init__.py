4541ad6f4d22435da66cf81a5f18e35367a59464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:05:01 +0000

    Add package anydi/0.61.0/anydi-0.61.0.tar.gz

---
+++ packages/anydi/anydi-0.61.0.tar.gz/anydi-0.61.0/anydi/__init__.py
@@ -0,0 +1,29 @@
+"""AnyDI public objects and functions."""
+
+from ._container import Container, import_container
+from ._decorators import injectable, provided, provider, request, singleton, transient
+from ._module import Module
+from ._provider import ProviderDef as Provider
+from ._types import Inject, Provide, Scope
+
+# Alias for dependency auto marker
+# TODO: deprecate it
+auto = Inject()
+
+
+__all__ = [
+    "Container",
+    "Inject",
+    "Module",
+    "Provide",
+    "Provider",
+    "Scope",
+    "auto",
+    "import_container",
+    "injectable",
+    "provided",
+    "provider",
+    "request",
+    "singleton",
+    "transient",
+]
