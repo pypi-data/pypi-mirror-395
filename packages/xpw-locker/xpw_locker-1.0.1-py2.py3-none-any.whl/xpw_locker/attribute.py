1bdc9224a5dd0fed466fd2b78f3b05669cd5a048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:05:02 +0000

    Add package xpw-locker/1.0.1/xpw_locker-1.0.1-py2.py3-none-any.whl

---
+++ packages/xpw-locker/xpw_locker-1.0.1-py2.py3-none-any.whl/xpw_locker/attribute.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+
+__project__ = "xpw-locker"
+__version__ = "1.0.1"
+__urlhome__ = "https://github.com/checkeys/locker/"
+__description__ = "Access authentication"
+__official_name__ = "Locker"
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
