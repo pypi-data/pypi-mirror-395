50540e906aa46e5ed90558f5dd8fda0dafbe0121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 09:06:40 +0000

    Add package ahumai/0.0.1/ahumai-0.0.1.tar.gz

---
+++ packages/ahumai/ahumai-0.0.1.tar.gz/ahumai-0.0.1/README.md
@@ -0,0 +1,15 @@
+# ahumai
+
+A simple package that prints "happy holidays" when imported.
+
+## Installation
+
+```bash
+pip install ahumai
+```
+
+## Usage
+
+```python
+import ahumai
+```
---
+++ packages/ahumai/ahumai-0.0.1.tar.gz/ahumai-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ahumai",
+    version="0.0.1",
+    description="A simple package that prints happy holidays",
+    author="Your Name",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
