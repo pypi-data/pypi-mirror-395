a7ffadab57a5ce662d0b19631b016532a1def48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 21:08:17 +0000

    Add package wigglystuff/0.2.3/wigglystuff-0.2.3-py3-none-any.whl

---
+++ packages/wigglystuff/wigglystuff-0.2.3-py3-none-any.whl/wigglystuff/__init__.py
@@ -0,0 +1,29 @@
+"""Public widget exports for the wigglystuff package."""
+
+from .color_picker import ColorPicker
+from .copy_to_clipboard import CopyToClipboard
+from .edge_draw import EdgeDraw
+from .gamepad import GamepadWidget
+from .keystroke import KeystrokeWidget
+from .matrix import Matrix
+from .paint import Paint
+from .slider2d import Slider2D
+from .sortable_list import SortableList
+from .tangle import TangleChoice, TangleSelect, TangleSlider
+from .talk import WebkitSpeechToTextWidget
+
+__all__ = [
+    "ColorPicker",
+    "CopyToClipboard",
+    "EdgeDraw",
+    "GamepadWidget",
+    "KeystrokeWidget",
+    "Matrix",
+    "Paint",
+    "Slider2D",
+    "SortableList",
+    "TangleChoice",
+    "TangleSelect",
+    "TangleSlider",
+    "WebkitSpeechToTextWidget",
+]
