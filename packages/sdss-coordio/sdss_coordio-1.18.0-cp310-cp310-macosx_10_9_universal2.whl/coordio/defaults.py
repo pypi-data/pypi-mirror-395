c3d3c2f3542aac73ae98da8eec64db77b038b3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 15:05:42 +0000

    Add package sdss-coordio/1.18.0/sdss_coordio-1.18.0-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/sdss-coordio/sdss_coordio-1.18.0-cp310-cp310-macosx_10_9_universal2.whl/coordio/defaults.py
@@ -0,0 +1,276 @@
+import os
+
+import pandas as pd
+
+from coordio.calibration import Calibration
+
+from .exceptions import CoordIOError
+
+
+# default/constant values collected here...for now
+
+# a scale factor between xy focal and xy wok
+# JSG: setting this to 1.0. We may remove it eventually.
+# FOCAL_SCALE = 1.0
+
+# SITE_TO_SCALE cale is focal plane scale mm / wok scale mm
+# found via dither analysis
+SITE_TO_SCALE = {"LCO": 0.9999588, "APO": 0.9998082}
+
+MICRONS_PER_MM = 1000
+GFA_PIXEL_SIZE = 13.5  # micron
+GFA_CHIP_CENTER = 1024  # unbinned pixels
+
+EPOCH = 2451545.0  # J2000
+WAVELENGTH = 6231.0  # angstrom, GFA wavelength
+VALID_WAVELENGTHS = set([5400.0, 6231.0, 16600.0])
+
+# wavelengths in angstroms
+WAVE_TO_INST = {5400.0: "Boss", 6231.0: "GFA", 16600.0: "Apogee"}  # sdss-r band
+
+INST_TO_WAVE = {"Boss": 5400.0, "GFA": 6231.0, "Apogee": 16600.0}
+
+PLATE_SCALE = {"APO": 217.7358, "LCO": 330.275}  # mm/deg
+
+POSITIONER_HEIGHT = 143.1  # mm, distance from wok surface to fiber
+ALPHA_LEN = 7.4  # mm, length of the alpha arm (distance between alpha and beta axes)
+BETA_LEN = 16.2  # mm, distance from beta axis to top flat edge of the robot
+# extracted from ZEMAX model model
+# z location of wok vertex in FP coords
+APO_WOK_Z_OFFSET_ZEMAX = -776.4791 - POSITIONER_HEIGHT
+LCO_WOK_Z_OFFSET_ZEMAX = -993.0665 - POSITIONER_HEIGHT
+
+APO_MAX_FIELD_R = 1.5  # max field radius (deg)
+LCO_MAX_FIELD_R = 1.1  # max field radius (deg)
+
+# nominal fiber positions in beta arm frame
+MET_BETA_XY = [14.314, 0]
+# boss xy position in solid model
+BOSS_BETA_XY = [14.965, -0.376]
+# apogee xy position in solid model
+AP_BETA_XY = [14.965, 0.376]
+
+# default orientation of tangent to a flat wok
+IHAT = [0, -1, 0]
+JHAT = [1, 0, 0]
+KHAT = [0, 0, 1]
+
+
+def getFPModelParams(site, direction, waveCat):
+    """Find the right focal plane model to use given direction and waveCat.
+
+    Parameters
+    ----------
+    site : str
+        The site string.
+    direction : str
+        "focal" or "field" for the direction of the conversion
+    waveCat : str
+        "Apogee", "Boss", or "GFA" for the wavelength
+
+    Returns
+    ---------
+    R: float
+        radius of spherical curvature (mm)
+    b: float
+        z-location of sphere center in focal plane coordinate system
+    c0: float
+        1st order coeff
+    c1: float
+        3rd order coeff
+    c2: float
+        5th order coeff
+    c3: float
+        7th order coeff
+    c4: float
+        9th order coeff
+    """
+
+    # filter for correct row in the model dataframe
+    if direction not in ["focal", "field"]:
+        raise CoordIOError(
+            "direction must be one of focal or field, got %s" % direction
+        )
+    if waveCat not in ["Apogee", "Boss", "GFA"]:
+        raise CoordIOError(
+            "waveCat must be one of Apogee Boss or GFA, got %s" % waveCat
+        )
+
+    row = calibration.FP_MODEL.loc[(site, direction, waveCat)]
+
+    R = float(row.R)
+    b = float(row.b)
+    c0 = float(row.c0)
+    c1 = float(row.c1)
+    c2 = float(row.c2)
+    c3 = float(row.c3)
+    c4 = float(row.c4)
+    return R, b, c0, c1, c2, c3, c4
+
+
+def getWokOrient(site):
+    """Return the wok orientation from the wok calibration files.
+
+    Returns
+    --------
+    x : float
+        x position of wok vertex in focal plane coords mm
+    y : float
+        y position of wok vertex in focal plane coords mm
+    z : float
+        z position of wok vertex in focal plane coords mm
+    xTilt : float
+        tilt of wok coord sys about focal plane x axis deg
+    yTilt : float
+        tilt of wok coord sys about focal plane y axis deg
+    """
+
+    row = calibration.wokOrient.loc[site]
+    x = float(row.x)
+    y = float(row.y)
+    z = float(row.z)
+    xTilt = float(row.xTilt)
+    yTilt = float(row.yTilt)
+
+    return x, y, z, xTilt, yTilt
+
+
+def getHoleOrient(site, holeID):
+    """Return orientation of hole position in the wok.
+
+    Parameters
+    ----------
+    site : str
+        The site string.
+    holeID : str or list of str
+        Either a string with a valid holeID (e.g. R+13C1) or a list of
+        valid holeIDs. In the latter case, the returned arrays are
+        2D with axis 1 being the holeID dimmension.
+
+    Returns
+    --------
+    b : numpy.ndarray
+        [x,y,z] base position of hole in wok coords (mm)
+    iHat : numpy.ndarray
+        [x,y,z] unit vector, direction of x Tangent in wok coords
+    jHat : numpy.ndarray
+        [x,y,z] unit vector, direction of y Tangent in wok coords
+    kHat : numpy.ndarray
+        [x,y,z] unit vector, direction of z Tangent in wok coords
+    """
+
+    wokCoordsH = calibration.wokCoords.loc[(site, holeID)]
+
+    b = wokCoordsH.loc[["xWok", "yWok", "zWok"]].to_numpy()
+    iHat = wokCoordsH.loc[["ix", "iy", "iz"]].to_numpy()
+    jHat = wokCoordsH.loc[["jx", "jy", "jz"]].to_numpy()
+    kHat = wokCoordsH.loc[["kx", "ky", "kz"]].to_numpy()
+
+    return b, iHat, jHat, kHat
+
+
+def getPositionerData(site, holeID):
+    """Return data specific to a positioner.
+
+    Returns:
+    --------
+    alphaArmLength : float
+        alpha arm length (mm)
+    metX : float
+        metrology fiber x (mm), in beta arm coordinates
+    metY : float
+        metrology fiber y (mm), in beta arm coordinates
+    apX : float
+        apogee fiber y (mm), in beta arm coordinates
+    apY : float
+        apogee fiber x (mm), in beta arm coordinates
+    bossX : float
+        boss fiber y (mm), in beta arm coordinates
+    bossY : float
+        boss fiber x (mm), in beta arm coordinates
+    alphaOffset : float
+        alpha arm offset at (0, 0), in degrees
+    betaOffset : float
+        beta arm offset at (0, 0), in degrees
+    dx: float
+        y offset (mm), calibration to capture small displacements of
+        tangent y
+    dy: float
+        z offset (mm), calibration to capture small displacements of
+        tangent x
+
+    """
+
+    if holeID not in calibration.VALID_HOLE_IDS:
+        raise CoordIOError("%s is not a valid hole ID" % holeID)
+
+    row = calibration.positionerTable.loc[(site, holeID)]
+
+    aal = float(row.alphaArmLen)
+    metX = float(row.metX)
+    metY = float(row.metY)
+    apX = float(row.apX)
+    apY = float(row.apY)
+    bossX = float(row.bossX)
+    bossY = float(row.bossY)
+    alphaOffset = float(row.alphaOffset)
+    betaOffset = float(row.betaOffset)
+    dx = float(row.dx)
+    dy = float(row.dy)
+
+    return aal, metX, metY, apX, apY, bossX, bossY, alphaOffset, betaOffset, dx, dy
+
+
+def parseDesignRef():
+    designRefFile = os.path.join(calibration.paths[0], "fps_DesignReference.txt")
+    _row = []
+    _col = []
+    _xWok = []
+    _yWok = []
+    _fType = []
+    _holeName = []
+
+    with open(designRefFile, "r") as f:
+        lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        line = line.split("#")[0]
+        if not line:
+            continue
+        row, col, x, y, fType = line.split()
+
+        col = int(col)
+        row = int(row)
+
+        # make col 1 indexed to match pdf maps
+        # and wok hole naming convention
+        if row == -99:
+            holeName = "F%i" % col
+            col = None
+            row = None
+
+        elif row <= 0:
+            holeName = "R%iC%i" % (row, col + 1)
+        else:
+            holeName = "R+%iC%i" % (row, col + 1)
+
+        _row.append(row)
+        _col.append(col)
+        _xWok.append(float(x))
+        _yWok.append(float(y))
+        _fType.append(fType)
+        _holeName.append(holeName)
+
+    d = {}
+    d["holeName"] = _holeName
+    d["row"] = _row
+    d["col"] = _col
+    d["xWok"] = _xWok
+    d["yWok"] = _yWok
+    d["fType"] = _fType
+
+    df = pd.DataFrame(d)
+    return df
+
+
+calibration = Calibration()
---
+++ packages/sdss-coordio/sdss_coordio-1.18.0-cp310-cp310-macosx_10_9_universal2.whl/coordio/telescope.py
@@ -0,0 +1,384 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy
+
+from . import conv, defaults
+from .conv import cart2FieldAngle, sph2Cart
+from .coordinate import (
+    Coordinate,
+    Coordinate2D,
+    Coordinate3D,
+    verifySite,
+    verifyWavelength
+)
+from .exceptions import CoordIOError
+
+
+if TYPE_CHECKING:
+    from .site import Site
+    from .sky import Observed
+
+
+__all__ = ["Field", "FocalPlane"]
+
+
+class Field(Coordinate2D):
+    """A representation of Field Coordinates.  A spherical coordinate system
+    defined by two angles: theta, phi.  Theta is the angle about the optical
+    axis measured from the direction of +RA. Phi is the angle off the optical
+    axis of the telescope.  So Phi=0 is the optical axis of the telescope
+    increasing from earth to sky.
+
+    In the Cartesian representation on a unit sphere, +x is aligned with +RA,
+    +y is aligned with +Dec, and so +z is aligned with the telescope's optical
+    axis and points from the earth to sky.
+
+    Parameters
+    ------------
+    value : numpy.ndarray
+        A Nx2 array.  First column is theta, the azimuthal angle from +RA
+        through +Dec in deg. Second angle is phi, polar angle angle (off axis
+        angle) in deg. Or `.Observed`.  Or `.FocalPlane`.
+
+    field_center : `.Observed`
+        An `.Observed` instance containing a single coordinate
+
+    Attributes
+    ------------
+    x : numpy.ndarray
+        unit-spherical x coordinate
+    y : numpy.ndarray
+        unit-spherical y coordinate
+    z : numpy.ndarray
+        unit-spherical z coordinate
+    x_angle : numpy.ndarray
+        zeemax-style x field angle (deg)
+    y_angle : numpy.ndarray
+        zeemax-style y field angle (deg)
+    field_warn: numpy.ndarray
+        boolean array indicating suspect conversions from large field angles
+    """
+
+    __computed_arrays__ = ["x", "y", "z", "x_angle", "y_angle"]
+    __warn_arrays__ = ["field_warn"]
+    __extra_params__ = ["field_center"]  # mandatory parameter
+    # may want to carry around position angle for all
+    # coordinates too through the chain?  Could reduce errors in guiding
+    # because direction to north or zenith varies across the field due to...
+    # spheres.  For now ignore it?
+
+    x: numpy.ndarray
+    y: numpy.ndarray
+    z: numpy.ndarray
+    x_angle: numpy.ndarray
+    y_angle: numpy.ndarray
+    field_warn: numpy.ndarray
+    field_center: Observed
+
+    def __new__(cls, value, **kwargs):
+
+        field_center = kwargs.get("field_center", None)
+        if field_center is None:
+            raise CoordIOError("field_center must be passed to Field")
+        else:
+            if not hasattr(field_center, "coordSysName"):
+                raise CoordIOError(
+                    "field_center must be an Observed coordinate"
+                )
+            if field_center.coordSysName != "Observed":
+                raise CoordIOError(
+                    "field_center must be an Observed coordinate"
+                )
+            if len(field_center) != 1:
+                raise CoordIOError("field_center must contain only one coord")
+
+        if isinstance(value, Coordinate):
+            if value.coordSysName == "Observed":
+                obj = super().__new__(cls, value, **kwargs)
+                obj._fromObserved(value)
+            elif value.coordSysName == "FocalPlane":
+                # focal plane is a 3D coord sys
+                # field is 2D (spherical)
+                initArray = numpy.zeros((len(value), 2))
+                obj = super().__new__(cls, initArray, **kwargs)
+                obj._fromFocalPlane(value)
+            else:
+                raise CoordIOError(
+                    "Cannot convert to Field from %s" % value.coordSysName
+                )
+        else:
+            obj = super().__new__(cls, value, **kwargs)
+            obj._fromRaw()
+
+        return obj
+
+    def _fromObserved(self, obsCoords):
+        """Converts from observed coords to field coords, given the field
+        center.  Populates the computed arrays
+
+        Parameters
+        -----------
+        obsCoords : `.Observed`
+        """
+        altAz = numpy.array(obsCoords)
+        alt, az = altAz[:, 0], altAz[:, 1]
+        altCenter, azCenter = self.field_center.flatten()
+        pa = float(self.field_center.pa)  # position angle
+        theta, phi = conv.observedToField(alt, az, altCenter, azCenter, pa)
+        self[:, 0] = theta
+        self[:, 1] = phi
+
+        self._fromRaw()
+
+    def _fromFocalPlane(self, fpCoords):
+        """Convert from FocalPlane coords to Field coords.
+
+        Parameters
+        ------------
+        fpCoords : `.FocalPlane`
+        """
+        siteName = fpCoords.site.name.upper()
+        for waveCat in ["Boss", "Apogee", "GFA"]:
+            # find which coords are intended for which
+            # wavelength
+            arg = numpy.argwhere(
+                fpCoords.wavelength == defaults.INST_TO_WAVE[waveCat]
+            )
+
+            if len(arg) == 0:
+                continue
+
+            arg = arg.squeeze()
+
+            xFP = numpy.atleast_1d(fpCoords[arg, 0].squeeze())
+            yFP = numpy.atleast_1d(fpCoords[arg, 1].squeeze())
+            zFP = numpy.atleast_1d(fpCoords[arg, 2].squeeze())
+
+            if hasattr(xFP, "__len__") and len(xFP) == 0:
+                continue
+
+            thetaFocal, phiFocal, fieldWarn = conv.focalToField(
+                xFP,
+                yFP,
+                zFP,
+                siteName,
+                waveCat
+            )
+            thetaPhi = numpy.array([thetaFocal, phiFocal]).T
+            self[arg] = thetaPhi
+            self.field_warn[arg] = fieldWarn
+
+        self._fromRaw()
+
+    def _fromRaw(self):
+        """Populates the computed arrays
+        """
+        # ensure wrapping
+        self[:,0] = self[:,0] % 360
+        self.x, self.y, self.z = sph2Cart(self[:, 0], self[:, 1])
+        self.x_angle, self.y_angle = cart2FieldAngle(self.x, self.y, self.z)
+
+
+class FocalPlane(Coordinate3D):
+    """The focal plane coordinate system is 3D Cartesian with units of mm.
+    The origin is the M1 vertex.  +x is aligned with +RA on the image, +y is
+    aligned with +Dec on the image, +z points from earth to sky along the
+    telescope's optical axis. `.FocalPlane` xy's are physically rotated 180
+    degrees with respect to `.Field` xy's because Cassegrain telescope rotates
+    the image.  The choice was made to put +x along +RA on the image, rather
+    than the sky.
+
+    For each wavelength, a sphereical surface fit has been determined.
+    The sphere's center lies along the optical axis at a positive z value.
+    The sphere's center is considered the 'ray origin', so all rays traveling
+    to the focal plane originate from this location.
+
+    The ray origin and a wavelength-dependent, z-symmetric, distortion model
+    is used to convert between `.FocalPlane` and `.Field` coordinates.
+    The distortion model is an odd-termed polynomial relating input field
+    angle to output focal plane model.
+
+    The ZEMAX models/scripts, and fitting code for this still lives in
+    github.com/csayres/sdssconv, but may be eventually moved to this package.
+
+    Parameters
+    ------------
+    value : numpy.ndarray
+        A Nx3 array where [x,y,z] are columns. Or `.Field`.  Or `.Wok`.
+    wavelength : float or numpy.ndarray
+        A 1D array with he observed wavelength, in angstroms.
+        Currently only values of 5400, 6231, and 16600 are valid, corresponding
+        to BOSS, Apogee, and sdss-r band (GFA).  Defaults to GFA wavelength.
+    site : `.Site`
+        Used to pick the focal plane model to use which is telescope dependent
+    fpScale : float
+        Multiplicative scale factor to apply between wok and focal
+        coords.  An adjustment to the focal plane model.  Defaults to
+        coordio.defaults.FOCAL_SCALE.
+    use_closest_wavelength : bool
+        If `True`, uses the ZEMAX model with the closes wavelength to the
+        input wavelength.
+
+    Attributes
+    -----------
+    b : numpy.ndarray
+        A 1D array containing the location of the spherical fit's center
+        (origin of rays) along the optical axis in mm.  Varies with wavelength.
+    R : numpy.ndarray
+        1D array containing the radius of curvature for the spherical fit in
+        mm.  Varies with wavelength.
+    field_warn: numpy.ndarray
+        boolean array indicating suspect conversions from large field angles
+    """
+
+    __extra_params__ = ["site", "fpScale", "use_closest_wavelength"]
+    __extra_arrays__ = ["wavelength"]
+    __computed_arrays__ = ["b", "R"]
+    __warn_arrays__ = ["field_warn"]
+
+    b: numpy.ndarray
+    R: numpy.ndarray
+    field_warn: numpy.ndarray
+    wavelength: numpy.ndarray
+    site: Site
+    fpScale: float
+    use_closest_wavelength: bool
+
+    def __new__(cls, value, **kwargs):
+
+        verifySite(kwargs)
+
+        use_closest_wavelength = kwargs.get('use_closest_wavelength', False)
+
+        # If using the closes wavelength, override the wavelength entry in kwargs
+        # with an array with the closest valid wavelength for each input wavelength.
+        wls = kwargs.get('wavelength', None)
+        if wls is not None and use_closest_wavelength:
+            if hasattr(wls, "__len__"):
+                # array passed
+                wls = numpy.array(wls, dtype=numpy.float64)
+            else:
+                # single value passed
+                wls = numpy.zeros(len(value)) + float(wls)
+
+            valid_wls = numpy.array(list(defaults.VALID_WAVELENGTHS))
+            nearest = valid_wls[abs(wls[None, :] - valid_wls[:, None]).argmin(axis=0)]
+            kwargs['wavelength'] = nearest
+
+        verifyWavelength(
+            kwargs,
+            len(value),
+            strict=True,
+        )
+
+        fpScale = kwargs.get("fpScale", None)
+        if fpScale is None:
+            if kwargs["site"].name == "APO":
+                kwargs["fpScale"] = defaults.SITE_TO_SCALE["APO"]
+            elif kwargs["site"].name == "LCO":
+                kwargs["fpScale"] = defaults.SITE_TO_SCALE["LCO"]
+            else:
+                kwargs["fpScale"] = 1
+
+        if isinstance(value, Coordinate):
+            if value.coordSysName == "Field":
+                # going from 2D -> 3D coord sys
+                # expand dimensionality
+                arrInit = numpy.zeros((len(value), 3))
+                obj = super().__new__(cls, arrInit, **kwargs)
+                obj._fromField(value)
+            elif value.coordSysName == "Wok":
+                # wok is already a 3D coord sys
+                obj = super().__new__(cls, value, **kwargs)
+                obj._fromWok(value)
+            else:
+                raise CoordIOError(
+                    'Cannot convert to FocalPlane from %s'%value.coordSysName
+                )
+        else:
+            obj = super().__new__(cls, value, **kwargs)
+            obj._fromRaw()
+
+        return obj
+
+    def _fromField(self, fieldCoord):
+        """ Convert from field coordinates to focal plane coordinates, using
+        a pre-fit focal plane model.
+
+        Parameters
+        -----------
+        fieldCoord : `.Field`
+        """
+        siteName = self.site.name.upper()
+        for waveCat in ["Boss", "Apogee", "GFA"]:
+            # find which coords are intended for which
+            # wavelength
+            arg = numpy.argwhere(
+                self.wavelength == defaults.INST_TO_WAVE[waveCat]
+            )
+
+            if len(arg) == 0:
+                continue
+
+            arg = arg.squeeze()
+
+            thetaField = numpy.atleast_1d(fieldCoord[arg, 0].squeeze())
+            phiField = numpy.atleast_1d(fieldCoord[arg, 1].squeeze())
+
+            xFP, yFP, zFP, R, b, fieldWarn = conv.fieldToFocal(
+                thetaField,
+                phiField,
+                siteName,
+                waveCat
+            )
+
+            xyzFP = numpy.array([xFP, yFP, zFP]).T
+            self[arg] = xyzFP
+            self.b[arg] = b
+            self.R[arg] = R
+            self.field_warn[arg] = fieldWarn
+
+    def _fromWok(self, wokCoord):
+        """ Convert from wok coordinates to focal plane coordinates.
+
+        Parameters
+        -----------
+        wokCoord : `.Wok`
+        """
+        self._fromRaw()   # for grabbing fp params, b and R
+
+        pa = wokCoord.obsAngle
+        siteName = self.site.name.upper()
+        xOff, yOff, zOff, tiltX, tiltY = defaults.getWokOrient(siteName)
+        xWok, yWok, zWok = wokCoord[:, 0], wokCoord[:, 1], wokCoord[:, 2]
+        xF, yF, zF = conv.wokToFocal(
+            xWok, yWok, zWok, pa, xOff,
+            yOff, zOff, tiltX, tiltY,
+            b=self.b, R=self.R, # set b and R to None if NOT using flat wok model
+            fpScale=self.fpScale
+        )
+
+        self[:, 0] = xF
+        self[:, 1] = yF
+        self[:, 2] = zF
+
+
+    def _fromRaw(self):
+        direction = "focal"  # irrelevant, just grabbing sphere params
+
+        for waveCat in ["Boss", "Apogee", "GFA"]:
+            arg = numpy.argwhere(
+                self.wavelength == defaults.INST_TO_WAVE[waveCat])
+
+            if len(arg) == 0:
+                continue
+
+            arg = arg.squeeze()
+
+            R, b, c0, c1, c2, c3, c4 = defaults.getFPModelParams(self.site.name,
+                                                                 direction,
+                                                                 waveCat)
+            self.b[arg] = b
+            self.R[arg] = R
