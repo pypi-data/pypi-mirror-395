c23aebd3ceee4113d90c43a54c4f35e21407ff91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:07:29 +0000

    Add package vmecpp/0.4.11/vmecpp-0.4.11-cp311-cp311-macosx_14_0_arm64.whl

---
+++ packages/vmecpp/vmecpp-0.4.11-cp311-cp311-macosx_14_0_arm64.whl/vmecpp/cpp/vmecpp/simsopt_compat/__init__.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2024-present Proxima Fusion GmbH <info@proximafusion.com>
+#
+# SPDX-License-Identifier: MIT
+from vmecpp.cpp.vmecpp.simsopt_compat._indata_to_surfacerzfourier import (
+    surfacerzfourier_from_any_vmec_indata,
+    surfacerzfourier_from_fourier_coeffs,
+    surfacerzfourier_from_vmecppindata,
+)
+
+__all__ = [
+    "surfacerzfourier_from_any_vmec_indata",
+    "surfacerzfourier_from_fourier_coeffs",
+    "surfacerzfourier_from_vmecppindata",
+]
