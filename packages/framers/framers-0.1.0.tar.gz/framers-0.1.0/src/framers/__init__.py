78f7cca6b64f0830dc0c37b5d38242a5664dc5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:08:32 +0000

    Add package framers/0.1.0/framers-0.1.0.tar.gz

---
+++ packages/framers/framers-0.1.0.tar.gz/framers-0.1.0/src/framers/__init__.py
@@ -0,0 +1,18 @@
+"""
+Framers - Namespace package for Framers AI tools.
+
+This package re-exports promptmachine-eval for convenience.
+For full functionality, use: pip install promptmachine-eval
+"""
+
+__version__ = "0.1.0"
+
+# Re-export from main package
+try:
+    from promptmachine_eval import *
+    from promptmachine_eval import __version__ as eval_version
+except ImportError:
+    pass
+
+__all__ = ["__version__"]
+
