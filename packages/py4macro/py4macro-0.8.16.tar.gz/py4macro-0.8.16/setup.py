b9a9071e7fa146e7a1bf86bf62c636bf5848bb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:37 +0000

    Add package py4macro/0.8.16/py4macro-0.8.16.tar.gz

---
+++ packages/py4macro/py4macro-0.8.16.tar.gz/py4macro-0.8.16/setup.py
@@ -0,0 +1,26 @@
+import glob
+from setuptools import find_packages, setup
+
+
+additional_files = []
+for filename in glob.iglob('./py4macro/**', recursive=True):
+    additional_files.append(filename.replace('./py4macro/', ''))
+
+
+setup(
+    name='py4macro',
+    version='0.8.16',
+    author='Tetsu Haruyama',
+    author_email='haruyama@econ.kobe-u.ac.jp',
+    packages=find_packages(exclude=("data_generation",)),
+    package_dir={'py4macro': './py4macro'},
+    include_package_data=True,
+    package_data={'py4macro': additional_files},
+    install_requires=['pandas'],
+    url='https://github.com/Py4Macro/py4macro',
+    license='MIT',
+    description='A module for py4macro.github.io',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=['data', 'Penn World Table', 'IMF World Economic Outlook', 'Maddison Project', 'Hodrick-Prescott filter', 'Japan']
+)
