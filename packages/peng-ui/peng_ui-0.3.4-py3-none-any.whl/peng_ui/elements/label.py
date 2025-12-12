87cf3833025a7e2d580c7ca3ed493705804b4f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 21:08:15 +0000

    Add package peng-ui/0.3.4/peng_ui-0.3.4-py3-none-any.whl

---
+++ packages/peng-ui/peng_ui-0.3.4-py3-none-any.whl/peng_ui/elements/label.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+import pygame as pg
+
+from peng_ui.elements.base_element import BaseElement
+from peng_ui.utils import RenderContext, ColorType
+
+
+class Label(BaseElement):
+    def __init__(
+            self, rect: pg.Rect, text: str, text_color: ColorType = (200, 200, 200),
+            bg_color: Optional[ColorType] = None
+    ):
+        super().__init__(rect)
+        self._text = text
+        self.text_color = text_color
+        self.bg_color = bg_color
+        self._text_surface: Optional[pg.Surface] = None
+
+    def handle_event(self, event: pg.event.Event):
+        super().handle_event(event)
+
+    def set_text(self, text: str):
+        self._text = text
+        self._text_surface = None
+
+    def draw(self, screen: pg.Surface, render_context: RenderContext):
+        if self.bg_color:
+            pg.draw.rect(screen, self.bg_color, self.rect)
+
+        if self._text:
+            if self._text_surface is None:
+                self._text_surface = render_context.font.render(self._text, True, self.text_color)
+                self.rect = self._text_surface.get_rect(center=self.rect.center)
+            screen.blit(self._text_surface, self.rect)
