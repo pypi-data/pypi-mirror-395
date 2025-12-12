c0d992178270e78b3053560f9e8efdaf2e40690b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:09:07 +0000

    Add package rdetoolkit/1.4.3/rdetoolkit-1.4.3-cp39-cp39-musllinux_1_2_aarch64.whl

---
+++ packages/rdetoolkit/rdetoolkit-1.4.3-cp39-cp39-musllinux_1_2_aarch64.whl/rdetoolkit/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.4.3"
+
+from rdetoolkit.core import DirectoryOps, ManagedDirectory, detect_encoding, resize_image_aspect_ratio
+
+from . import errors, exceptions, img2thumb, invoicefile, modeproc, rde2util, rdelogger, workflows
+from .impl import *
+from .interfaces import *
+from .models import config, invoice, invoice_schema, metadata, rde2types
---
+++ packages/rdetoolkit/rdetoolkit-1.4.3-cp39-cp39-musllinux_1_2_aarch64.whl/rdetoolkit/cmd/gen_excelinvoice.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import pathlib
+from typing import Literal
+
+import click
+
+from rdetoolkit.exceptions import InvoiceSchemaValidationError
+from rdetoolkit.invoicefile import ExcelInvoiceFile
+from rdetoolkit.rdelogger import get_logger
+
+logger = get_logger(__name__)
+
+
+class GenerateExcelInvoiceCommand:
+
+    def __init__(self, invoice_schema_file: pathlib.Path, output_path: pathlib.Path, mode: Literal["file", "folder"]) -> None:
+        self.invoice_schema_file = invoice_schema_file
+        self.output_path = output_path
+        self.mode = mode
+
+    def invoke(self) -> None:
+        """Generate an Excel invoice template from the provided schema."""
+        rule_excelinvoice_suffix = '_excel_invoice.xlsx'
+        click.echo("📄 Generating ExcelInvoice template...")
+        click.echo(f"- Schema: {self.invoice_schema_file}")
+        click.echo(f"- Output: {self.output_path}")
+        click.echo(f"- Mode: {self.mode}")
+
+        if not self.output_path.name.endswith(rule_excelinvoice_suffix):
+            click.echo(click.style(f"🔥 Warning: The output file name '{self.output_path.name}' must end with '{rule_excelinvoice_suffix}'.", fg="yellow"))
+            raise click.Abort
+
+        try:
+            if not self.invoice_schema_file.exists():
+                emsg = f"Schema file not found: {self.invoice_schema_file}"
+                raise FileNotFoundError(emsg)
+            ExcelInvoiceFile.generate_template(self.invoice_schema_file, self.output_path, self.mode)
+            click.echo(click.style(f"✨ ExcelInvoice template generated successfully! : {self.output_path}", fg="green"))
+        except InvoiceSchemaValidationError as e:
+            logger.error(f"File error: {e}")
+            click.echo(click.style(f"🔥 Schema Error: {e}", fg="red"))
+            raise click.Abort from e
+        except Exception as e:
+            logger.error(f"Unexpected error: {e}")
+            click.echo(click.style(f"🔥 Error: An unexpected error occurred: {e}", fg="red"))
+            raise click.Abort from e
