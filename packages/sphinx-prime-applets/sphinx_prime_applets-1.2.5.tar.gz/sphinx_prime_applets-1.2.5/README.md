f549c72c19e8fc371096bfcca483250dfdc3d57f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:31:14 +0000

    Add package sphinx-prime-applets/1.2.5/sphinx_prime_applets-1.2.5.tar.gz

---
+++ packages/sphinx-prime-applets/sphinx_prime_applets-1.2.5.tar.gz/sphinx_prime_applets-1.2.5/README.md
@@ -0,0 +1,124 @@
+# Sphinx Extension: PRIME applets
+
+## Introduction
+
+This extension provides an interface to include [PRIME applets](https://openla.ewi.tudelft.nl/) with relative ease.
+
+## What does it do?
+
+This extension provides one Sphinx directives (`applet`) that can be used to quickly insert a PRIME applet.
+
+## Installation
+To use this extension, follow these steps:
+
+**Step 1: Install the Package**
+
+Install the module `sphinx-prime-applets` package using `pip`:
+```
+pip install sphinx-prime-applets
+```
+    
+**Step 2: Add to `requirements.txt`**
+
+Make sure that the package is included in your project's `requirements.txt` to track the dependency:
+```
+sphinx-prime-applets
+```
+
+**Step 3: Enable in `_config.yml`**
+
+In your `_config.yml` file, add the extension to the list of Sphinx extra extensions (**important**: underscore, not dash this time):
+```
+sphinx: 
+    extra_extensions:
+        .
+        .
+        .
+        - sphinx_prime_applets
+        .
+        .
+        .
+```
+
+## Applet directive
+
+````md
+```{applet}
+:url: lines_and_planes/normal_equation_plane_origin
+:fig: Images/image_shown_in_print_version.svg
+:name: name_that_is_used_to_refer_to_this_figure
+:class: dark-light
+:title: This title is shown when you full-screen the applet
+
+A plane through the point $P$.
+```
+````
+
+> [!NOTE]
+> The `url` parameter should be the part of the URL after `/applet/`. So if the full URL is `https://openla.ewi.tudelft.nl/applet/lines_and_planes/normal_equation_plane_origin`, you should set the parameter to `lines_and_planes/normal_equation_plane_origin`.
+
+## Parameters for an applet
+
+Some parameters can be set for an applet. Only the `url` and `name` parameters are required; the rest is optional. It is recommended to add a `status` to the applet, which can be `unreviewed`, `in-review` or `reviewed`.
+
+````md
+```{applet}
+:url: lines_and_planes/normal_equation_plane_origin # Required url
+:fig: Images/lines_and_planes/normal_equation_plane_origin.svg  # Image shown in print version
+:status: reviewed # default is "unreviewed". Other options are "in-review" and "reviewed"
+:name: Fig:InnerProduct:ProjectionVectorLine
+
+A title that describes the applet
+```
+````
+
+### Optional parameters
+
+| Parameter                                                                                                                           | Description                                                                                  | Default      |
+| ----------------------------------------------------------------------------------------------------------------------------------- | -------------------------------------------------------------------------------------------- | ------------ |
+| `fig`                                                                                                                               | The path to an image that will be shown in the print version of the document                  | None         |
+| `title`                                                                                                                             | A string that will be shown as the title of the applet when the applet is in fullscreen mode | ""           |
+| `status`                                                                                                                            | The status of the applet. Can be `unreviewed`, `in-review` or `reviewed`                     | `unreviewed` |
+| `width`                                                                                                                             | The width of the applet in pixels                                                            | 100%         |
+| `height`                                                                                                                            | The height of the applet in pixels                                                           | 400px        |
+
+> [!NOTE]
+> If the `fig` parameter is not defined, the code will insert the image provided by the applet itself as a fallback.
+
+### Control parameters
+
+> [!WARNING]
+> Work in progress
+
+### 2D Specific parameters
+
+> [!TIP]
+> You should add split-\* before the parameter to make it apply to the right scene
+
+| Parameter  | Description                                  | Default |
+| ---------- | -------------------------------------------- | ------- |
+| position2D | The position of the applet in the 2D plane   | 0,0     |
+| zoom2D     | The zoom level of the applet in the 2D plane | 1       |
+
+### 3D Specific parameters
+
+> [!TIP]
+> You should add split-\* before the parameter to make it apply to the right scene
+
+| Parameter  | Description                                  | Default |
+| ---------- | -------------------------------------------- | ------- |
+| position3D | The position of the applet in the 3D plane   | 0,0,0   |
+| zoom3D     | The zoom level of the applet in the 3D plane | 1       |
+
+## Contribute
+
+This tool's repository is stored on [GitHub](https://github.com/TeachBooks/Sphinx-PRIME-applets). If you'd like to contribute, you can create a fork and open a pull request on the [GitHub repository](https://github.com/TeachBooks/Sphinx-PRIME-applets).
+
+The `README.md` of the branch `manual` is also part of the [TeachBooks manual](https://teachbooks.io/manual/intro.html) as a submodule.
+
+## Metadata
+
+By default the applet directive adds metadata to the figure node it creates. This metadata includes information such as author, license, copyright, and source. This information is useful for documentation and attribution purposes. If you want to disable this feature, you can set the `prime_applets_metadata` configuration option to `False` in your Sphinx configuration file.
+
+> [!NOTE]
+> This extension always loads the extension `sphinx_metadata_figure` as it depends on it to add metadata to the figures. This means that also the configuration options of that extension are available and if this is not desired, you must disable the workings of that extensions by applying appropriate settings in `metadata_figure_settings` in your Sphinx configuration file. See the [documentation](https://teachbooks.io/manual/_git/github.com_TeachBooks_Sphinx-Metadata-Figure/main/MANUAL.html) of the `sphinx_metadata_figure` extension for more information.
---
+++ packages/sphinx-prime-applets/sphinx_prime_applets-1.2.5.tar.gz/sphinx_prime_applets-1.2.5/src/sphinx_prime_applets/__init__.py
@@ -0,0 +1,262 @@
+import os
+from urllib.parse import quote
+from docutils import nodes
+from docutils.parsers.rst import directives
+from typing import Optional
+from sphinx.application import Sphinx
+from sphinx.directives.patches import Figure
+from sphinx_metadata_figure import MetadataFigure
+import requests
+from datetime import datetime
+from sphinx.util import logging
+
+logger = logging.getLogger(__name__)
+
+TOKEN = os.getenv("GITHUB_TOKEN")
+
+DEFAULT_BASE_URL = "https://openla.ewi.tudelft.nl/applet/"
+
+def get_last_modified_date(file_url, token=TOKEN):
+
+    # to avoid rate limiting issues with the GitHub API, only proceed if a token is provided. Otherwise, return None
+    if token is None:
+        return None
+    # Extract parts from the GitHub URL
+    parts = file_url.split('/')
+    owner = parts[3]
+    repo = parts[4]
+    branch = parts[6]  # branch name comes after 'blob'
+    file_path = '/'.join(parts[7:])  # file path after branch
+    
+    # GitHub API endpoint
+    api_url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    params = {'path': file_path, 'sha': branch, 'per_page': 1}
+    
+    headers = {}
+    if token:
+        headers['Authorization'] = f'token {token}'
+    
+    response = requests.get(api_url, params=params, headers=headers)
+    response.raise_for_status()
+    
+    data = response.json()
+    if data:
+        # Extract the date and format it as YYYY-MM-DD
+        iso_date = data[0]['commit']['author']['date']
+        formatted_date = datetime.fromisoformat(iso_date.replace('Z', '+00:00')).strftime('%Y-%m-%d')
+        return formatted_date
+    else:
+        return None
+
+def generate_style(height: Optional[str], width: Optional[str]):
+	'''
+	Given a height and width, generates an inline style that can be used in HTML.
+	'''
+
+	styles = ''
+
+	if height:
+		styles += f'height: {height};'
+
+	if width:
+		styles += f'width: {width};'
+
+	return styles
+
+
+def parse_value(val: str) -> str:
+	'''
+	Parses a string value to a string that can be used in a URL query parameter. This is a hacky way to use boolean in docutils.
+	(For some reason docutils can't parse 'true' or 'True' strings??)
+	'''
+
+	if val == 'enabled':
+		return 'true'
+	elif val == 'disabled':
+		return 'false'
+	else:
+		return str(val)
+
+
+def parse_options(options: dict) -> dict:
+	# Settings keys that are passed along to the applet iframe
+	applet_keys = ['title', 'background', 'autoPlay', 'position', 'isPerspectiveCamera', 'enablePan', 'distance', 'zoom']
+
+	return {key: parse_value(val) for key, val in options.items() if key in applet_keys and val != ''}
+
+class AppletDirective(MetadataFigure):
+    option_spec = MetadataFigure.option_spec.copy()
+    option_spec.update(
+        {
+            "url": directives.unchanged_required,
+            "fig": directives.unchanged_required,
+            "title": directives.unchanged,
+            "background": directives.unchanged,
+            "autoPlay": directives.unchanged,
+            "position": directives.unchanged,
+            "isPerspectiveCamera": directives.unchanged,
+            "enablePan": directives.unchanged,
+            "distance": directives.unchanged,
+            "zoom": directives.unchanged,
+            "height": directives.unchanged,
+            "width": directives.unchanged,
+            "status": directives.unchanged,
+        }
+    )
+    required_arguments = 0
+
+    def run(self):
+        url = self.options.get("url")
+        fig = self.options.get("fig")
+
+        assert url is not None
+        if "?" in url:
+             url, url_params = url.split("?", 1)
+        else:
+             url_params = ""
+        if fig is None:
+            fig = DEFAULT_BASE_URL + url + "/image.png"
+        
+        iframe_class = self.options.get("class")  # expect a list/string of classes
+
+        if iframe_class is None:
+            iframe_class = ""
+        elif isinstance(iframe_class, list):
+            iframe_class = " ".join(iframe_class)
+        else:
+            iframe_class = str(iframe_class)
+
+        self.arguments = [fig]
+        self.options["class"] = ["applet-print-figure"]
+
+        # Access environment/config for whether to use metadata figure
+        env = getattr(self.state.document.settings, 'env', None)
+        config = getattr(env.app, 'config', None) if env else None
+        metadata = getattr(config, 'prime_applets_metadata', True) if config else True
+        if metadata:
+            # Use MetadataFigure to add metadata to the figure
+            self.options["author"] = self.options["author"] if "author" in self.options else "PRIME"
+            self.options["license"] = self.options["license"] if "license" in self.options else "CC-BY"
+            repo_url = f"https://github.com/PRIME-TU-Delft/Open-LA-Applets/blob/main/src/routes/applet/{url}"
+            last_modified_date = get_last_modified_date(repo_url)
+            if last_modified_date:
+                self.options["date"] = self.options["date"] if "date" in self.options else last_modified_date
+                year = last_modified_date.split("-")[0]
+                self.options["copyright"] = self.options["copyright"] if "copyright" in self.options else f"© TU Delft {year}"
+            else:
+                self.options["copyright"] = self.options["copyright"] if "copyright" in self.options else "© TU Delft"
+            self.options["source"] = self.options["source"] if "source" in self.options else f"[Open-LA-Applets]({repo_url})"
+            # force placement to caption, unless margin or admonition is used as default
+            metadata_settings = getattr(config, 'metadata_figure_settings', {}) if config else {}
+            style_settings = metadata_settings.get('style', {})
+            placement = self.options.get('placement', style_settings.get('placement', 'caption'))
+            if placement not in ['margin', 'admonition']: # hide is always overruled to caption
+                self.options["placement"] = 'caption'
+                (figure_node,) = MetadataFigure.run(self)
+                other_nodes = None
+            elif placement == 'admonition':
+                self.options["placement"] = 'admonition'
+                figure_nodes = MetadataFigure.run(self)
+                figure_node = figure_nodes[0]
+                other_nodes = figure_nodes[1]
+            elif placement == 'margin':
+                self.options["placement"] = 'margin'
+                figure_nodes = MetadataFigure.run(self)
+                figure_node = figure_nodes[1]
+                other_nodes = figure_nodes[0]
+        else:
+            # Just create a normal figure node without metadata
+            (figure_node,) = Figure.run(self)
+            other_nodes = None
+
+        # Generate GET params and inline styling
+        # we do not perform validation or sanitization
+        params_dict = parse_options(self.options)
+        params_dict["iframe"] = (
+            "true"  # To let the applet know its being run in an iframe
+        )
+        if url_params != "":
+            for param in url_params.split("&"):
+                if "=" in param:
+                    key, value = param.split("=", 1)
+                    params_dict[key] = value
+                else:
+                    params_dict[param] = "true"
+        # overwrite language based on document language
+        lang = self.state.document.settings.env.config.language
+        if lang is None:
+            lang = "en"
+        params_dict["lang"] = lang  # language is always overwritten
+        params = "&".join(
+            [f"{key}={quote(value)}" for key, value in params_dict.items()]
+        )
+        style = generate_style(
+            self.options.get("width", None), self.options.get("height", None)
+        )
+
+        base_url = os.environ.get("BASE_URL", DEFAULT_BASE_URL)
+        full_url = f'{base_url}{url}{"?" if params else ""}{params}'
+        applet_html = f"""
+			<div class="applet" style="{style}; ">
+					<iframe class="prime-applet {iframe_class}" src="{full_url}" allow="fullscreen" loading="lazy" frameborder="0"></iframe>
+			</div>
+		"""
+        applet_node = nodes.raw(None, applet_html, format="html")
+
+        # Add applet as the first child node of figure
+        figure_node.insert(0, applet_node)
+
+        if metadata:
+            if self.options.get('placement', style_settings.get('placement', 'caption')) not in ['margin', 'admonition']:
+                return [figure_node]
+            elif self.options.get('placement', style_settings.get('placement', 'caption')) == 'admonition':
+                return [figure_node] + [other_nodes]
+            elif self.options.get('placement', style_settings.get('placement', 'caption')) == 'margin':
+                return [other_nodes] + [figure_node]
+        else:
+            return [figure_node]
+        
+def setup(app):
+
+    app.setup_extension('sphinx_metadata_figure')
+    app.add_config_value("prime_applets_metadata", True, "env")
+    app.add_directive("applet", AppletDirective)
+    app.add_css_file('prime_applets.css')
+    app.connect("build-finished",write_css)
+    return {
+        "version": "0.1",
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
+
+def write_css(app: Sphinx,exc):
+  CSS_content = """.applet {
+  height: 500px;
+}
+
+.applet * {
+  width: 100%;
+  height: 500px; /* TODO: subject for discussion */
+}
+
+.applet-print-figure {
+  display: none;
+}
+
+.applet iframe {
+  border-radius: 10px;
+}
+
+@media print {
+  .applet iframe {
+    display: none;
+  }
+  .applet-print-figure {
+    display: initial;
+  }
+}"""
+  # write the css file
+  staticdir = os.path.join(app.builder.outdir, '_static')
+  filename = os.path.join(staticdir,'prime_applets.css')
+  with open(filename,"w") as css:
+        css.write(CSS_content)	    
