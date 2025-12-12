14c693800752d5495fb150a0cc915556c43f38af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:32 +0000

    Add package fastapi-voyager/0.12.11/fastapi_voyager-0.12.11.tar.gz

---
+++ packages/fastapi-voyager/fastapi_voyager-0.12.11.tar.gz/fastapi_voyager-0.12.11/tests/programatic.py
@@ -0,0 +1,14 @@
+from fastapi_voyager import create_voyager
+# from tests.demo_anno import app
+from tests.demo import app
+
+app.mount(
+    '/voyager', 
+    create_voyager(
+        app, 
+        module_color={"tests.service": "purple"}, 
+        module_prefix="tests.service", 
+        swagger_url="/docs",
+        initial_page_policy='first',
+        ga_id='G-R64S7Q49VL',
+        online_repo_url="https://github.com/allmonday/fastapi-voyager/blob/main"))
