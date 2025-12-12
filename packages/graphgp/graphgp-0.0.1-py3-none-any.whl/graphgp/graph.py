40dadbfe8933a32858eeb736f8374de1790ef892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 09:06:15 +0000

    Add package graphgp/0.0.1/graphgp-0.0.1-py3-none-any.whl

---
+++ packages/graphgp/graphgp-0.0.1-py3-none-any.whl/graphgp/graph.py
@@ -0,0 +1,137 @@
+from dataclasses import dataclass, field
+from typing import Tuple
+
+import jax
+import jax.numpy as jnp
+from jax.tree_util import Partial, register_dataclass
+from jax import Array
+
+
+from .tree import build_tree, query_preceding_neighbors
+
+try:
+    import graphgp_cuda
+
+    has_cuda = True
+except ImportError:
+    has_cuda = False
+
+
+@register_dataclass
+@dataclass
+class Graph:
+    """
+    Nearest-neighbor dependency graph for Gaussian process generation.
+
+    This object is provided for convenient use with ``generate`` and to easily mark ``offsets`` as static for JIT compilation.
+    Users who are comfortable with the GraphGP algorithm should feel free to construct their own graphs however they like.
+    We provide a function ``check_graph`` to verify that the graph can be used with other GraphGP components.
+    The ``generate_dense`` and ``refine`` functions take raw arrays as arguments if users do not wish to use this object.
+
+    Fields:
+        points: Modeled points in tree order of shape ``(N, d)``.
+        neighbors: Indices of the neighbors of shape ``(N - offsets[0], k)``.
+        offsets: Tuple of length ``B`` representing the end index of each batch.
+        indices: Original indices of the points of shape ``(N,)``. Can be ``None``.
+    """
+
+    points: Array
+    neighbors: Array
+    offsets: Tuple[int, ...] = field(metadata=dict(static=True))
+    indices: Array | None = None
+
+
+def check_graph(graph: Graph):
+    """
+    Verify the graph is valid for use with GraphGP.
+
+    Requirements:
+        1. Points are in topological order (i.e. neighbors come before in the order).
+        2. Length of ``neighbors`` is less than length of ``points`` by ``offsets[0]``.
+        3. Neighbors are not in the same batch as defined by ``offsets``.
+        4. Offsets are increasing and do not exceed the number of points.
+    """
+    points, neighbors, offsets = graph.points, graph.neighbors, graph.offsets
+    offsets = jnp.asarray(offsets)
+
+    # Ensure offsets are valid
+    assert offsets[0] == len(points) - len(neighbors), "Neighbors should start from first offset"
+    assert jnp.all(offsets[1:] >= offsets[:-1]), "Offsets must be non-decreasing"
+    assert offsets[-1] <= len(points), "Last offset must be less than or equal to the number of points"
+
+    # Ensure topological order
+    max_neighbors = jnp.max(neighbors, axis=1)
+    index = jnp.arange(len(neighbors)) + offsets[0]
+    ok = max_neighbors < index
+    assert jnp.all(ok), "Points are not in topological order"
+
+    # Ensure only coarse points
+    offsets_index = jnp.searchsorted(offsets, index, side="right") - 1
+    assert jnp.all(max_neighbors < offsets[offsets_index]), "Neighbors must not be in the same batch"
+
+
+def build_graph(points: Array, *, n0: int, k: int, cuda: bool = False) -> Graph:
+    """
+    Build a graph where each point depends on its ``k`` nearest neighbors which precede it in a k-d tree ordering (the original point order does not matter).
+
+    Args:
+        points: The input points of shape ``(N, d)``.
+        n0: The number of initial points.
+        k: The number of neighbors to include.
+        cuda: Whether to use optional CUDA extension, if installed. Will still use CUDA GPU via JAX if available. Default is ``False`` but recommended if possible for performance.
+
+    Returns:
+        A ``Graph`` dataclass containing ``points``, ``neighbors``, ``offsets``, and ``indices``.
+    """
+    if cuda:
+        if not has_cuda:
+            raise ImportError("CUDA extension not installed, cannot use cuda=True.")
+        points, indices, neighbors, depths = graphgp_cuda.build_graph(points, n0=n0, k=k)
+    else:
+        points, split_dims, indices = build_tree(points)
+        neighbors = query_preceding_neighbors(points, split_dims, n0=n0, k=k)
+        depths = compute_depths(neighbors, n0=n0)
+        points, indices, neighbors, depths = order_by_depth(points, indices, neighbors, depths)
+    offsets = jnp.searchsorted(depths, jnp.arange(1, jnp.max(depths) + 2))
+    offsets = tuple(int(x) for x in offsets)
+    # TODO: neighbors[:, ::-1] from far to close feels more stable but not sure if it matters
+    return Graph(points, neighbors, offsets, indices)
+
+
+@Partial(jax.jit, static_argnames=("n0", "cuda"))
+def compute_depths(neighbors, *, n0, cuda=False):
+    if cuda:
+        if not has_cuda:
+            raise ImportError("CUDA extension not installed, cannot use cuda=True.")
+        depths = graphgp_cuda.compute_depths_parallel(neighbors, n0=n0)
+    else:
+        depths = jnp.zeros(n0 + len(neighbors), dtype=jnp.int32)
+
+        def update(carry):
+            old_depths, depths = carry
+            new_depths = depths.at[jnp.arange(n0, len(depths))].set(1 + jnp.max(depths[neighbors], axis=1))
+            return depths, new_depths
+
+        def cond(carry):
+            old_depths, depths = carry
+            return jnp.any(old_depths != depths)
+
+        depths = jax.lax.while_loop(cond, update, (depths - 1, depths))[1]
+    return depths
+
+
+@Partial(jax.jit, static_argnames=("cuda",))
+def order_by_depth(points, indices, neighbors, depths, *, cuda=False):
+    if cuda:
+        if not has_cuda:
+            raise ImportError("CUDA extension not installed, cannot use cuda=True.")
+        points, indices, neighbors, depths = graphgp_cuda.order_by_depth(points, indices, neighbors, depths)
+    else:
+        n0 = len(points) - len(neighbors)
+        order = jnp.argsort(depths)
+        points, indices, depths = points[order], indices[order], depths[order]
+        neighbors = neighbors[order[n0:] - n0]  # first n0 should stay in order
+        inv_order = jnp.arange(len(points), dtype=int)
+        inv_order = inv_order.at[order].set(inv_order)
+        neighbors = inv_order[neighbors]
+    return points, indices, neighbors, depths
---
+++ packages/graphgp/graphgp-0.0.1-py3-none-any.whl/graphgp/refine.py
@@ -0,0 +1,305 @@
+from typing import Tuple
+
+import jax
+import jax.numpy as jnp
+from jax.tree_util import Partial
+from jax import Array
+from jax import lax
+
+import numpy as np
+
+from .graph import Graph
+
+try:
+    import graphgp_cuda
+
+    has_cuda = True
+except ImportError:
+    has_cuda = False
+
+
+def generate(
+    graph: Graph,
+    covariance: Tuple[Array, Array],
+    xi: Array,
+    *,
+    cuda: bool = False,
+    fast_jit: bool = True,
+) -> Array:
+    """
+    Generate a GP with dense Cholesky for the first layer followed by conditional refinement.
+    It is recommended to JIT compile before use.
+
+    Args:
+        graph: An instance of ``Graph``, can be checked for validity with ``check_graph``.
+        covariance: Tuple of arrays (cov_bins, cov_vals) storing discretized covariance. If using your own covariance, inflate k(0) by a small factor to ensure positive definite.
+        xi: Unit normal distributed parameters of shape ``(N,).``
+        reorder: Whether to reorder parameters and values according to the original order of the points. Default is ``True``.
+        cuda: Whether to use optional CUDA extension, if installed. Will still use CUDA GPU via JAX if available. Default is ``False`` but recommended if possible for performance.
+        fast_jit: Whether to use version of refinement that compiles faster, if cuda=False. Default is ``True`` but runtime performance and memory usage will suffer slightly.
+
+    Returns:
+        The generated values of shape ``(N,).``
+    """
+    if len(xi) != len(graph.points):
+        raise ValueError("Length of xi must match number of points in graph.")
+    n0 = len(graph.points) - len(graph.neighbors)
+    if graph.indices is not None:
+        xi = xi[graph.indices]
+    initial_values = generate_dense(graph.points[:n0], covariance, xi[:n0])
+    values = refine(
+        graph.points, graph.neighbors, graph.offsets, covariance, initial_values, xi[n0:], cuda=cuda, fast_jit=fast_jit
+    )
+    if graph.indices is not None:
+        values = jnp.empty_like(values).at[graph.indices].set(values, unique_indices=True)
+    values = jnp.where(jnp.any(jnp.isnan(values)), jnp.full_like(values, jnp.nan), values)
+    return values
+
+
+def generate_dense(points: Array, covariance: Tuple[Array, Array], xi: Array) -> Array:
+    """
+    Generate a GP with a dense Cholesky decomposition. Note that to compare with the GraphGP values,
+    the points must be provided in tree order.
+
+    Args:
+        points: Locations of points to model of shape ``(N, d)``
+        covariance: Tuple of arrays (cov_bins, cov_vals) storing discretized covariance. If using your own covariance, inflate k(0) by a small factor to ensure positive definite.
+        xi: Unit normal distributed parameters of shape ``(N,).``
+    Returns:
+        The generated values of shape ``(N,).``
+    """
+    if len(xi) != len(points):
+        raise ValueError("Length of xi must match number of points.")
+    K = compute_cov_matrix(covariance, points, points)
+    L = jnp.linalg.cholesky(K)
+    values = L @ xi
+    return values
+
+
+def refine(
+    points: Array,
+    neighbors: Array,
+    offsets: Tuple[int, ...],
+    covariance: Tuple[Array, Array],
+    initial_values: Array,
+    xi: Array,
+    *,
+    cuda: bool = False,
+    fast_jit: bool = True,
+) -> Array:
+    """
+    Conditionally generate using initial values according to GraphGP algorithm. Most users can use ``generate``, which
+    automatically generates the initial values and accepts a ``Graph`` object as input. This function is provided
+    if more flexibility is needed, for example to conditionally upsample already generated values.
+
+    It is recommended to JIT compile before use due to internal for loops.
+
+    Args:
+        points: Modeled points in tree order of shape ``(N, d)``.
+        neighbors: Indices of the neighbors of shape ``(N - offsets[0], k)``.
+        offsets: Tuple of length ``B`` representing the end index of each batch.
+        covariance: Tuple of arrays (cov_bins, cov_vals) storing discretized covariance. If using your own covariance, inflate k(0) by a small factor to ensure positive definite.
+        initial_values: Initial values of shape ``(offsets[0],).``
+        xi: Unit normal distributed parameters of shape ``(N - offsets[0],).``
+        cuda: Whether to use optional CUDA extension, if installed. Will still use CUDA GPU via JAX if available. Default is ``False`` but recommended if possible for performance.
+        fast_jit: Whether to use version of refinement that compiles faster, if cuda=False. Default is ``True`` but runtime performance and memory usage will suffer.
+
+    Returns:
+        The refined values of shape ``(N,).``
+
+    """
+    n0 = len(points) - len(neighbors)  # should equal offsets[0]
+    if len(initial_values) != n0:
+        raise ValueError("Length of initial_values must match number of initial points.")
+    if len(xi) != len(points) - n0:
+        raise ValueError("Length of xi must match number of refined points.")
+    if cuda:
+        if not has_cuda:
+            raise ImportError("CUDA extension not installed, cannot use cuda=True.")
+        values = graphgp_cuda.refine(
+            points, neighbors, jnp.asarray(offsets, dtype=neighbors.dtype), *covariance, initial_values, xi
+        )
+
+    elif fast_jit:
+        k = neighbors.shape[1]
+        max_batch = np.max(np.diff(np.array(offsets)))
+        values = jnp.zeros(len(points))
+        values = values.at[:n0].set(initial_values)
+
+        # Precompute matrix factorizations for all points
+        coarse_points = points[neighbors]
+        joint_points = jnp.concatenate([coarse_points, points[n0:, None]], axis=1)
+        K = jax.vmap(compute_cov_matrix, in_axes=(None, 0, 0))(covariance, joint_points, joint_points)
+        L = jnp.linalg.cholesky(K)
+        mean_vec = jnp.linalg.solve(L[:, :k, :k].transpose(0, 2, 1), L[:, k, :k][..., None]).squeeze(-1)
+        std = L[:, k, k]
+
+        # For each batch defined by offsets, dot neighbor values with mean_vec and add noise
+        def step(values, start):
+            neighbor_values = values[lax.dynamic_slice(neighbors, (start - n0, 0), (max_batch, k))]
+            mean_slice = jnp.sum(lax.dynamic_slice(mean_vec, (start - n0, 0), (max_batch, k)) * neighbor_values, axis=1)
+            noise_slice = lax.dynamic_slice(std * xi, (start - n0,), (max_batch,))
+            values = lax.dynamic_update_slice(values, mean_slice + noise_slice, (start,))
+            return values, None
+
+        values, _ = lax.scan(step, values, jnp.array(offsets[:-1]))
+
+    else:
+        values = initial_values
+        for i in range(1, len(offsets)):
+            start = offsets[i - 1]
+            end = offsets[i]
+            coarse_points = jnp.take(points, neighbors[start - n0 : end - n0], axis=0)
+            coarse_values = jnp.take(values, neighbors[start - n0 : end - n0], axis=0)
+            fine_point = points[start:end]
+            fine_xi = xi[start - n0 : end - n0]
+            mean, std = jax.vmap(Partial(_conditional_mean_std, covariance))(coarse_points, coarse_values, fine_point)
+            values = jnp.concatenate([values, mean + std * fine_xi], axis=0)
+
+    return values
+
+
+def generate_inv(
+    graph: Graph,
+    covariance: Tuple[Array, Array],
+    values: Array,
+    *,
+    cuda: bool = False,
+) -> Array:
+    """
+    Inverse of ``generate``. Ensure that the choice for ``reorder`` is the same. Recommended to JIT compile.
+    """
+    if len(values) != len(graph.points):
+        raise ValueError("Length of values must match number of points in graph.")
+    n0 = len(graph.points) - len(graph.neighbors)
+    if graph.indices is not None:
+        values = values[graph.indices]
+    initial_values, xi = refine_inv(graph.points, graph.neighbors, graph.offsets, covariance, values, cuda=cuda)
+    initial_xi = generate_dense_inv(graph.points[:n0], covariance, initial_values)
+    xi = jnp.concatenate([initial_xi, xi], axis=0)
+    if graph.indices is not None:
+        xi = jnp.empty_like(xi).at[graph.indices].set(xi, unique_indices=True)
+    xi = jnp.where(jnp.any(jnp.isnan(xi)), jnp.full_like(xi, jnp.nan), xi)
+    return xi
+
+
+def generate_dense_inv(points: Array, covariance: Tuple[Array, Array], values: Array) -> Array:
+    """
+    Inverse of ``generate_dense``.
+    """
+    if len(values) != len(points):
+        raise ValueError("Length of values must match number of points.")
+    K = compute_cov_matrix(covariance, points, points)
+    L = jnp.linalg.cholesky(K)
+    xi = jnp.linalg.solve(L, values)
+    return xi
+
+
+def refine_inv(
+    points: Array,
+    neighbors: Array,
+    offsets: Tuple[int, ...],
+    covariance: Tuple[Array, Array],
+    values: Array,
+    *,
+    cuda: bool = False,
+) -> Tuple[Array, Array]:
+    """
+    Inverse of ``refine``.
+    """
+    n0 = len(points) - len(neighbors)  # should equal offsets[0]
+    if len(values) != len(points):
+        raise ValueError("Length of values must match number of points.")
+    if cuda:
+        if not has_cuda:
+            raise ImportError("CUDA extension not installed, cannot use cuda=True.")
+        initial_values, xi = graphgp_cuda.refine_inv(
+            points, neighbors, jnp.asarray(offsets, dtype=neighbors.dtype), *covariance, values
+        )
+    else:
+        k = neighbors.shape[1]
+        coarse_points = points[neighbors]
+        joint_points = jnp.concatenate([coarse_points, points[n0:, None]], axis=1)
+        K = jax.vmap(compute_cov_matrix, in_axes=(None, 0, 0))(covariance, joint_points, joint_points)
+        L = jnp.linalg.cholesky(K)
+        mean_vec = jnp.linalg.solve(L[:, :k, :k].transpose(0, 2, 1), L[:, k, :k][..., None]).squeeze(-1)
+        mean = jnp.sum(mean_vec * values[neighbors], axis=1)
+        std = L[:, k, k]
+
+        xi = (values[n0:] - mean) / std
+        initial_values = values[:n0]
+    return initial_values, xi
+
+
+def generate_logdet(graph: Graph, covariance: Tuple[Array, Array], *, cuda: bool = False) -> Array:
+    """
+    Log determinant of ``generate``.
+    """
+    n0 = len(graph.points) - len(graph.neighbors)
+    dense_logdet = generate_dense_logdet(graph.points[:n0], covariance)
+    return dense_logdet + refine_logdet(graph.points, graph.neighbors, graph.offsets, covariance, cuda=cuda)
+
+
+def generate_dense_logdet(points: Array, covariance: Tuple[Array, Array]) -> Array:
+    """
+    Log determinant of ``generate_dense``.
+    """
+    K = compute_cov_matrix(covariance, points, points)
+    return jnp.linalg.slogdet(K)[1] / 2
+
+
+def refine_logdet(
+    points: Array,
+    neighbors: Array,
+    offsets: Tuple[int, ...],
+    covariance: Tuple[Array, Array],
+    *,
+    cuda: bool = False,
+) -> Array:
+    """
+    Log determinant of ``refine``.
+    """
+    if cuda:
+        if not has_cuda:
+            raise ImportError("CUDA extension not installed, cannot use cuda=True.")
+        logdet = graphgp_cuda.refine_logdet(points, neighbors, jnp.asarray(offsets, dtype=neighbors.dtype), *covariance)
+    else:
+        n0 = len(points) - len(neighbors)
+        k = neighbors.shape[1]
+        coarse_points = points[neighbors]
+        joint_points = jnp.concatenate([coarse_points, points[n0:, None]], axis=1)
+        K = jax.vmap(compute_cov_matrix, in_axes=(None, 0, 0))(covariance, joint_points, joint_points)
+        L = jnp.linalg.cholesky(K)
+        std = L[:, k, k]
+        logdet = jnp.sum(jnp.log(std))
+    return logdet
+
+
+def _conditional_mean_std(covariance, coarse_points, coarse_values, fine_point):
+    k = len(coarse_points)
+    joint_points = jnp.concatenate([coarse_points, fine_point[jnp.newaxis]], axis=0)
+    K = compute_cov_matrix(covariance, joint_points, joint_points)
+    L = jnp.linalg.cholesky(K)
+    mean = L[k, :k] @ jnp.linalg.solve(L[:k, :k], coarse_values)
+    std = L[k, k]
+    return mean, std
+
+
+def compute_cov_matrix(covariance: Tuple[Array, Array], points_a: Array, points_b: Array) -> Array:
+    distances = jnp.expand_dims(points_a, -2) - jnp.expand_dims(points_b, -3)
+    distances = jnp.linalg.norm(distances, axis=-1)
+    if isinstance(covariance, Tuple) and isinstance(covariance[0], Array) and isinstance(covariance[1], Array):
+        cov_bins, cov_vals = covariance
+        return cov_lookup(distances, cov_bins, cov_vals)
+    else:
+        raise ValueError("Invalid covariance specification.")
+
+
+def cov_lookup(r, cov_bins, cov_vals):
+    """
+    Look up covariance in array of sampled `cov_vals` at radii `cov_bins` (equal-sized arrays).
+    If `r` is inside of bounds, a linearly interpolated value is returned.
+    If `r` is below the first bin, the first value is returned. But really the first bin should always be 0.0.
+    If `r` is above the last bin, the last value is returned. Maybe the last value should be zero.
+    """
+    return jnp.interp(r, cov_bins, cov_vals)
