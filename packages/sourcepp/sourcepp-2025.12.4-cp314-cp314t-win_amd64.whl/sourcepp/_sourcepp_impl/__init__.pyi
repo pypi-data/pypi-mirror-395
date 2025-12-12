13e5759ee7a2be234e30b10242f73079dc2488af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 12:05:53 +0000

    Add package sourcepp/2025.12.4/sourcepp-2025.12.4-cp314-cp314t-win_amd64.whl

---
+++ packages/sourcepp/sourcepp-2025.12.4-cp314-cp314t-win_amd64.whl/sourcepp/_sourcepp_impl/__init__.pyi
@@ -0,0 +1,11 @@
+from . import (
+    bsppp as bsppp,
+    gamepp as gamepp,
+    kvpp as kvpp,
+    sourcepp as sourcepp,
+    steampp as steampp,
+    toolpp as toolpp,
+    vcryptpp as vcryptpp,
+    vpkpp as vpkpp,
+    vtfpp as vtfpp
+)
