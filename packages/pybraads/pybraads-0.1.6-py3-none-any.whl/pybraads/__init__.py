9be2634f49151cee6ca02a022153df300dbabbed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:05:02 +0000

    Add package pybraads/0.1.6/pybraads-0.1.6-py3-none-any.whl

---
+++ packages/pybraads/pybraads-0.1.6-py3-none-any.whl/pybraads/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.1.6"
+
+def version()->str:
+    """Return the version of the lib
+
+    Parameters
+    ----------
+    
+    Returns
+    -------
+    str
+        The string with version
+    """
+    return __version__
+
+# ads functions
+from .adsodbc import AdsConnection, AdsQuery
+
+__all__ = [version, AdsConnection, AdsQuery]
