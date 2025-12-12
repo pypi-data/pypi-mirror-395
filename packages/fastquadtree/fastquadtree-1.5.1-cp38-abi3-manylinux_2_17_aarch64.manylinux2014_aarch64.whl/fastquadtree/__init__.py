75adf1ef8a0f3c3988b396e6f4ae714ab357d1b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 22:06:55 +0000

    Add package fastquadtree/1.5.1/fastquadtree-1.5.1-cp38-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/fastquadtree/fastquadtree-1.5.1-cp38-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/fastquadtree/__init__.py
@@ -0,0 +1,5 @@
+from ._item import Item, PointItem, RectItem
+from .point_quadtree import QuadTree
+from .rect_quadtree import RectQuadTree
+
+__all__ = ["Item", "PointItem", "QuadTree", "RectItem", "RectQuadTree"]
