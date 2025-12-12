e1b34b0587801943718989fa3253c73f2bcab015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:06:56 +0000

    Add package split-openfeature/1.0.0/split_openfeature-1.0.0-py3-none-any.whl

---
+++ packages/split-openfeature/split_openfeature-1.0.0-py3-none-any.whl/split_openfeature/split_provider.py
@@ -0,0 +1,145 @@
+import typing
+from json import JSONDecodeError
+
+from open_feature.hooks.hook import Hook
+from open_feature.provider import provider
+from open_feature.evaluation_context.evaluation_context import EvaluationContext
+from open_feature.exception import exceptions
+from open_feature.exception.error_code import ErrorCode
+from open_feature.flag_evaluation.reason import Reason
+from open_feature.flag_evaluation.flag_evaluation_details import FlagEvaluationDetails
+from splitio import get_factory
+from splitio.exceptions import TimeoutException
+import json
+
+
+class SplitProvider(provider.AbstractProvider):
+
+    def __init__(self, api_key="", client=None):
+        if api_key == "" and client is None:
+            raise Exception("Must provide apiKey or Split Client")
+        if api_key != "":
+            factory = get_factory(api_key)
+            try:
+                factory.block_until_ready(1)
+            except TimeoutException:
+                raise exceptions.GeneralError("Error occurred initializing the client.")
+            self.split_client = factory.client()
+        else:
+            self.split_client = client
+
+    def get_metadata(self) -> provider.Metadata:
+        return provider.Metadata("Split")
+
+    def get_provider_hooks(self) -> typing.List[Hook]:
+        return []
+
+    def resolve_boolean_details(self, flag_key: str, default_value: bool,
+                                evaluation_context: EvaluationContext = EvaluationContext()):
+        try:
+            evaluated = self.evaluate_treatment(flag_key, evaluation_context)
+            if SplitProvider.no_treatment(evaluated):
+                return SplitProvider.construct_provider_evaluation(flag_key, default_value, evaluated, Reason.DEFAULT,
+                                                                   ErrorCode.FLAG_NOT_FOUND)
+            evaluated_lower = evaluated.lower()
+            if evaluated_lower in ["true", "on"]:
+                value = True
+            elif evaluated_lower in ["false", "off"]:
+                value = False
+            else:
+                raise exceptions.ParseError("Could not convert treatment to boolean")
+            return SplitProvider.construct_provider_evaluation(flag_key, value, evaluated)
+        except exceptions.OpenFeatureError:
+            raise
+        except Exception:
+            raise exceptions.GeneralError("Error getting boolean evaluation")
+
+    def resolve_string_details(self, flag_key: str, default_value: str,
+                               evaluation_context: EvaluationContext = EvaluationContext()):
+        try:
+            evaluated = self.evaluate_treatment(flag_key, evaluation_context)
+            if SplitProvider.no_treatment(evaluated):
+                return SplitProvider.construct_provider_evaluation(flag_key, default_value, evaluated, Reason.DEFAULT,
+                                                                   ErrorCode.FLAG_NOT_FOUND)
+            return SplitProvider.construct_provider_evaluation(flag_key, evaluated, evaluated)
+        except exceptions.OpenFeatureError:
+            raise
+        except Exception:
+            raise exceptions.GeneralError("Error getting boolean evaluation")
+
+    def resolve_integer_details(self, flag_key: str, default_value: int,
+                                evaluation_context: EvaluationContext = EvaluationContext()):
+        try:
+            evaluated = self.evaluate_treatment(flag_key, evaluation_context)
+            if SplitProvider.no_treatment(evaluated):
+                return SplitProvider.construct_provider_evaluation(flag_key, default_value, evaluated, Reason.DEFAULT,
+                                                                   ErrorCode.FLAG_NOT_FOUND)
+            try:
+                value = int(evaluated)
+            except ValueError:
+                raise exceptions.ParseError("Could not convert treatment to integer")
+            return SplitProvider.construct_provider_evaluation(flag_key, value, evaluated)
+        except exceptions.OpenFeatureError:
+            raise
+        except Exception:
+            raise exceptions.GeneralError("Error getting boolean evaluation")
+
+    def resolve_float_details(self, flag_key: str, default_value: float,
+                              evaluation_context: EvaluationContext = EvaluationContext()):
+        try:
+            evaluated = self.evaluate_treatment(flag_key, evaluation_context)
+            if SplitProvider.no_treatment(evaluated):
+                return SplitProvider.construct_provider_evaluation(flag_key, default_value, evaluated, Reason.DEFAULT,
+                                                                   ErrorCode.FLAG_NOT_FOUND)
+            try:
+                value = float(evaluated)
+            except ValueError:
+                raise exceptions.ParseError("Could not convert treatment to float")
+            return SplitProvider.construct_provider_evaluation(flag_key, value, evaluated)
+        except exceptions.OpenFeatureError:
+            raise
+        except Exception:
+            raise exceptions.GeneralError("Error getting boolean evaluation")
+
+    def resolve_object_details(self, flag_key: str, default_value: dict,
+                               evaluation_context: EvaluationContext = EvaluationContext()):
+        try:
+            evaluated = self.evaluate_treatment(flag_key, evaluation_context)
+            if SplitProvider.no_treatment(evaluated):
+                return SplitProvider.construct_provider_evaluation(flag_key, default_value, evaluated, Reason.DEFAULT,
+                                                                   ErrorCode.FLAG_NOT_FOUND)
+            value = json.loads(evaluated)
+            return SplitProvider.construct_provider_evaluation(flag_key, value, evaluated)
+        except JSONDecodeError:
+            raise exceptions.ParseError("Could not convert treatment to dict")
+        except exceptions.OpenFeatureError:
+            raise
+        except Exception:
+            raise exceptions.GeneralError("Error getting boolean evaluation")
+
+    # *** --- Helpers --- ***
+
+    def evaluate_treatment(self, key: str, evaluation_context: EvaluationContext):
+        if evaluation_context is None:
+            raise exceptions.GeneralError("Evaluation Context must be provided for the Split Provider")
+
+        targeting_key = evaluation_context.targeting_key
+        if not targeting_key:
+            raise exceptions.TargetingKeyMissingError("Missing targeting key")
+
+        attributes = SplitProvider.transform_context(evaluation_context)
+        return self.split_client.get_treatment(targeting_key, key, attributes)
+
+    @staticmethod
+    def transform_context(evaluation_context: EvaluationContext):
+        return evaluation_context.attributes
+
+    @staticmethod
+    def no_treatment(treatment: str):
+        return not treatment or treatment == "control"
+
+    @staticmethod
+    def construct_provider_evaluation(key: str, value, variant: str, reason: Reason = Reason.TARGETING_MATCH,
+                                      error_code: ErrorCode = None):
+        return FlagEvaluationDetails(flag_key=key, value=value, reason=reason,
+                                     error_code=error_code, variant=variant)
---
+++ packages/split-openfeature/split_openfeature-1.0.0-py3-none-any.whl/split_openfeature_provider/split_provider.py
@@ -0,0 +1,206 @@
+import typing
+import logging
+import json
+
+from openfeature.hook import Hook
+from openfeature.evaluation_context import EvaluationContext
+from openfeature.exception import ErrorCode, GeneralError, ParseError, OpenFeatureError, TargetingKeyMissingError
+from openfeature.flag_evaluation import Reason, FlagResolutionDetails
+from openfeature.provider import AbstractProvider, Metadata
+from split_openfeature_provider.split_client_wrapper import SplitClientWrapper
+
+_LOGGER = logging.getLogger(__name__)
+
+class SplitProviderBase(AbstractProvider):
+
+    def get_metadata(self) -> Metadata:
+        return Metadata("Split")
+
+    def get_provider_hooks(self) -> typing.List[Hook]:
+        return []
+
+    def _evaluate_treatment(self, key: str, evaluation_context: EvaluationContext, default_value):
+        if evaluation_context is None:
+            raise GeneralError("Evaluation Context must be provided for the Split Provider")
+
+        if not self._split_client_wrapper.is_sdk_ready():
+            return SplitProvider.construct_flag_resolution(default_value, None, None, Reason.ERROR,
+                                                               ErrorCode.PROVIDER_NOT_READY)
+
+        targeting_key = evaluation_context.targeting_key
+        if not targeting_key:
+            raise TargetingKeyMissingError("Missing targeting key")
+
+        attributes = SplitProvider.transform_context(evaluation_context)
+        evaluated = self._split_client_wrapper.split_client.get_treatment_with_config(targeting_key, key, attributes)
+        return self._process_treatment(evaluated, default_value)
+
+    def _process_treatment(self, evaluated, default_value):
+        try:
+            treatment = None
+            config = None
+            if evaluated != None:
+                treatment = evaluated[0]
+                config = evaluated[1]
+
+            if SplitProvider.no_treatment(treatment) or treatment == "control":
+                return SplitProvider.construct_flag_resolution(default_value, treatment, None, Reason.DEFAULT,
+                                                               ErrorCode.FLAG_NOT_FOUND)
+            value = treatment
+            try:
+                if type(default_value) is int:
+                    value = int(treatment)
+                elif isinstance(default_value, float):
+                    value = float(treatment)
+                elif isinstance(default_value, bool):
+                    evaluated_lower = treatment.lower()
+                    if evaluated_lower in ["true", "on"]:
+                        value = True
+                    elif evaluated_lower in ["false", "off"]:
+                        value = False
+                    else:
+                        raise ParseError
+                elif isinstance(default_value, dict):
+                    value = json.loads(treatment)
+
+            except Exception:
+                raise ParseError
+
+            return SplitProvider.construct_flag_resolution(value, treatment, config)
+
+        except ParseError as ex:
+            _LOGGER.error("Evaluation Parse error")
+            _LOGGER.debug(ex)
+            raise ParseError("Could not convert treatment")
+
+        except OpenFeatureError as ex:
+            _LOGGER.error("Evaluation OpenFeature Exception")
+            _LOGGER.debug(ex)
+            raise
+
+        except Exception as ex:
+            _LOGGER.error("Evaluation Exception")
+            _LOGGER.debug(ex)
+            raise GeneralError("Failed to evaluate treatment")
+
+    @staticmethod
+    def transform_context(evaluation_context: EvaluationContext):
+        return evaluation_context.attributes
+
+    @staticmethod
+    def no_treatment(treatment: str):
+        return not treatment or treatment == "control"
+
+    @staticmethod
+    def construct_flag_resolution(value, variant, config, reason: Reason = Reason.TARGETING_MATCH,
+                                  error_code: ErrorCode = None):
+        return FlagResolutionDetails(value=value, error_code=error_code, reason=reason, variant=variant,
+                                     flag_metadata={"config": config})
+
+    def resolve_boolean_details(self, flag_key: str, default_value: bool,
+                                evaluation_context: EvaluationContext = EvaluationContext()):
+        pass
+
+    def resolve_string_details(self, flag_key: str, default_value: str,
+                               evaluation_context: EvaluationContext = EvaluationContext()):
+        pass
+
+    def resolve_integer_details(self, flag_key: str, default_value: int,
+                                evaluation_context: EvaluationContext = EvaluationContext()):
+        pass
+
+    def resolve_float_details(self, flag_key: str, default_value: float,
+                              evaluation_context: EvaluationContext = EvaluationContext()):
+        pass
+
+    def resolve_object_details(self, flag_key: str, default_value: dict,
+                               evaluation_context: EvaluationContext = EvaluationContext()):
+        pass
+
+    async def resolve_boolean_details_async(self, flag_key: str, default_value: bool,
+                                evaluation_context: EvaluationContext = EvaluationContext()):
+        pass
+
+    async def resolve_string_details_async(self, flag_key: str, default_value: str,
+                               evaluation_context: EvaluationContext = EvaluationContext()):
+        pass
+    async def resolve_integer_details_async(self, flag_key: str, default_value: int,
+                                evaluation_context: EvaluationContext = EvaluationContext()):
+        pass
+
+    async def resolve_float_details_async(self, flag_key: str, default_value: float,
+                              evaluation_context: EvaluationContext = EvaluationContext()):
+        pass
+
+    async def resolve_object_details_async(self, flag_key: str, default_value: dict,
+                               evaluation_context: EvaluationContext = EvaluationContext()):
+        pass
+
+class SplitProvider(SplitProviderBase):
+    def __init__(self, initial_context):
+        self._split_client_wrapper = SplitClientWrapper(initial_context)
+
+    def resolve_boolean_details(self, flag_key: str, default_value: bool,
+                                evaluation_context: EvaluationContext = EvaluationContext()):
+        return self._evaluate_treatment(flag_key, evaluation_context, default_value)
+
+    def resolve_string_details(self, flag_key: str, default_value: str,
+                               evaluation_context: EvaluationContext = EvaluationContext()):
+        return self._evaluate_treatment(flag_key, evaluation_context, default_value)
+
+    def resolve_integer_details(self, flag_key: str, default_value: int,
+                                evaluation_context: EvaluationContext = EvaluationContext()):
+        return self._evaluate_treatment(flag_key, evaluation_context, default_value)
+
+    def resolve_float_details(self, flag_key: str, default_value: float,
+                              evaluation_context: EvaluationContext = EvaluationContext()):
+        return self._evaluate_treatment(flag_key, evaluation_context, default_value)
+
+    def resolve_object_details(self, flag_key: str, default_value: dict,
+                               evaluation_context: EvaluationContext = EvaluationContext()):
+        return self._evaluate_treatment(flag_key, evaluation_context, default_value)
+
+class SplitProviderAsync(SplitProviderBase):
+    def __init__(self, initial_context):
+        if isinstance(initial_context, dict):
+            initial_context["ThreadingMode"] = "asyncio"
+        self._split_client_wrapper = SplitClientWrapper(initial_context)
+
+    async def create(self):
+        await self._split_client_wrapper.create()
+
+    async def resolve_boolean_details_async(self, flag_key: str, default_value: bool,
+                                evaluation_context: EvaluationContext = EvaluationContext()):
+        return await self._evaluate_treatment_async(flag_key, evaluation_context, default_value)
+
+    async def resolve_string_details_async(self, flag_key: str, default_value: str,
+                               evaluation_context: EvaluationContext = EvaluationContext()):
+        return await self._evaluate_treatment_async(flag_key, evaluation_context, default_value)
+
+    async def resolve_integer_details_async(self, flag_key: str, default_value: int,
+                                evaluation_context: EvaluationContext = EvaluationContext()):
+        return await self._evaluate_treatment_async(flag_key, evaluation_context, default_value)
+
+    async def resolve_float_details_async(self, flag_key: str, default_value: float,
+                              evaluation_context: EvaluationContext = EvaluationContext()):
+        return await self._evaluate_treatment_async(flag_key, evaluation_context, default_value)
+
+    async def resolve_object_details_async(self, flag_key: str, default_value: dict,
+                               evaluation_context: EvaluationContext = EvaluationContext()):
+        return await self._evaluate_treatment_async(flag_key, evaluation_context, default_value)
+
+    async def _evaluate_treatment_async(self, key: str, evaluation_context: EvaluationContext, default_value):
+        if evaluation_context is None:
+            raise GeneralError("Evaluation Context must be provided for the Split Provider")
+
+        if not await self._split_client_wrapper.is_sdk_ready_async():
+            return SplitProvider.construct_flag_resolution(default_value, None, None, Reason.ERROR,
+                                                               ErrorCode.PROVIDER_NOT_READY)
+
+        targeting_key = evaluation_context.targeting_key
+        if not targeting_key:
+            raise TargetingKeyMissingError("Missing targeting key")
+
+        attributes = SplitProvider.transform_context(evaluation_context)
+        evaluated = await self._split_client_wrapper.split_client.get_treatment_with_config(targeting_key, key, attributes)
+        return self._process_treatment(evaluated, default_value)
