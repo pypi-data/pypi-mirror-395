b6259aef0569e43df76e1771d3f12412d940ddb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 17:06:44 +0000

    Add package talisman-dm/1.3.10/talisman_dm-1.3.10.tar.gz

---
+++ packages/talisman-dm/talisman_dm-1.3.10.tar.gz/talisman_dm-1.3.10/tdm/datamodel/mentions/video.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+
+from tdm.abstract.datamodel import AbstractNodeMention
+from tdm.abstract.json_schema import generate_model
+from tdm.datamodel.nodes import VideoNode
+from ._abstract import AbstractBBox, AbstractSegment
+
+
+@generate_model
+@dataclass(frozen=True)
+class VideoNodeMention(AbstractSegment, AbstractBBox, AbstractNodeMention):
+    """
+    Represents spatiotemporal volume mention for video node.
+
+    Attributes
+    --------
+    node:
+        The video node being mentioned.
+    left:
+        The left position of the spatiotemporal volume (px).
+    top:
+        The top position of the spatiotemporal volume (px).
+    right:
+         The right position of the spatiotemporal volume (px).
+    bottom:
+        The bottom position of the spatiotemporal volume (px).
+    start:
+        The start of the spatiotemporal volume (ms).
+    end:
+        The end of the spatiotemporal volume (ms).
+    """
+    node: VideoNode
+
+    def __post_init__(self):
+        if not isinstance(self.node, VideoNode):
+            raise ValueError(f"Incorrect node type {type(self.node)}. Expected {VideoNode}")
+        self._validate_bbox(self.node.metadata)
+        self._validate_segment(self.node.metadata)
