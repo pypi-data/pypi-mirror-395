87f875c9657347c58b128add60d265f81e42edc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:04:42 +0000

    Add package odd-or-even-diya/0.1.2/odd_or_even_diya-0.1.2.tar.gz

---
+++ packages/odd-or-even-diya/odd_or_even_diya-0.1.2.tar.gz/odd_or_even_diya-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="odd-or-even-diya",
+    version="0.1.2",  # ⬅️ change this
+    description="A simple library to check if a number is odd or even",
+    author="Diya George",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
