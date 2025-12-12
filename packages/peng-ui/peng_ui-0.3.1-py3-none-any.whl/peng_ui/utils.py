dc7ec251d08b38f20a3e364b2aac016ccfc1d8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 15:05:24 +0000

    Add package peng-ui/0.3.1/peng_ui-0.3.1-py3-none-any.whl

---
+++ packages/peng-ui/peng_ui-0.3.1-py3-none-any.whl/peng_ui/utils.py
@@ -0,0 +1,31 @@
+import warnings
+from typing import Union, Tuple
+
+import pygame as pg
+
+
+ColorType = Union[Tuple[int, int, int], pg.Color]
+
+
+class RenderContext:
+    def __init__(self, font: pg.font.Font):
+        self.font = font
+        self.mouse_pressed = False
+
+    @staticmethod
+    def default():
+        font = load_font()
+        return RenderContext(font)
+
+
+def load_font():
+    """
+    Helper function to load the default font.
+    :return: The font to use
+    """
+    try:
+        font = pg.font.Font(None, 36)
+    except pg.error:
+        warnings.warn("Warning: Could not load default font. Text will not be rendered.")
+        font = None
+    return font
---
+++ packages/peng-ui/peng_ui-0.3.1-py3-none-any.whl/scripts/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import pygame as pg
+
+from peng_ui.elements import TextField, EditField, Label, Button
+from peng_ui.viewer import Viewer
+
+
+class MyViewer(Viewer):
+    def __init__(self):
+        super().__init__(screen_size=(0, 0), flags=pg.FULLSCREEN)
+        self.button = Button(pg.Rect(50, 50, 120, 40), "hello")
+        self.label = Label(pg.Rect(50, 100, 120, 40), "hello")
+        self.edit_field = EditField(pg.Rect(200, 50, 220, 40), "hello")
+        self.text_field = TextField(pg.Rect(200, 100, 220, 140), "hello")
+
+    def tick(self):
+        if self.button.is_clicked:
+            print('button clicked')
+        if self.label.is_clicked:
+            print('label clicked')
+
+
+if __name__ == '__main__':
+    viewer = MyViewer()
+    viewer.run()
