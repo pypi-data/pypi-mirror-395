1068c9e5249c3a04c5aad2b21c65ea9f6480ef73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 09:06:48 +0000

    Add package isar/1.34.12/isar-1.34.12.tar.gz

---
+++ packages/isar/isar-1.34.12.tar.gz/isar-1.34.12/tests/isar/state_machine/states/test_stopping_return_home_state.py
@@ -0,0 +1,31 @@
+from typing import Optional, cast
+
+from isar.eventhandlers.eventhandler import EventHandlerBase, EventHandlerMapping
+from isar.state_machine.state_machine import StateMachine
+from robot_interface.models.mission.mission import Mission
+from tests.test_mocks.task import StubTask
+
+
+def test_return_home_cancelled_when_new_mission_received(
+    sync_state_machine: StateMachine,
+) -> None:
+    sync_state_machine.shared_state.robot_battery_level.trigger_event(80.0)
+    sync_state_machine.state = sync_state_machine.returning_home_state.name  # type: ignore
+
+    returning_home_state: EventHandlerBase = cast(
+        EventHandlerBase, sync_state_machine.returning_home_state
+    )
+    event_handler: Optional[EventHandlerMapping] = (
+        returning_home_state.get_event_handler_by_name("start_mission_event")
+    )
+
+    mission: Mission = Mission(name="Dummy misson", tasks=[StubTask.take_image()])
+
+    assert event_handler is not None
+
+    event_handler.event.trigger_event(mission)
+    transition = event_handler.handler(event_handler.event)
+
+    assert transition is sync_state_machine.stop_return_home  # type: ignore
+    transition()
+    assert sync_state_machine.state is sync_state_machine.stopping_return_home_state.name  # type: ignore
