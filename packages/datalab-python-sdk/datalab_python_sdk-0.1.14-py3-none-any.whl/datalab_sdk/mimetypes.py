8d63a2111c7c6138f3e1b6cf77434bacf37461c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 20:05:53 +0000

    Add package datalab-python-sdk/0.1.14/datalab_python_sdk-0.1.14-py3-none-any.whl

---
+++ packages/datalab-python-sdk/datalab_python_sdk-0.1.14-py3-none-any.whl/datalab_sdk/mimetypes.py
@@ -0,0 +1,18 @@
+MIMETYPE_MAP = {
+    ".pdf": "application/pdf",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".gif": "image/gif",
+    ".tiff": "image/tiff",
+    ".webp": "image/webp",
+    ".docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    ".doc": "application/msword",
+    ".xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    ".xls": "application/vnd.ms-excel",
+    ".pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    ".ppt": "application/vnd.ms-powerpoint",
+    ".html": "text/html",
+    ".epub": "application/epub+zip",
+}
+SUPPORTED_EXTENSIONS = list(MIMETYPE_MAP.keys())
