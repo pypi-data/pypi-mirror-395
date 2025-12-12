86b80474e9bf6f1e10e7e21f6d944f00b7a32f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 09:07:00 +0000

    Add package reaktiv/0.19.4/reaktiv-0.19.4.tar.gz

---
+++ packages/reaktiv/reaktiv-0.19.4.tar.gz/reaktiv-0.19.4/src/reaktiv/__init__.py
@@ -0,0 +1,33 @@
+"""Signals for Python - inspired by Angular Signals / SolidJS. Reactive Declarative State Management Library for Python - automatic dependency tracking and reactive updates for your application state."""
+
+from .context import untracked
+from .scheduler import batch
+from .signal import Signal, Computed, ComputeSignal, ReadonlySignal
+from .linked import LinkedSignal, PreviousState
+from .effect import Effect
+from .utils import to_async_iter
+from .thread_safety import set_thread_safety, is_thread_safety_enabled
+from .protocols import ReadableSignal, WritableSignal
+
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
+__version__ = "0.19.4"
+__all__ = [
+    "Signal",
+    "ReadonlySignal",
+    "ComputeSignal",
+    "Computed",
+    "Effect",
+    "LinkedSignal",
+    "PreviousState",
+    "ReadableSignal",
+    "WritableSignal",
+    "batch",
+    "untracked",
+    "to_async_iter",
+    "set_thread_safety",
+    "is_thread_safety_enabled",
+]
