4cdd03c2d13836d36b93df8cde88f7f2dedb1f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 16:09:13 +0000

    Add package planet-terp-client/0.1.4/planet_terp_client-0.1.4-py3-none-any.whl

---
+++ packages/planet-terp-client/planet_terp_client-0.1.4-py3-none-any.whl/planet_terp/__init__.py
@@ -0,0 +1,12 @@
+from .client import PlanetTerp
+from .models import Course, Instructor, Professor, TA, Grade, SearchResult, Review
+
+__all__ = [
+    "PlanetTerp",
+    "Course",
+    "Instructor",
+    "Professor",
+    "TA",
+    "Grade",
+    "SearchResult",
+]
