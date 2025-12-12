879f5ec49d829478cd56f06f0f858104c1c31d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:30:58 +0000

    Add package numerous-apps/0.4.1/numerous_apps-0.4.1.tar.gz

---
+++ packages/numerous-apps/numerous_apps-0.4.1.tar.gz/numerous_apps-0.4.1/src/numerous/apps/__init__.py
@@ -0,0 +1,22 @@
+"""Module containing the app framework."""
+
+from collections.abc import Callable
+from functools import wraps
+from typing import Any, TypeVar
+
+from .app_server import create_app as create_app
+
+
+T = TypeVar("T")
+
+
+def action(func: Callable[..., T]) -> Callable[..., T]:
+    """Decorate a method to mark it as an action that can be called via the API."""
+
+    @wraps(func)
+    def wrapper(*args: tuple[Any, ...], **kwargs: dict[str, Any]) -> T:
+        return func(*args, **kwargs)
+
+    # Mark the function as an action
+    wrapper._is_action = True  # type: ignore[attr-defined] # noqa: SLF001
+    return wrapper
