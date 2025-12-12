003d543802e262d98c5c59591c3475d96016a3e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 18:06:38 +0000

    Add package KratosMultiphysics/10.4.0/kratosmultiphysics-10.4.0-cp38-cp38-manylinux_2_28_x86_64.whl

---
+++ packages/KratosMultiphysics/kratosmultiphysics-10.4.0-cp38-cp38-manylinux_2_28_x86_64.whl/KratosMultiphysics/EntitiesUtilities.pyi
@@ -0,0 +1,11 @@
+import KratosMultiphysics as Kratos
+import KratosMultiphysics as Kratos
+
+def InitializeAllEntities(arg0: Kratos.ModelPart) -> None:
+    """InitializeAllEntities(arg0: Kratos.ModelPart) -> None"""
+def InitializeConditions(arg0: Kratos.ModelPart) -> None:
+    """InitializeConditions(arg0: Kratos.ModelPart) -> None"""
+def InitializeElements(arg0: Kratos.ModelPart) -> None:
+    """InitializeElements(arg0: Kratos.ModelPart) -> None"""
+def InitializeMasterSlaveConstraints(arg0: Kratos.ModelPart) -> None:
+    """InitializeMasterSlaveConstraints(arg0: Kratos.ModelPart) -> None"""
