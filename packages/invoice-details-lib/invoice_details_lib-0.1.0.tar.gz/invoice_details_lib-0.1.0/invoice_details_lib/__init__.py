7f233cb2afa6f068418cc588100cf49987e66b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:30:44 +0000

    Add package invoice-details-lib/0.1.0/invoice_details_lib-0.1.0.tar.gz

---
+++ packages/invoice-details-lib/invoice_details_lib-0.1.0.tar.gz/invoice_details_lib-0.1.0/invoice_details_lib/__init__.py
@@ -0,0 +1,4 @@
+from .invoice_details import get_transaction_date, get_transaction_code, get_current_date
+
+__version__ = "0.1.0"
+__all__ = ["get_transaction_date, get_transaction_code, get_current_date"]
