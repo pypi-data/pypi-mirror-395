c08ce53a8f0d5af458bfb185aabb811ece06d23a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 19:06:35 +0000

    Add package fps_auth_fief/0.9.6/fps_auth_fief-0.9.6-py3-none-any.whl

---
+++ packages/fps_auth_fief/fps_auth_fief-0.9.6-py3-none-any.whl/fps_auth_fief/backend.py
@@ -0,0 +1,112 @@
+from dataclasses import dataclass
+from typing import Any
+
+from fastapi import Depends, HTTPException, Request, Response, WebSocket, status
+from fastapi.security import APIKeyCookie
+from fief_client import FiefAccessTokenInfo, FiefAsync, FiefUserInfo
+from fief_client.integrations.fastapi import FiefAuth
+from jupyverse_api.auth import User
+
+from .config import _AuthFiefConfig
+
+
+@dataclass
+class Res:
+    fief: FiefAsync
+    session_cookie_name: str
+    auth: FiefAuth
+    current_user: Any
+    update_user: Any
+    websocket_auth: Any
+
+
+def get_backend(auth_fief_config: _AuthFiefConfig) -> Res:
+    class CustomFiefAuth(FiefAuth):
+        client: FiefAsync
+
+        async def get_unauthorized_response(self, request: Request, response: Response):
+            if auth_fief_config.callback_url:
+                redirect_uri = auth_fief_config.callback_url
+            else:
+                redirect_uri = str(request.url_for("auth_callback"))
+            auth_url = await self.client.auth_url(redirect_uri, scope=["openid", "offline_access"])
+            raise HTTPException(
+                status_code=status.HTTP_307_TEMPORARY_REDIRECT,
+                headers={"Location": auth_url},
+            )
+
+    fief = FiefAsync(
+        auth_fief_config.base_url,
+        auth_fief_config.client_id,
+        auth_fief_config.client_secret,
+    )
+
+    session_cookie_name = "fps_auth_fief_user_session"
+    scheme = APIKeyCookie(name=session_cookie_name, auto_error=False)
+    auth = CustomFiefAuth(fief, scheme)
+
+    async def update_user(
+        user: FiefUserInfo = Depends(auth.current_user()),
+        access_token_info: FiefAccessTokenInfo = Depends(auth.authenticated()),
+    ):
+        async def _(data: dict[str, Any]) -> FiefUserInfo:
+            user = await fief.update_profile(access_token_info["access_token"], {"fields": data})
+            return user
+
+        return _
+
+    def websocket_auth(permissions: dict[str, list[str]] | None = None):
+        async def _(
+            websocket: WebSocket,
+        ) -> tuple[WebSocket, dict[str, list[str]] | None] | None:
+            accept_websocket = False
+            checked_permissions: dict[str, list[str]] | None = None
+            if session_cookie_name in websocket._cookies:
+                access_token = websocket._cookies[session_cookie_name]
+                if permissions is None:
+                    accept_websocket = True
+                else:
+                    checked_permissions = {}
+                    for resource, actions in permissions.items():
+                        allowed = checked_permissions[resource] = []
+                        for action in actions:
+                            try:
+                                await fief.validate_access_token(
+                                    access_token, required_permissions=[f"{resource}:{action}"]
+                                )
+                            except BaseException:
+                                pass
+                            else:
+                                allowed.append(action)
+                                accept_websocket = True
+            if accept_websocket:
+                return websocket, checked_permissions
+            else:
+                await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+                return None
+
+        return _
+
+    def current_user(permissions=None):
+        if permissions is not None:
+            permissions = [
+                f"{resource}:{action}"
+                for resource, actions in permissions.items()
+                for action in actions
+            ]
+
+        async def _(
+            user: FiefUserInfo = Depends(auth.current_user(permissions=permissions)),
+        ):
+            return User(**user["fields"])
+
+        return _
+
+    return Res(
+        fief=fief,
+        session_cookie_name=session_cookie_name,
+        auth=auth,
+        current_user=current_user,
+        update_user=update_user,
+        websocket_auth=websocket_auth,
+    )
---
+++ packages/fps_auth_fief/fps_auth_fief-0.9.6-py3-none-any.whl/fps_auth_fief/routes.py
@@ -0,0 +1,170 @@
+import json
+from collections.abc import Awaitable, Callable
+from typing import Any
+
+import httpx
+from fastapi import APIRouter, Depends, Query, Request, Response
+from fastapi.responses import RedirectResponse
+from fief_client import FiefAccessTokenInfo
+from jupyverse_api.app import App
+from jupyverse_api.auth import Auth, User
+
+from jupyverse_api import Router
+
+from .backend import get_backend
+from .config import _AuthFiefConfig
+
+
+def auth_factory(
+    app: App,
+    auth_fief_config: _AuthFiefConfig,
+):
+    backend = get_backend(auth_fief_config)
+
+    class _AuthFief(Auth, Router):
+        def __init__(self) -> None:
+            super().__init__(app)
+
+            router = APIRouter()
+
+            @router.get("/auth-callback", name="auth_callback")
+            async def auth_callback(
+                request: Request,
+                response: Response,
+                code: str = Query(...),
+            ):
+                if auth_fief_config.callback_url:
+                    redirect_uri = auth_fief_config.callback_url
+                else:
+                    redirect_uri = str(request.url_for("auth_callback"))
+                tokens, user_info = await backend.fief.auth_callback(code, redirect_uri)
+
+                user_id = user_info["sub"]
+                async with httpx.AsyncClient() as client:
+                    headers = {"Authorization": f"Bearer {auth_fief_config.admin_api_key}"}
+                    r = await client.get(
+                        f"{auth_fief_config.base_url}/admin/api/oauth-providers/{auth_fief_config.oauth_provider_id}/access-token/{user_id}",
+                        headers=headers,
+                    )
+
+                # FIXME: this is hard-coded for GitHub authentication
+                access_token = r.json()["access_token"]
+                async with httpx.AsyncClient() as client:
+                    headers = {
+                        "Authorization": f"Bearer {access_token}",
+                        "Accept": "application/vnd.github+json",
+                        "X-GitHub-Api-Version": "2022-11-28",
+                    }
+                    r = await client.get("https://api.github.com/user", headers=headers)
+
+                d = r.json()
+                data = {
+                    "username": d["login"],
+                    "name": d["name"],
+                    "display_name": d["name"],
+                    "initials": "".join([word[0] for word in d["name"].split()]),
+                    "avatar_url": d["avatar_url"],
+                }
+                await backend.fief.update_profile(tokens["access_token"], {"fields": data})
+
+                # set permissions
+                async with httpx.AsyncClient() as client:
+                    headers = {
+                        "Authorization": f"Bearer {auth_fief_config.admin_api_key}",
+                        "accept": "application/json",
+                    }
+                    skip = 0
+                    nb = 100
+                    count = None
+                    got = 0
+                    while True:
+                        params = {"limit": nb, "skip": skip}
+                        r = await client.get(
+                            f"{auth_fief_config.base_url}/admin/api/permissions/",
+                            headers=headers,
+                            params=params,
+                        )
+                        d = r.json()
+                        if count is None:
+                            count = d["count"]
+
+                        permission_id = {
+                            result["codename"]: result["id"] for result in d["results"]
+                        }
+
+                        headers = {"Authorization": f"Bearer {auth_fief_config.admin_api_key}"}
+                        for permission, id in permission_id.items():
+                            data = {"id": id}
+                            await client.post(
+                                f"{auth_fief_config.base_url}/admin/api/users/{user_id}/permissions",
+                                headers=headers,
+                                json=data,
+                            )
+
+                        got += len(d["results"])
+                        if got < count:
+                            skip += nb
+                        else:
+                            break
+
+                refresh_token = tokens["refresh_token"]
+                assert refresh_token is not None
+                tokens, user_info = await backend.fief.auth_refresh_token(refresh_token)
+
+                response = RedirectResponse(request.url_for("root"))
+                response.set_cookie(
+                    backend.session_cookie_name,
+                    tokens["access_token"],
+                    max_age=tokens["expires_in"],
+                    httponly=True,
+                    secure=False,
+                )
+
+                return response
+
+            @router.get("/api/me")
+            async def get_api_me(
+                request: Request,
+                user: User = Depends(self.current_user()),
+                access_token_info: FiefAccessTokenInfo = Depends(backend.auth.authenticated()),
+            ):
+                checked_permissions: dict[str, list[str]] = {}
+                permissions = json.loads(
+                    dict(request.query_params).get("permissions", "{}").replace("'", '"')
+                )
+                if permissions:
+                    user_permissions: dict[str, list[str]] = {}
+                    for permission in access_token_info["permissions"]:
+                        resource, action = permission.split(":")
+                        if resource not in user_permissions:
+                            user_permissions[resource] = []
+                        user_permissions[resource].append(action)
+                    for resource, actions in permissions.items():
+                        user_resource_permissions = user_permissions.get(resource, [])
+                        allowed = checked_permissions[resource] = []
+                        for action in actions:
+                            if action in user_resource_permissions:
+                                allowed.append(action)
+
+                keys = ["username", "name", "display_name", "initials", "avatar_url", "color"]
+                identity = {k: getattr(user, k) for k in keys}
+                return {
+                    "identity": identity,
+                    "permissions": checked_permissions,
+                }
+
+            self.include_router(router)
+
+        def current_user(self, permissions: dict[str, list[str]] | None = None) -> Callable:
+            return backend.current_user(permissions)
+
+        async def update_user(self, update_user=Depends(backend.update_user)) -> Callable:
+            return update_user
+
+        def websocket_auth(
+            self,
+            permissions: dict[str, list[str]] | None = None,
+        ) -> Callable[[Any], Awaitable[tuple[Any, dict[str, list[str]] | None] | None]]:
+            return backend.websocket_auth(permissions)
+
+    return _AuthFief()
