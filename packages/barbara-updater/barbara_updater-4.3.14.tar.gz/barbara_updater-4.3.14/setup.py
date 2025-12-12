b04f7767aa3e1bf6a91b5eee7fd0462d725aa32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:07:32 +0000

    Add package barbara-updater/4.3.14/barbara_updater-4.3.14.tar.gz

---
+++ packages/barbara-updater/barbara_updater-4.3.14.tar.gz/barbara_updater-4.3.14/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='barbara_updater',
+    version='4.3.14',
+    author='santod',
+    description='weather observer',
+    py_modules=['barbara3']  # Ensure this contains the actual Python files you want to include
+)
