38b1468cb757bb87a40f8857021a45cff21c043d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 08:05:11 +0000

    Add package turbopuffer/1.9.0b4/turbopuffer-1.9.0b4-py3-none-any.whl

---
+++ packages/turbopuffer/turbopuffer-1.9.0b4-py3-none-any.whl/turbopuffer/types/namespace_multi_query_response.py
@@ -0,0 +1,29 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Dict, List, Optional
+
+from .row import Row
+from .._models import BaseModel
+from .query_billing import QueryBilling
+from .aggregation_group import AggregationGroup
+from .query_performance import QueryPerformance
+
+__all__ = ["NamespaceMultiQueryResponse", "Result"]
+
+
+class Result(BaseModel):
+    aggregation_groups: Optional[List[AggregationGroup]] = None
+
+    aggregations: Optional[Dict[str, object]] = None
+
+    rows: Optional[List[Row]] = None
+
+
+class NamespaceMultiQueryResponse(BaseModel):
+    billing: QueryBilling
+    """The billing information for a query."""
+
+    performance: QueryPerformance
+    """The performance information for a query."""
+
+    results: List[Result]
