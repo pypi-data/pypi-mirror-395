4eb8458b0297d83328f2d6a2978aff798f52aa4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:38 +0000

    Add package irispie-ce/0.74.1/irispie_ce-0.74.1-py3-none-any.whl

---
+++ packages/irispie-ce/irispie_ce-0.74.1-py3-none-any.whl/irispie/conveniences/copies.py
@@ -0,0 +1,23 @@
+"""
+Deep copy mixin
+"""
+
+
+#[
+from __future__ import annotations
+
+from typing import (Self, )
+import copy as _cp
+#]
+
+
+class Mixin:
+    """
+    """
+    #[
+    def copy(self, ) -> Self:
+        """
+        """
+        return _cp.deepcopy(self)
+    #]
+
---
+++ packages/irispie-ce/irispie_ce-0.74.1-py3-none-any.whl/irispie/executables/__init__.py
@@ -0,0 +1,3 @@
+"""
+Third-party executables
+"""
