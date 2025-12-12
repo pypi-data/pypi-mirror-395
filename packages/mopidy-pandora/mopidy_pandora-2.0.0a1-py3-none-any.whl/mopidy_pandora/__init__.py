71eccd2cc4761736782616ae1c4afa6686f26d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 23:05:08 +0000

    Add package mopidy-pandora/2.0.0a1/mopidy_pandora-2.0.0a1-py3-none-any.whl

---
+++ packages/mopidy-pandora/mopidy_pandora-2.0.0a1-py3-none-any.whl/mopidy_pandora/__init__.py
@@ -0,0 +1,93 @@
+import logging
+import pathlib
+from importlib.metadata import version
+
+from mopidy import config, ext
+
+__version__ = version("mopidy-pandora")
+
+logger = logging.getLogger(__name__)
+
+
+class Extension(ext.Extension):
+    dist_name = "mopidy-pandora"
+    ext_name = "pandora"
+    version = __version__
+
+    def get_default_config(self):
+        return config.read(pathlib.Path(__file__).parent / "ext.conf")
+
+    def get_config_schema(self):
+        from pandora.client import BaseAPIClient  # noqa: PLC0415
+
+        schema = super().get_config_schema()
+        schema["api_host"] = config.String()
+        schema["partner_encryption_key"] = config.String()
+        schema["partner_decryption_key"] = config.String()
+        schema["partner_username"] = config.String()
+        schema["partner_password"] = config.String()
+        schema["partner_device"] = config.String()
+        schema["username"] = config.String()
+        schema["password"] = config.Secret()
+        schema["preferred_audio_quality"] = config.String(
+            choices=[
+                BaseAPIClient.LOW_AUDIO_QUALITY,
+                BaseAPIClient.MED_AUDIO_QUALITY,
+                BaseAPIClient.HIGH_AUDIO_QUALITY,
+            ]
+        )
+        schema["sort_order"] = config.String(choices=["date", "A-Z", "a-z"])
+        schema["auto_setup"] = config.Boolean()
+        schema["auto_set_repeat"] = config.Deprecated()
+        schema["cache_time_to_live"] = config.Integer(minimum=0)
+        schema["event_support_enabled"] = config.Boolean()
+        schema["double_click_interval"] = config.String()
+        schema["on_pause_resume_click"] = config.String(
+            choices=[
+                "thumbs_up",
+                "thumbs_down",
+                "sleep",
+                "add_artist_bookmark",
+                "add_song_bookmark",
+                "delete_station",
+            ]
+        )
+        schema["on_pause_next_click"] = config.String(
+            choices=[
+                "thumbs_up",
+                "thumbs_down",
+                "sleep",
+                "add_artist_bookmark",
+                "add_song_bookmark",
+                "delete_station",
+            ]
+        )
+        schema["on_pause_previous_click"] = config.String(
+            choices=[
+                "thumbs_up",
+                "thumbs_down",
+                "sleep",
+                "add_artist_bookmark",
+                "add_song_bookmark",
+                "delete_station",
+            ]
+        )
+        schema["on_pause_resume_pause_click"] = config.String(
+            choices=[
+                "thumbs_up",
+                "thumbs_down",
+                "sleep",
+                "add_artist_bookmark",
+                "add_song_bookmark",
+                "delete_station",
+            ]
+        )
+        return schema
+
+    def setup(self, registry):
+        from .backend import PandoraBackend  # noqa: PLC0415
+        from .frontend import EventMonitorFrontend, PandoraFrontend  # noqa: PLC0415
+
+        registry.add("backend", PandoraBackend)
+        registry.add("frontend", PandoraFrontend)
+        registry.add("frontend", EventMonitorFrontend)
