3a5ee0b2e5a0334ec1c04c8fcd7123aeaeb226d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 22:06:42 +0000

    Add package target-gcs/1.2.3/target_gcs-1.2.3-py3-none-any.whl

---
+++ packages/target-gcs/target_gcs-1.2.3-py3-none-any.whl/target_gcs/sinks.py
@@ -0,0 +1,86 @@
+"""GCS target sink class, which handles writing streams.
+Opens an I/O stream pointed to a blob in the GCS bucket
+As messages are received from the source, they are streamed to the bucket. As the max batch size is reached, the queue is emptied and written to the I/O streams
+
+"""
+import time
+from collections import defaultdict
+from datetime import datetime
+from io import FileIO
+from typing import Optional
+
+import orjson
+import smart_open
+from google.cloud.storage import Client
+from singer_sdk.sinks import RecordSink
+
+
+class GCSSink(RecordSink):
+    """GCS target sink class."""
+
+    max_size = 1000  # Max records to write in one batch
+
+    def __init__(self, target, stream_name, schema, key_properties):
+        super().__init__(
+            target=target,
+            stream_name=stream_name,
+            schema=schema,
+            key_properties=key_properties,
+        )
+        self._gcs_write_handle: Optional[FileIO] = None
+        self._key_name: str = ""
+
+    @property
+    def key_name(self) -> str:
+        """Return the key name."""
+        if not self._key_name:
+            extraction_timestamp = round(time.time())
+            base_key_name = self.config.get(
+                "key_naming_convention",
+                f"{self.stream_name}_{extraction_timestamp}.{self.output_format}",
+            )
+            prefixed_key_name = (
+                f'{self.config.get("key_prefix", "")}/{base_key_name}'.replace(
+                    "//", "/"
+                )
+            ).lstrip("/")
+            date = datetime.today().strftime(self.config.get("date_format", "%Y-%m-%d"))
+            self._key_name = prefixed_key_name.format_map(
+                defaultdict(
+                    str,
+                    stream=self.stream_name,
+                    date=date,
+                    timestamp=extraction_timestamp,
+                )
+            )
+        return self._key_name
+
+    @property
+    def gcs_write_handle(self) -> FileIO:
+        """Opens a stream for writing to the target cloud object"""
+        if not self._gcs_write_handle:
+            credentials_path = self.config.get("credentials_file")
+            client = Client() if credentials_path is None else Client.from_service_account_json(credentials_path)
+            self._gcs_write_handle = smart_open.open(
+                f'gs://{self.config.get("bucket_name")}/{self.key_name}',
+                "wb",
+                transport_params=dict(
+                    client=client
+                ),
+            )
+        return self._gcs_write_handle
+
+    @property
+    def output_format(self) -> str:
+        """In the future maybe we will support more formats"""
+        return "jsonl"
+
+    def process_record(self, record: dict, context: dict) -> None:
+        """Process the record.
+
+        Developers may optionally read or write additional markers within the
+        passed `context` dict from the current batch.
+        """
+        self.gcs_write_handle.write(
+            orjson.dumps(record, option=orjson.OPT_APPEND_NEWLINE)
+        )
