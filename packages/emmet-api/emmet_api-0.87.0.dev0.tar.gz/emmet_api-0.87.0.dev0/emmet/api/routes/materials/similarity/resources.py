567947a190883013c93e61738804bb95de2f995d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 15:07:39 +0000

    Add package emmet-api/0.87.0.dev0/emmet_api-0.87.0.dev0.tar.gz

---
+++ packages/emmet-api/emmet_api-0.87.0.dev0.tar.gz/emmet_api-0.87.0.dev0/emmet/api/routes/materials/similarity/resources.py
@@ -0,0 +1,26 @@
+from emmet.api.query_operator import PaginationQuery, SparseFieldsQuery
+from emmet.api.resource import ReadOnlyResource
+from emmet.api.core.global_header import GlobalHeaderProcessor
+from emmet.api.core.settings import MAPISettings
+from emmet.api.routes.materials.materials.query_operators import MultiMaterialIDQuery
+
+from emmet.core.similarity import SimilarityDoc
+
+
+def similarity_resource(similarity_store):
+    resource = ReadOnlyResource(
+        similarity_store,
+        SimilarityDoc,
+        query_operators=[
+            MultiMaterialIDQuery(),
+            PaginationQuery(),
+            SparseFieldsQuery(SimilarityDoc, default_fields=["material_id"]),
+        ],
+        header_processor=GlobalHeaderProcessor(),
+        tags=["Materials Similarity"],
+        sub_path="/similarity/",
+        disable_validation=True,
+        timeout=MAPISettings().TIMEOUT,  # type: ignore
+    )
+
+    return resource
