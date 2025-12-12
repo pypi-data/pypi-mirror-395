c1045eb0fea0685c68f788b4ac16f96275cf47e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 22:06:09 +0000

    Add package evolution-sdk-python/0.0.1/evolution_sdk_python-0.0.1-py3-none-any.whl

---
+++ packages/evolution-sdk-python/evolution_sdk_python-0.0.1-py3-none-any.whl/evolutionapi/exceptions.py
@@ -0,0 +1,16 @@
+class EvolutionAPIError(Exception):
+    """Erro genérico da API Evolution."""
+
+    pass
+
+
+class EvolutionAuthenticationError(EvolutionAPIError):
+    """Erro de autenticação com a API Evolution."""
+
+    pass
+
+
+class EvolutionNotFoundError(EvolutionAPIError):
+    """Recurso não encontrado na API Evolution."""
+
+    pass
