cbdd044c0ac4dfc0e360fef07ddb27cd62a4e9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:25 +0000

    Add package cpdts/3.0.7/cpdts-3.0.7-py3-none-any.whl

---
+++ packages/cpdts/cpdts-3.0.7-py3-none-any.whl/cpdts/utils/__init__.py
@@ -0,0 +1,10 @@
+"""Utils package for cpats.
+
+This package contains utility functions for file operations and other common tasks.
+"""
+
+from .genfile import create_file_with_path
+
+__all__ = [
+    "create_file_with_path",
+]
