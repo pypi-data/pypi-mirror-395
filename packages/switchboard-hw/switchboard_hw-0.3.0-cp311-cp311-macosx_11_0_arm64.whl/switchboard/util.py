81a9393500762ae82050f6d255f9bcfd565f2106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:06:28 +0000

    Add package switchboard-hw/0.3.0/switchboard_hw-0.3.0-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/switchboard-hw/switchboard_hw-0.3.0-cp311-cp311-macosx_11_0_arm64.whl/switchboard/util.py
@@ -0,0 +1,131 @@
+# General utilities for working with switchboard
+
+# Copyright (c) 2024 Zero ASIC Corporation
+# This code is licensed under Apache License 2.0 (see LICENSE for details)
+
+import atexit
+import signal
+import subprocess
+import shlex
+
+
+def plusargs_to_args(plusargs):
+    args = []
+    if plusargs is not None:
+        if not isinstance(plusargs, list):
+            raise TypeError('plusargs must be a list')
+        for plusarg in plusargs:
+            if isinstance(plusarg, (list, tuple)):
+                if len(plusarg) != 2:
+                    raise ValueError('Only lists/tuples of length 2 allowed')
+                args += [f'+{plusarg[0]}={plusarg[1]}']
+            else:
+                args += [f'+{plusarg}']
+    return args
+
+
+def binary_run(bin, args=None, stop_timeout=10, use_sigint=False,
+    quiet=False, print_command=False, cwd=None, env=None):
+
+    cmd = []
+
+    cmd += [bin]
+
+    if args is not None:
+        if not isinstance(args, list):
+            raise TypeError('args must be a list')
+        cmd += args
+
+    cmd = [str(elem) for elem in cmd]
+    if print_command:
+        print(' '.join([shlex.quote(elem) for elem in cmd]))
+
+    kwargs = {}
+    if quiet:
+        kwargs['stdout'] = subprocess.DEVNULL
+        kwargs['stderr'] = subprocess.DEVNULL
+
+    p = subprocess.Popen(cmd, cwd=cwd, env=env, **kwargs)
+
+    def stop_bin(p=p, stop_timeout=stop_timeout, use_sigint=use_sigint):
+        poll = p.poll()
+        if poll is not None:
+            # process has stopped
+            return
+
+        if use_sigint:
+            try:
+                p.send_signal(signal.SIGINT)
+                p.wait(stop_timeout)
+                return
+            except:  # noqa: E722
+                # if there is an exception for any reason, including
+                # Ctrl-C during the wait() call, want to make sure
+                # that the process is actually terminated
+                pass
+
+        # if we get to this point, the process is still running
+        # and sigint didn't work (or we didn't try it)
+        p.terminate()
+
+    # note: tried combining all process terminations into a single
+    # function registered with atexit, but it didn't appear to make
+    # a difference when running with a few hundred verilator sim
+    # processes - still took a few dozen milliseconds to stop each
+    # simulator.
+
+    atexit.register(stop_bin)
+
+    return p
+
+
+class ProcessCollection:
+    def __init__(self):
+        self.processes = []
+
+    def add(self, process):
+        self.processes.append(process)
+
+    def wait(self):
+        import subprocess
+        import multiprocessing
+
+        for process in self.processes:
+            if isinstance(process, subprocess.Popen):
+                process.wait()
+            elif isinstance(process, multiprocessing.Process):
+                process.join()
+            else:
+                raise Exception(f'Unknown process type: {type(process)}')
+
+    def terminate(
+        self,
+        stop_timeout=10,
+        use_sigint=False
+    ):
+        if not self.processes:
+            return
+
+        for p in self.processes:
+            if isinstance(p, ProcessCollection):
+                p.terminate(stop_timeout=stop_timeout, use_sigint=use_sigint)
+            else:
+                poll = p.poll()
+                if poll is not None:
+                    # process has stopped
+                    return
+
+                if use_sigint:
+                    try:
+                        p.send_signal(signal.SIGINT)
+                        p.wait(stop_timeout)
+                        return
+                    except:  # noqa: E722
+                        # if there is an exception for any reason, including
+                        # Ctrl-C during the wait() call, want to make sure
+                        # that the process is actually terminated
+                        pass
+
+                # if we get to this point, the process is still running
+                # and sigint didn't work (or we didn't try it)
+                p.terminate()
---
+++ packages/switchboard-hw/switchboard_hw-0.3.0-cp311-cp311-macosx_11_0_arm64.whl/switchboard/verilog/sim/sb_to_queue_sim.sv
@@ -0,0 +1,196 @@
+// sb_to_queue_sim
+
+// ready_mode settings (in all cases, ready remains low if an outbound packet is stuck)
+// ready_mode=0: ready waits for valid before asserting
+// ready_mode=1: ready remains asserted as long as an outbound packet is not stuck
+// ready_mode=2: ready toggles randomly as long as an outbound packet is not stuck
+
+// Copyright (c) 2024 Zero ASIC Corporation
+// This code is licensed under Apache License 2.0 (see LICENSE for details)
+
+`default_nettype none
+
+module sb_to_queue_sim #(
+    parameter integer READY_MODE_DEFAULT=0,
+    parameter integer DW=416,
+    parameter FILE=""
+) (
+    input clk,
+    input reset,
+    input [DW-1:0] data,
+    input [31:0] dest,
+    input last,
+    output reg ready=1'b0,
+    input valid
+);
+    // SBDW value corresponds to UMI DW=256
+    // 32b (cmd) + 64b (srcaddr) + 64b (dstaddr) + 256b = 416b
+
+    // SBDW must be a multiple of 32 (constrained by VPI driver,
+    // which transfers data in 32-bit chunks)
+
+    localparam SBDW = 416;
+
+    `ifdef __ICARUS__
+        `define SB_EXT_FUNC(x) $``x``
+        `define SB_START_FUNC task
+        `define SB_END_FUNC endtask
+    `else
+        `define SB_EXT_FUNC(x) x
+        `define SB_START_FUNC function void
+        `define SB_END_FUNC endfunction
+
+        import "DPI-C" function void pi_sb_tx_init (output int id,
+            input string uri, input int width);
+        import "DPI-C" function void pi_sb_send (input int id, input bit [SBDW-1:0] sdata,
+            input bit [31:0] sdest, input bit slast, output int success);
+    `endif
+
+    // internal signals
+
+    integer id = -1;
+
+    `SB_START_FUNC init(input string uri);
+        /* verilator lint_off IGNOREDRETURN */
+        `SB_EXT_FUNC(pi_sb_tx_init)(id, uri, (DW + 7)/8);
+        /* verilator lint_on IGNOREDRETURN */
+    `SB_END_FUNC
+
+    integer success = 0;
+    reg pending = 1'b0;
+
+    wire [SBDW-1:0] data_padded;
+    generate
+        if (SBDW > DW) begin
+            assign data_padded = {{(SBDW-DW){1'b0}}, data};
+        end else begin
+            assign data_padded = data;
+        end
+    endgenerate
+
+    reg [SBDW-1:0] sdata = 'b0;
+    reg [31:0] sdest = 32'b0;
+    reg slast = 1'b0;
+
+    // ready mode
+
+    integer ready_mode = READY_MODE_DEFAULT;
+
+    `SB_START_FUNC set_ready_mode(input integer value);
+        /* verilator lint_off IGNOREDRETURN */
+        ready_mode = value;
+        /* verilator lint_on IGNOREDRETURN */
+    `SB_END_FUNC
+
+    // main logic
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            pending <= 1'b0;
+            ready <= 1'b0;
+            slast <= 1'b0;
+        end else begin
+            if (ready && valid) begin
+                // try to send a packet, with success==1 indicating that the
+                // send was successful.  in general, sends should succeed,
+                // unless the queue they're trying to push to is full.
+                if (id != -1) begin
+                    /* verilator lint_off IGNOREDRETURN */
+                    `SB_EXT_FUNC(pi_sb_send)(id, data_padded, dest, last, success);
+                    /* verilator lint_on IGNOREDRETURN */
+                end else begin
+                    /* verilator lint_off BLKSEQ */
+                    success = 32'd0;
+                    /* verilator lint_on BLKSEQ */
+                end
+
+                // if the send was not successful, mark it pending. ready cannot be asserted
+                // if there is a pending re-send, since the next send may fail, and there
+                // would be no place to store the data for the new resend.  we could have a
+                // queue, but that would have finite depth, so we would still have to be able
+                // to apply backpressure.
+                if (success == 32'd0) begin
+                    pending <= 1'b1;
+                    ready <= 1'b0;
+                    sdata <= data_padded;
+                    sdest <= dest;
+                    slast <= last;
+                end else begin
+                    pending <= 1'b0;
+                    if (ready_mode == 32'd0) begin
+                        ready <= 1'b0;
+                    end else if (ready_mode == 32'd1) begin
+                        ready <= 1'b1;
+                    end else begin
+                        /* verilator lint_off WIDTH */
+                        ready <= ($random % 2);
+                        /* verilator lint_on WIDTH */
+                    end
+                end
+            end else if (pending) begin
+                // try to re-send a packet.  note that in a given cycle, a packet can be sent
+                // for the first time or re-sent, but not both, because ready cannot be asserted
+                // if there is a packet pending, for the reason given above.
+                if (id != -1) begin
+                    /* verilator lint_off IGNOREDRETURN */
+                    `SB_EXT_FUNC(pi_sb_send)(id, sdata, sdest, slast, success);
+                    /* verilator lint_on IGNOREDRETURN */
+                end else begin
+                    /* verilator lint_off BLKSEQ */
+                    success = 32'd0;
+                    /* verilator lint_on BLKSEQ */
+                end
+
+                // if the re-send was unsuccessful, we have to keep ready de-asserted,
+                // but if it was successful we can assert ready if we want to,
+                // depending on ready_mode
+                if (success == 32'd0) begin
+                    pending <= 1'b1;
+                    ready <= 1'b0;
+                end else begin
+                    pending <= 1'b0;
+                    if (ready_mode == 32'd0) begin
+                        ready <= 1'b0;
+                    end else if (ready_mode == 32'd1) begin
+                        ready <= 1'b1;
+                    end else begin
+                        /* verilator lint_off WIDTH */
+                        ready <= ($random % 2);
+                        /* verilator lint_on WIDTH */
+                    end
+                end
+            end else begin
+                // if there's nothing pending, then we can assert ready
+                // if we want to.  whether we do or not depends on ready_mode.
+                if (ready_mode == 32'd0) begin
+                    ready <= valid;
+                end else if (ready_mode == 32'd1) begin
+                    ready <= 1'b1;
+                end else begin
+                    /* verilator lint_off WIDTH */
+                    ready <= ($random % 2);
+                    /* verilator lint_on WIDTH */
+                end
+            end
+        end
+    end
+
+    // initialize
+
+    initial begin
+        if (FILE != "") begin
+            /* verilator lint_off IGNOREDRETURN */
+            init(FILE);
+            /* verilator lint_on IGNOREDRETURN */
+        end
+    end
+
+    // clean up macros
+
+    `undef SB_EXT_FUNC
+    `undef SB_START_FUNC
+    `undef SB_END_FUNC
+
+endmodule
+
+`default_nettype wire
