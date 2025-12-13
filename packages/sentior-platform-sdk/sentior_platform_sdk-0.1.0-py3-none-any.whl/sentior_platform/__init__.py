3e8678ec75dbfd67f247caa00e686622b34931ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 12:07:27 +0000

    Add package sentior-platform-sdk/0.1.0/sentior_platform_sdk-0.1.0-py3-none-any.whl

---
+++ packages/sentior-platform-sdk/sentior_platform_sdk-0.1.0-py3-none-any.whl/sentior_platform/__init__.py
@@ -0,0 +1,7 @@
+"""
+Sentior Platform SDK (Python)
+
+Gebruik:
+    from sentior_platform.generated import ENTITLEMENTS, COLLECTIONS, EVENTS, SDK_VERSION
+"""
+from . import generated  # type: ignore
