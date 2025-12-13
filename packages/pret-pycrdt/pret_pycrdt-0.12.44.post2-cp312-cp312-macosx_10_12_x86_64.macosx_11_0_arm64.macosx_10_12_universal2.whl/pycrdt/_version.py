2c80fea583b5e5f9eba2a6a81c0d6a0a10a097ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 09:06:28 +0000

    Add package pret-pycrdt/0.12.44.post2/pret_pycrdt-0.12.44.post2-cp312-cp312-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl

---
+++ packages/pret-pycrdt/pret_pycrdt-0.12.44.post2-cp312-cp312-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl/pycrdt/_version.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:  # pragma: no cover
+    from importlib_metadata import (  # type: ignore[assignment, import-not-found, no-redef]
+        PackageNotFoundError,
+        version,
+    )
+
+try:
+    __version__ = version("pycrdt")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "uninstalled"
