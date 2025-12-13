c4bc523cb766c85078ff9757f809921375c982c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 08:06:32 +0000

    Add package smt/2.10.1/smt-2.10.1-cp313-cp313-musllinux_1_2_x86_64.whl

---
+++ packages/smt/smt-2.10.1-cp313-cp313-musllinux_1_2_x86_64.whl/smt/surrogate_models/__init__.py
@@ -0,0 +1,52 @@
+from .ls import LS
+from .qp import QP
+from .krg import KRG
+from .kpls import KPLS
+from .gekpls import GEKPLS
+from .kplsk import KPLSK
+from .gpx import GPX
+from .genn import GENN
+from .mgp import MGP
+from .sgp import SGP
+
+from .krg_based import MixIntKernelType
+
+from smt.design_space import (
+    DesignSpace,
+    CategoricalVariable,
+    FloatVariable,
+    IntegerVariable,
+    OrdinalVariable,
+)
+from smt.utils.kriging import MixHrcKernelType
+
+__all__ = [
+    "LS",
+    "QP",
+    "KRG",
+    "KPLS",
+    "GEKPLS",
+    "KPLSK",
+    "GPX",
+    "GENN",
+    "MGP",
+    "SGP",
+    "MixIntKernelType",
+    "DesignSpace",
+    "FloatVariable",
+    "IntegerVariable",
+    "OrdinalVariable",
+    "CategoricalVariable",
+    "MixHrcKernelType",
+]
+
+try:
+    from .idw import IDW
+    from .rbf import RBF
+    from .rmtc import RMTC
+    from .rmtb import RMTB
+
+    __all__ = __all__ + ["IDW", "RBF", "RMTC", "RMTB"]
+
+except ImportError:
+    pass
---
+++ packages/smt/smt-2.10.1-cp313-cp313-musllinux_1_2_x86_64.whl/smt/surrogate_models/kplsk.py
@@ -0,0 +1,48 @@
+"""
+Author: Dr. Mohamed A. Bouhlel <mbouhlel@umich.edu>
+
+This package is distributed under New BSD license.
+"""
+
+from smt.surrogate_models import KPLS
+from smt.utils.kriging import componentwise_distance, componentwise_distance_PLS
+
+
+class KPLSK(KPLS):
+    name = "KPLSK"
+
+    def _initialize(self):
+        super()._initialize()
+        declare = self.options.declare
+        # KPLSK used only with "squar_exp" correlations
+        declare(
+            "corr",
+            "squar_exp",
+            values=("squar_exp"),
+            desc="Correlation function type",
+            types=(str),
+        )
+
+    def _componentwise_distance(self, dx, opt=0, theta=None, return_derivative=False):
+        if opt == 0:
+            # Kriging step
+            d = componentwise_distance(
+                dx,
+                self.options["corr"],
+                self.nx,
+                power=self.options["pow_exp_power"],
+                theta=theta,
+                return_derivative=return_derivative,
+            )
+        else:
+            # KPLS step
+            d = componentwise_distance_PLS(
+                dx,
+                self.options["corr"],
+                self.options["n_comp"],
+                self.coeff_pls,
+                power=self.options["pow_exp_power"],
+                theta=theta,
+                return_derivative=return_derivative,
+            )
+        return d
