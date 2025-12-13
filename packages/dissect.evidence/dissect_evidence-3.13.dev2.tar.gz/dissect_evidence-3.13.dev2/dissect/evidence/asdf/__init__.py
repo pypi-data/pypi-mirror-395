ab4584546b40055ae690cdd6a0de5f65926408c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:06:21 +0000

    Add package dissect.evidence/3.13.dev2/dissect_evidence-3.13.dev2.tar.gz

---
+++ packages/dissect.evidence/dissect_evidence-3.13.dev2.tar.gz/dissect_evidence-3.13.dev2/dissect/evidence/asdf/__init__.py
@@ -0,0 +1,8 @@
+from dissect.evidence.asdf.asdf import FILE_MAGIC, AsdfSnapshot, AsdfStream, AsdfWriter
+
+__all__ = [
+    "FILE_MAGIC",
+    "AsdfSnapshot",
+    "AsdfStream",
+    "AsdfWriter",
+]
