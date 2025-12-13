2153cf4e95f9589b8a32087737a8cdec48a1ee98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:07:02 +0000

    Add package win-can-tool/1.2.24/win_can_tool-1.2.24.tar.gz

---
+++ packages/win-can-tool/win_can_tool-1.2.24.tar.gz/win_can_tool-1.2.24/win_can_tool/ics/structures/op_eth_general_settings.py
@@ -0,0 +1,72 @@
+# This file was auto generated; Do not modify, if you value your sanity!
+import ctypes
+import enum
+
+
+
+class Nameless19471(ctypes.Structure):
+    _pack_ = 2
+    _fields_ = [
+        ('tap1ToVspy', ctypes.c_uint8, 1),
+        ('tap2ToVspy', ctypes.c_uint8, 1),
+        ('tap3ToVspy', ctypes.c_uint8, 1),
+        ('tap4ToVspy', ctypes.c_uint8, 1),
+        ('tap5ToVspy', ctypes.c_uint8, 1),
+        ('tap6ToVspy', ctypes.c_uint8, 1),
+        ('tap7ToVspy', ctypes.c_uint8, 1),
+        ('tap8ToVspy', ctypes.c_uint8, 1),
+    ]
+
+
+
+class Nameless11965(ctypes.Union):
+    _pack_ = 2
+    _anonymous_  = ('Nameless19471',)
+    _fields_ = [
+        ('tapPairOpt', ctypes.c_uint8),
+        ('Nameless19471', Nameless19471),
+    ]
+
+
+
+class flags(ctypes.Structure):
+    _pack_ = 2
+    _fields_ = [
+        ('bTapEnSwitch', ctypes.c_uint32, 1),
+        ('bTapEnPtp', ctypes.c_uint32, 1),
+        ('bEnReportLinkQuality', ctypes.c_uint32, 1),
+        ('bEnTapTxReceipts', ctypes.c_uint32, 1),
+        ('reserved', ctypes.c_uint32, 28),
+    ]
+
+
+
+class Nameless23998(ctypes.Union):
+    _pack_ = 2
+    _fields_ = [
+        ('flags', flags),
+        ('uFlags', ctypes.c_uint32),
+    ]
+
+
+
+class op_eth_general_settings(ctypes.Structure):
+    _pack_ = 2
+    _anonymous_  = ('Nameless11965', 'Nameless23998')
+    _fields_ = [
+        ('ucInterfaceType', ctypes.c_uint8),
+        ('Nameless11965', Nameless11965),
+        ('reserved0', ctypes.c_uint8 * 2),
+        ('tapPair0', ctypes.c_uint16),
+        ('tapPair1', ctypes.c_uint16),
+        ('tapPair2', ctypes.c_uint16),
+        ('tapPair3', ctypes.c_uint16),
+        ('tapPair4', ctypes.c_uint16),
+        ('tapPair5', ctypes.c_uint16),
+        ('Nameless23998', Nameless23998),
+    ]
+
+
+OP_ETH_GENERAL_SETTINGS_t = op_eth_general_settings
+OP_ETH_GENERAL_SETTINGS = op_eth_general_settings
+
