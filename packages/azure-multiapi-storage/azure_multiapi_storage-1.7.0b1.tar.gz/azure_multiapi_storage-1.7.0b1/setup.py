ae5b7c80da90d6fb9461ae9430dadab1c4c59f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 08:07:42 +0000

    Add package azure-multiapi-storage/1.7.0b1/azure_multiapi_storage-1.7.0b1.tar.gz

---
+++ packages/azure-multiapi-storage/azure_multiapi_storage-1.7.0b1.tar.gz/azure_multiapi_storage-1.7.0b1/setup.py
@@ -0,0 +1,57 @@
+﻿#!/usr/bin/env python
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from setuptools import find_packages, setup
+import sys
+
+# azure v0.x is not compatible with this package
+# azure v0.x used to have a __version__ attribute (newer versions don't)
+try:
+    import azure
+    try:
+        ver = azure.__version__
+        raise Exception(
+            'This package is incompatible with azure=={}. '.format(ver) +
+            'Uninstall it with "pip uninstall azure".'
+        )
+    except AttributeError:
+        pass
+except ImportError:
+    pass
+
+setup(
+    name='azure-multiapi-storage',
+    version='1.7.0b1',
+    description='Microsoft Azure Storage Client Library for Python with multi API version support.',
+    long_description=open('README.rst', 'r').read(),
+    license='MIT',
+    author='Microsoft Corporation',
+    author_email='azpycli@microsoft.com',
+    url='https://github.com/Azure/azure-multiapi-storage-python',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+    ],
+    zip_safe=False,
+    packages=find_packages(exclude=["azure"]),
+    install_requires=[
+        'azure-common',
+        'python-dateutil',
+        'requests',
+        "azure-core<2.0.0,>=1.10.0",
+        "msrest>=0.6.18",
+        "cryptography>=2.1.4"
+    ],
+    python_requires=">=3.9.0"
+)
