b7fed11d66de1b74c711f144cbd5a16d2d12c806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:32 +0000

    Add package fabric-datalake-manager/0.1.22/fabric_datalake_manager-0.1.22-py3-none-any.whl

---
+++ packages/fabric-datalake-manager/fabric_datalake_manager-0.1.22-py3-none-any.whl/fabric_datalake_manager/interfaces/config_interface.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from fabric_datalake_manager.ingestion_models.app_config import Configuration
+
+class ILakeConfig(ABC):
+    pass
+
+
+class IConfig(ABC):
+    """Interface for Data configuration."""
+    @abstractmethod
+    def configure(self, config: Configuration):
+        raise NotImplementedError
