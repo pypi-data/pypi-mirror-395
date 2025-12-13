a81e807516f8b93d0e0fa21838745a42e1b6ac99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 03:27:28 +0000

    Add package vision-agents-plugins-roboflow/0.2.3/vision_agents_plugins_roboflow-0.2.3-py3-none-any.whl

---
+++ packages/vision-agents-plugins-roboflow/vision_agents_plugins_roboflow-0.2.3-py3-none-any.whl/vision_agents/plugins/roboflow/utils.py
@@ -0,0 +1,36 @@
+from typing import Iterable, Optional
+
+import cv2
+import numpy as np
+import supervision as sv
+
+
+def annotate_image(
+    image: np.ndarray,
+    detections: sv.Detections,
+    classes: dict[int, str],
+    dim_factor: Optional[float] = None,
+) -> np.ndarray:
+    """
+    Draw bounding boxes and labels on frame.
+    """
+
+    # Dim the background to make detected objects brigther
+    if dim_factor:
+        mask = np.zeros(image.shape[:2], dtype=np.uint8)
+        for xyxy in detections.xyxy:
+            x1, y1, x2, y2 = xyxy.astype(int)
+            cv2.rectangle(mask, (x1, y1), (x2, y2), 255, -1)
+        image[mask == 0] = (image[mask == 0] * dim_factor).astype(np.uint8)
+
+    boxed_image = sv.BoxAnnotator(thickness=1).annotate(image.copy(), detections)
+    detected_class_ids: Iterable[int] = (
+        detections.class_id if detections.class_id is not None else []
+    )
+    labels = [classes[class_id] for class_id in detected_class_ids]
+    labeled_image = sv.LabelAnnotator(
+        text_position=sv.Position.BOTTOM_CENTER,
+        text_scale=0.25,
+        text_padding=1,
+    ).annotate(boxed_image, detections, labels)
+    return labeled_image
