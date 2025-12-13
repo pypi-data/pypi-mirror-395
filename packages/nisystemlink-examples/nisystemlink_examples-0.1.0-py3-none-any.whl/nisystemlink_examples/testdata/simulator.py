b2acc742eab9df5fb1eec3c2c84dd4f2cb1e9cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 13:14:09 +0000

    Add package nisystemlink-examples/0.1.0/nisystemlink_examples-0.1.0-py3-none-any.whl

---
+++ packages/nisystemlink-examples/nisystemlink_examples-0.1.0-py3-none-any.whl/nisystemlink_examples/testdata/simulator.py
@@ -0,0 +1,26 @@
+"""This module provides a `Simulator` class for simulating test sequences in SystemLink.
+
+Classes:
+    Simulator: Simulates test sequences by printing provided test parameters.
+"""
+
+
+class Simulator:
+    """Simulates test sequences based on a test plan for a product using its specifications."""
+
+    def __init__(self):
+        """Initializes the Simulator."""
+        pass
+
+    def simulate_sequence(
+        self,
+        part_number: str,
+        serial_number: str,
+        test_plan_id: str,
+        system_id: str,
+        test_program: str,
+        operator: str,
+        hostname: str,
+    ) -> str:
+        """Simulates a test sequence."""
+        return "Hello World"
