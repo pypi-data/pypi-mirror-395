680a86b7786b5e3f2bd18cf9b0c70d067e46d1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 01:34:11 +0000

    Add package dusted/1.4.1/dusted-1.4.1.tar.gz

---
+++ packages/dusted/dusted-1.4.1.tar.gz/dusted-1.4.1/src/dusted/config.py
@@ -0,0 +1,53 @@
+import configparser
+from enum import Enum
+from pathlib import Path
+from typing import NamedTuple
+
+import platformdirs
+
+
+class _ConfigOption(NamedTuple):
+    name: str
+    default: str | bool
+
+
+class ConfigOption(Enum):
+    DUSTFORCE_PATH = _ConfigOption(
+        "dustforce_path", r"C:\Program Files (x86)\Steam\steamapps\common\Dustforce"
+    )
+    SHOW_LEVEL = _ConfigOption("show_level", True)
+
+    @classmethod
+    def defaults(cls):
+        return {option.value.name: option.value.default for option in cls}
+
+
+class Config:
+    def __init__(self):
+        self.path = Path(platformdirs.user_config_dir("dusted")) / "config.ini"
+        self.config = configparser.ConfigParser(ConfigOption.defaults())
+        self.read()
+
+    def get(self, option: ConfigOption):
+        name = option.value.name
+        if isinstance(option.value.default, bool):
+            return self.config["DEFAULT"].getboolean(name)
+        return self.config["DEFAULT"].get(name)
+
+    def set(self, option: ConfigOption, value: str | bool):
+        if isinstance(value, bool):
+            real_value = "true" if value else "false"
+        else:
+            real_value = value
+        self.config["DEFAULT"][option.value.name] = real_value
+
+    def read(self):
+        self.config.read(self.path)
+
+    def write(self):
+        self.path.parent.mkdir(parents=True, exist_ok=True)
+        with self.path.open("w+") as file:
+            self.config.write(file)
+
+
+config = Config()
