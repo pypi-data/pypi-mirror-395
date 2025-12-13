67a8b775005e07ecc53cbe1216a3d17ce55274ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 01:33:37 +0000

    Add package fastrub/2.4.8/fastrub-2.4.8.tar.gz

---
+++ packages/fastrub/fastrub-2.4.8.tar.gz/fastrub-2.4.8/fast_rub/__init__.py
@@ -0,0 +1,7 @@
+from .Client import Client
+from .filters import Filter
+from .version import v
+
+__author__="Seyyed Mohamad Hosein Moosavi Raja"
+__version__=v
+__all__ = ['Client', 'Filter','version']
