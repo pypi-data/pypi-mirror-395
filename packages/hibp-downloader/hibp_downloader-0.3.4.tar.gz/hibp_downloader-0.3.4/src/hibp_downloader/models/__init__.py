248d97bf487d3beaa6067df64b1221805ca7ecea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 05:07:27 +0000

    Add package hibp-downloader/0.3.4/hibp_downloader-0.3.4.tar.gz

---
+++ packages/hibp-downloader/hibp_downloader-0.3.4.tar.gz/hibp_downloader-0.3.4/src/hibp_downloader/models/__init__.py
@@ -0,0 +1,5 @@
+from .app_context import AppContext
+from .hash_type import HashType
+from .prefix_metadata import PrefixMetadata, PrefixMetadataDataSource
+from .stats import QueueItemStats, QueueItemStatsCompute, QueueRunningStats
+from .worker_args import WorkerArgs
---
+++ packages/hibp-downloader/hibp_downloader-0.3.4.tar.gz/hibp_downloader-0.3.4/src/hibp_downloader/models/worker_args.py
@@ -0,0 +1,26 @@
+from dataclasses import asdict, dataclass
+from pathlib import Path
+from typing import Optional
+
+from .hash_type import HashType
+
+
+@dataclass()
+class WorkerArgs:
+    hash_type: HashType
+    data_path: Path
+    metadata_path: Path
+    encoding_type: str
+
+    http_timeout: int
+    http_max_retries: int
+    http_proxy: str
+    http_certificates: str
+    http_debug: bool
+
+    ignore_etag: bool
+    local_cache_ttl: int
+    worker_index: Optional[int] = None
+
+    def as_dict(self):
+        return {k: v for k, v in asdict(self).items()}
