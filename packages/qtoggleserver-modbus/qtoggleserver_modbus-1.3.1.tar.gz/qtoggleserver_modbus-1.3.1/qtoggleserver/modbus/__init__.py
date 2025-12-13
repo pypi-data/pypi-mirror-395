3d42bb05f3ba40a0f168a877104676ed7efbddf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 01:33:38 +0000

    Add package qtoggleserver-modbus/1.3.1/qtoggleserver_modbus-1.3.1.tar.gz

---
+++ packages/qtoggleserver-modbus/qtoggleserver_modbus-1.3.1.tar.gz/qtoggleserver_modbus-1.3.1/qtoggleserver/modbus/__init__.py
@@ -0,0 +1,7 @@
+from .client import ModbusSerialClient, ModbusTcpClient, PassiveModbusSerialClient, PassiveModbusTcpClient
+
+
+__all__ = ["ModbusSerialClient", "ModbusTcpClient", "PassiveModbusSerialClient", "PassiveModbusTcpClient"]
+
+
+VERSION = "0.0.0"
---
+++ packages/qtoggleserver-modbus/qtoggleserver_modbus-1.3.1.tar.gz/qtoggleserver_modbus-1.3.1/qtoggleserver/modbus/server.py
@@ -0,0 +1,50 @@
+import logging
+
+from qtoggleserver.peripherals import Peripheral
+
+from .base import BaseModbus
+
+
+class BaseModbusServer(BaseModbus, Peripheral):
+    logger = logging.getLogger(__name__)
+
+    def __init__(
+        self,
+        *,
+        identity_vendor_name: str = "",
+        identity_product_code: str = "",
+        identity_major_minor_revision: str = "",
+        identity_vendor_url: str = "",
+        identity_product_name: str = "",
+        identity_model_name: str = "",
+        identity_user_application_name: str = "",
+        **kwargs,
+    ) -> None:
+        self.identity_vendor_name: str = identity_vendor_name
+        self.identity_product_code: str = identity_product_code
+        self.identity_major_minor_revision: str = identity_major_minor_revision
+        self.identity_vendor_url: str = identity_vendor_url
+        self.identity_product_name: str = identity_product_name
+        self.identity_model_name: str = identity_model_name
+        self.identity_user_application_name: str = identity_user_application_name
+
+        super().__init__(**kwargs)
+
+    # async def make_port_args(self) -> list[Union[dict[str, Any], type[core_ports.BasePort]]]:
+    #     port_args = []
+    #     for id_, details in self.port_details.items():
+    #         port_args.append({
+    #             'driver': ...,
+    #             'id': id_,
+    #             **details
+    #         })
+    #
+    #     return port_args
+
+    # async def write_port_value(
+    #     self,
+    #     port: core_ports.BasePort,
+    #     request_details: dict[str, Any],
+    #     context: dict[str, Any]
+    # ) -> None:
+    # ...
