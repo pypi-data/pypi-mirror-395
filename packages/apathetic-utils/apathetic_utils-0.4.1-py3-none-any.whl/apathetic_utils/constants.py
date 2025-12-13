170945e1940e7137f1f07354c484cc34cf128d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 03:27:24 +0000

    Add package apathetic-utils/0.4.1/apathetic_utils-0.4.1-py3-none-any.whl

---
+++ packages/apathetic-utils/apathetic_utils-0.4.1-py3-none-any.whl/apathetic_utils/constants.py
@@ -0,0 +1,14 @@
+# src/apathetic_utils/constants.py
+"""Constants for Apathetic Utils."""
+
+from __future__ import annotations
+
+
+class ApatheticUtils_Internal_Constants:  # noqa: N801  # pyright: ignore[reportUnusedClass]
+    """Constants for apathetic utils functionality.
+
+    This class contains all constant values used by apathetic_utils.
+    It's kept separate for organizational purposes.
+    """
+
+    # Constants will be added here as utilities are implemented
