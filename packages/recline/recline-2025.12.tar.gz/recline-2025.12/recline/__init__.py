b265223735ae853c1fb7606733c291b51d576793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:30 +0000

    Add package recline/2025.12/recline-2025.12.tar.gz

---
+++ packages/recline/recline-2025.12.tar.gz/recline-2025.12/recline/__init__.py
@@ -0,0 +1,16 @@
+"""
+Original © NetApp 2024
+"""
+
+import os
+import sys
+
+PROGRAM_NAME = None
+PROMPT = '> '
+NEXT_JOB_PID = 1
+JOBS = {}
+
+from recline.commands.cli_command import command  # pylint: disable=wrong-import-position
+from recline.repl.shell import relax  # pylint: disable=wrong-import-position
+
+__all__ = ['PROGRAM_NAME', 'PROMPT', 'command', 'relax']
---
+++ packages/recline/recline-2025.12.tar.gz/recline-2025.12/tests/conftest.py
@@ -0,0 +1,18 @@
+"""
+Original © NetApp 2024
+
+pytest configuration for recline unit tests
+"""
+
+import pytest
+
+import recline
+
+
+@pytest.fixture(scope='function')
+def clean_jobs():
+    """Make sure the globals are fresh in each test"""
+
+    recline.JOBS = {}
+    recline.NEXT_JOB_PID = 1
+    recline.commands.COMMAND_REGISTRY = {}
