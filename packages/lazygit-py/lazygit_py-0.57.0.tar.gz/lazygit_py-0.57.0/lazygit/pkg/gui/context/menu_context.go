24b42ec561bfc8502932ab5e47ea21db46d88644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 07:06:07 +0000

    Add package lazygit-py/0.57.0/lazygit_py-0.57.0.tar.gz

---
+++ packages/lazygit-py/lazygit_py-0.57.0.tar.gz/lazygit_py-0.57.0/lazygit/pkg/gui/context/menu_context.go
@@ -0,0 +1,249 @@
+package context
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/jesseduffield/lazygit/pkg/gui/keybindings"
+	"github.com/jesseduffield/lazygit/pkg/gui/style"
+	"github.com/jesseduffield/lazygit/pkg/gui/types"
+	"github.com/jesseduffield/lazygit/pkg/i18n"
+	"github.com/jesseduffield/lazygit/pkg/utils"
+	"github.com/samber/lo"
+)
+
+type MenuContext struct {
+	c *ContextCommon
+
+	*MenuViewModel
+	*ListContextTrait
+}
+
+var _ types.IListContext = (*MenuContext)(nil)
+
+func NewMenuContext(
+	c *ContextCommon,
+) *MenuContext {
+	viewModel := NewMenuViewModel(c)
+
+	return &MenuContext{
+		c:             c,
+		MenuViewModel: viewModel,
+		ListContextTrait: &ListContextTrait{
+			Context: NewSimpleContext(NewBaseContext(NewBaseContextOpts{
+				View:                  c.Views().Menu,
+				WindowName:            "menu",
+				Key:                   "menu",
+				Kind:                  types.TEMPORARY_POPUP,
+				Focusable:             true,
+				HasUncontrolledBounds: true,
+			})),
+			ListRenderer: ListRenderer{
+				list:                viewModel,
+				getDisplayStrings:   viewModel.GetDisplayStrings,
+				getColumnAlignments: func() []utils.Alignment { return viewModel.columnAlignment },
+				getNonModelItems:    viewModel.GetNonModelItems,
+			},
+			c: c,
+		},
+	}
+}
+
+type MenuViewModel struct {
+	c                         *ContextCommon
+	menuItems                 []*types.MenuItem
+	prompt                    string
+	promptLines               []string
+	columnAlignment           []utils.Alignment
+	allowFilteringKeybindings bool
+	*FilteredListViewModel[*types.MenuItem]
+}
+
+func NewMenuViewModel(c *ContextCommon) *MenuViewModel {
+	self := &MenuViewModel{
+		menuItems: nil,
+		c:         c,
+	}
+
+	filterKeybindings := false
+
+	self.FilteredListViewModel = NewFilteredListViewModel(
+		func() []*types.MenuItem { return self.menuItems },
+		func(item *types.MenuItem) []string {
+			if filterKeybindings {
+				return []string{keybindings.LabelFromKey(item.Key)}
+			}
+
+			return item.LabelColumns
+		},
+	)
+
+	self.FilteredListViewModel.SetPreprocessFilterFunc(func(filter string) string {
+		if self.allowFilteringKeybindings && strings.HasPrefix(filter, "@") {
+			filterKeybindings = true
+			return filter[1:]
+		}
+
+		filterKeybindings = false
+		return filter
+	})
+
+	return self
+}
+
+func (self *MenuViewModel) SetMenuItems(items []*types.MenuItem, columnAlignment []utils.Alignment) {
+	self.menuItems = items
+	self.columnAlignment = columnAlignment
+}
+
+func (self *MenuViewModel) GetPrompt() string {
+	return self.prompt
+}
+
+func (self *MenuViewModel) SetPrompt(prompt string) {
+	self.prompt = prompt
+	self.promptLines = nil
+}
+
+func (self *MenuViewModel) GetPromptLines() []string {
+	return self.promptLines
+}
+
+func (self *MenuViewModel) SetPromptLines(promptLines []string) {
+	self.promptLines = promptLines
+}
+
+func (self *MenuViewModel) SetAllowFilteringKeybindings(allow bool) {
+	self.allowFilteringKeybindings = allow
+}
+
+// TODO: move into presentation package
+func (self *MenuViewModel) GetDisplayStrings(_ int, _ int) [][]string {
+	menuItems := self.FilteredListViewModel.GetItems()
+
+	return lo.Map(menuItems, func(item *types.MenuItem, _ int) []string {
+		displayStrings := item.LabelColumns
+		if item.DisabledReason != nil {
+			displayStrings[0] = style.FgDefault.SetStrikethrough().Sprint(displayStrings[0])
+		}
+
+		keyLabel := ""
+		if item.Key != nil {
+			keyLabel = style.FgCyan.Sprint(keybindings.LabelFromKey(item.Key))
+		}
+
+		checkMark := ""
+		switch item.Widget {
+		case types.MenuWidgetNone:
+			// do nothing
+		case types.MenuWidgetRadioButtonSelected:
+			checkMark = "(•)"
+		case types.MenuWidgetRadioButtonUnselected:
+			checkMark = "( )"
+		case types.MenuWidgetCheckboxSelected:
+			checkMark = "[✓]"
+		case types.MenuWidgetCheckboxUnselected:
+			checkMark = "[ ]"
+		}
+
+		displayStrings = utils.Prepend(displayStrings, keyLabel, checkMark)
+		return displayStrings
+	})
+}
+
+func (self *MenuViewModel) GetNonModelItems() []*NonModelItem {
+	result := []*NonModelItem{}
+	result = append(result, lo.Map(self.promptLines, func(line string, _ int) *NonModelItem {
+		return &NonModelItem{
+			Index:   0,
+			Column:  0,
+			Content: line,
+		}
+	})...)
+
+	// Don't display section headers when we are filtering, and the filter mode
+	// is fuzzy. The reason is that filtering changes the order of the items
+	// (they are sorted by best match), so all the sections would be messed up.
+	if self.FilteredListViewModel.IsFiltering() && self.c.UserConfig().Gui.UseFuzzySearch() {
+		return result
+	}
+
+	menuItems := self.FilteredListViewModel.GetItems()
+	var prevSection *types.MenuSection
+	for i, menuItem := range menuItems {
+		if menuItem.Section != nil && menuItem.Section != prevSection {
+			if prevSection != nil {
+				result = append(result, &NonModelItem{
+					Index:   i,
+					Column:  1,
+					Content: "",
+				})
+			}
+
+			result = append(result, &NonModelItem{
+				Index:   i,
+				Column:  1,
+				Content: style.FgGreen.SetBold().Sprintf("--- %s ---", menuItem.Section.Title),
+			})
+			prevSection = menuItem.Section
+		}
+	}
+
+	return result
+}
+
+func (self *MenuContext) GetKeybindings(opts types.KeybindingsOpts) []*types.Binding {
+	basicBindings := self.ListContextTrait.GetKeybindings(opts)
+	menuItemsWithKeys := lo.Filter(self.menuItems, func(item *types.MenuItem, _ int) bool {
+		return item.Key != nil
+	})
+
+	menuItemBindings := lo.Map(menuItemsWithKeys, func(item *types.MenuItem, _ int) *types.Binding {
+		return &types.Binding{
+			Key:     item.Key,
+			Handler: func() error { return self.OnMenuPress(item) },
+		}
+	})
+
+	// appending because that means the menu item bindings have lower precedence.
+	// So if a basic binding is to escape from the menu, we want that to still be
+	// what happens when you press escape. This matters when we're showing the menu
+	// for all keybindings of say the files context.
+	return append(basicBindings, menuItemBindings...)
+}
+
+func (self *MenuContext) OnMenuPress(selectedItem *types.MenuItem) error {
+	if selectedItem != nil && selectedItem.DisabledReason != nil {
+		if selectedItem.DisabledReason.ShowErrorInPanel {
+			return errors.New(selectedItem.DisabledReason.Text)
+		}
+
+		self.c.ErrorToast(self.c.Tr.DisabledMenuItemPrefix + selectedItem.DisabledReason.Text)
+		return nil
+	}
+
+	self.c.Context().Pop()
+
+	if selectedItem == nil {
+		return nil
+	}
+
+	if err := selectedItem.OnPress(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// There is currently no need to use range-select in a menu so we're disabling it.
+func (self *MenuContext) RangeSelectEnabled() bool {
+	return false
+}
+
+func (self *MenuContext) FilterPrefix(tr *i18n.TranslationSet) string {
+	if self.allowFilteringKeybindings {
+		return tr.FilterPrefixMenu
+	}
+
+	return self.FilteredListViewModel.FilterPrefix(tr)
+}
---
+++ packages/lazygit-py/lazygit_py-0.57.0.tar.gz/lazygit_py-0.57.0/lazygit/pkg/gui/menu_panel.go
@@ -0,0 +1,73 @@
+package gui
+
+import (
+	"fmt"
+
+	"github.com/jesseduffield/lazygit/pkg/gui/keybindings"
+	"github.com/jesseduffield/lazygit/pkg/gui/types"
+	"github.com/jesseduffield/lazygit/pkg/theme"
+)
+
+// note: items option is mutated by this function
+func (gui *Gui) createMenu(opts types.CreateMenuOptions) error {
+	if !opts.HideCancel {
+		// this is mutative but I'm okay with that for now
+		opts.Items = append(opts.Items, &types.MenuItem{
+			LabelColumns: []string{gui.c.Tr.Cancel},
+			OnPress: func() error {
+				return nil
+			},
+		})
+	}
+
+	maxColumnSize := 1
+	confirmKey := keybindings.GetKey(gui.c.UserConfig().Keybinding.Universal.ConfirmMenu)
+
+	for _, item := range opts.Items {
+		if item.LabelColumns == nil {
+			item.LabelColumns = []string{item.Label}
+		}
+
+		if item.OpensMenu {
+			item.LabelColumns[0] = fmt.Sprintf("%s...", item.LabelColumns[0])
+		}
+
+		maxColumnSize = max(maxColumnSize, len(item.LabelColumns))
+
+		// Remove all item keybindings that are the same as the confirm binding
+		if item.Key == confirmKey && !opts.KeepConfirmKeybindings {
+			item.Key = nil
+		}
+	}
+
+	for _, item := range opts.Items {
+		if len(item.LabelColumns) < maxColumnSize {
+			// we require that each item has the same number of columns so we're padding out with blank strings
+			// if this item has too few
+			item.LabelColumns = append(item.LabelColumns, make([]string, maxColumnSize-len(item.LabelColumns))...)
+		}
+	}
+
+	gui.State.Contexts.Menu.SetMenuItems(opts.Items, opts.ColumnAlignment)
+	gui.State.Contexts.Menu.SetPrompt(opts.Prompt)
+	gui.State.Contexts.Menu.SetAllowFilteringKeybindings(opts.AllowFilteringKeybindings)
+	gui.State.Contexts.Menu.SetSelection(0)
+
+	gui.Views.Menu.Title = opts.Title
+	gui.Views.Menu.FgColor = theme.GocuiDefaultTextColor
+
+	gui.Views.Tooltip.Wrap = true
+	gui.Views.Tooltip.FgColor = theme.GocuiDefaultTextColor
+	gui.Views.Tooltip.Visible = true
+
+	// resetting keybindings so that the menu-specific keybindings are registered
+	if err := gui.resetKeybindings(); err != nil {
+		return err
+	}
+
+	gui.c.PostRefreshUpdate(gui.State.Contexts.Menu)
+
+	// TODO: ensure that if we're opened a menu from within a menu that it renders correctly
+	gui.c.Context().Push(gui.State.Contexts.Menu, types.OnFocusOpts{})
+	return nil
+}
