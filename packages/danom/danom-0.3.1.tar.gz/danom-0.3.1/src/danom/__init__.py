0649d9c8c7ff7ad12b349532c58c90fb4e07d439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:29 +0000

    Add package danom/0.3.1/danom-0.3.1.tar.gz

---
+++ packages/danom/danom-0.3.1.tar.gz/danom-0.3.1/src/danom/__init__.py
@@ -0,0 +1,7 @@
+from danom._err import Err
+from danom._ok import Ok
+from danom._result import Result
+from danom._safe import safe, safe_method
+from danom._stream import ParStream, Stream, compose
+
+__all__ = ["Err", "Ok", "ParStream", "Result", "Stream", "compose", "safe", "safe_method"]
