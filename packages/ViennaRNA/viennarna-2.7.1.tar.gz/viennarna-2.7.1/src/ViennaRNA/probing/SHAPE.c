5c6ad0edac411d2e239003f4e444f3040932e0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 02:37:14 +0000

    Add package ViennaRNA/2.7.1/viennarna-2.7.1.tar.gz

---
+++ packages/ViennaRNA/viennarna-2.7.1.tar.gz/viennarna-2.7.1/src/ViennaRNA/probing/SHAPE.c
@@ -0,0 +1,249 @@
+/* SHAPE reactivity data handling */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+#include <limits.h>
+
+#include "ViennaRNA/utils/basic.h"
+#include "ViennaRNA/io/utils.h"
+#include "ViennaRNA/io/file_formats.h"
+#include "ViennaRNA/probing/basic.h"
+#include "ViennaRNA/probing/strategy_deigan.h"
+#include "ViennaRNA/probing/strategy_zarringhalam.h"
+#include "ViennaRNA/probing/strategy_eddy.h"
+
+#include "ViennaRNA/probing/SHAPE.h"
+
+
+/*
+ #################################
+ # GLOBAL VARIABLES              #
+ #################################
+ */
+
+/*
+ #################################
+ # PRIVATE VARIABLES             #
+ #################################
+ */
+
+/*
+ #################################
+ # PRIVATE FUNCTION DECLARATIONS #
+ #################################
+ */
+
+PRIVATE double **
+load_n_distribute(unsigned int  n_seq,
+                  unsigned int  *ns,
+                  const char    **sequences,
+                  const char    **file_names,
+                  const int     *file_name_association,
+                  unsigned int  options);
+
+
+/*
+ #################################
+ # BEGIN OF FUNCTION DEFINITIONS #
+ #################################
+ */
+PUBLIC int
+vrna_sc_add_SHAPE_zarringhalam(vrna_fold_compound_t *vc,
+                               const double         *reactivities,
+                               double               b,
+                               double               default_value,
+                               const char           *shape_conversion,
+                               unsigned int         options)
+{
+  int ret;
+
+  ret = 0; /* error */
+
+  if ((vc) &&
+      (reactivities && (vc->type == VRNA_FC_TYPE_SINGLE))) {
+    vrna_probing_data_t d = vrna_probing_data_zarringhalam(reactivities,
+                                                           vc->length,
+                                                           b,
+                                                           shape_conversion,
+                                                           default_value);
+    ret = vrna_sc_probing(vc, d);
+    vrna_probing_data_free(d);
+  }
+
+  return ret;
+}
+
+
+PUBLIC int
+vrna_sc_add_SHAPE_deigan(vrna_fold_compound_t *vc,
+                         const double         *reactivities,
+                         double               m,
+                         double               b,
+                         unsigned int         options)
+{
+  int                 ret = 0;
+  vrna_probing_data_t d;
+
+  if ((vc) &&
+      (reactivities)) {
+    switch (vc->type) {
+      case VRNA_FC_TYPE_SINGLE:
+        d   = vrna_probing_data_deigan(reactivities, vc->length, m, b);
+        ret = vrna_sc_probing(vc, d);
+        vrna_probing_data_free(d);
+        break;
+
+      case VRNA_FC_TYPE_COMPARATIVE:
+        vrna_log_warning("vrna_sc_add_SHAPE_deigan() not implemented for comparative prediction! "
+                         "Use vrna_sc_add_SHAPE_deigan_ali() instead!");
+        break;
+    }
+  }
+
+  return ret;
+}
+
+
+PUBLIC int
+vrna_sc_add_SHAPE_deigan_ali(vrna_fold_compound_t *vc,
+                             const char           **shape_files,
+                             const int            *shape_file_association,
+                             double               m,
+                             double               b,
+                             unsigned int         options)
+{
+  int           ret;
+  unsigned int  s;
+  double        **r;
+
+  ret = 0;
+
+  if ((vc) &&
+      (vc->type == VRNA_FC_TYPE_COMPARATIVE)) {
+    r = load_n_distribute(vc->n_seq,
+                          vc->alignment->gapfree_size,
+                          (const char **)vc->alignment->gapfree_seq,
+                          shape_files,
+                          shape_file_association,
+                          VRNA_PROBING_DATA_CHECK_SEQUENCE);
+
+    vrna_probing_data_t d = vrna_probing_data_deigan_comparative((const double **)r,
+                                                                 vc->alignment->gapfree_size,
+                                                                 vc->n_seq,
+                                                                 &m,
+                                                                 &b,
+                                                                 VRNA_PROBING_METHOD_MULTI_PARAMS_0);
+    ret = vrna_sc_probing(vc, d);
+    vrna_probing_data_free(d);
+
+    for (s = 0; s < vc->n_seq; s++)
+      free(r[s]);
+
+    free(r);
+  }
+
+  return ret;
+}
+
+
+PUBLIC int
+vrna_sc_add_SHAPE_eddy_2(vrna_fold_compound_t *fc,
+                         const double         *reactivities,
+                         int                  unpaired_nb,
+                         const double         *unpaired_data,
+                         int                  paired_nb,
+                         const double         *paired_data)
+{
+  int ret;
+
+  ret = 0; /* error */
+
+  if ((fc) &&
+      (reactivities) &&
+      (fc->type == VRNA_FC_TYPE_SINGLE)) {
+    vrna_probing_data_t d = vrna_probing_data_eddy(reactivities,
+                                                   fc->length,
+                                                   fc->params->temperature,
+                                                   VRNA_PROBING_STRATEGY_EDDY_OPTIONS_DEFAULT,
+                                                   unpaired_data,
+                                                   unpaired_nb,
+                                                   paired_data,
+                                                   paired_nb);
+    ret = vrna_sc_probing(fc, d);
+    vrna_probing_data_free(d);
+  }
+
+  return ret;
+}
+
+
+PRIVATE double **
+load_n_distribute(unsigned int  n_seq,
+                  unsigned int  *ns,
+                  const char    **sequences,
+                  const char    **file_names,
+                  const int     *file_name_association,
+                  unsigned int  options)
+{
+  char          *sequence;
+  unsigned int  s, ss;
+  double        *values, **r;
+
+  r = NULL;
+
+  if ((ns) &&
+      (file_names) &&
+      (file_name_association)) {
+    r = (double **)vrna_alloc(sizeof(double *) * n_seq);
+
+    for (s = 0; file_name_association[s] >= 0; s++) {
+      ss = file_name_association[s]; /* actual sequence number in alignment */
+
+      if (ss >= n_seq) {
+        vrna_log_warning(
+          "Failed to associate probing data file \"%s\" with sequence %d in alignment! "
+          "Omitting data since alignment has only %d sequences!",
+          file_names[s],
+          ss,
+          n_seq);
+        continue;
+      }
+
+      sequence  = vrna_alloc(sizeof(char) * (ns[ss] + 1));
+      values    = vrna_alloc(sizeof(double) * (ns[ss] + 1));
+
+      if (vrna_file_SHAPE_read(file_names[s], ns[ss], -1, sequence, values)) {
+        r[ss] = values;
+
+        if ((sequence) &&
+            (sequences) &&
+            (options & VRNA_PROBING_DATA_CHECK_SEQUENCE)) {
+          /* double check information by comparing the sequence read from */
+          if (strcmp(sequence, sequences[ss]))
+            vrna_log_warning(
+              "Input sequence %d differs from sequence provided via probing data file!\n%s\n%s",
+              file_name_association[s] + 1,
+              sequences[ss],
+              sequence);
+        }
+      } else {
+        vrna_log_warning("Failed to open probing data file \"%d\"! "
+                         "No data will be used for sequence %d.",
+                         s,
+                         ss + 1);
+      }
+
+      free(sequence);
+    }
+  }
+
+  return r;
+}
---
+++ packages/ViennaRNA/viennarna-2.7.1.tar.gz/viennarna-2.7.1/src/ViennaRNA/probing/SHAPE.h
@@ -0,0 +1,151 @@
+#ifndef VIENNA_RNA_PACKAGE_PROBING_SHAPE_H
+#define VIENNA_RNA_PACKAGE_PROBING_SHAPE_H
+
+#include <ViennaRNA/fold_compound.h>
+#include <ViennaRNA/probing/basic.h>
+
+/**
+ *  @file     ViennaRNA/probing/SHAPE.h
+ *  @ingroup SHAPE_reactivities
+ *  @brief  This module provides function to incorporate RNA structure
+ *          probing data, e.g. SHAPE reactivities, into the folding recursions
+ *          by means of soft constraints
+ */
+
+
+/**
+ *  @addtogroup SHAPE_reactivities
+ *  @{
+ */
+
+
+#define VRNA_PROBING_DATA_CHECK_SEQUENCE                          1U
+
+/**
+ *  @brief  Add SHAPE reactivity data as soft constraints (Deigan et al. method)
+ *
+ *  This approach of SHAPE directed RNA folding uses the simple linear ansatz
+ *
+ *  @f[ \Delta G_{\text{SHAPE}}(i) = m \ln(\text{SHAPE reactivity}(i)+1)+ b @f]
+ *
+ *  to convert SHAPE reactivity values to pseudo energies whenever a
+ *  nucleotide @f$ i @f$ contributes to a stacked pair. A positive slope @f$ m @f$
+ *  penalizes high reactivities in paired regions, while a negative intercept @f$ b @f$
+ *  results in a confirmatory *bonus* free energy for correctly predicted base pairs.
+ *  Since the energy evaluation of a base pair stack involves two pairs, the pseudo
+ *  energies are added for all four contributing nucleotides. Consequently, the
+ *  energy term is applied twice for pairs inside a helix and only once for pairs
+ *  adjacent to other structures. For all other loop types the energy model remains
+ *  unchanged even when the experimental data highly disagrees with a certain motif.
+ *
+ *  @note For further details, we refer to @rstinline :cite:t:`deigan:2009` @endrst.
+ *
+ *  @see  vrna_sc_remove(), vrna_sc_add_SHAPE_zarringhalam(), vrna_sc_minimize_pertubation()
+ *
+ *  @param  fc            The #vrna_fold_compound_t the soft constraints are associated with
+ *  @param  reactivities  A vector of normalized SHAPE reactivities
+ *  @param  m             The slope of the conversion function
+ *  @param  b             The intercept of the conversion function
+ *  @param  options       The options flag indicating how/where to store the soft constraints
+ *  @return               1 on successful extraction of the method, 0 on errors
+ */
+int
+vrna_sc_add_SHAPE_deigan(vrna_fold_compound_t *fc,
+                         const double         *reactivities,
+                         double               m,
+                         double               b,
+                         unsigned int         options);
+
+
+/**
+ *  @brief  Add SHAPE reactivity data from files as soft constraints for consensus structure prediction (Deigan et al. method)
+ *
+ *  @param  fc            The #vrna_fold_compound_t the soft constraints are associated with
+ *  @param  shape_files   A set of filenames that contain normalized SHAPE reactivity data
+ *  @param  shape_file_association  An array of integers that associate the files with sequences in the alignment
+ *  @param  m             The slope of the conversion function
+ *  @param  b             The intercept of the conversion function
+ *  @param  options       The options flag indicating how/where to store the soft constraints
+ *  @return               1 on successful extraction of the method, 0 on errors
+ */
+int
+vrna_sc_add_SHAPE_deigan_ali(vrna_fold_compound_t *fc,
+                             const char           **shape_files,
+                             const int            *shape_file_association,
+                             double               m,
+                             double               b,
+                             unsigned int         options);
+
+
+/**
+ *  @brief  Add SHAPE reactivity data as soft constraints (Zarringhalam et al. method)
+ *
+ *  This method first converts the observed SHAPE reactivity of nucleotide @f$ i @f$ into a
+ *  probability @f$ q_i @f$ that position @f$ i @f$ is unpaired by means of a non-linear map.
+ *  Then pseudo-energies of the form
+ *
+ *  @f[ \Delta G_{\text{SHAPE}}(x,i) = \beta\ |x_i - q_i| @f]
+ *
+ *  are computed, where @f$ x_i=0 @f$ if position @f$ i @f$ is unpaired and @f$ x_i=1 @f$
+ *  if @f$ i @f$ is paired in a given secondary structure. The parameter @f$ \beta @f$ serves as
+ *  scaling factor. The magnitude of discrepancy between prediction and experimental observation
+ *  is represented by @f$ |x_i - q_i| @f$.
+ *
+ *  @note For further details, we refer to @rstinline :cite:t:`zarringhalam:2012` @endrst
+ *
+ *  @see  vrna_sc_remove(), vrna_sc_add_SHAPE_deigan(), vrna_sc_minimize_pertubation()
+ *
+ *  @param  fc                The #vrna_fold_compound_t the soft constraints are associated with
+ *  @param  reactivities      A vector of normalized SHAPE reactivities
+ *  @param  b                 The scaling factor @f$ \beta @f$ of the conversion function
+ *  @param  default_value     The default value for a nucleotide where reactivity data is missing for
+ *  @param  shape_conversion  A flag that specifies how to convert reactivities to probabilities
+ *  @param  options           The options flag indicating how/where to store the soft constraints
+ *  @return                   1 on successful extraction of the method, 0 on errors
+ */
+int
+vrna_sc_add_SHAPE_zarringhalam(vrna_fold_compound_t *fc,
+                               const double         *reactivities,
+                               double               b,
+                               double               default_value,
+                               const char           *shape_conversion,
+                               unsigned int         options);
+
+
+/**
+ *  @brief  Add SHAPE reactivity data as soft constraints (Eddy/RNAprob-2 method)
+ *
+ *  This approach of SHAPE directed RNA folding uses the probability framework proposed by Eddy
+ *
+ *  @f[ \Delta G_{\text{SHAPE}}(i) = - RT\ln(\mathbb{P}(\text{SHAPE reactivity}(i)\mid x_i\pi_i))+ @f]
+ *
+ *  to convert SHAPE reactivity values to pseudo energies for given nucleotide @f$ x_i @f$ and
+ *  pairedness @f$ \pi_i @f$ at position @f$ i @f$. The reactivity distribution is computed
+ *  using Gaussian kernel density estimation (KDE) with bandwidth @f$ h @f$ computed using
+ *  Scott factor
+ *
+ * @f[ h = n^{-\frac{1}{5}} @f]
+ *
+ * where @f$ n @f$ is the number of data points.
+ *
+ *  @param  fc            The #vrna_fold_compound_t the soft constraints are associated with
+ *  @param  reactivities  A vector of normalized SHAPE reactivities
+ *  @param  unpaired_nb   Length of the array of unpaired SHAPE reactivities
+ *  @param  unpaired_data Pointer to an array of unpaired SHAPE reactivities
+ *  @param  paired_nb     Length of the array of paired SHAPE reactivities
+ *  @param  paired_data   Pointer to an array of paired SHAPE reactivities
+ *  @return               1 on successful extraction of the method, 0 on errors
+ */
+int
+vrna_sc_add_SHAPE_eddy_2(vrna_fold_compound_t *fc,
+                         const double         *reactivities,
+                         int                  unpaired_nb,
+                         const double         *unpaired_data,
+                         int                  paired_nb,
+                         const double         *paired_data);
+
+
+/**
+ *  @}
+ */
+#endif
