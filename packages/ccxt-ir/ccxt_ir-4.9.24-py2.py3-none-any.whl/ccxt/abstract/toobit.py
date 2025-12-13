3c9a3f40f411ec45bbda4b16b175f4722c05a992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:06:39 +0000

    Add package ccxt-ir/4.9.24/ccxt_ir-4.9.24-py2.py3-none-any.whl

---
+++ packages/ccxt-ir/ccxt_ir-4.9.24-py2.py3-none-any.whl/ccxt/abstract/toobit.py
@@ -0,0 +1,7 @@
+from ccxt.base.types import Entry
+
+
+class ImplicitAPI:
+    public_get_quote_v1_ticker_24hr = publicGetQuoteV1Ticker24hr = Entry('quote/v1/ticker/24hr', 'public', 'GET', {'cost': 1})
+    public_get_quote_v1_klines = publicGetQuoteV1Klines = Entry('quote/v1/klines', 'public', 'GET', {'cost': 1})
+    public_get_api_v1_exchangeinfo = publicGetApiV1ExchangeInfo = Entry('/api/v1/exchangeInfo', 'public', 'GET', {'cost': 1})
