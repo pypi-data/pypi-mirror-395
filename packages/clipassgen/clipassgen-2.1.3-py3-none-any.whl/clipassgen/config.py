716b6c407247820c4a885723580305120d9b28c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 03:27:36 +0000

    Add package clipassgen/2.1.3/clipassgen-2.1.3-py3-none-any.whl

---
+++ packages/clipassgen/clipassgen-2.1.3-py3-none-any.whl/clipassgen/config.py
@@ -0,0 +1,8 @@
+# Copyright © 2025, Alexander Suvorov
+
+
+class Config:
+    name = 'Console Smart Password Generator'
+    url = 'https://github.com/smartlegionlab/clipassgen/'
+    copyright_ = 'Copyright © 2025, Alexander Suvorov. All rights reserved.'
+    version = "v2.1.3"
