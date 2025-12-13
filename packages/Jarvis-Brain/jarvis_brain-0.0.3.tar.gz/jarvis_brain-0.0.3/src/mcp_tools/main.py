bf087db3e89d46982650d15b10c276a40dabe82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 07:06:54 +0000

    Add package Jarvis-Brain/0.0.3/jarvis_brain-0.0.3.tar.gz

---
+++ packages/Jarvis-Brain/jarvis_brain-0.0.3.tar.gz/jarvis_brain-0.0.3/src/mcp_tools/main.py
@@ -0,0 +1,19 @@
+# main.py
+import os
+from src.mcp_tools.dp_tools import *
+from fastmcp import FastMCP
+
+mcp = FastMCP("Jarvis Brain DrissionPage mcp")
+
+# 根据环境变量加载模块
+enabled_modules = os.getenv("MCP_MODULES", "DrissionPage").split(",")
+
+if "DrissionPage" in enabled_modules:
+    register_visit_url(mcp)
+
+
+# if "api" in enabled_modules:
+#     register_api_tools(mcp)
+
+def main():
+    mcp.run()
