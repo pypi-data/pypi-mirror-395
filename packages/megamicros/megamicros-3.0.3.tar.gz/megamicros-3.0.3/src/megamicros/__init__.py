3a262b36886f129f41de7921ddadeb1a824c5faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:07:10 +0000

    Add package megamicros/3.0.3/megamicros-3.0.3.tar.gz

---
+++ packages/megamicros/megamicros-3.0.3.tar.gz/megamicros-3.0.3/src/megamicros/__init__.py
@@ -0,0 +1,7 @@
+__version__="3.0.2"
+welcome_msg = '-'*20 + '\n' + 'Megamicros python library\n \
+Copyright (C) 2024-2025 Bimea\n \
+This program comes with ABSOLUTELY NO WARRANTY; for details see the source code\'.\n \
+This is free software, and you are welcome to redistribute it\n \
+under certain conditions; see the source code for details.\n' + '-'*20 + '\n' + '\
+MegaMicros documentation is available on https://readthedoc.bimea.io.\n' + '-'*20
