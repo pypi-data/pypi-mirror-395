2518dfb1ec9faf0195c4f0665d9ffc051fdf509e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 01:33:42 +0000

    Add package in-layers-core/0.1.2/in_layers_core-0.1.2.tar.gz

---
+++ packages/in-layers-core/in_layers_core-0.1.2.tar.gz/in_layers_core-0.1.2/src/in_layers/core/entries.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import os
+from collections.abc import Mapping
+from typing import Any
+
+from box import Box
+
+from .globals import features as globals_features
+from .globals import name as globals_name
+from .globals import services as globals_services
+from .layers import features as layers_features
+from .layers import name as layers_name
+from .layers import services as layers_services
+
+
+async def load_system(args: Mapping[str, Any]):
+    environment: str = args["environment"]
+    config: Mapping[str, Any] | None = args.get("config")
+
+    global_services = globals_services.create(
+        {
+            "environment": environment,
+            "working_directory": os.getcwd(),
+        }
+    )
+    global_features = globals_features.create(
+        Box(
+            {
+                "services": {
+                    globals_name: global_services,
+                }
+            }
+        )
+    )
+    globals_context = await global_features.load_globals(config or environment)
+
+    # layers
+
+    the_layers_services = layers_services.create()
+    the_layers_features = layers_features.create(
+        Box(
+            {
+                **globals_context,
+                "services": {
+                    layers_name: the_layers_services,
+                },
+            }
+        )
+    )
+    layers_loaded = await the_layers_features.load_layers()
+    try:
+        if "services" in layers_loaded and layers_name in layers_loaded.services:
+            del layers_loaded.services[layers_name]
+    except Exception:  # noqa: S110
+        pass
+    return layers_loaded
