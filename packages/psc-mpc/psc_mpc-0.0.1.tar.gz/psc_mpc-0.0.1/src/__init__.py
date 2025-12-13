0c46eb77186d108406390c592888062b499f9bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 07:06:01 +0000

    Add package psc-mpc/0.0.1/psc_mpc-0.0.1.tar.gz

---
+++ packages/psc-mpc/psc_mpc-0.0.1.tar.gz/psc_mpc-0.0.1/src/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+普通话水平测试(PSC)报名查询工具
+
+提供查询普通话水平测试报名信息的功能，包括:
+- 查询支持报名的省份列表
+- 查询各省当前开放报名的测试站点
+- 查询各省即将开放报名的测试站点
+"""
+
+__version__ = "0.0.1"
+
+from .psc import mcp, get_provinces, get_open_stations, get_next_stations
+
+__all__ = [
+    "mcp",
+    "get_provinces",
+    "get_open_stations",
+    "get_next_stations",
+    "__version__"
+]
