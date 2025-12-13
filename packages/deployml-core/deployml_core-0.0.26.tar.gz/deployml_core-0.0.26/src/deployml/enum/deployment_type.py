5a6e61046259fa9e5afb451525e37af8a0ad470e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 01:33:24 +0000

    Add package deployml-core/0.0.26/deployml_core-0.0.26.tar.gz

---
+++ packages/deployml-core/deployml_core-0.0.26.tar.gz/deployml_core-0.0.26/src/deployml/enum/deployment_type.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class DeploymentType(Enum):
+    """
+    Supported deployment types for MLOps infrastructure
+    """
+
+    DOCKER_COMPOSE = "docker_compose"
+    DOCKER = "docker"
+    CLOUD_RUN = "cloud_run"
+    CLOUD_VM = "cloud_vm"
