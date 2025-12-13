65fc95deb65eb202904ce900e2f627c2854402e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:25 +0000

    Add package cpdts/3.0.6/cpdts-3.0.6-py3-none-any.whl

---
+++ packages/cpdts/cpdts-3.0.6-py3-none-any.whl/cpdts/fire_project/__init__.py
@@ -0,0 +1,12 @@
+"""Fire projects package for cpats.
+
+This package contains functionality for creating and managing fire projects.
+"""
+
+from .fire_create import fire_create 
+
+__all__ = [
+    "fire_create",
+]
+
+
