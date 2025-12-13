d05158d75210482b9a30082ff66e7dd6ef462bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:09 +0000

    Add package circle-of-confusion/0.2.5/circle_of_confusion-0.2.5.tar.gz

---
+++ packages/circle-of-confusion/circle_of_confusion-0.2.5.tar.gz/circle_of_confusion-0.2.5/build.rs
@@ -0,0 +1,18 @@
+use std::io::Result;
+
+fn main() -> Result<()> {
+    #[cfg(feature = "compile-protobuf-src")]
+    std::env::set_var("PROTOC", protobuf_src::protoc());
+
+    let mut config = prost_build::Config::new();
+
+    #[cfg(feature = "documented")]
+    config.type_attribute(".", "#[derive(documented::DocumentedFields)]");
+
+    #[cfg(feature = "serde")]
+    config.type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]");
+
+    config.compile_protos(&["circle_of_confusion.proto"], &["proto/"])?;
+
+    Ok(())
+}
