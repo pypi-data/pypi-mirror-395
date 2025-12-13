947c9d4819749091b890678cd65361a398112965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 05:07:00 +0000

    Add package dataflow-core/2.1.26/dataflow_core-2.1.26-py3-none-any.whl

---
+++ packages/dataflow-core/dataflow_core-2.1.26-py3-none-any.whl/dataflow/models/dataflow_setting.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, Boolean
+from dataflow.db import Base
+
+
+class DataflowSetting(Base):
+    """
+    Table DATAFLOW_SETTING
+    Stores application-level configurations such as auto onboarding.
+    """
+
+    __tablename__ = "DATAFLOW_SETTING"
+
+    id = Column(Integer, primary_key=True, index=True, unique=True, nullable=False, autoincrement=True)
+    allow_auto_org_onboarding = Column(Boolean, nullable=False, default=True, server_default="true")
