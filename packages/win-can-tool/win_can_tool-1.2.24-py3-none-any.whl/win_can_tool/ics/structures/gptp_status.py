34c452036a89b2ea0371b5b44f52f8c77d0631f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:07:01 +0000

    Add package win-can-tool/1.2.24/win_can_tool-1.2.24-py3-none-any.whl

---
+++ packages/win-can-tool/win_can_tool-1.2.24-py3-none-any.whl/win_can_tool/ics/structures/gptp_status.py
@@ -0,0 +1,31 @@
+# This file was auto generated; Do not modify, if you value your sanity!
+import ctypes
+import enum
+
+from ics.structures.current_ds_ import *
+from ics.structures.parent_ds_ import *
+from ics.structures.priority_vector import *
+from ics.structures.timestamp_ import *
+
+
+class gptp_status(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [
+        ('current_time', timestamp_),
+        ('gm_priority', priority_vector),
+        ('ms_offset_ns', ctypes.c_int64),
+        ('is_sync', ctypes.c_uint8),
+        ('link_status', ctypes.c_uint8),
+        ('link_delay_ns', ctypes.c_int64),
+        ('selected_role', ctypes.c_uint8),
+        ('as_capable', ctypes.c_uint8),
+        ('is_syntonized', ctypes.c_uint8),
+        ('last_rx_sync_ts', timestamp_),
+        ('current_ds', current_ds_),
+        ('parent_ds', parent_ds_),
+    ]
+
+
+_GPTPStatus = gptp_status
+GPTPStatus = gptp_status
+
