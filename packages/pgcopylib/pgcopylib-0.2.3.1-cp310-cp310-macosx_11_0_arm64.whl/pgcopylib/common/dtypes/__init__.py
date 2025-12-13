b402779ed17665bd8d4a6350226010447942725e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 07:06:57 +0000

    Add package pgcopylib/0.2.3.1/pgcopylib-0.2.3.1-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/pgcopylib/pgcopylib-0.2.3.1-cp310-cp310-macosx_11_0_arm64.whl/pgcopylib/common/dtypes/__init__.py
@@ -0,0 +1,11 @@
+"""Functions and objects for read and write PostgreSQL Data Types."""
+
+from .dtype import (
+    PGTypeFunc,
+    PostgreSQLDtype,
+)
+
+__all__ = (
+    "PGTypeFunc",
+    "PostgreSQLDtype",
+)
