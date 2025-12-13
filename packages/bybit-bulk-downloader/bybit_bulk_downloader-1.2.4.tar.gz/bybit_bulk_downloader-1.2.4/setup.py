abbe8610319c490079e06aa469ee805601ee8bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:07:00 +0000

    Add package bybit-bulk-downloader/1.2.4/bybit_bulk_downloader-1.2.4.tar.gz

---
+++ packages/bybit-bulk-downloader/bybit_bulk_downloader-1.2.4.tar.gz/bybit_bulk_downloader-1.2.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="bybit-bulk-downloader",
+    version="1.2.4",
+    description=" Python library to efficiently and concurrently download historical data files from Bybit. Supports all asset types (spot, USDT-M, COIN-M, options) and all data frequencies.",
+    install_requires=["requests", "rich", "pytest", "beautifulsoup4", "pybit"],
+    author="aoki-h-jp",
+    author_email="hirotaka.aoki@solopreneurs.tech",
+    license="MIT",
+    packages=["bybit_bulk_downloader"],
+)
