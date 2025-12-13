45e300afa0f6956cd914c64fb84b925f6865835e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:06:28 +0000

    Add package mymath-by-mamata/0.0.1/mymath_by_mamata-0.0.1.tar.gz

---
+++ packages/mymath-by-mamata/mymath_by_mamata-0.0.1.tar.gz/mymath_by_mamata-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mymath_by_mamata",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple math package",
+    author="Mamata",
+    author_email="mamatabogati783@gmail.com",
+)
