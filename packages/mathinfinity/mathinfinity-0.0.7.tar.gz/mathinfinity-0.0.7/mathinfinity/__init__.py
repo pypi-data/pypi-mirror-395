6414651ae6fc6b5170156466bb558fc4b9bb6ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 06:07:36 +0000

    Add package mathinfinity/0.0.7/mathinfinity-0.0.7.tar.gz

---
+++ packages/mathinfinity/mathinfinity-0.0.7.tar.gz/mathinfinity-0.0.7/mathinfinity/__init__.py
@@ -0,0 +1,12 @@
+from .arithmeticoperation import arithmeticoperation
+from .posnegzero import posnegzero
+from .printzerorange import printzerorange
+from .printzerorange import printzerorangearray
+from .printrange import printrange
+from .printrange import printrangearray
+__all__ = [
+    "arithmeticoperation",
+    "posnegzero",
+    "printzerorange", "printzerorangearray",
+    "printrange", "printrangearray",
+]
