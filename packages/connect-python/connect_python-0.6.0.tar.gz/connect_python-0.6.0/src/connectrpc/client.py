59efdff0e087db6a1922091268fc1fd76d467ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 07:06:15 +0000

    Add package connect-python/0.6.0/connect_python-0.6.0.tar.gz

---
+++ packages/connect-python/connect_python-0.6.0.tar.gz/connect_python-0.6.0/src/connectrpc/client.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+__all__ = ["ConnectClient", "ConnectClientSync", "ResponseMetadata"]
+
+
+from ._client_async import ConnectClient
+from ._client_shared import ResponseMetadata
+from ._client_sync import ConnectClientSync
