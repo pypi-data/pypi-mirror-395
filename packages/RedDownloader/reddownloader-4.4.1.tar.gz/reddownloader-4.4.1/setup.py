4dfd8d852b5a8044529a314a6d4ca3a684b1e09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:06:32 +0000

    Add package RedDownloader/4.4.1/reddownloader-4.4.1.tar.gz

---
+++ packages/RedDownloader/reddownloader-4.4.1.tar.gz/reddownloader-4.4.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RedDownloader",
+    version="4.4.1",
+    author="Arnav Bajaj",
+    author_email="arnavbajaj9@gmail.com",
+    description="A package to download Reddit hosted videos easily and efficiently.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JackhammerYT/RedVidDownloader",
+    project_urls={
+        "Bug Tracker": "https://github.com/JackhammerYT/RedDownloader/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=['requests' , 'tqdm==4.62.3' , 'moviepy', 'pytube'],
+    python_requires=">=3.6",
+)
