b7ffd3efeaefc70987d691c45f8d958540241919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 08:07:13 +0000

    Add package pyxxl/0.4.5/pyxxl-0.4.5-py3-none-any.whl

---
+++ packages/pyxxl/pyxxl-0.4.5-py3-none-any.whl/pyxxl/tests/test_context.py
@@ -0,0 +1,32 @@
+import pytest
+
+from pyxxl.ctx import g
+from pyxxl.enum import executorBlockStrategy
+from pyxxl.executor import Executor
+from pyxxl.schema import RunData
+
+
+@pytest.mark.asyncio
+async def test_runner_callback(executor: Executor):
+    @executor.handler.register
+    async def test_ctx():
+        logId = g.xxl_run_data.logId
+        assert logId == 1
+
+    @executor.handler.register
+    def test_ctx_sync():
+        logId = g.xxl_run_data.logId
+        assert logId == 1
+
+    for handler in ["test_ctx", "test_ctx_sync"]:
+        data = RunData.from_dict(
+            dict(
+                logId=1,
+                jobId=11,
+                executorHandler=handler,
+                executorBlockStrategy=executorBlockStrategy.SERIAL_EXECUTION.value,
+            )
+        )
+        await executor.run_job(data)
+        await executor.graceful_close()
+        assert executor.xxl_client.callback_result.get(1) == 200
