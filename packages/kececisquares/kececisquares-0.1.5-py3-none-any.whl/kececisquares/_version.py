f83573ded710ce188b3c65c2a4213f7c51f8af75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 02:35:13 +0000

    Add package kececisquares/0.1.5/kececisquares-0.1.5-py3-none-any.whl

---
+++ packages/kececisquares/kececisquares-0.1.5-py3-none-any.whl/kececisquares/_version.py
@@ -0,0 +1,9 @@
+# _version.py
+
+__version__ = "0.1.5"
+__license__ = "MIT"
+__description__ = "Keçeci Binomial Squares (Keçeci Binom Kareleri): The Keçeci Binomial Square is a series of binomial coefficients forming a square region within Khayyam (مثلث خیام), Pascal, Binomial Triangle, selected from a specified starting row with defined size and alignment."
+__author__ = "Mehmet Keçeci"
+__url__ = "https://github.com/WhiteSymmetry/kececisquares"
+__docs__ = "https://github.com/WhiteSymmetry/kececisquares"  # Opsiyonel: Dokümantasyon linki
+__dependencies__ = ["python>=3.10"]  # Diğer bağımlılıkları da ekleyebilirsiniz
