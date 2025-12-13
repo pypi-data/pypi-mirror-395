b2c551b6726b440be1bbcce6b4de1736d86996f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 08:06:16 +0000

    Add package sqlshell/0.3.5/sqlshell-0.3.5-py3-none-any.whl

---
+++ packages/sqlshell/sqlshell-0.3.5-py3-none-any.whl/sqlshell/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+Utility functions for SQLShell
+"""
+
+# Import profile_entropy for convenient access
+from sqlshell.utils.profile_entropy import profile, visualize_profile, EntropyProfiler
+
+# Empty init file to make the directory a package 
