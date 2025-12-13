6c0b3b74da9cf3ea563d2a0e607b60dd52a0f2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 08:06:24 +0000

    Add package xcube-cci/0.12/xcube_cci-0.12.tar.gz

---
+++ packages/xcube-cci/xcube_cci-0.12.tar.gz/xcube_cci-0.12/test/test_dataframeaccess.py
@@ -0,0 +1,25 @@
+import unittest
+
+import pandas as pd
+
+from xcube_cci.cciodp import CciOdp
+from xcube_cci.dataframeaccess import DataFrameAccessor
+
+GHG_DS_ID = "esacci.GHG.satellite-orbit-frequency.L2.CH4.SCIAMACHY.Envisat.IMAP.v7-2.r1"
+
+class DataFrameAccessTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        ccicdc = CciOdp(data_type='geodataframe')
+        self._dfa = DataFrameAccessor(ccicdc, GHG_DS_ID, {})
+
+    def test_get_geodataframe(self):
+        gdf = self._dfa.get_geodataframe()
+        self.assertIsNotNone(gdf)
+
+    def test_get_geodataframe_for_dataset(self):
+        gdf = self._dfa._get_features_from_cci_cdc(
+            GHG_DS_ID,
+            (pd.Timestamp('2003-01-08 00:00:00'), pd.Timestamp('2003-01-08 23:59:59'))
+        )
+        self.assertIsNotNone(gdf)
