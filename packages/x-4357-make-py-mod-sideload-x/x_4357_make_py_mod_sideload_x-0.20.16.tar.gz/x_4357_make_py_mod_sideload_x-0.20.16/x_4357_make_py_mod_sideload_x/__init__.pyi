5e806cd838c1b6e27b63c05e0ab99c6f81297036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 02:36:48 +0000

    Add package x-4357-make-py-mod-sideload-x/0.20.16/x_4357_make_py_mod_sideload_x-0.20.16.tar.gz

---
+++ packages/x-4357-make-py-mod-sideload-x/x_4357_make_py_mod_sideload_x-0.20.16.tar.gz/x_4357_make_py_mod_sideload_x-0.20.16/x_4357_make_py_mod_sideload_x/__init__.pyi
@@ -0,0 +1,4 @@
+# Type stubs for package x_4357_make_py_mod_sideload_x
+from typing import Any
+
+__all__: list[str]
---
+++ packages/x-4357-make-py-mod-sideload-x/x_4357_make_py_mod_sideload_x-0.20.16.tar.gz/x_4357_make_py_mod_sideload_x-0.20.16/x_4357_make_py_mod_sideload_x/x_cls_make_py_mod_sideload_x.pyi
@@ -0,0 +1,3 @@
+# Stub for x_cls_make_py_mod_sideload_x.py
+from typing import Any
+
