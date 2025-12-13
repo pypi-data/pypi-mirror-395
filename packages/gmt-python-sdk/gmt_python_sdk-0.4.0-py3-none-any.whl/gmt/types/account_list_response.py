fc732164a4964d96fe8608fb32b36ee070553b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 08:06:36 +0000

    Add package gmt-python-sdk/0.4.0/gmt_python_sdk-0.4.0-py3-none-any.whl

---
+++ packages/gmt-python-sdk/gmt_python_sdk-0.4.0-py3-none-any.whl/gmt/types/account_list_response.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .._models import BaseModel
+
+__all__ = ["AccountListResponse", "DisplayName", "Price"]
+
+
+class DisplayName(BaseModel):
+    en: str
+    """Name in English."""
+
+    ru: str
+    """Name in Russian."""
+
+
+class Price(BaseModel):
+    amount: str
+    """Monetary amount as a string with up to 2 decimal places."""
+
+    currency_code: str
+    """ISO 4217 currency code."""
+
+
+class AccountListResponse(BaseModel):
+    available: bool
+    """Indicates if account is available for purchase."""
+
+    country_code: str
+    """ISO 3166-1 alpha-2 country code (e.g., US, RU, GB)."""
+
+    display_name: DisplayName
+
+    price: Price
