a1d290a9ea0b69d1958ca3c967c424a01153acab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 08:07:53 +0000

    Add package genTik/1.0/gentik-1.0.tar.gz

---
+++ packages/genTik/gentik-1.0.tar.gz/gentik-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="genTik",
+    version="1.0",
+    packages=find_packages(),
+    description="TikTok Sign Generator",
+    author="Your Name"
+)
