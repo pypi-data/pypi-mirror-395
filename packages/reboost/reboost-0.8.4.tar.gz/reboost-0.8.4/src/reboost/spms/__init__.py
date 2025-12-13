f593ec086fef6f5a4832cd14fcf4ce48b42f91a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:06:42 +0000

    Add package reboost/0.8.4/reboost-0.8.4.tar.gz

---
+++ packages/reboost/reboost-0.8.4.tar.gz/reboost-0.8.4/src/reboost/spms/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from .pe import detected_photoelectrons, emitted_scintillation_photons, load_optmap, load_optmap_all
+
+__all__ = [
+    "detected_photoelectrons",
+    "emitted_scintillation_photons",
+    "load_optmap",
+    "load_optmap_all",
+]
---
+++ packages/reboost/reboost-0.8.4.tar.gz/reboost-0.8.4/tests/conftest.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import shutil
+import uuid
+from getpass import getuser
+from pathlib import Path
+from tempfile import gettempdir
+
+import numba
+import pytest
+from legendtestdata import LegendTestData
+
+_tmptestdir = Path(gettempdir()) / f"reboost-tests-{getuser()}-{uuid.uuid4()!s}"
+
+
+@pytest.fixture(scope="session")
+def tmptestdir_global():
+    _tmptestdir.mkdir(exist_ok=False)
+    return _tmptestdir
+
+
+@pytest.fixture(scope="session")
+def legendtestdata():
+    ldata = LegendTestData()
+    ldata.checkout("0a56ff3")
+    return ldata
+
+
+@pytest.fixture(scope="module")
+def tmptestdir(tmptestdir_global, request):
+    p = tmptestdir_global / request.module.__name__
+    p.mkdir(exist_ok=True)  # note: will be cleaned up globally.
+    return p
+
+
+def pytest_sessionfinish(exitstatus):
+    if exitstatus == 0 and Path.exists(_tmptestdir):
+        shutil.rmtree(_tmptestdir)
+
+
+def patch_numba_for_tests():
+    """Globally disable numba cache and enable bounds checking (for testing)."""
+    njit_old = numba.njit
+
+    def njit_patched(*args, **kwargs):
+        kwargs.update({"cache": False, "boundscheck": True})
+        return njit_old(*args, **kwargs)
+
+    numba.njit = njit_patched
+
+
+patch_numba_for_tests()
