75c759e98e3cc220cd6f38c30988eef4d13127e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 06:06:51 +0000

    Add package dolze-image-templates/1.2.8/dolze_image_templates-1.2.8.tar.gz

---
+++ packages/dolze-image-templates/dolze_image_templates-1.2.8.tar.gz/dolze_image_templates-1.2.8/dolze_image_templates/data/__init__.py
@@ -0,0 +1,11 @@
+"""
+Core functionality for the Dolze Templates library.
+
+This module provides the main classes and functions for working with templates.
+"""
+
+from .template_variables import TemplateVariables
+
+__all__ = [
+    "TemplateVariables",
+]
