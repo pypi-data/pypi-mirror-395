4802647bb53cb189c0828dba177120e5fb6cc352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 12:07:13 +0000

    Add package rics/6.1.0/rics-6.1.0.tar.gz

---
+++ packages/rics/rics-6.1.0.tar.gz/rics-6.1.0/src/rics/__init__.py
@@ -0,0 +1,23 @@
+"""My personal little ML engineering library.
+
+Environment variables
+---------------------
+.. envvar:: JTWILI
+
+    Set ``JTWILI=true`` to disable the warning emitted by :func:`rics.configure_stuff`.
+
+
+"""
+
+import logging as _logging
+
+from ._just_the_way_i_like_it import configure_stuff
+
+__all__ = [
+    "__version__",
+    "configure_stuff",
+]
+
+__version__ = "6.1.0"
+
+_logging.getLogger(__name__).addHandler(_logging.NullHandler())
