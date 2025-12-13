97d9e8ebd1a34adc8e1daf269f73fdcee5cead90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 09:06:26 +0000

    Add package invenio-banners/5.2.1/invenio_banners-5.2.1.tar.gz

---
+++ packages/invenio-banners/invenio_banners-5.2.1.tar.gz/invenio_banners-5.2.1/invenio_banners/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2024 CERN.
+# Copyright (C) 2024-2025 Graz University of Technology.
+# Copyright (C) 2025      KTH Royal Institute of Technology.
+#
+# Invenio-Banners is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Create and show banners with useful messages to users."""
+
+from .ext import InvenioBanners
+
+__version__ = "5.2.1"
+
+__all__ = ("__version__", "InvenioBanners")
