de5fddff2af8c02ec26de0ebffbbcb55ab5f46e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 04:09:16 +0000

    Add package serbian-py/0.1.4/serbian_py-0.1.4.tar.gz

---
+++ packages/serbian-py/serbian_py-0.1.4.tar.gz/serbian_py-0.1.4/src/serbian_py/__init__.py
@@ -0,0 +1,26 @@
+"""Serbian language adaptations and utility functions for Python programming."""
+
+from .main_commands import (
+    ispiši,
+    input_tekst,
+    input_broj,
+    unos_lista_sa_razmacima,
+    unos_tuple_sa_razmacima,
+    unos_set_sa_razmacima,
+    unos_dict_sa_razmacima,
+    da_li_je_pyinstaller,
+)
+
+__version__ = "0.1.4"
+__author__ = "Pavle012"
+
+__all__ = [
+    "ispiši",
+    "input_tekst",
+    "input_broj",
+    "unos_lista_sa_razmacima",
+    "unos_tuple_sa_razmacima",
+    "unos_set_sa_razmacima",
+    "unos_dict_sa_razmacima",
+    "da_li_je_pyinstaller",
+]
