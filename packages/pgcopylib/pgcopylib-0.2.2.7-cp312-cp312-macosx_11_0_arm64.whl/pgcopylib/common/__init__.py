b25671dcd890b5a10e7564d3744efe5ff2f7d51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 07:06:23 +0000

    Add package pgcopylib/0.2.2.7/pgcopylib-0.2.2.7-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/pgcopylib/pgcopylib-0.2.2.7-cp312-cp312-macosx_11_0_arm64.whl/pgcopylib/common/__init__.py
@@ -0,0 +1,18 @@
+from .enums import (
+    ArrayOidToOid,
+    PGOid,
+    PGOidToDType,
+)
+from .errors import (
+    PGCopyRecordError,
+    PGCopySignatureError,
+)
+
+
+__all__ = (
+    "ArrayOidToOid",
+    "PGCopyRecordError",
+    "PGCopySignatureError",
+    "PGOid",
+    "PGOidToDType",
+)
