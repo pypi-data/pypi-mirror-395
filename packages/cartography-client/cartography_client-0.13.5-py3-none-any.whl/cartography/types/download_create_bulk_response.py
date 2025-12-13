fb6d9bea0ff17247c65a25037e4de49ba2671c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 14:06:48 +0000

    Add package cartography-client/0.13.5/cartography_client-0.13.5-py3-none-any.whl

---
+++ packages/cartography-client/cartography_client-0.13.5-py3-none-any.whl/cartography/types/download_create_bulk_response.py
@@ -0,0 +1,41 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Dict, List, Optional
+
+from .._models import BaseModel
+from .bulk_download_result import BulkDownloadResult
+
+__all__ = ["DownloadCreateBulkResponse", "Results"]
+
+
+class Results(BaseModel):
+    cached: Optional[List[BulkDownloadResult]] = None
+
+    failed: Optional[List[BulkDownloadResult]] = None
+
+    save_failed: Optional[List[BulkDownloadResult]] = None
+
+    success: Optional[List[BulkDownloadResult]] = None
+
+
+class DownloadCreateBulkResponse(BaseModel):
+    cached_count: int
+
+    crawl_id: str
+
+    failed_count: int
+
+    results: Results
+    """Grouped results by status"""
+
+    save_failed_count: int
+
+    success_count: int
+
+    timestamp: str
+
+    total_downloads_attempted: int
+
+    total_urls: int
+
+    debug_frame: Optional[List[Dict[str, object]]] = None
