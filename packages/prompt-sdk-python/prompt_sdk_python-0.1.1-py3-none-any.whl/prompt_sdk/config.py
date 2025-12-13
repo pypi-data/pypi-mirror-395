9502a94bb51447b22a458565ab9cf9d3c464508b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 04:09:48 +0000

    Add package prompt-sdk-python/0.1.1/prompt_sdk_python-0.1.1-py3-none-any.whl

---
+++ packages/prompt-sdk-python/prompt_sdk_python-0.1.1-py3-none-any.whl/prompt_sdk/config.py
@@ -0,0 +1,46 @@
+from pydantic_settings.sources.providers.pyproject import (
+    PyprojectTomlConfigSettingsSource,
+)
+from pathlib import Path
+
+from pydantic.types import DirectoryPath
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+)
+
+from prompt_sdk.validators import PyFile
+
+
+class Settings(BaseSettings):
+    PROJ_ROOT: Path = Path(__file__).resolve().parents[2]
+    TEMPLATES_DIR: Path = PROJ_ROOT / "src" / "prompt_sdk" / "templates"
+    # print(f"Running in Project Root: {PROJ_ROOT}")
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (PyprojectTomlConfigSettingsSource(settings_cls),)
+
+
+class GeneratorSettings(Settings):
+    model_config = SettingsConfigDict(
+        toml_file="pyproject.toml",
+        pyproject_toml_table_header=("tool", "prompt-sdk"),
+        extra="ignore",
+    )
+    input_path: DirectoryPath | None = None
+    print("Input path", input_path)
+    output_path: PyFile | None = None
+    use_class: bool = True
+    class_name: str = "SDK"
+
+
+settings = GeneratorSettings()  # type: ignore
