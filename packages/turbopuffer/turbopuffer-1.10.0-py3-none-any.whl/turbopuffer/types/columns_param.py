afbfb28d61a47ed3673e5d6c52c955d8daca376b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 02:35:31 +0000

    Add package turbopuffer/1.10.0/turbopuffer-1.10.0-py3-none-any.whl

---
+++ packages/turbopuffer/turbopuffer-1.10.0-py3-none-any.whl/turbopuffer/types/columns_param.py
@@ -0,0 +1,23 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Dict, Union, Iterable
+from typing_extensions import Required, TypeAlias, TypedDict
+
+from .._types import SequenceNotStr
+from .id_param import IDParam
+from .vector_param import VectorParam
+
+__all__ = ["ColumnsParam"]
+
+
+class ColumnsParamTyped(TypedDict, total=False):
+    id: Required[SequenceNotStr[IDParam]]
+    """The IDs of the documents."""
+
+    vector: Union[SequenceNotStr[VectorParam], Iterable[float], str]
+    """The vector embeddings of the documents."""
+
+
+ColumnsParam: TypeAlias = Union[ColumnsParamTyped, Dict[str, Iterable[object]]]
