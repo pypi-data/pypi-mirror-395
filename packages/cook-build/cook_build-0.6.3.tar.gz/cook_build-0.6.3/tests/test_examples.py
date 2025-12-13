2f1914ad9e4175c3e80799bd952281d8311561b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:10 +0000

    Add package cook-build/0.6.3/cook_build-0.6.3.tar.gz

---
+++ packages/cook-build/cook_build-0.6.3.tar.gz/cook_build-0.6.3/tests/test_examples.py
@@ -0,0 +1,19 @@
+from cook.__main__ import __main__
+from cook.util import working_directory
+from pathlib import Path
+import pytest
+
+
+@pytest.mark.parametrize(
+    "name, task",
+    [
+        ("hellomake", "say-hello"),
+    ],
+)
+def test_example(name: str, task: str) -> None:
+    with working_directory(f"examples/{name}"):
+        # Remove the database file before testing for consistent execution.
+        db = Path(".cook")
+        if db.is_file():
+            db.unlink()
+        __main__(["exec", task])
