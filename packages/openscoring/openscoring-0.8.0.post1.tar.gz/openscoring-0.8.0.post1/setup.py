b2210bf6628f3a8a382e430c39738c51ec78df8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 01:33:46 +0000

    Add package openscoring/0.8.0.post1/openscoring-0.8.0.post1.tar.gz

---
+++ packages/openscoring/openscoring-0.8.0.post1.tar.gz/openscoring-0.8.0.post1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+exec(open("openscoring/metadata.py").read())
+
+setup(
+	name = "openscoring",
+	version = __version__,
+	description = "Python client library for the Openscoring REST web service (https://github.com/openscoring/openscoring)",
+	author = "Villu Ruusmann",
+	author_email = "villu.ruusmann@gmail.com",
+	url = "https://github.com/openscoring/openscoring-python",
+	download_url = "https://github.com/openscoring/openscoring-python/archive/" + __version__ + ".tar.gz",
+	license = __license__,
+	classifiers = [
+		"Development Status :: 5 - Production/Stable",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Science/Research",
+		"Topic :: Software Development",
+		"Topic :: Scientific/Engineering"
+	],
+	packages = find_packages(exclude = ["*.tests.*", "*.tests"]),
+	exclude_package_data = {
+		"" : ["README.md"],
+	},
+	install_requires = [
+		"pandas",
+		"requests>=2.10.0"
+	]
+)
