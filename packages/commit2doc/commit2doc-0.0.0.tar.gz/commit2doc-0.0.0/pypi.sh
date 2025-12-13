40d668fb95b53108171d30874a61e75a6beec65f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:07:02 +0000

    Add package commit2doc/0.0.0/commit2doc-0.0.0.tar.gz

---
+++ packages/commit2doc/commit2doc-0.0.0.tar.gz/commit2doc-0.0.0/pypi.sh
@@ -0,0 +1,10 @@
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv venv --python 3.12
+source .venv/bin/activate
+uv pip install -r pypi.txt
+uv pip freeze > pypi.lock
+python3 -m build
+python3 -m twine upload dist/*
+deactivate
+rm -rf .venv
+rm -rf dist
