8364601a3bb1f5273682f27fa17fb3d365d82576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:06:27 +0000

    Add package PyAres/0.1.5/pyares-0.1.5.tar.gz

---
+++ packages/PyAres/pyares-0.1.5.tar.gz/pyares-0.1.5/PyAres/Test/analyzer_test.py
@@ -0,0 +1,27 @@
+from PyAres import AresAnalyzerService, AnalysisRequest, Analysis, AresDataType, Outcome
+
+def analyze(request: AnalysisRequest) -> Analysis:
+    #Custom Analysis Logic
+    temperature = request.inputs.get("Temperature")
+
+    if not isinstance(temperature, float):
+        print("Temperature was not a float")
+        temperature = 0.0
+
+    print(f"Temperature: {temperature}")
+
+    analysis = Analysis(result=temperature)
+    return analysis
+
+
+if __name__ == "__main__":
+    #Basic details about your analyzer
+    name = "Python Test Analyzer"
+    version = "0.0.1"
+    description = "This is a test analyzer to demonstrate working with PyAres to create analyzers!"
+    pythonDemoAnalyzer = AresAnalyzerService(analyze, name, version, description)
+
+    #Add Analysis Parameters
+    pythonDemoAnalyzer.add_analysis_parameter("Temperature", AresDataType.NUMBER)
+
+    pythonDemoAnalyzer.start()
