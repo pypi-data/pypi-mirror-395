e2f36a5c0bf4a80bdb6ccbe1172d062a39cc984c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 09:06:47 +0000

    Add package cubexpress/0.1.18/cubexpress-0.1.18-py3-none-any.whl

---
+++ packages/cubexpress/cubexpress-0.1.18-py3-none-any.whl/cubexpress/__init__.py
@@ -0,0 +1,19 @@
+from cubexpress.conversion import lonlat2rt, geo2utm
+from cubexpress.geotyping import RasterTransform, Request, RequestSet
+from cubexpress.cloud_utils import s2_table
+from cubexpress.cube import get_cube
+from cubexpress.request import table_to_requestset
+# import importlib.metadata
+
+
+__all__ = [
+    "lonlat2rt",
+    "RasterTransform",
+    "Request",
+    "RequestSet",
+    "geo2utm",
+    "get_cube",
+    "s2_table",
+    "table_to_requestset"
+]
+# __version__ = importlib.metadata.version("cubexpress")
