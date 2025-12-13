7492b32d815fd719375b282e0b0b51db319f4701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:12 +0000

    Add package solving-auth-middleware/0.3.0/solving_auth_middleware-0.3.0.tar.gz

---
+++ packages/solving-auth-middleware/solving_auth_middleware-0.3.0.tar.gz/solving_auth_middleware-0.3.0/solving_auth_middleware/enums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class UserTypeEnum(Enum): 
+    PRO = 'PRO'
+    PUBLIC = 'PUBLIC'
+    USER_ADMIN = 'USER_ADMIN'
+    SOFTWARE_ADMIN = 'SOFTWARE_ADMIN'
+    SYSTEM = 'SYSTEM'
+    
