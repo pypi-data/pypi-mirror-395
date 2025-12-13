cbface4a9b03346b41054f14b4283953dd4332c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 06:06:35 +0000

    Add package dlglab/0.1.0/dlglab-0.1.0-py3-none-any.whl

---
+++ packages/dlglab/dlglab-0.1.0-py3-none-any.whl/dlglab/__init__.py
@@ -0,0 +1,13 @@
+"""
+dlglab - A simple package with a print function
+"""
+
+def print():
+    """
+    Print hello world to the console.
+    """
+    return "Hello World"
+
+
+__version__ = "0.1.0"
+__all__ = ["print"]
