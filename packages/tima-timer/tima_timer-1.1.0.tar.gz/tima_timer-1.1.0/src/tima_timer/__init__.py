a967a84afcfdd842a1ca85889b59ed141789ae8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 03:28:14 +0000

    Add package tima-timer/1.1.0/tima_timer-1.1.0.tar.gz

---
+++ packages/tima-timer/tima_timer-1.1.0.tar.gz/tima_timer-1.1.0/src/tima_timer/__init__.py
@@ -0,0 +1,11 @@
+"""
+Tima - Activity Timer
+A desktop productivity timer that cycles through your projects.
+"""
+
+__version__ = "1.1.0"
+__author__ = "Amir Rosenfeld"
+
+from tima_timer.app_flet import main
+
+__all__ = ['main']
