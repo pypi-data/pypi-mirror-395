38923de13c2183e8c0de4bf8876bfc98f5240a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:10 +0000

    Add package colt/0.18.0/colt-0.18.0-py3-none-any.whl

---
+++ packages/colt/colt-0.18.0-py3-none-any.whl/colt/__init__.py
@@ -0,0 +1,119 @@
+from importlib.metadata import version
+from typing import Any, Callable, Optional, Sequence, Type, TypeVar, Union, overload
+
+from colt.builder import ColtBuilder
+from colt.callback import ColtCallback, SkipCallback
+from colt.context import ColtContext
+from colt.default_registry import DefaultRegistry
+from colt.error import ConfigurationError
+from colt.jsonschema import JsonSchemaGenerator
+from colt.lazy import Lazy
+from colt.placeholder import Placeholder
+from colt.registrable import Registrable
+from colt.utils import import_modules
+
+__version__ = version("colt")
+__all__ = [
+    "Lazy",
+    "Registrable",
+    "ColtContext",
+    "ConfigurationError",
+    "DefaultRegistry",
+    "JsonSchemaGenerator",
+    "Placeholder",
+    "SkipCallback",
+    "import_modules",
+    "register",
+    "build",
+    "dry_run",
+]
+
+T = TypeVar("T")
+
+
+def register(
+    name: str,
+    constructor: Optional[str] = None,
+    exist_ok: bool = False,
+) -> Callable[[Type[T]], Type[T]]:
+    def decorator(cls: Type[T]) -> Type[T]:
+        DefaultRegistry.register(name, constructor, exist_ok)(cls)
+        return cls
+
+    return decorator
+
+
+@overload
+def build(
+    config: Any,
+    cls: Type[T],
+    *,
+    typekey: Optional[str] = ...,
+    argskey: Optional[str] = ...,
+    strict: bool = ...,
+    callback: Optional[Union[ColtCallback, Sequence[ColtCallback]]] = ...,
+) -> T: ...
+
+
+@overload
+def build(
+    config: Any,
+    cls: Callable[..., T],
+    *,
+    typekey: Optional[str] = ...,
+    argskey: Optional[str] = ...,
+    strict: bool = ...,
+    callback: Optional[Union[ColtCallback, Sequence[ColtCallback]]] = ...,
+) -> T: ...
+
+
+@overload
+def build(
+    config: Any,
+    cls: None = ...,
+    *,
+    typekey: Optional[str] = ...,
+    argskey: Optional[str] = ...,
+    strict: bool = ...,
+    callback: Optional[Union[ColtCallback, Sequence[ColtCallback]]] = ...,
+) -> Any: ...
+
+
+def build(
+    config: Any,
+    cls: Optional[Union[Type[T], Callable[..., T]]] = None,
+    *,
+    typekey: Optional[str] = None,
+    argskey: Optional[str] = None,
+    schemakey: Optional[str] = None,
+    strict: bool = False,
+    callback: Optional[Union[ColtCallback, Sequence[ColtCallback]]] = None,
+) -> Union[T, Any]:
+    builder = ColtBuilder(
+        typekey=typekey,
+        argskey=argskey,
+        schemakey=schemakey,
+        strict=strict,
+        callback=callback,
+    )
+    return builder(config, cls)
+
+
+def dry_run(
+    config: Any,
+    cls: Optional[Union[Type[T], Callable[..., T]]] = None,
+    *,
+    typekey: Optional[str] = None,
+    argskey: Optional[str] = None,
+    schemakey: Optional[str] = None,
+    strict: bool = False,
+    callback: Optional[Union[ColtCallback, Sequence[ColtCallback]]] = None,
+) -> None:
+    builder = ColtBuilder(
+        typekey=typekey,
+        argskey=argskey,
+        schemakey=schemakey,
+        strict=strict,
+        callback=callback,
+    )
+    builder.dry_run(config, cls)
