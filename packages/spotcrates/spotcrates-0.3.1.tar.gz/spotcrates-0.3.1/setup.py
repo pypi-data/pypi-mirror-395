c337972e38433eefda2a85ae28f388ad0376354d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 02:37:44 +0000

    Add package spotcrates/0.3.1/spotcrates-0.3.1.tar.gz

---
+++ packages/spotcrates/spotcrates-0.3.1.tar.gz/spotcrates-0.3.1/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['spotcrates']
+
+package_data = \
+{'': ['*'], 'spotcrates': ['templates/*']}
+
+install_requires = \
+['appdirs>=1.4.4,<2.0.0',
+ 'flask-session>=0.4.0,<0.5.0',
+ 'flask>=2.2.2,<3.0.0',
+ 'jinja2>=3.1.2,<4.0.0',
+ 'pygtrie>=2.5.0,<3.0.0',
+ 'spotipy>=2.22.0,<3.0.0',
+ 'types-appdirs>=1.4.3.1,<2.0.0.0']
+
+entry_points = \
+{'console_scripts': ['spotcrates = spotcrates.cli:main']}
+
+setup_kwargs = {
+    'name': 'spotcrates',
+    'version': '0.3.1',
+    'description': '',
+    'long_description': '# Spotcrates\n### A set of tools for finding and managing music on Spotify\n\n![Code Coverage](https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/cmayes/3c8214e2bd942821496440b93acd3582/raw/covbadge.json)\n\n# Installation\n\n(TODO: Add detailed instructions once uploaded to PyPI)\n\n# Commands\n\nThe installation script puts the command `spotcrates` in your Python environment\n(e.g. `/.pyenv/shims/spotcrates`). The command `spotcrates commands` lists all of the \ncommands available along with a short description of each.\n\n## daily\n`spotcrates daily` collects the contents of the "Daily Mix" playlists, filters them \nagainst an exclusion list ("Overplayed" by default), and adds them to the end of \na target list ("Now" by default).\n\n## list-playlists\n`spotcrates list-playlists` lists playlists\' name, owner, track count, and description.\nThe command accepts `-f` for filter expressions and `-s` for sort expressions. (TODO: \nadd description of filter and search expressions and link to it here)',
+    'author': 'Chris Mayes',
+    'author_email': 'cmayes@cmay.es',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/spotcrates/spotcrates-0.3.1.tar.gz/spotcrates-0.3.1/spotcrates/cli.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+
+"""
+CLI runner for Spotify automation.
+"""
+
+import argparse
+import logging
+import sys
+
+from spotcrates.common import truncate_long_value
+from spotcrates.filters import FieldName
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+__author__ = "cmayes"
+
+from pathlib import Path
+
+import spotipy
+
+from spotcrates.playlists import Playlists
+from appdirs import user_config_dir, user_cache_dir
+
+logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+DEFAULT_CONFIG_DIR = user_config_dir("spotcrates")
+DEFAULT_CONFIG_FILE = Path(DEFAULT_CONFIG_DIR, "spotcrates_config.toml")
+DEFAULT_CACHE_DIR = user_cache_dir("spotcrates")
+DEFAULT_AUTH_CACHE_FILE = Path(DEFAULT_CACHE_DIR, "spotcrates_auth_cache")
+DEFAULT_AUTH_SCOPES = ["playlist-modify-private", "playlist-read-private"]
+
+COMMANDS = ["daily", "list-playlists", "commands"]
+
+COMMAND_DESCRIPTION = f"""
+{'COMMAND NAME':<16} DESCRIPTION
+{'daily':<16} Add "Daily Mix" entries to the end of the target playlist, filtering for excluded entries.
+{'list-playlists':<16} Prints a table describing your playlists.
+{'commands':<16} Prints this command list.
+"""
+
+
+def print_commands():
+    """Prints available commands."""
+    print(COMMAND_DESCRIPTION)
+
+
+def get_config(config_file):
+    """Loads the specified TOML-formatted config file or returns an empty dict"""
+    if config_file.exists():
+        with open(config_file, mode="rb") as fp:
+            return tomllib.load(fp)
+    else:
+        logging.debug(f"Config file '{config_file}' does not exist")
+        return {}
+
+
+def prepare_auth_cache_loc(config):
+    auth_cache_file = Path(config.get("auth_cache", DEFAULT_AUTH_CACHE_FILE))
+    if not auth_cache_file.exists():
+        auth_cache_file.parent.mkdir(parents=True, exist_ok=True)
+    return auth_cache_file
+
+
+def get_spotify_handle(config):
+    spotify_cfg = config.get("spotify")
+    auth_scopes = spotify_cfg.get("auth_scopes", DEFAULT_AUTH_SCOPES)
+    cache_path = prepare_auth_cache_loc(spotify_cfg)
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=cache_path)
+    if spotify_cfg:
+        auth_manager = spotipy.oauth2.SpotifyOAuth(
+            client_id=spotify_cfg.get("client_id"),
+            client_secret=spotify_cfg.get("client_secret"),
+            redirect_uri=spotify_cfg.get("redirect_uri"),
+            cache_handler=cache_handler,
+            scope=auth_scopes,
+        )
+    else:
+        auth_manager = spotipy.oauth2.SpotifyOAuth(
+            cache_handler=cache_handler, scope=auth_scopes
+        )
+    return spotipy.Spotify(auth_manager=auth_manager)
+
+
+def append_daily_mix(config):
+    sp = get_spotify_handle(config)
+
+    playlists = Playlists(sp, config.get("playlists"))
+    playlists.append_daily_mix()
+
+
+def list_playlists(config, args):
+    sp = get_spotify_handle(config)
+    playlists = Playlists(sp, config.get("playlists"))
+
+    try:
+        all_playlists = playlists.list_all_playlists(
+            filters=args.filters, sort_fields=args.sort_fields
+        )
+    except Exception as e:
+        logger.warning(f"Problems listing playlists: {e}")
+        return 1
+
+    print(f"{'PLAYLIST NAME':<32} {'SIZE':<6} {'OWNER':<16} DESCRIPTION")
+    for playlist_row in all_playlists:
+        print(
+            f"""{truncate_long_value(playlist_row[FieldName.PLAYLIST_NAME], 32):<32} \
+{playlist_row[FieldName.SIZE]:<6} \
+{truncate_long_value(playlist_row[FieldName.OWNER], 16):<16} \
+{truncate_long_value(playlist_row[FieldName.PLAYLIST_DESCRIPTION], 75)}"""
+        )
+
+
+def parse_cmdline(argv):
+    """
+    Returns the parsed argument list and return code.
+    `argv` is a list of arguments, or `None` for ``sys.argv[1:]``.
+    """
+    if argv is None:
+        argv = sys.argv[1:]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--config_file",
+        help="The location of the config file",
+        default=DEFAULT_CONFIG_FILE,
+        type=Path,
+    )
+    parser.add_argument(
+        "-s", "--sort_fields", help="The fields to sort against, applied in order"
+    )
+    parser.add_argument("-f", "--filters", help="Filters to apply to the list")
+    parser.add_argument(
+        "command",
+        metavar="COMMAND",
+        help=f"The command to run (one of {','.join(COMMANDS)})",
+    )
+    args = None
+    try:
+        args = parser.parse_args(argv)
+    except IOError as e:
+        logger.warning("Problems reading file:", e)
+        parser.print_help()
+        return args, 2
+
+    return args, 0
+
+
+def main(argv=None):
+    args, ret = parse_cmdline(argv)
+    if ret != 0:
+        return ret
+    config = get_config(args.config_file)
+    command = args.command.lower()
+
+    if command == "daily":
+        return append_daily_mix(config)
+    elif command == "list-playlists":
+        return list_playlists(config, args)
+    elif command == "commands":
+        return print_commands()
+    else:
+        print(
+            f"Invalid command '{args.command}'.  Valid commands: {','.join(COMMANDS)}"
+        )
+        return 1
+
+
+if __name__ == "__main__":
+    status = main()
+    sys.exit(status)
