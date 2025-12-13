389339c5f0fe17a81521f8d86616df78c23bbda4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 07:06:09 +0000

    Add package debugtwin-cli/1.0.3/debugtwin_cli-1.0.3.tar.gz

---
+++ packages/debugtwin-cli/debugtwin_cli-1.0.3.tar.gz/debugtwin_cli-1.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="debugtwin-cli",
+    version="1.0.3",
+    packages=find_packages(),
+    install_requires=[
+        "click>=8.0.0",
+        "aiohttp>=3.8.0",
+        "rich>=13.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "debugtwin=debugtwin_cli.main:cli",
+        ],
+    },
+)
