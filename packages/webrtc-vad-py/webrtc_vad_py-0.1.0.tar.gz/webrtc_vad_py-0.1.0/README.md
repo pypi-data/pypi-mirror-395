b21830a1b05e585693813fdf48aebcc77bc52e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:19 +0000

    Add package webrtc-vad-py/0.1.0/webrtc_vad_py-0.1.0.tar.gz

---
+++ packages/webrtc-vad-py/webrtc_vad_py-0.1.0.tar.gz/webrtc_vad_py-0.1.0/README.md
@@ -0,0 +1,3 @@
+# webrtc-vad-py
+
+webrtc-vad-py wrapper for trimming speech clips
---
+++ packages/webrtc-vad-py/webrtc_vad_py-0.1.0.tar.gz/webrtc_vad_py-0.1.0/src/webrtc_vad/__init__.py
@@ -0,0 +1,3 @@
+from webrtc_vad import effects  # noqa: F401
+from webrtc_vad.effects import split, trim  # noqa: F401
+from webrtc_vad.vad import vad  # noqa: F401
