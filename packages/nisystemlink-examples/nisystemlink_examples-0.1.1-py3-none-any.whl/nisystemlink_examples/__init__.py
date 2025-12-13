a870de772519e9043d0b6ec8f00a04659f386860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 13:14:15 +0000

    Add package nisystemlink-examples/0.1.1/nisystemlink_examples-0.1.1-py3-none-any.whl

---
+++ packages/nisystemlink-examples/nisystemlink_examples-0.1.1-py3-none-any.whl/nisystemlink_examples/__init__.py
@@ -0,0 +1,16 @@
+"""NI SystemLink Enterprise demo package.
+
+This package provides demo utilities and examples for SystemLink Enterprise integrations.
+It includes test data generators, simulators, and helper functions for common SystemLink
+operations.
+
+Modules:
+    testdata: Test data utilities and simulators
+"""
+
+from . import testdata
+
+__version__ = "0.1.0"
+__all__ = [
+    "testdata",
+]
