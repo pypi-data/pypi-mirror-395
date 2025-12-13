3e1f9b3d71e5fb788b17cd9c3cf8f3cfbd5b68b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 02:36:00 +0000

    Add package vex-reader/0.9.4.1/vex_reader-0.9.4.1.tar.gz

---
+++ packages/vex-reader/vex_reader-0.9.4.1.tar.gz/vex_reader-0.9.4.1/tests/test_vex1.py
@@ -0,0 +1,373 @@
+import os
+import sys
+from unittest import TestCase
+import json
+import requests
+
+# Add the parent directory to the path so we can import vex
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from vex import Vex
+from vex import VexPackages
+from vex import NVD
+
+class TestVex(TestCase):
+    pass
+
+
+class TestCVE_2024_40951(TestVex):
+    def setUp(self):
+        # Use the correct path relative to the tests directory
+        self.cve = 'CVE-2024-40951'
+        test_file = os.path.join(os.path.dirname(__file__), f'{self.cve.lower()}.json')
+        self.vex      = Vex(test_file)
+        self.packages = VexPackages(self.vex.raw)
+
+    def test_cve_name(self):
+        self.assertEqual(self.vex.cve, self.cve)
+
+    def test_public_date(self):
+        self.assertEqual(self.vex.release_date, '2024-07-11')
+
+    def test_impact(self):
+        self.assertEqual(self.vex.global_impact, 'Moderate')
+
+    def test_bzid(self):
+        self.assertEqual(self.vex.bz_id, '2297535')
+
+    def test_cvss_vector(self):
+        self.assertEqual(self.vex.global_cvss.vectorString, 'NOT AVAILABLE ')
+
+    def test_cvss_base_score(self):
+        self.assertEqual(self.vex.global_cvss.baseScore, '')
+
+    def test_number_of_refs(self):
+        self.assertEqual(len(self.vex.references), 4)
+
+    def test_number_of_mitigations(self):
+        self.assertEqual(len(self.packages.mitigation), 0)
+
+    def test_number_of_fixes(self):
+        self.assertEqual(len(self.packages.fixes), 0)
+
+    def test_number_of_noaffects(self):
+        self.assertEqual(len(self.packages.not_affected), 7)
+
+
+class TestCVE_2024_21626(TestVex):
+    def setUp(self):
+        # Use the correct path relative to the tests directory
+        self.cve = 'CVE-2024-21626'
+        test_file = os.path.join(os.path.dirname(__file__), f'{self.cve.lower()}.json')
+        self.vex      = Vex(test_file)
+        self.packages = VexPackages(self.vex.raw)
+
+    def test_cve_name(self):
+        self.assertEqual(self.vex.cve, self.cve)
+
+    def test_public_date(self):
+        self.assertEqual(self.vex.release_date, '2024-01-31')
+
+    def test_impact(self):
+        self.assertEqual(self.vex.global_impact, 'Important')
+
+    def test_bzid(self):
+        self.assertEqual(self.vex.bz_id, '2258725')
+
+    def test_cvss_vector(self):
+        self.assertEqual(self.vex.global_cvss.vectorString, 'CVSS:3.1/AV:L/AC:L/PR:N/UI:R/S:C/C:H/I:H/A:H')
+
+    def test_cvss_base_score(self):
+        self.assertEqual(self.vex.global_cvss.baseScore, 8.6)
+
+    def test_nvd_cvss_vector(self):
+        response = requests.get(f'https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={self.cve}')
+        self.nvd_cve  = response.json()
+        if self.nvd_cve['vulnerabilities'][0]['cve']['id'] == self.cve:
+            # we got the right result
+            self.nvd = NVD(self.nvd_cve)
+        else:
+            self.nvd = NVD(None)
+        self.assertEqual(self.nvd.cvss31.vectorString, 'CVSS:3.1/AV:L/AC:L/PR:N/UI:R/S:C/C:H/I:H/A:H')
+
+    def test_nvd_cvss_base_score(self):
+        response = requests.get(f'https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={self.cve}')
+        self.nvd_cve  = response.json()
+        if self.nvd_cve['vulnerabilities'][0]['cve']['id'] == self.cve:
+            # we got the right result
+            self.nvd = NVD(self.nvd_cve)
+        else:
+            self.nvd = NVD(None)
+        self.assertEqual(self.nvd.cvss31.baseScore, 8.6)
+
+    def test_number_of_refs(self):
+        self.assertEqual(len(self.vex.references), 4)
+
+    def test_number_of_mitigations(self):
+        self.assertEqual(len(self.packages.mitigation), 1)
+
+    def test_number_of_fixes(self):
+        self.assertEqual(len(self.packages.fixes), 17)
+
+    def test_number_of_noaffects(self):
+        self.assertEqual(len(self.packages.not_affected), 25)
+
+
+class TestCVE_2002_0803(TestVex):
+    def setUp(self):
+        # Use the correct path relative to the tests directory
+        self.cve = 'CVE-2002-0803'
+        test_file = os.path.join(os.path.dirname(__file__), f'{self.cve.lower()}.json')
+        self.vex      = Vex(test_file)
+        self.packages = VexPackages(self.vex.raw)
+
+    def test_cve_name(self):
+        self.assertEqual(self.vex.cve, self.cve)
+
+    def test_public_date(self):
+        self.assertEqual(self.vex.release_date, '2002-06-07')
+
+    def test_impact(self):
+        self.assertEqual(self.vex.global_impact, 'None')
+
+    def test_bzid(self):
+        self.assertEqual(self.vex.bz_id, '1616805')
+
+    def test_cvss_vector(self):
+        self.assertEqual(self.vex.global_cvss.vectorString, 'NOT AVAILABLE ')
+
+    def test_cvss_base_score(self):
+        self.assertEqual(self.vex.global_cvss.baseScore, '')
+
+    def test_number_of_refs(self):
+        self.assertEqual(len(self.vex.references), 3)
+
+    def test_number_of_mitigations(self):
+        self.assertEqual(len(self.packages.mitigation), 0)
+
+    def test_number_of_fixes(self):
+        self.assertEqual(len(self.packages.fixes), 1)
+
+    def test_number_of_noaffects(self):
+        self.assertEqual(len(self.packages.not_affected), 0)
+
+
+class TestCVE_2021_44228(TestVex):
+    def setUp(self):
+        # Use the correct path relative to the tests directory
+        self.cve = 'CVE-2021-44228'
+        test_file = os.path.join(os.path.dirname(__file__), f'{self.cve.lower()}.json')
+        self.vex      = Vex(test_file)
+        self.packages = VexPackages(self.vex.raw)
+
+    def test_cve_name(self):
+        self.assertEqual(self.vex.cve, self.cve)
+
+    def test_public_date(self):
+        self.assertEqual(self.vex.release_date, '2021-12-09')
+
+    def test_impact(self):
+        self.assertEqual(self.vex.global_impact, 'Critical')
+
+    def test_bzid(self):
+        self.assertEqual(self.vex.bz_id, '2030932')
+
+    def test_cvss_vector(self):
+        self.assertEqual(self.vex.global_cvss.vectorString, 'CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H')
+
+    def test_cvss_base_score(self):
+        self.assertEqual(self.vex.global_cvss.baseScore, 9.8)
+
+    def test_nvd_cvss_vector(self):
+        response = requests.get(f'https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={self.cve}')
+        self.nvd_cve  = response.json()
+        if self.nvd_cve['vulnerabilities'][0]['cve']['id'] == self.cve:
+            # we got the right result
+            self.nvd = NVD(self.nvd_cve)
+        else:
+            self.nvd = NVD(None)
+        self.assertEqual(self.nvd.cvss31.vectorString, 'CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:C/C:H/I:H/A:H')
+
+    def test_nvd_cvss_base_score(self):
+        response = requests.get(f'https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={self.cve}')
+        self.nvd_cve  = response.json()
+        if self.nvd_cve['vulnerabilities'][0]['cve']['id'] == self.cve:
+            # we got the right result
+            self.nvd = NVD(self.nvd_cve)
+        else:
+            self.nvd = NVD(None)
+        self.assertEqual(self.nvd.cvss31.baseScore, 10.0)
+
+    def test_number_of_refs(self):
+        self.assertEqual(len(self.vex.references), 8)
+
+    def test_number_of_mitigations(self):
+        self.assertEqual(len(self.packages.mitigation), 1)
+
+    def test_number_of_fixes(self):
+        self.assertEqual(len(self.packages.fixes), 23)
+
+    def test_number_of_noaffects(self):
+        self.assertEqual(len(self.packages.not_affected), 130)
+
+
+class TestCVE_2025_29087(TestVex):
+    def setUp(self):
+        # Use the correct path relative to the tests directory
+        self.cve = 'CVE-2025-29087'
+        test_file = os.path.join(os.path.dirname(__file__), f'{self.cve.lower()}.json')
+        self.vex      = Vex(test_file)
+        self.packages = VexPackages(self.vex.raw)
+
+    def test_cve_name(self):
+        self.assertEqual(self.vex.cve, self.cve)
+
+    def test_public_date(self):
+        self.assertEqual(self.vex.release_date, '2025-04-06')
+
+    def test_impact(self):
+        self.assertEqual(self.vex.global_impact, 'Moderate')
+
+    def test_bzid(self):
+        self.assertEqual(self.vex.bz_id, '2358028')
+
+    def test_cvss_vector(self):
+        self.assertEqual(self.vex.global_cvss.vectorString, 'CVSS:3.1/AV:L/AC:L/PR:L/UI:N/S:U/C:N/I:N/A:H')
+
+    def test_cvss_base_score(self):
+        self.assertEqual(self.vex.global_cvss.baseScore, 5.5)
+
+    def test_nvd_cvss_vector(self):
+        response = requests.get(f'https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={self.cve}')
+        self.nvd_cve  = response.json()
+        if self.nvd_cve['vulnerabilities'][0]['cve']['id'] == self.cve:
+            # we got the right result
+            self.nvd = NVD(self.nvd_cve)
+        else:
+            self.nvd = NVD(None)
+        self.assertEqual(self.nvd.cvss31.vectorString, 'CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:N/I:N/A:H')
+
+    def test_nvd_cvss_base_score(self):
+        response = requests.get(f'https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={self.cve}')
+        self.nvd_cve  = response.json()
+        if self.nvd_cve['vulnerabilities'][0]['cve']['id'] == self.cve:
+            # we got the right result
+            self.nvd = NVD(self.nvd_cve)
+        else:
+            self.nvd = NVD(None)
+        self.assertEqual(self.nvd.cvss31.baseScore, 7.5)
+
+    def test_number_of_refs(self):
+        self.assertEqual(len(self.vex.references), 4)
+
+    def test_number_of_mitigations(self):
+        self.assertEqual(len(self.packages.mitigation), 1)
+
+    def test_number_of_fixes(self):
+        self.assertEqual(len(self.packages.fixes), 0)
+
+    def test_number_of_wontfixes(self):
+        self.assertEqual(len(self.packages.wontfix), 12)
+
+    def test_number_of_noaffects(self):
+        self.assertEqual(len(self.packages.not_affected), 3)
+
+
+class TestCVE_2025_59840(TestVex):
+    def setUp(self):
+        # Use the correct path relative to the tests directory
+        self.cve = 'CVE-2025-59840'
+        test_file = os.path.join(os.path.dirname(__file__), f'{self.cve.lower()}.json')
+        self.vex      = Vex(test_file)
+        self.packages = VexPackages(self.vex.raw)
+
+    def test_cve_name(self):
+        self.assertEqual(self.vex.cve, self.cve)
+
+    def test_public_date(self):
+        self.assertEqual(self.vex.release_date, '2025-11-13')
+
+    def test_impact(self):
+        self.assertEqual(self.vex.global_impact, 'Important')
+
+    def test_bzid(self):
+        self.assertEqual(self.vex.bz_id, '2414907')
+
+    def test_cvss_vector(self):
+        self.assertEqual(self.vex.global_cvss.vectorString, 'CVSS:3.1/AV:N/AC:L/PR:N/UI:R/S:U/C:H/I:H/A:N')
+
+    def test_cvss_base_score(self):
+        self.assertEqual(self.vex.global_cvss.baseScore, 8.1)
+
+    def test_nvd_cvss_vector(self):
+        response = requests.get(f'https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={self.cve}')
+        self.nvd_cve  = response.json()
+        if self.nvd_cve['vulnerabilities'][0]['cve']['id'] == self.cve:
+            # we got the right result
+            self.nvd = NVD(self.nvd_cve)
+        else:
+            self.nvd = NVD(None)
+        # as of 2025/11/27 NVD has not assigned a CVSS
+        self.assertEqual(self.nvd.cvss31.vectorString, 'NOT AVAILABLE ')
+
+    def test_nvd_cvss_base_score(self):
+        response = requests.get(f'https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={self.cve}')
+        self.nvd_cve  = response.json()
+        if self.nvd_cve['vulnerabilities'][0]['cve']['id'] == self.cve:
+            # we got the right result
+            self.nvd = NVD(self.nvd_cve)
+        else:
+            self.nvd = NVD(None)
+        self.assertEqual(self.nvd.cvss31.baseScore, '')
+
+    def test_number_of_refs(self):
+        self.assertEqual(len(self.vex.references), 4)
+
+    def test_number_of_mitigations(self):
+        self.assertEqual(len(self.packages.mitigation), 1)
+
+    def test_number_of_fixes(self):
+        self.assertEqual(len(self.packages.fixes), 0)
+
+    def test_number_of_affects(self):
+        self.assertEqual(len(self.packages.affected), 1)
+
+    def test_number_of_noaffects(self):
+        self.assertEqual(len(self.packages.not_affected), 5)
+
+
+"""
+class TestCVE_Cisco_rce_2024(TestVex):
+    def setUp(self):
+        test_file = os.path.join(os.path.dirname(__file__), 'cisco-sa-openssh-rce-2024.json')
+        self.vex      = Vex(test_file)
+        self.packages = VexPackages(self.vex.raw)
+
+    def test_cve_name(self):
+        self.assertEqual(self.vex.cve, 'CVE-2024-6387')
+
+    def test_public_date(self):
+        self.assertEqual(self.vex.release_date, '2024-07-02')
+
+    def test_impact(self):
+        self.assertIsNone(self.vex.global_impact)
+
+    def test_bzid(self):
+        self.assertIsNone(self.vex.bz_id)
+
+    def test_cvss_vector(self):
+        self.assertEqual(self.vex.global_cvss['vectorString'], 'CVSS:3.1/AV:N/AC:H/PR:N/UI:N/S:U/C:H/I:H/A:H')
+
+    def test_number_of_refs(self):
+        self.assertEqual(len(self.vex.references), 0)
+
+    def test_number_of_mitigations(self):
+        self.assertEqual(len(self.packages.mitigation), 0)
+
+    def test_number_of_fixes(self):
+        self.assertEqual(len(self.packages.fixes), 1)
+
+    def test_number_of_noaffects(self):
+        self.assertEqual(len(self.packages.not_affected), 0)
+
+"""
---
+++ packages/vex-reader/vex_reader-0.9.4.1.tar.gz/vex_reader-0.9.4.1/vex/package.py
@@ -0,0 +1,259 @@
+# Copyright (c) 2024 Vincent Danen
+# License: GPLv3+
+
+from datetime import datetime
+import pytz
+from .constants import (
+    filter_components,
+    VENDOR_ADVISORY,
+    ARCHES,
+    TZ
+)
+
+
+def product_lookup(product, pmap):
+    # lookup product information by identifier, return a tuple of (name, cpe, purl)
+    name = None
+    cpe  = None
+    purl = None
+
+    for x in pmap:
+        if product in x.keys():
+            if 'name' in x[product]:
+                name = x[product]['name']
+            if 'cpe' in x[product]:
+                cpe = x[product]['cpe']
+            if 'purl' in x[product]:
+                purl = x[product]['purl']
+
+            return (name, cpe, purl)
+
+
+def dedupe(component_list):
+    return list(dict.fromkeys(component_list))
+
+
+def strip_arch(oldComponent):
+    """
+    Strip any reference to an architecture from the component name; this will help to reduce duplicates, i.e.
+    foo-1.2-1.x86_64 and foo-1.2-1.ppc64 should be one entry, not two, so iterate through ARCHES to see if they
+    are present and if so, remove them and return a new component name
+
+    :param oldComponent: the component string to remove architecture references from
+    :return: string of the component without architecture
+    """
+    newComponent = None
+
+    for a in ARCHES:
+        if a in oldComponent:
+            newComponent = oldComponent.replace(f'.{a}', '')
+
+    if not newComponent:
+        newComponent = oldComponent
+
+    return newComponent
+
+
+def product_and_components(y):
+    components = []
+    t = y.split(':')
+    product = t[0]
+    components.append(':'.join(t[1:]))
+
+    return (product, components)
+
+
+class Fix(object):
+    """
+    class to handle vendor fixes
+    """
+
+    def __init__(self, x, pmap):
+            self.id         = None
+            self.url        = ''
+            self.date       = None
+            self.components = []
+
+            if 'url' in x:
+                self.url = x['url']
+
+            if 'date' in x:
+                if x['date'].endswith('Z'):
+                    pd       = datetime.fromisoformat(x['date'][:-1])
+                    pd       = pd.replace(tzinfo=pytz.timezone('UTC'))
+                else:
+                    pd        = datetime.fromisoformat(x['date'])
+                self.date = pd.astimezone(pytz.timezone(TZ)).strftime('%B %d, %Y')
+
+            for v in VENDOR_ADVISORY:
+                if v in self.url:
+                    # most advisories have the advisory name at the end of the URL
+                    self.id = self.url.split('/')[-1]
+                    self.vendor = VENDOR_ADVISORY[v]
+                    # ... but some don't
+                    if self.vendor == 'Gentoo':
+                        self.id = f'{v}-{self.id}'
+
+            for y in filter_components(x['product_ids']):
+                if len(y.split(':')) == 1:
+                    # we may not have a component or version, just a product name
+                    self.pid = y
+                    (self.product, self.cpe, self.purl) = product_lookup(y, pmap)
+                elif len(y.split(':')) == 2:
+                    # bloody containers without versions
+                    self.pid = y
+                    (self.product, self.cpe, self.purl) = product_lookup(y, pmap)
+                else:
+                    # modular components can have 7 colons
+                    (product, component, version) = y.split(':', maxsplit=2)
+                    self.pid = product
+                    self.components.append(':'.join([component, version]))
+                    (self.product, self.cpe, self.purl) = product_lookup(product, pmap)
+
+            self.components = dedupe(self.components)
+
+
+class WontFix(object):
+    """
+    class to handle affects that the vendor will not fix.  These come as one (or multiple?) list of product ids
+    with a reason
+    """
+
+    def __init__(self, y, x, pmap):
+        (product, self.component) = y.split(':', maxsplit=1)
+        self.raw                  = y
+        self.reason               = x['details']
+        self.pid                  = product
+        (self.product, self.cpe, self.purl) = product_lookup(product, pmap)
+
+
+class NotAffected(object):
+    """
+    class to handle products listed as not affected
+    """
+
+    def __init__(self, y, pmap):
+        (product, components) = product_and_components(y)
+        self.raw              = y
+        self.components       = components
+        self.pid              = product
+        (self.product, self.cpe, self.purl) = product_lookup(product, pmap)
+
+
+class Affected(object):
+    """
+    class to handle products listed as affected with no resolution
+    """
+
+    def __init__(self, y, pmap):
+        (product, components) = product_and_components(y)
+        self.raw              = y
+        self.components       = components
+        self.pid              = product
+        (self.product, self.cpe, self.purl) = product_lookup(product, pmap)
+
+
+class Mitigation(object):
+    """
+    class to handle products listed with mitigations
+
+    There should only be one mitigation and not multiple; there may be one mitigiation with
+    a large component list, however -- we probably don't need the components but will keep them
+    just in case
+    """
+
+    def __init__(self, x):
+        self.details  = x['details']
+        self.packages = filter_components(x['product_ids'])
+
+
+class VexPackages(object):
+    """
+    class to handle packages
+    """
+
+    def __init__(self, vexdata):
+        self.raw = vexdata
+        self.build_product_tree()
+        self.parse_packages()
+
+    def build_product_tree(self):
+        """
+        Parse included packages
+        :return:
+        """
+
+        self.pmap = []
+        for p in self.raw['product_tree']['branches']:
+            # TODO there seems to be a bug in the VEX output respective to branch nesting, it's very convoluted =(
+            for b in p['branches']:
+                if 'branches' in b.keys():
+                    for c in b['branches']:
+                        if 'category' in c.keys():
+                            if c['category'] == 'product_name':
+                                name = c['name']
+                                # seems we can also nest branches here?
+                                if 'branches' in c.keys():
+                                    for d in c['branches']:
+                                        cpe  = None
+                                        purl = None
+                                        id  = d['product']['product_id']
+                                        if 'product_identification_helper' in d['product']:
+                                            if 'cpe' in d['product']['product_identification_helper']:
+                                                cpe = d['product']['product_identification_helper']['cpe']
+                                            if 'purl' in d['product']['product_identification_helper']:
+                                                purl = d['product']['product_identification_helper']['purl']
+                                else:
+                                    cpe  = None
+                                    purl = None
+                                    id  = c['product']['product_id']
+                                    if 'product_identification_helper' in c['product']:
+                                        if 'cpe' in c['product']['product_identification_helper']:
+                                            cpe = c['product']['product_identification_helper']['cpe']
+                                        if 'purl' in c['product']['product_identification_helper']:
+                                            purl = c['product']['product_identification_helper']['purl']
+                                self.pmap.append({id: {'name': name, 'cpe': cpe, 'purl': purl}})
+
+
+    def parse_packages(self):
+        # errata
+        self.fixes        = []
+        self.mitigation   = []
+        self.wontfix      = []
+        self.affected     = []
+        self.not_affected = []
+
+        for k in self.raw['vulnerabilities']:
+            if 'remediations' in k:
+                for x in k['remediations']:
+                    if x['category'] == 'vendor_fix':
+                        # TODO: the assumption here is that there is one product, and potentially many components
+                        # which doesn't seem to be the case, see https://www.sick.com/.well-known/csaf/white/2024/sca-2024-0001.json
+                        # which has two vendor_fix statements, but the second has more than one product_ids; this will
+                        # require some rejiggering to make it show products and not just None
+                        self.fixes.append(Fix(x, self.pmap))
+                        #print(f'Appended: {x}')
+
+                    if x['category'] == 'workaround':
+                        self.mitigation.append(Mitigation(x))
+
+                    if x['category'] == 'no_fix_planned':
+                        # don't filter anything on components with no fix planned as there aren't
+                        # any real components (so no .src or .[arch] packages to filter)
+                        for y in (x['product_ids']):
+                            self.wontfix.append(WontFix(y, x, self.pmap))
+
+            if 'product_status' in k:
+                for x in k['product_status']:
+                    if x == 'known_affected':
+                        for y in filter_components(k['product_status']['known_affected']):
+                            self.affected.append(Affected(y, self.pmap))
+                    if x == 'known_not_affected':
+                        for y in filter_components(k['product_status']['known_not_affected']):
+                            # check to make sure we're not adding a dupe
+                            addMe = True
+                            for na in self.not_affected:
+                                if strip_arch(y) in na.raw:
+                                    addMe = False
+                            if addMe:
+                                self.not_affected.append(NotAffected(strip_arch(y), self.pmap))
