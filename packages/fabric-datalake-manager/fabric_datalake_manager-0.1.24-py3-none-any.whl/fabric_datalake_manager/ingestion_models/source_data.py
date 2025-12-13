f667956bb728d5565a90c01f4a3344db8cd0ca6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:15 +0000

    Add package fabric-datalake-manager/0.1.24/fabric_datalake_manager-0.1.24-py3-none-any.whl

---
+++ packages/fabric-datalake-manager/fabric_datalake_manager-0.1.24-py3-none-any.whl/fabric_datalake_manager/ingestion_models/source_data.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from pyspark.sql import DataFrame
+
+# ==========================
+# SourceData Class
+# ==========================
+
+class SourceData:
+    def __init__(self, data: DataFrame):
+        self.df: DataFrame = data
