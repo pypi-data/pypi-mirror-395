2097ace451f9f90b929d1346e0792168bb38b821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 04:09:03 +0000

    Add package parallaxapis-sdk-py/1.2.0/parallaxapis_sdk_py-1.2.0.tar.gz

---
+++ packages/parallaxapis-sdk-py/parallaxapis_sdk_py-1.2.0.tar.gz/parallaxapis_sdk_py-1.2.0/parallaxapis_sdk_py/perimeterx.py
@@ -0,0 +1,45 @@
+from typing_extensions import override
+from .sdk import SDK, AsyncSDK, SDKConfig, SDKKind
+from .solutions import (
+    GenerateHoldCaptchaSolution,
+    GeneratePXCookiesSolution,
+)
+
+from .tasks import TaskGenerateHoldCaptcha, TaskGeneratePXCookies, TaskGenerateUserAgent
+
+class PerimeterxSDK(SDK):
+    def __init__(self, cfg: SDKConfig):
+        super().__init__(cfg=cfg, sdk_kind=SDKKind.PERIMETERX)
+
+    def generate_cookies(
+        self, task: TaskGeneratePXCookies
+    ) -> GeneratePXCookiesSolution:
+        return self.api_call("/gen", task, GeneratePXCookiesSolution)
+
+    def generate_hold_captcha(
+        self, task: TaskGenerateHoldCaptcha
+    ) -> GenerateHoldCaptchaSolution:
+        return self.api_call("/holdcaptcha", task, GenerateHoldCaptchaSolution)
+
+
+class AsyncPerimeterxSDK(AsyncSDK):
+    def __init__(self, cfg: SDKConfig):
+        super().__init__(cfg=cfg, sdk_kind=SDKKind.PERIMETERX)
+
+    @override
+    async def __aenter__(self):
+        return self
+
+    @override
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.aclose()
+
+    async def generate_cookies(
+        self, task: TaskGeneratePXCookies
+    ) -> GeneratePXCookiesSolution:
+        return await self.api_call("/gen", task, GeneratePXCookiesSolution)
+
+    async def generate_hold_captcha(
+        self, task: TaskGenerateHoldCaptcha
+    ) -> GenerateHoldCaptchaSolution:
+        return await self.api_call("/holdcaptcha", task, GenerateHoldCaptchaSolution)
