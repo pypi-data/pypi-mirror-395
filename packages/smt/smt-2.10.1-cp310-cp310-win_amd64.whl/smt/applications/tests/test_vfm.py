2b0196e3df6d979fd105f5636590d6f3a842757e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 08:06:30 +0000

    Add package smt/2.10.1/smt-2.10.1-cp310-cp310-win_amd64.whl

---
+++ packages/smt/smt-2.10.1-cp310-cp310-win_amd64.whl/smt/applications/tests/test_vfm.py
@@ -0,0 +1,266 @@
+"""
+Author: Mohamed Amine Bouhlel <mbouhlel@umich.edu>
+
+This package is distributed under New BSD license.
+"""
+
+import unittest
+
+try:
+    import matplotlib
+
+    matplotlib.use("Agg")
+    NO_MATPLOTLIB = False
+except ImportError:
+    NO_MATPLOTLIB = True
+
+import numpy as np
+
+from smt.applications import VFM
+from smt.examples.rans_crm_wing.rans_crm_wing import (
+    get_rans_crm_wing,
+)
+from smt.problems import WaterFlow, WaterFlowLFidelity
+from smt.sampling_methods import LHS
+from smt.utils.misc import compute_relative_error
+from smt.utils.silence import Silence
+from smt.utils.sm_test_case import SMTestCase
+
+
+def setupCRM(LF_candidate="QP", Bridge_candidate="KRG", type_bridge="Additive"):
+    xt, yt, _ = get_rans_crm_wing()
+
+    optionsLF = {}
+    optionsB = {}
+
+    M = VFM(
+        type_bridge=type_bridge,
+        name_model_LF=LF_candidate,
+        name_model_bridge=Bridge_candidate,
+        X_LF=xt,
+        y_LF=yt,
+        X_HF=xt,
+        y_HF=yt,
+        options_LF=optionsLF,
+        options_bridge=optionsB,
+    )
+
+    return M, xt
+
+
+class TestVFM(SMTestCase):
+    def test_vfm(self):
+        # Problem set up
+        ndim = 8
+        ntest = 500
+        ndoeLF = int(10 * ndim)
+        ndoeHF = int(3)
+        funLF = WaterFlowLFidelity(ndim=ndim)
+        funHF = WaterFlow(ndim=ndim)
+        deriv1 = True
+        deriv2 = True
+        LF_candidate = "QP"
+        Bridge_candidate = "KRG"
+        type_bridge = "Multiplicative"
+        optionsLF = {}
+        optionsB = {
+            "theta0": [1e-2] * ndim,
+            "print_prediction": False,
+            "deriv": False,
+            "hyper_opt": "Cobyla",
+        }
+
+        # Construct low/high fidelity data and validation points
+        sampling = LHS(xlimits=funLF.xlimits, criterion="m", seed=42)
+        xLF = sampling(ndoeLF)
+        yLF = funLF(xLF)
+        if deriv1:
+            dy_LF = np.zeros((ndoeLF, 1))
+            for i in range(ndim):
+                yd = funLF(xLF, kx=i)
+                dy_LF = np.concatenate((dy_LF, yd), axis=1)
+
+        sampling = LHS(xlimits=funHF.xlimits, criterion="m", seed=43)
+        xHF = sampling(ndoeHF)
+        yHF = funHF(xHF)
+        if deriv2:
+            dy_HF = np.zeros((ndoeHF, 1))
+            for i in range(ndim):
+                yd = funHF(xHF, kx=i)
+                dy_HF = np.concatenate((dy_HF, yd), axis=1)
+
+        xtest = sampling(ntest)
+        ytest = funHF(xtest)
+        dytest = np.zeros((ntest, ndim))
+        for i in range(ndim):
+            dytest[:, i] = funHF(xtest, kx=i).T
+
+        # Initialize VFM
+        vfm = VFM(
+            type_bridge=type_bridge,
+            name_model_LF=LF_candidate,
+            name_model_bridge=Bridge_candidate,
+            X_LF=xLF,
+            y_LF=yLF,
+            X_HF=xHF,
+            y_HF=yHF,
+            options_LF=optionsLF,
+            options_bridge=optionsB,
+            dy_LF=dy_LF,
+            dy_HF=dy_HF,
+        )
+
+        # Prediction of the validation points
+        rms_error = compute_relative_error(vfm, xtest, ytest)
+        self.assert_error(rms_error, 0.0, 3e-1)
+
+    @staticmethod
+    def run_vfm_example(self):
+        import matplotlib.pyplot as plt
+        import numpy as np
+
+        from smt.applications import VFM
+        from smt.problems import WaterFlow, WaterFlowLFidelity
+        from smt.sampling_methods import LHS
+
+        # Problem set up
+        ndim = 8
+        ntest = 500
+        ndoeLF = int(10 * ndim)
+        ndoeHF = int(3)
+        funLF = WaterFlowLFidelity(ndim=ndim)
+        funHF = WaterFlow(ndim=ndim)
+        deriv1 = True
+        deriv2 = True
+        LF_candidate = "QP"
+        Bridge_candidate = "KRG"
+        type_bridge = "Multiplicative"
+        optionsLF = {}
+        optionsB = {
+            "theta0": [1e-2] * ndim,
+            "print_prediction": False,
+            "deriv": False,
+            "hyper_opt": "Cobyla",
+        }
+
+        # Construct low/high fidelity data and validation points
+        sampling = LHS(xlimits=funLF.xlimits, criterion="m")
+        xLF = sampling(ndoeLF)
+        yLF = funLF(xLF)
+        if deriv1:
+            dy_LF = np.zeros((ndoeLF, 1))
+            for i in range(ndim):
+                yd = funLF(xLF, kx=i)
+                dy_LF = np.concatenate((dy_LF, yd), axis=1)
+
+        sampling = LHS(xlimits=funHF.xlimits, criterion="m")
+        xHF = sampling(ndoeHF)
+        yHF = funHF(xHF)
+        if deriv2:
+            dy_HF = np.zeros((ndoeHF, 1))
+            for i in range(ndim):
+                yd = funHF(xHF, kx=i)
+                dy_HF = np.concatenate((dy_HF, yd), axis=1)
+
+        xtest = sampling(ntest)
+        ytest = funHF(xtest)
+        dytest = np.zeros((ntest, ndim))
+        for i in range(ndim):
+            dytest[:, i] = funHF(xtest, kx=i).T
+
+        # Initialize the extension VFM
+        M = VFM(
+            type_bridge=type_bridge,
+            name_model_LF=LF_candidate,
+            name_model_bridge=Bridge_candidate,
+            X_LF=xLF,
+            y_LF=yLF,
+            X_HF=xHF,
+            y_HF=yHF,
+            options_LF=optionsLF,
+            options_bridge=optionsB,
+            dy_LF=dy_LF,
+            dy_HF=dy_HF,
+        )
+
+        # Prediction of the validation points
+        y = M.predict_values(x=xtest)
+
+        plt.figure()
+        plt.plot(ytest, ytest, "-.")
+        plt.plot(ytest, y, ".")
+        plt.xlabel(r"$y$ True")
+        plt.ylabel(r"$y$ prediction")
+        plt.show()
+
+    def test_KRG_KRG_additive(self):
+        with Silence():
+            M, xt = setupCRM(
+                LF_candidate="KRG", Bridge_candidate="KRG", type_bridge="Additive"
+            )
+
+        with Silence():
+            yp = M.predict_values(np.atleast_2d(xt[0]))
+            dyp = M.predict_derivatives(np.atleast_2d(xt[0]), kx=0)
+        self.assert_error(yp, np.array([[0.015368, 0.367424]]), atol=2e-2, rtol=3e-2)
+        self.assert_error(
+            dyp,
+            np.array([[0.31539861, 7.30043182]]),
+            atol=5e-1,
+            rtol=5e-2,
+        )
+
+    def test_QP_KRG_additive(self):
+        with Silence():
+            M, xt = setupCRM(
+                LF_candidate="QP", Bridge_candidate="KRG", type_bridge="Additive"
+            )
+
+        with Silence():
+            yp = M.predict_values(np.atleast_2d(xt[0]))
+            dyp = M.predict_derivatives(np.atleast_2d(xt[0]), kx=0)
+
+        self.assert_error(
+            yp, np.array([[0.01535549, 0.36720433]]), atol=1e-2, rtol=1e-2
+        )
+        self.assert_error(
+            dyp, np.array([[0.27030465, 5.89010571]]), atol=5e-1, rtol=5e-2
+        )
+
+    def test_KRG_KRG_mult(self):
+        with Silence():
+            M, xt = setupCRM(
+                LF_candidate="KRG", Bridge_candidate="KRG", type_bridge="Multiplicative"
+            )
+
+        with Silence():
+            yp = M.predict_values(np.atleast_2d(xt[0]))
+            dyp = M.predict_derivatives(np.atleast_2d(xt[0]), kx=0)
+
+        self.assert_error(
+            yp, np.array([[0.01536264, 0.36713384]]), atol=2e-2, rtol=3e-2
+        )
+        self.assert_error(
+            dyp, np.array([[0.29039224, 7.16030401]]), atol=5e-1, rtol=5e-2
+        )
+
+    def test_QP_KRG_mult(self):
+        with Silence():
+            M, xt = setupCRM(
+                LF_candidate="QP", Bridge_candidate="KRG", type_bridge="Multiplicative"
+            )
+
+        with Silence():
+            yp = M.predict_values(np.atleast_2d(xt[0]))
+            dyp = M.predict_derivatives(np.atleast_2d(xt[0]), kx=0)
+
+        self.assert_error(
+            yp, np.array([[0.01536423, 0.36742269]]), atol=3e-1, rtol=1e-2
+        )
+        self.assert_error(
+            dyp, np.array([[0.19562493, 5.39545518]]), atol=3e-1, rtol=1e-2
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/smt/smt-2.10.1-cp310-cp310-win_amd64.whl/smt/surrogate_models/genn.py
@@ -0,0 +1,226 @@
+"""
+Gradient-Enhanced Neural Networks (GENN)
+
+Author: Steven H. Berguin <steven.berguin@gtri.gatech.edu>
+
+This package is distributed under New BSD license.
+"""
+
+from typing import Dict, List, Tuple, Union
+
+import numpy as np
+import jenn
+
+from smt.surrogate_models.surrogate_model import SurrogateModel
+from smt.utils import persistence
+
+# The missing type
+SMTrainingPoints = Dict[Union[int, None], Dict[int, List[np.ndarray]]]
+
+
+def _smt_to_genn(
+    training_points: SMTrainingPoints,
+) -> Tuple[np.ndarray, np.ndarray, Union[np.ndarray, None]]:
+    """Translate data structure from SMT to GENN.
+
+    :param training_points: training data (as per SMT API)
+    :return: X, array of shape (n_x, m) where n_x = number of inputs, m = number of examples
+    :return: Y, array of shape (n_y, m) where n_y = number of outputs
+    :return: J, array of shape (n_y, n_x, m)
+    """
+    # Retrieve training data from SMT training_points
+    xt, yt = training_points[None][
+        0
+    ]  # training_points[name][0] = [np.array(xt), np.array(yt)]
+
+    # Deduce number of dimensions and training examples
+    m, n_x = xt.shape
+    _, n_y = yt.shape
+
+    # Assign training data but transpose to match neural net implementation
+    X = xt
+    Y = yt
+
+    if len(training_points[None]) == 1:
+        return X.T, Y.T, None
+
+    # Loop to retrieve each partial derivative from SMT training_points
+    J = np.zeros((m, n_x, n_y))
+    for k in range(0, n_x):
+        xt, dyt_dxt = training_points[None][k + 1]
+
+        # assert that dimensions match
+        assert xt.shape[0] == m
+        assert xt.shape[1] == n_x
+        assert dyt_dxt.shape[0] == m
+        assert dyt_dxt.shape[1] == n_y
+
+        # Assert that derivatives provided are for the same training points
+        assert xt.all() == X.all()
+
+        # Assign training derivative but transpose to match neural net implementation
+        J[:, k, :] = dyt_dxt
+
+    return X.T, Y.T, J.T
+
+
+class GENN(SurrogateModel):
+    """Gradient-Enhanced Neural Net."""
+
+    def load_data(self, xt, yt, dyt_dxt=None):
+        """Load all training data into surrogate model in one step.
+
+        :param xt: smt data points at which response is evaluated
+        :param yt: response at xt
+        :param dyt_dxt: gradient at xt
+        """
+        m, n_x = (xt.size, 1) if xt.ndim <= 1 else xt.shape
+        m, n_y = (yt.size, 1) if yt.ndim <= 1 else yt.shape
+
+        # Reshape arrays
+        xt = xt.reshape((m, n_x))
+        yt = yt.reshape((m, n_y))
+
+        # Load values
+        self.set_training_values(xt, yt)
+
+        # Load partials
+        if dyt_dxt is not None:
+            dyt_dxt = dyt_dxt.reshape((m, n_x))
+            for i in range(n_x):
+                self.set_training_derivatives(xt, dyt_dxt[:, i].reshape((m, 1)), i)
+
+    @property
+    def name(self) -> str:
+        return "GENN"
+
+    def _initialize(self):
+        self.supports["derivatives"] = True
+        self.supports["training_derivatives"] = True
+
+        self.options.declare(
+            "alpha",
+            default=0.05,
+            types=(int, float),
+            desc="optimizer learning rate",
+        )
+        self.options.declare(
+            "beta1",
+            default=0.9,
+            types=(int, float),
+            desc="Adam optimizer tuning parameter",
+        )
+        self.options.declare(
+            "beta2",
+            default=0.99,
+            types=(int, float),
+            desc="Adam optimizer tuning parameter",
+        )
+        self.options.declare(
+            "lambd",
+            default=0.01,
+            types=(int, float),
+            desc="regularization coefficient",
+        )
+        self.options.declare(
+            "gamma",
+            default=1.0,
+            types=(int, float),
+            desc="gradient-enhancement coefficient",
+        )
+        self.options.declare(
+            "hidden_layer_sizes",
+            default=[12, 12],
+            types=list,
+            desc="number of nodes per hidden layer",
+        )
+        self.options.declare(
+            "mini_batch_size",
+            default=-1,
+            types=int,
+            desc="split data into batches of specified size",
+        )
+        self.options.declare(
+            "num_epochs",
+            default=1,
+            types=int,
+            desc="number of random passes through the data",
+        )
+        self.options.declare(
+            "num_iterations",
+            default=1000,
+            types=int,
+            desc="number of optimizer iterations per mini-batch",
+        )
+        self.options.declare(
+            "seed",
+            default=-1,
+            types=int,
+            desc="random seed to control repeatability",
+        )
+        self.options.declare(
+            "is_print",
+            default=False,
+            types=bool,
+            desc="print progress (or not)",
+        )
+        self.options.declare(
+            "is_normalize",
+            default=True,
+            types=bool,
+            desc="normalize training by mean and variance",
+        )
+        self.options.declare(
+            "is_backtracking",
+            default=False,
+            types=bool,
+            desc="refine step step during line search (fixed otherwise)",
+        )
+
+    def _final_initialize(self):
+        inputs = [1]  # will be overwritten during training (dummy value)
+        output = [1]  # will be overwritten during training (dummy value)
+        hidden = self.options["hidden_layer_sizes"]
+        layer_sizes = inputs + hidden + output
+        self.model = jenn.NeuralNet(layer_sizes)
+
+    def _train(self):
+        X, Y, J = _smt_to_genn(self.training_points)
+        n_x = X.shape[0]
+        n_y = Y.shape[0]
+        hidden = self.options["hidden_layer_sizes"]
+        layer_sizes = [n_x] + hidden + [n_y]
+        self.model.parameters.layer_sizes = layer_sizes
+        self.model.parameters.initialize()
+        kwargs = dict(
+            is_normalize=self.options["is_normalize"],
+            alpha=self.options["alpha"],
+            lambd=self.options["lambd"],
+            gamma=self.options["gamma"],
+            beta1=self.options["beta1"],
+            beta2=self.options["beta2"],
+            epochs=self.options["num_epochs"],
+            batch_size=(
+                None
+                if self.options["mini_batch_size"] < 0
+                else self.options["mini_batch_size"]
+            ),
+            max_iter=self.options["num_iterations"],
+            is_backtracking=self.options["is_backtracking"],
+            is_verbose=self.options["is_print"],
+            random_state=None if self.options["seed"] < 0 else self.options["seed"],
+        )
+        self.model.fit(X, Y, J, **kwargs)
+
+    def save(self, filename):
+        persistence.save(self, filename)
+
+    @staticmethod
+    def load(filename):
+        return persistence.load(filename)
+
+    def _predict_values(self, x):
+        return self.model.predict(x.T).T
+
+    def _predict_derivatives(self, x, kx):
+        return self.model.predict_partials(x.T)[:, kx, :].T
