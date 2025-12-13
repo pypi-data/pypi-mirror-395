74e821550c43fd7c464a34919d7a371d7d0e7353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 13:13:43 +0000

    Add package kani-ext-vllm/0.2.2/kani_ext_vllm-0.2.2-py3-none-any.whl

---
+++ packages/kani-ext-vllm/kani_ext_vllm-0.2.2-py3-none-any.whl/kani/ext/vllm/http_engine.py
@@ -0,0 +1,134 @@
+import logging
+
+import torch
+from kani import PromptPipeline, model_specific
+from kani.ai_function import AIFunction
+from kani.engines import Completion
+from kani.models import ChatMessage
+from openai import AsyncOpenAI
+from transformers import AutoTokenizer
+
+from .bases import VLLMBase
+from .utils import max_context_size_from_autoconfig
+from .vllm_server import VLLMServer
+
+log = logging.getLogger(__name__)
+
+
+class VLLMServerEngine(VLLMBase):
+    """
+    Like the VLLMEngine, but uses an HTTP server and the OpenAI client with the client handling chat
+    translation/tokenization.
+
+    Will only work for models with HF tokenizers. Replace ``model_load_kwargs`` with ``vllm_args`` and move any
+    ``sampling_params`` to top-level kwargs.
+
+    Useful for serving multiple models in parallel.
+    """
+
+    def __init__(
+        self,
+        model_id: str,
+        max_context_size: int | None = None,
+        prompt_pipeline: PromptPipeline[str | torch.Tensor] = None,
+        *,
+        timeout: int = 600,
+        vllm_args: dict = None,
+        vllm_host: str = "127.0.0.1",
+        vllm_port: int = None,
+        use_managed_server=True,
+        chat_template_kwargs: dict = None,
+        **hyperparams,
+    ):
+        r"""
+        :param model_id: The ID of the model to load from HuggingFace.
+        :param max_context_size: The context size of the model. Defaults to the context size specified in the model
+            config.
+        :param prompt_pipeline: A kani PromptPipeline to use.
+        :param vllm_args: See https://docs.vllm.ai/en/stable/cli/serve.html.
+            Underscores will be converted to hyphens, dashes will be added, and values of True will be present.
+            (e.g. ``{"enable_auto_tool_choice": True, "tool_call_parser": "mistral"}`` becomes
+            ``--enable-auto-tool-choice --tool-call-parser mistral``\ .)
+        :param vllm_host: The host to bind the vLLM server to. Defaults to localhost.
+        :param vllm_port: The port to bind the vLLM server to. Defaults to a random free port.
+        :param use_managed_server: Whether to start and manage the vLLM server process (default). If False, connects
+            to an already-started vLLM server at the given host and port.
+        :param chat_template_kwargs: The keyword arguments to pass to ``tokenizer.apply_chat_template`` if using a chat
+            template prompt pipeline.
+        :param hyperparams: Additional arguments to supply the model during generation, see
+            https://docs.vllm.ai/en/latest/serving/openai_compatible_server.html#completions-api_1.
+        """
+        if chat_template_kwargs is None:
+            chat_template_kwargs = {}
+
+        self.model_id = model_id
+        self.hyperparams = hyperparams
+
+        self.server = VLLMServer(model_id=model_id, vllm_args=vllm_args, host=vllm_host, port=vllm_port)
+        if use_managed_server:
+            self.server.start()
+        self.client = AsyncOpenAI(
+            base_url=f"http://127.0.0.1:{self.server.port}/v1",
+            api_key="<the library wants this but it isn't needed>",
+            timeout=timeout,
+        )
+
+        # load the pipeline
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        if prompt_pipeline is None:
+            # try and load a manual impl, or default to chat template if not available
+            prompt_pipeline = model_specific.prompt_pipeline_for_hf_model(
+                model_id, tokenizer, chat_template_kwargs=chat_template_kwargs
+            )
+
+        # get max context size if not set
+        if max_context_size is None:
+            max_context_size = max_context_size_from_autoconfig(model_id)
+        super().__init__(tokenizer=tokenizer, max_context_size=max_context_size, prompt_pipeline=prompt_pipeline)
+
+    # ===== main =====
+    async def predict(
+        self,
+        messages: list[ChatMessage],
+        functions: list[AIFunction] | None = None,
+        *,
+        decode_kwargs: dict = None,  # to prevent HF compat things from breaking the call to .generate()
+        **hyperparams,
+    ) -> Completion:
+        if decode_kwargs is None:
+            decode_kwargs = {}
+        decode_kwargs.setdefault("skip_special_tokens", False)
+
+        prompt = self.build_prompt(messages, functions)
+        # tokenize it ourselves in order to capture special tokens correctly
+        if isinstance(prompt, list):
+            prompt_toks = prompt
+        elif isinstance(prompt, torch.Tensor):
+            prompt_toks = prompt[0].tolist()
+        else:
+            prompt_toks = self.tokenizer.encode(prompt, add_special_tokens=False)
+
+        kwargs = {
+            "max_tokens": self.max_context_size - len(prompt_toks),  # setting this to None causes a 500 for some reason
+            **(self.hyperparams | hyperparams),
+        }
+
+        # run it through the model
+        await self.server.wait_for_healthy()
+        # generation from vllm api entrypoint
+        completion = await self.client.completions.create(
+            model=self.model_id, prompt=prompt_toks, extra_body=decode_kwargs, **kwargs
+        )
+        content = completion.choices[0].text.strip()
+
+        input_len = completion.usage.prompt_tokens
+        output_len = completion.usage.completion_tokens
+        log.debug(f"COMPLETION ({input_len=}, {output_len=}): {content}")
+        return Completion(
+            ChatMessage.assistant(content),
+            prompt_tokens=input_len,
+            completion_tokens=output_len,
+        )
+
+    async def close(self):
+        await self.server.close()
---
+++ packages/kani-ext-vllm/kani_ext_vllm-0.2.2-py3-none-any.whl/kani/ext/vllm/vllm_server.py
@@ -0,0 +1,115 @@
+import asyncio
+import logging
+import signal
+import socket
+import subprocess
+
+import httpx
+
+log = logging.getLogger(__name__)
+
+
+class VLLMServer:
+    """Class to manage a vLLM server instance."""
+
+    def __init__(self, model_id: str, vllm_args: dict = None, *, host: str = "127.0.0.1", port: int = None):
+        """
+        Launch a vLLM server instance serving the given model ID with the given CLI arguments.
+
+        Optionally, the host and port can be overridden, but defaults to localhost and a random free port.
+        """
+        if vllm_args is None:
+            vllm_args = {}
+        # launch the server
+        if port is None:
+            port = str(get_free_port())
+        else:
+            port = str(port)
+        self.port = port
+        self.host = host
+        self.cli_args = kwargs_to_cli(vllm_args)
+        self._vargs = [
+            "vllm",
+            "serve",
+            model_id,
+            "--host",
+            host,
+            "--port",
+            port,
+            *self.cli_args,
+        ]
+        self.process = None
+        self.http = httpx.AsyncClient(base_url=f"http://127.0.0.1:{port}")
+        self._server_healthy = False
+
+    def start(self):
+        log.info(f"Launching vLLM server with following command: {self._vargs}")
+        self.process = subprocess.Popen(self._vargs)
+
+    async def wait_for_healthy(self, route="/health"):
+        """Wait until a GET request to the given route returns a 2XX response code."""
+        # we can early return if the server has been healthy once, hopefully
+        if self._server_healthy:
+            return
+        log.info("Waiting until vLLM server is healthy...")
+        i = 0
+        while not self._server_healthy:
+            if self.process is not None and self.process.poll() is not None:
+                raise RuntimeError("vLLM server died! Check the logs for the vLLM exception.")
+            i += 1
+            try:
+                log.debug(f"Checking for healthy server (request {i})...")
+                resp = await self.http.get(route)
+                resp.raise_for_status()
+                self._server_healthy = 200 <= resp.status_code < 300
+            except Exception as e:
+                log.debug(f"Unhealthy server (request {i}), waiting for 5 seconds...", exc_info=e)
+                if i % 12 == 0 and i > 1:
+                    log.warning(f"vLLM server is still unhealthy after {i} health checks: {e}")
+                await asyncio.sleep(5)
+
+    async def close(self):
+        if self.process is None:
+            return
+        # Ctrl-C behaviour
+        self.process.send_signal(signal.SIGINT)
+        try:
+            await asyncio.get_event_loop().run_in_executor(None, self.process.wait, 20)
+        except TimeoutError:
+            # if it doesn't shutdown in 20s, KILL it
+            log.warning("vLLM server did not shut down in 20s, KILLing it...")
+            self.process.kill()
+
+    def __del__(self):
+        """If we weren't cleaned up properly, SIGINT the server process"""
+        if self.process is not None and self.process.poll() is None:
+            self.process.send_signal(signal.SIGINT)
+
+
+# ==== utils ====
+def kwargs_to_cli(args: dict) -> list[str]:
+    """
+    Convert vLLM engine-style kwargs to CLI-style kwargs.
+
+    Example: {"tensor_parallel_size": 3} -> ["--tensor-parallel-size", "3"]
+    """
+    out = []
+    for k, v in args.items():
+        if v is False:
+            continue
+        k = f"--{k.replace('-', '_')}"
+        out.append(k)
+        if v is not True:
+            out.append(str(v))
+
+    return out
+
+
+def get_free_port() -> int:
+    """Return a random free port on the host."""
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", 0))
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    port = s.getsockname()[1]
+    s.close()
+    return port
