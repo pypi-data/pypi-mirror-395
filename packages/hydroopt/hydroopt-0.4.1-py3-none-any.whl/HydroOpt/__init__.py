fe88aad36aefc6b629bc7f5ac8a93ddec8e0ef25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 07:07:00 +0000

    Add package hydroopt/0.4.1/hydroopt-0.4.1-py3-none-any.whl

---
+++ packages/hydroopt/hydroopt-0.4.1-py3-none-any.whl/HydroOpt/__init__.py
@@ -0,0 +1,7 @@
+from .core import testar_ldiametro, testar_rede, executar_todos_testes
+from .rede import Rede
+from .diametros import LDiametro
+from .otimizador import Otimizador
+
+__version__ = "0.4.1"
+__all__ = ['Rede', 'LDiametro', 'Otimizador', 'testar_ldiametro', 'testar_rede', 'executar_todos_testes']
