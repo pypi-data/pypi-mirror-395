31bac5c6fba16cd92360474100e475399c9aedd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 08:06:33 +0000

    Add package smt/2.10.1/smt-2.10.1-cp313-cp313-win_amd64.whl

---
+++ packages/smt/smt-2.10.1-cp313-cp313-win_amd64.whl/smt/surrogate_models/krg.py
@@ -0,0 +1,42 @@
+"""
+Author: Dr. Mohamed A. Bouhlel <mbouhlel@umich.edu>
+
+This package is distributed under New BSD license.
+"""
+
+from smt.kernels import Kernel
+from smt.surrogate_models.krg_based import KrgBased
+from smt.utils.kriging import componentwise_distance
+
+
+class KRG(KrgBased):
+    name = "Kriging"
+
+    def _initialize(self):
+        super()._initialize()
+        declare = self.options.declare
+        declare(
+            "corr",
+            "squar_exp",
+            values=(
+                "pow_exp",
+                "abs_exp",
+                "squar_exp",
+                "squar_sin_exp",
+                "matern52",
+                "matern32",
+            ),
+            desc="Correlation function type",
+            types=(str, Kernel),
+        )
+
+    def _componentwise_distance(self, dx, opt=0, theta=None, return_derivative=False):
+        d = componentwise_distance(
+            dx,
+            self.options["corr"],
+            self.nx,
+            self.options["pow_exp_power"],
+            theta=theta,
+            return_derivative=return_derivative,
+        )
+        return d
