f677e38cd2bfb5b90b22571676aa0345efec79a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 12:07:27 +0000

    Add package sentior-platform-sdk/0.1.0/sentior_platform_sdk-0.1.0.tar.gz

---
+++ packages/sentior-platform-sdk/sentior_platform_sdk-0.1.0.tar.gz/sentior_platform_sdk-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sentior-platform-sdk",
+    version="0.1.0",
+    description="Internal Sentior platform SDK (entitlements, collections, events)",
+    author="Sentior",
+    packages=find_packages(),
+    install_requires=[],
+)
