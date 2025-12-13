e98535445205fc6b007596198f4d9c8175e3165b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 07:07:10 +0000

    Add package image-interlacement/0.2.0/image_interlacement-0.2.0.tar.gz

---
+++ packages/image-interlacement/image_interlacement-0.2.0.tar.gz/image_interlacement-0.2.0/src/__init__.py
@@ -0,0 +1,5 @@
+"""Image Interlacement Program - A Python CLI tool for image interlacing operations."""
+
+__version__ = "0.2.0"
+__author__ = "Your Name"
+__description__ = "Image interlacement program with support for various interlacing techniques"
