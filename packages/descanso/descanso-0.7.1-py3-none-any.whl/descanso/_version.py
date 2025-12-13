88be084bd841a606b625fa5d657507978f991ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:29 +0000

    Add package descanso/0.7.1/descanso-0.7.1-py3-none-any.whl

---
+++ packages/descanso/descanso-0.7.1-py3-none-any.whl/descanso/_version.py
@@ -0,0 +1,13 @@
+# This file auto generated!
+# DO NOT EDIT MANUALLY!
+
+version: str = "0.7.1"
+__version__: str = "0.7.1"
+
+commit_hash: str = "None"
+version_timestamp: str = "2025-12-06 16:20:39.140302+00:00"
+
+tag: str = "0.7.1"
+branch: str = "None"
+commit_date: str = "None"
+commit_count_since_tag: int = 0
