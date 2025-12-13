874057eba76f71aa13eaa4586702c5616f514e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 13:14:09 +0000

    Add package mamboframe/0.1.2/mamboframe-0.1.2-py3-none-any.whl

---
+++ packages/mamboframe/mamboframe-0.1.2-py3-none-any.whl/mambo/api/routers/general_router.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, status, Depends, Request
+from fastapi.responses import Response, FileResponse
+from mambo.services.core_services import CoreServices
+from mambo.utils.http.response_utils import HttpResponses
+from pathlib import Path
+
+from typing import Annotated
+from fastapi.security import OAuth2PasswordBearer
+
+class GeneralRouter:
+    def __init__(self, services: CoreServices) -> None:
+        self.prefix: str = ''
+        self.router: APIRouter = APIRouter() 
+        self.oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+        @self.router.get('/', tags=['General'])
+        def home(response: Response) -> dict[str, object]:
+            all_users = services.get_all_users()
+            return HttpResponses.standard_response(
+                response=response,
+                status_code=status.HTTP_200_OK,
+                status_title='Ok',
+                content_response={
+                    'ping': 'pong'
+                }
+            )
