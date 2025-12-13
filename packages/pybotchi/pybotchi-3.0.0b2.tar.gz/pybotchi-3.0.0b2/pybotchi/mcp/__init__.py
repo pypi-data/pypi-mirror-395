491d1614fc0c81b4679cfc8707241f356594d8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 05:07:06 +0000

    Add package pybotchi/3.0.0b2/pybotchi-3.0.0b2.tar.gz

---
+++ packages/pybotchi/pybotchi-3.0.0b2.tar.gz/pybotchi-3.0.0b2/pybotchi/mcp/__init__.py
@@ -0,0 +1,24 @@
+"""Pybotchi MCP."""
+
+try:
+    from .action import MCPAction, MCPToolAction, graph, mount_mcp_groups
+    from .common import MCPConfig, MCPConnection, MCPIntegration, MCPMode
+    from .context import MCPContext
+
+    __all__ = [
+        "MCPAction",
+        "MCPToolAction",
+        "graph",
+        "mount_mcp_groups",
+        "MCPConfig",
+        "MCPConnection",
+        "MCPIntegration",
+        "MCPMode",
+        "MCPContext",
+    ]
+except ModuleNotFoundError as e:
+    raise ModuleNotFoundError(
+        """MCP feature not installed. Please install pybotchi with the `mcp` extra dependency.
+Try: pip install pybotchi[mcp]
+From Source: poetry install --extras mcp"""
+    ) from e
