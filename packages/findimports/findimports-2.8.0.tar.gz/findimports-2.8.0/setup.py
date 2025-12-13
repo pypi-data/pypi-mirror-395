de366a86850db9393e88d4b9e44cb2ba5fe3edcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 01:34:04 +0000

    Add package findimports/2.8.0/findimports-2.8.0.tar.gz

---
+++ packages/findimports/findimports-2.8.0.tar.gz/findimports-2.8.0/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import ast
+import email.utils
+import os
+import re
+
+from setuptools import setup
+
+
+def read(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read()
+
+
+metadata = dict(
+    (k, ast.literal_eval(v)) for k, v in
+    re.findall('^(__version__|__author__|__url__|__licence__) = (.*)$',
+               read('findimports.py'), flags=re.MULTILINE)
+)
+version = metadata['__version__']
+author, author_email = email.utils.parseaddr(metadata['__author__'])
+url = metadata['__url__']
+licence = metadata['__licence__']
+
+
+setup(
+    name='findimports',
+    version=version,
+    author=author,
+    author_email=author_email,
+    url=url,
+    description='Python module import analysis tool',
+    long_description=read('README.rst') + '\n\n' + read('CHANGES.rst'),
+    long_description_content_type='text/x-rst',
+    license=licence,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    python_requires='>=3.10',
+
+    py_modules=['findimports'],
+    zip_safe=False,
+    entry_points="""
+    [console_scripts]
+    findimports = findimports:main
+    """,
+)
---
+++ packages/findimports/findimports-2.8.0.tar.gz/findimports-2.8.0/tests/sample-tree/box/cat.py
@@ -0,0 +1,5 @@
+import gc
+from box import yarn
+import decoy  # ha!  3rd-party module
+
+yarn.play()
