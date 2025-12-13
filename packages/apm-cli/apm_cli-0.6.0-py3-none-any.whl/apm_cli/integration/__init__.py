f89d70aa6221c5bcb9dae7b37c47245e63d4df0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 11:06:21 +0000

    Add package apm-cli/0.6.0/apm_cli-0.6.0-py3-none-any.whl

---
+++ packages/apm-cli/apm_cli-0.6.0-py3-none-any.whl/apm_cli/integration/__init__.py
@@ -0,0 +1,7 @@
+"""APM package integration utilities."""
+
+from .prompt_integrator import PromptIntegrator
+from .agent_integrator import AgentIntegrator
+from .skill_integrator import SkillIntegrator
+
+__all__ = ['PromptIntegrator', 'AgentIntegrator', 'SkillIntegrator']
