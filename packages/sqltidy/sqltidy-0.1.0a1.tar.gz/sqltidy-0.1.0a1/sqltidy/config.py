35be1582200e22cf9085db52cfde58f6cd719b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 00:07:15 +0000

    Add package sqltidy/0.1.0a1/sqltidy-0.1.0a1.tar.gz

---
+++ packages/sqltidy/sqltidy-0.1.0a1.tar.gz/sqltidy-0.1.0a1/sqltidy/config.py
@@ -0,0 +1,5 @@
+class FormatterConfig:
+    def __init__(self, uppercase_keywords=True, newline_after_select=False, compact=False):
+        self.uppercase_keywords = uppercase_keywords
+        self.newline_after_select = newline_after_select
+        self.compact = compact
