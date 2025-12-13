9f97def81fe4b152d01e1747db556b46f2ec56a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 03:27:21 +0000

    Add package fabric-datalake-manager/0.1.28/fabric_datalake_manager-0.1.28.tar.gz

---
+++ packages/fabric-datalake-manager/fabric_datalake_manager-0.1.28.tar.gz/fabric_datalake_manager-0.1.28/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read README.md using UTF-8 to avoid Windows cp1252 decoding errors
+long_description = Path("README.md").read_text(encoding="utf-8")
+
+setup(
+    name='fabric_datalake_manager',
+    version='0.1.28',
+    description='A Fabric extension for managing data lakes',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Mahmudul Hasan Roni',
+    packages=find_packages(),
+    python_requires='>=3.11',
+    install_requires=[
+        "delta-spark",
+        "dacite>=1.9.2",
+        "adlfs",
+    ],
+)
