c0e9cd939a022e77b101de188a76e6c8e7d704e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-13 06:07:06 +0000

    Add package hydroopt/0.3.5/hydroopt-0.3.5.tar.gz

---
+++ packages/hydroopt/hydroopt-0.3.5.tar.gz/hydroopt-0.3.5/HydroOpt/__init__.py
@@ -0,0 +1,7 @@
+from .core import testar_ldiametro, testar_rede, executar_todos_testes
+from .rede import Rede
+from .diametros import LDiametro
+from .otimizador import Otimizador
+
+__version__ = "0.3.5"
+__all__ = ['Rede', 'LDiametro', 'Otimizador', 'testar_ldiametro', 'testar_rede', 'executar_todos_testes']
